f2bf7fc802eea7915d893e8c33adf1dd4f1f7b97
Jason Petersen <jason@citusdata.com>
2016-02-11 15:21:40 -0700

    Rename GetConnection to address name conflict
    The postgres_fdw extension has an extern function with an identical
    signature, which can cause problems when both extensions are loaded.
    A simple rename can fix this for now (this is the only function with)
    such a conflict.

--- src/include/distributed/connection_cache.h
+++ src/include/distributed/connection_cache.h
@@ -51,7 +51,7 @@ typedef struct NodeConnectionEntry
 
 
 /* function declarations for obtaining and using a connection */
-extern PGconn * GetConnection(char *nodeName, int32 nodePort);
+extern PGconn * GetOrEstablishConnection(char *nodeName, int32 nodePort);
 extern void PurgeConnection(PGconn *connection);
 extern void ReportRemoteError(PGconn *connection, PGresult *result);
 
