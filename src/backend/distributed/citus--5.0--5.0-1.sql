e1fc079d075676394862feac0f91d5afd0f66626
Andres Freund <andres@anarazel.de>
2016-04-19 14:30:54 -0700

    Replace direct inserts in csql's \stage by serverside functions.
    \stage so far directly inserted into pg_dist_shard and
    pg_dist_shard_placement. That makes it hard to do effective permission
    checks.  Thus move the inserts into two C functions.
    
    These two new functions aren't the nicest abstraction. But as we are
    planning to obsolete \stage, it doesn't seem worthwhile to refactor the
    client-side code of \stage to allow the use of
    master_create_empty_shard() et al.

--- src/backend/distributed/citus--5.0--5.0-1.sql
+++ src/backend/distributed/citus--5.0--5.0-1.sql
@@ -1,3 +1,24 @@
 /* citus--5.0--5.0-1.sql */
 
--- Currently nothing to do here
+CREATE FUNCTION pg_catalog.master_stage_shard_row(logicalrelid oid,
+                                                  shardid bigint,
+                                                  shardstorage "char",
+                                                  shardminvalue text,
+                                                  shardmaxvalue text)
+    RETURNS VOID
+    LANGUAGE C
+    AS 'MODULE_PATHNAME', $$master_stage_shard_row$$;
+COMMENT ON FUNCTION pg_catalog.master_stage_shard_row(oid, bigint, "char", text, text)
+    IS 'deprecated function to insert a row into pg_dist_shard';
+
+CREATE FUNCTION pg_catalog.master_stage_shard_placement_row(shardid int8,
+                                                            shardstate int4,
+                                                            shardlength int8,
+                                                            nodename text,
+                                                            nodeport int4)
+    RETURNS VOID
+    STRICT
+    LANGUAGE C
+    AS 'MODULE_PATHNAME', $$master_stage_shard_placement_row$$;
+COMMENT ON FUNCTION pg_catalog.master_stage_shard_placement_row(int8, int4, int8, text, int4)
+    IS 'deprecated function to insert a row into pg_dist_shard_placement';
