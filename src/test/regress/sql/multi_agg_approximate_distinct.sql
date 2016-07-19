20debfc0ee92ddb727487377cdc7d30b527bd034
Burak Yucesoy <burak@citusdata.com>
2016-07-19 15:21:20 +0300

    Fix COUNT DISTINCT approximation with schema
    Fixes #555
    
    Before this change, we were resolving HLL function and type Oid without qualified name.
    Now we find the schema name where HLL objects are stored and generate qualified names for
    each objects.
    
    Similar fix is also applied for cstore_table_size function call.

--- src/test/regress/sql/multi_agg_approximate_distinct.sql
+++ src/test/regress/sql/multi_agg_approximate_distinct.sql
@@ -52,6 +52,37 @@ SELECT count(DISTINCT l_orderkey) as distinct_order_count, l_quantity FROM linei
 	ORDER BY distinct_order_count ASC, l_quantity ASC
 	LIMIT 10;
 
+-- Check that approximate count(distinct) works at a table in a schema other than public
+-- create necessary objects
+CREATE SCHEMA test_count_distinct_schema;
+
+CREATE TABLE test_count_distinct_schema.nation_hash(
+    n_nationkey integer not null,
+    n_name char(25) not null,
+    n_regionkey integer not null,
+    n_comment varchar(152)
+);
+SELECT master_create_distributed_table('test_count_distinct_schema.nation_hash', 'n_nationkey', 'hash');
+SELECT master_create_worker_shards('test_count_distinct_schema.nation_hash', 4, 2);
+
+\COPY test_count_distinct_schema.nation_hash FROM STDIN with delimiter '|';
+0|ALGERIA|0|haggle. carefully final deposits detect slyly agai
+1|ARGENTINA|1|al foxes promise slyly according to the regular accounts. bold requests alon
+2|BRAZIL|1|y alongside of the pending deposits. carefully special packages are about the ironic forges. slyly special 
+3|CANADA|1|eas hang ironic, silent packages. slyly regular packages are furiously over the tithes. fluffily bold
+4|EGYPT|4|y above the carefully unusual theodolites. final dugouts are quickly across the furiously regular d
+5|ETHIOPIA|0|ven packages wake quickly. regu
+\.
+
+SET search_path TO public;
+SET citus.count_distinct_error_rate TO 0.01;
+SELECT COUNT (DISTINCT n_regionkey) FROM test_count_distinct_schema.nation_hash;
+
+-- test with search_path is set
+SET search_path TO test_count_distinct_schema;
+SELECT COUNT (DISTINCT n_regionkey) FROM nation_hash;
+SET search_path TO public;
+
 -- If we have an order by on count(distinct) that we intend to push down to
 -- worker nodes, we need to error out. Otherwise, we are fine.
 
