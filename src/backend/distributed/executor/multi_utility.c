74f4e0003b7c29cbbeea27fb362eb6c68ea81097
Jason Petersen <jason@citusdata.com>
2016-08-31 13:19:13 -0400

    Permit multiple DDL commands in a transaction
    Three changes here to get to true multi-statement, multi-relation DDL
    transactions (same functionality pre-5.2, with benefits of atomicity):
    
        1. Changed the multi-shard utility hook to always run (consistency
           with router executor hook, removes ad-hoc "installed" boolean)
    
        2. Change the global connection list in multi_shard_transaction to
           instead be a hash; update related functions to operate on global
           hash instead of local hash/global list
    
        3. Remove check within DDL code to prevent subsequent DDL commands;
           place unset/reset guard around call to ConnectToNode to permit
           connecting to additional nodes after DDL transaction has begun
    
    In addition, code has been added to raise an error if a ROLLBACK TO
    SAVEPOINT is attempted (similar to router executor), and comprehensive
    tests execute all multi-DDL scenarios (full success, user ROLLBACK, any
    actual errors (say, duplicate index), partial failure (duplicate index
    on one node but not others), partial COMMIT (one node fails), and 2PC
    partial PREPARE (one node fails)). Interleavings with other commands
    (DML, \copy) are similarly all covered.

--- src/backend/distributed/executor/multi_utility.c
+++ src/backend/distributed/executor/multi_utility.c
@@ -1255,11 +1255,11 @@ ExecuteDistributedDDLCommand(Oid relationId, const char *ddlCommandString,
 {
 	bool executionOK = false;
 
-	if (XactModificationLevel > XACT_MODIFICATION_NONE)
+	if (XactModificationLevel == XACT_MODIFICATION_DATA)
 	{
 		ereport(ERROR, (errcode(ERRCODE_ACTIVE_SQL_TRANSACTION),
 						errmsg("distributed DDL commands must not appear within "
-							   "transaction blocks containing other modifications")));
+							   "transaction blocks containing data modifications")));
 	}
 
 	ShowNoticeIfNotUsing2PC();
@@ -1322,18 +1322,12 @@ ExecuteCommandOnWorkerShards(Oid relationId, const char *commandString)
 {
 	List *shardIntervalList = LoadShardIntervalList(relationId);
 	char *tableOwner = TableOwner(relationId);
-	HTAB *shardConnectionHash = NULL;
 	ListCell *shardIntervalCell = NULL;
 	Oid schemaId = get_rel_namespace(relationId);
 	char *schemaName = get_namespace_name(schemaId);
 
-	MemoryContext oldContext = MemoryContextSwitchTo(TopTransactionContext);
-
 	LockShards(shardIntervalList, ShareLock);
-
-	shardConnectionHash = OpenTransactionsToAllShardPlacements(shardIntervalList,
-															   tableOwner);
-	MemoryContextSwitchTo(oldContext);
+	OpenTransactionsToAllShardPlacements(shardIntervalList, tableOwner);
 
 	foreach(shardIntervalCell, shardIntervalList)
 	{
@@ -1345,9 +1339,7 @@ ExecuteCommandOnWorkerShards(Oid relationId, const char *commandString)
 		char *escapedCommandString = quote_literal_cstr(commandString);
 		StringInfo applyCommand = makeStringInfo();
 
-		shardConnections = GetShardConnections(shardConnectionHash,
-											   shardId,
-											   &shardConnectionsFound);
+		shardConnections = GetShardConnections(shardId, &shardConnectionsFound);
 		Assert(shardConnectionsFound);
 
 		/* build the shard ddl command */
