2a3d8b2913e7cb8b713982f36e968a5fd3d9e077
Jason Petersen <jason@citusdata.com>
2016-09-29 17:35:19 -0600

    Remove references to 9.4
    Some still lingered.

--- src/test/regress/sql/multi_upsert.sql
+++ src/test/regress/sql/multi_upsert.sql
@@ -1,6 +1,4 @@
 -- this test file aims to test UPSERT feature on Citus
--- note that output of this file for postgresql 9.4 will
--- be full syntax errors, which is expected.
 
 
 ALTER SEQUENCE pg_catalog.pg_dist_shardid_seq RESTART 980000;
