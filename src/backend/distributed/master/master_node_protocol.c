39ddf360847e05f26d6d29fda1b20edeae47f589
Metin Doslu <metin@citusdata.com>
2016-10-05 17:11:55 +0300

    Add guc variable for shard count

--- src/backend/distributed/master/master_node_protocol.c
+++ src/backend/distributed/master/master_node_protocol.c
@@ -55,6 +55,7 @@
 
 
 /* Shard related configuration */
+int ShardCount = 32;
 int ShardReplicationFactor = 2; /* desired replication factor for shards */
 int ShardMaxSize = 1048576;     /* maximum size in KB one shard can grow to */
 int ShardPlacementPolicy = SHARD_PLACEMENT_ROUND_ROBIN;
