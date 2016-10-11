8f477d18f18e97aabc996a516663bc45a85bb9e1
Eren Basak <aamederen@gmail.com>
2016-10-11 12:18:38 +0300

    Add pg_dist_local_group Metadata Table
    This change adds the pg_dist_local_group metadata table, which indicates
    the group id of the current node. It is expected that this table contains
    one and only one row, which only contains the group id of the node as an
    integer.

---
+++ src/backend/distributed/citus--6.0-8--6.0-9.sql
@@ -0,0 +1,9 @@
+CREATE TABLE citus.pg_dist_local_group(
+    groupid int NOT NULL PRIMARY KEY)
+;
+
+/* insert the default value for being the coordinator node */
+INSERT INTO citus.pg_dist_local_group VALUES (0);
+
+ALTER TABLE citus.pg_dist_local_group SET SCHEMA pg_catalog;
+GRANT SELECT ON pg_catalog.pg_dist_local_group TO public;
