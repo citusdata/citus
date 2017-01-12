88e9a429e14614dd61a04e7655fc1773e9d2c249
Eren Basak <aamederen@gmail.com>
2017-01-12 10:43:53 +0300

    Add Regression Tests For Querying MX Tables from Workers

---
+++ src/test/regress/sql/multi_mx_create_table.sql
@@ -0,0 +1,444 @@
+--
+-- MULTI_MX_CREATE_TABLE
+--
+
+ALTER SEQUENCE pg_catalog.pg_dist_shardid_seq RESTART 1220000;
+ALTER SEQUENCE pg_catalog.pg_dist_jobid_seq RESTART 1220000;
+
+SELECT start_metadata_sync_to_node('localhost', :worker_1_port);
+SELECT start_metadata_sync_to_node('localhost', :worker_2_port);
+
+-- create schema to test schema support
+CREATE SCHEMA citus_mx_test_schema;
+CREATE SCHEMA citus_mx_test_schema_join_1;
+CREATE SCHEMA citus_mx_test_schema_join_2;
+
+-- create UDFs that we're going to use in our tests
+SET search_path TO public;
+CREATE OR REPLACE FUNCTION simpleTestFunction(theValue integer)
+    RETURNS text AS
+$$
+DECLARE
+    strresult text;
+BEGIN
+    RETURN theValue * 3 / 2 + 1;
+END;
+$$
+LANGUAGE 'plpgsql' IMMUTABLE;
+
+SET search_path TO citus_mx_test_schema;
+CREATE OR REPLACE FUNCTION simpleTestFunction2(theValue integer)
+    RETURNS text AS
+$$
+DECLARE
+    strresult text;
+BEGIN
+    RETURN theValue * 3 / 2 + 1;
+END;
+$$
+LANGUAGE 'plpgsql' IMMUTABLE;
+
+CREATE FUNCTION public.immutable_append_mx(old_values int[], new_value int)
+RETURNS int[] AS $$ SELECT old_values || new_value $$ LANGUAGE SQL IMMUTABLE;
+
+CREATE OPERATOR citus_mx_test_schema.=== (
+    LEFTARG = int,
+    RIGHTARG = int,
+    PROCEDURE = int4eq,
+    COMMUTATOR = ===,
+    NEGATOR = !==,
+    HASHES, MERGES
+);
+
+SET search_path TO public;
+CREATE COLLATION citus_mx_test_schema.english FROM "en_US";
+
+CREATE TYPE citus_mx_test_schema.new_composite_type as (key1 text, key2 text);
+CREATE TYPE order_side_mx AS ENUM ('buy', 'sell');
+
+-- now create required stuff in the worker 1
+\c - - - :worker_1_port
+
+-- create schema to test schema support
+CREATE SCHEMA citus_mx_test_schema;
+CREATE SCHEMA citus_mx_test_schema_join_1;
+CREATE SCHEMA citus_mx_test_schema_join_2;
+
+-- create UDFs in worker node
+CREATE OR REPLACE FUNCTION simpleTestFunction(theValue integer)
+    RETURNS text AS
+$$
+DECLARE
+    strresult text;
+BEGIN
+    RETURN theValue * 3 / 2 + 1;
+END;
+$$
+LANGUAGE 'plpgsql' IMMUTABLE;
+
+SET search_path TO citus_mx_test_schema;
+CREATE OR REPLACE FUNCTION simpleTestFunction2(theValue integer)
+    RETURNS text AS
+$$
+DECLARE
+    strresult text;
+BEGIN
+    RETURN theValue * 3 / 2 + 1;
+END;
+$$
+LANGUAGE 'plpgsql' IMMUTABLE;
+
+CREATE FUNCTION public.immutable_append_mx(old_values int[], new_value int)
+RETURNS int[] AS $$ SELECT old_values || new_value $$ LANGUAGE SQL IMMUTABLE;
+
+-- create operator
+CREATE OPERATOR citus_mx_test_schema.=== (
+    LEFTARG = int,
+    RIGHTARG = int,
+    PROCEDURE = int4eq,
+    COMMUTATOR = ===,
+    NEGATOR = !==,
+    HASHES, MERGES
+);
+
+SET search_path TO public;
+CREATE COLLATION citus_mx_test_schema.english FROM "en_US";
+
+SET search_path TO public;
+CREATE TYPE citus_mx_test_schema.new_composite_type as (key1 text, key2 text);
+CREATE TYPE order_side_mx AS ENUM ('buy', 'sell');
+
+-- now create required stuff in the worker 2
+\c - - - :worker_2_port
+
+-- create schema to test schema support
+CREATE SCHEMA citus_mx_test_schema;
+CREATE SCHEMA citus_mx_test_schema_join_1;
+CREATE SCHEMA citus_mx_test_schema_join_2;
+
+
+-- create UDF
+CREATE OR REPLACE FUNCTION simpleTestFunction(theValue integer)
+    RETURNS text AS
+$$
+DECLARE
+    strresult text;
+BEGIN
+    RETURN theValue * 3 / 2 + 1;
+END;
+$$
+LANGUAGE 'plpgsql' IMMUTABLE;
+
+SET search_path TO citus_mx_test_schema;
+CREATE OR REPLACE FUNCTION simpleTestFunction2(theValue integer)
+    RETURNS text AS
+$$
+DECLARE
+    strresult text;
+BEGIN
+    RETURN theValue * 3 / 2 + 1;
+END;
+$$
+LANGUAGE 'plpgsql' IMMUTABLE;
+
+CREATE FUNCTION public.immutable_append_mx(old_values int[], new_value int)
+RETURNS int[] AS $$ SELECT old_values || new_value $$ LANGUAGE SQL IMMUTABLE;
+
+-- create operator
+CREATE OPERATOR citus_mx_test_schema.=== (
+    LEFTARG = int,
+    RIGHTARG = int,
+    PROCEDURE = int4eq,
+    COMMUTATOR = ===,
+    NEGATOR = !==,
+    HASHES, MERGES
+);
+
+
+SET search_path TO public;
+CREATE COLLATION citus_mx_test_schema.english FROM "en_US";
+
+SET search_path TO public;
+CREATE TYPE citus_mx_test_schema.new_composite_type as (key1 text, key2 text);
+CREATE TYPE order_side_mx AS ENUM ('buy', 'sell');
+
+-- connect back to the master, and do some more tests
+\c - - - :master_port
+
+SET citus.shard_replication_factor TO 1;
+SET citus.replication_model TO streaming;
+SET search_path TO public;
+
+CREATE TABLE nation_hash(
+    n_nationkey integer not null,
+    n_name char(25) not null,
+    n_regionkey integer not null,
+    n_comment varchar(152)
+);
+
+SET citus.shard_count TO 16;
+SELECT create_distributed_table('nation_hash', 'n_nationkey');
+
+SET search_path TO citus_mx_test_schema;
+
+-- create mx tables that we're going to use for our tests
+CREATE TABLE citus_mx_test_schema.nation_hash(
+    n_nationkey integer not null,
+    n_name char(25) not null,
+    n_regionkey integer not null,
+    n_comment varchar(152)
+);
+
+SELECT create_distributed_table('nation_hash', 'n_nationkey');
+
+CREATE TABLE citus_mx_test_schema_join_1.nation_hash (
+    n_nationkey integer not null,
+    n_name char(25) not null,
+    n_regionkey integer not null,
+    n_comment varchar(152));
+
+SET citus.shard_count TO 4;
+SELECT create_distributed_table('citus_mx_test_schema_join_1.nation_hash', 'n_nationkey');
+
+CREATE TABLE citus_mx_test_schema_join_1.nation_hash_2 (
+    n_nationkey integer not null,
+    n_name char(25) not null,
+    n_regionkey integer not null,
+    n_comment varchar(152));
+
+SELECT create_distributed_table('citus_mx_test_schema_join_1.nation_hash_2', 'n_nationkey');
+
+SET search_path TO citus_mx_test_schema_join_2;
+CREATE TABLE nation_hash (
+    n_nationkey integer not null,
+    n_name char(25) not null,
+    n_regionkey integer not null,
+    n_comment varchar(152));
+
+SELECT create_distributed_table('nation_hash', 'n_nationkey');
+
+SET search_path TO citus_mx_test_schema;
+CREATE TABLE nation_hash_collation_search_path(
+    n_nationkey integer not null,
+    n_name char(25) not null COLLATE english,
+    n_regionkey integer not null,
+    n_comment varchar(152)
+);
+SELECT create_distributed_table('nation_hash_collation_search_path', 'n_nationkey');
+
+\COPY nation_hash_collation_search_path FROM STDIN with delimiter '|';
+0|ALGERIA|0|haggle. carefully final deposits detect slyly agai
+1|ARGENTINA|1|al foxes promise slyly according to the regular accounts. bold requests alon
+2|BRAZIL|1|y alongside of the pending deposits. carefully special packages are about the ironic forges. slyly special 
+3|CANADA|1|eas hang ironic, silent packages. slyly regular packages are furiously over the tithes. fluffily bold
+4|EGYPT|4|y above the carefully unusual theodolites. final dugouts are quickly across the furiously regular d
+5|ETHIOPIA|0|ven packages wake quickly. regu
+\.
+
+CREATE TABLE citus_mx_test_schema.nation_hash_composite_types(
+    n_nationkey integer not null,
+    n_name char(25) not null,
+    n_regionkey integer not null,
+    n_comment varchar(152),
+    test_col citus_mx_test_schema.new_composite_type
+);
+
+SELECT create_distributed_table('citus_mx_test_schema.nation_hash_composite_types', 'n_nationkey');
+
+-- insert some data to verify composite type queries
+\COPY citus_mx_test_schema.nation_hash_composite_types FROM STDIN with delimiter '|';
+0|ALGERIA|0|haggle. carefully final deposits detect slyly agai|(a,a)
+1|ARGENTINA|1|al foxes promise slyly according to the regular accounts. bold requests alon|(a,b)
+2|BRAZIL|1|y alongside of the pending deposits. carefully special packages are about the ironic forges. slyly special |(a,c)
+3|CANADA|1|eas hang ironic, silent packages. slyly regular packages are furiously over the tithes. fluffily bold|(a,d)
+4|EGYPT|4|y above the carefully unusual theodolites. final dugouts are quickly across the furiously regular d|(a,e)
+5|ETHIOPIA|0|ven packages wake quickly. regu|(a,f)
+\.
+
+-- now create tpch tables 
+-- Create new table definitions for use in testing in distributed planning and
+-- execution functionality. Also create indexes to boost performance.
+SET search_path TO public;
+
+CREATE TABLE lineitem_mx (
+    l_orderkey bigint not null,
+    l_partkey integer not null,
+    l_suppkey integer not null,
+    l_linenumber integer not null,
+    l_quantity decimal(15, 2) not null,
+    l_extendedprice decimal(15, 2) not null,
+    l_discount decimal(15, 2) not null,
+    l_tax decimal(15, 2) not null,
+    l_returnflag char(1) not null,
+    l_linestatus char(1) not null,
+    l_shipdate date not null,
+    l_commitdate date not null,
+    l_receiptdate date not null,
+    l_shipinstruct char(25) not null,
+    l_shipmode char(10) not null,
+    l_comment varchar(44) not null,
+    PRIMARY KEY(l_orderkey, l_linenumber) );
+
+SET citus.shard_count TO 16;
+SELECT create_distributed_table('lineitem_mx', 'l_orderkey');
+
+CREATE INDEX lineitem_mx_time_index ON lineitem_mx (l_shipdate);
+
+CREATE TABLE orders_mx (
+    o_orderkey bigint not null,
+    o_custkey integer not null,
+    o_orderstatus char(1) not null,
+    o_totalprice decimal(15,2) not null,
+    o_orderdate date not null,
+    o_orderpriority char(15) not null,
+    o_clerk char(15) not null,
+    o_shippriority integer not null,
+    o_comment varchar(79) not null,
+    PRIMARY KEY(o_orderkey) );
+SELECT create_distributed_table('orders_mx', 'o_orderkey');
+
+CREATE TABLE customer_mx (
+    c_custkey integer not null,
+    c_name varchar(25) not null,
+    c_address varchar(40) not null,
+    c_nationkey integer not null,
+    c_phone char(15) not null,
+    c_acctbal decimal(15,2) not null,
+    c_mktsegment char(10) not null,
+    c_comment varchar(117) not null);
+
+SET citus.shard_count TO 1;
+SELECT create_distributed_table('customer_mx', 'c_custkey');
+
+CREATE TABLE nation_mx (
+    n_nationkey integer not null,
+    n_name char(25) not null,
+    n_regionkey integer not null,
+    n_comment varchar(152));
+
+SELECT create_distributed_table('nation_mx', 'n_nationkey');
+
+CREATE TABLE part_mx (
+    p_partkey integer not null,
+    p_name varchar(55) not null,
+    p_mfgr char(25) not null,
+    p_brand char(10) not null,
+    p_type varchar(25) not null,
+    p_size integer not null,
+    p_container char(10) not null,
+    p_retailprice decimal(15,2) not null,
+    p_comment varchar(23) not null);
+
+SELECT create_distributed_table('part_mx', 'p_partkey');
+
+CREATE TABLE supplier_mx
+(
+    s_suppkey integer not null,
+    s_name char(25) not null,
+    s_address varchar(40) not null,
+    s_nationkey integer,
+    s_phone char(15) not null,
+    s_acctbal decimal(15,2) not null,
+    s_comment varchar(101) not null
+);
+
+SELECT create_distributed_table('supplier_mx', 's_suppkey');
+
+-- Create test table for ddl
+CREATE TABLE mx_ddl_table (
+    key int primary key,
+    value int
+);
+
+SET citus.shard_count TO 4;
+SELECT create_distributed_table('mx_ddl_table', 'key', 'hash');
+
+-- Load some test data
+COPY mx_ddl_table (key, value) FROM STDIN WITH (FORMAT 'csv');
+1,10
+2,11
+3,21
+4,37
+5,60
+6,100
+10,200
+11,230
+\.
+
+-- test table for modifications
+CREATE TABLE limit_orders_mx (
+    id bigint PRIMARY KEY,
+    symbol text NOT NULL,
+    bidder_id bigint NOT NULL,
+    placed_at timestamp NOT NULL,
+    kind order_side_mx NOT NULL,
+    limit_price decimal NOT NULL DEFAULT 0.00 CHECK (limit_price >= 0.00)
+);
+
+SET citus.shard_count TO 2;
+SELECT create_distributed_table('limit_orders_mx', 'id');
+
+-- test table for modifications
+CREATE TABLE multiple_hash_mx (
+    category text NOT NULL,
+    data text NOT NULL
+);
+
+SELECT create_distributed_table('multiple_hash_mx', 'category');
+
+SET citus.shard_count TO 4;
+CREATE TABLE app_analytics_events_mx (id bigserial, app_id integer, name text);
+SELECT create_distributed_table('app_analytics_events_mx', 'app_id');
+
+
+CREATE TABLE researchers_mx (
+    id bigint NOT NULL,
+    lab_id int NOT NULL,
+    name text NOT NULL
+);
+
+SET citus.shard_count TO 2;
+SELECT create_distributed_table('researchers_mx', 'lab_id');
+
+CREATE TABLE labs_mx (
+    id bigint NOT NULL,
+    name text NOT NULL
+);
+
+SET citus.shard_count TO 1;
+SELECT create_distributed_table('labs_mx', 'id');
+
+-- now, for some special failures...
+CREATE TABLE objects_mx (
+    id bigint PRIMARY KEY,
+    name text NOT NULL
+);
+
+SELECT create_distributed_table('objects_mx', 'id', 'hash');
+
+CREATE TABLE articles_hash_mx (
+    id bigint NOT NULL,
+    author_id bigint NOT NULL,
+    title varchar(20) NOT NULL,
+    word_count integer
+);
+
+-- this table is used in router executor tests
+CREATE TABLE articles_single_shard_hash_mx (LIKE articles_hash_mx);
+
+SET citus.shard_count TO 2;
+SELECT create_distributed_table('articles_hash_mx', 'author_id');
+
+SET citus.shard_count TO 1;
+SELECT create_distributed_table('articles_single_shard_hash_mx', 'author_id');
+
+SET citus.shard_count TO 4;
+CREATE TABLE company_employees_mx (company_id int, employee_id int, manager_id int); 
+SELECT create_distributed_table('company_employees_mx', 'company_id');
+
+WITH shard_counts AS (
+	SELECT logicalrelid, count(*) AS shard_count FROM pg_dist_shard GROUP BY logicalrelid
+	)
+SELECT logicalrelid, colocationid, shard_count, partmethod, repmodel 
+FROM pg_dist_partition NATURAL JOIN shard_counts 
+ORDER BY colocationid, logicalrelid;
---
+++ src/test/regress/sql/multi_mx_metadata.sql
@@ -0,0 +1,162 @@
+-- Test creation of mx tables and metadata syncing
+
+-- get rid of the previously created entries in pg_dist_transaction
+-- for the sake of getting consistent results in this test file
+SELECT recover_prepared_transactions();
+
+CREATE TABLE distributed_mx_table (
+    key text primary key,
+    value jsonb
+);
+CREATE INDEX ON distributed_mx_table USING GIN (value);
+
+SET citus.shard_replication_factor TO 1;
+SET citus.replication_model TO streaming;
+
+SET citus.shard_count TO 4;
+SELECT create_distributed_table('distributed_mx_table', 'key');
+
+-- Verify that we've logged commit records
+SELECT count(*) FROM pg_dist_transaction;
+
+-- Confirm that the metadata transactions have been committed
+SELECT recover_prepared_transactions();
+
+-- Verify that the commit records have been removed
+SELECT count(*) FROM pg_dist_transaction;
+
+\c - - - :worker_1_port
+
+\d distributed_mx_table
+
+SELECT repmodel FROM pg_dist_partition
+WHERE logicalrelid = 'distributed_mx_table'::regclass;
+
+SELECT count(*) FROM pg_dist_shard JOIN pg_dist_shard_placement USING (shardid)
+WHERE logicalrelid = 'distributed_mx_table'::regclass;
+
+\c - - - :worker_2_port
+
+\d distributed_mx_table
+
+SELECT repmodel FROM pg_dist_partition
+WHERE logicalrelid = 'distributed_mx_table'::regclass;
+
+SELECT count(*) FROM pg_dist_shard JOIN pg_dist_shard_placement USING (shardid)
+WHERE logicalrelid = 'distributed_mx_table'::regclass;
+
+-- Create a table and then roll back the transaction
+\c - - - :master_port
+SET citus.shard_replication_factor TO 1;
+SET citus.replication_model TO streaming;
+
+BEGIN;
+CREATE TABLE should_not_exist (
+    key text primary key,
+    value jsonb
+);
+SELECT create_distributed_table('should_not_exist', 'key');
+ABORT;
+
+-- Verify that the table does not exist on the worker
+\c - - - :worker_1_port
+SELECT count(*) FROM pg_tables WHERE tablename = 'should_not_exist';
+
+-- Ensure that we don't allow prepare on a metadata transaction
+\c - - - :master_port
+SET citus.shard_replication_factor TO 1;
+SET citus.replication_model TO streaming;
+
+BEGIN;
+CREATE TABLE should_not_exist (
+    key text primary key,
+    value jsonb
+);
+SELECT create_distributed_table('should_not_exist', 'key');
+PREPARE TRANSACTION 'this_should_fail';
+
+-- now show that we can create tables and schemas withing a single transaction
+BEGIN;
+CREATE SCHEMA IF NOT EXISTS citus_mx_schema_for_xacts;
+SET search_path TO citus_mx_schema_for_xacts;
+SET citus.shard_replication_factor TO 1;
+SET citus.shard_count TO 1;
+
+CREATE TABLE objects_for_xacts (
+    id bigint PRIMARY KEY,
+    name text NOT NULL
+);
+SELECT create_distributed_table('objects_for_xacts', 'id');
+
+COMMIT;
+
+-- see that the table actually created and distributed
+\c - - - :worker_1_port
+SELECT repmodel FROM pg_dist_partition
+WHERE logicalrelid = 'citus_mx_schema_for_xacts.objects_for_xacts'::regclass;
+
+SELECT count(*) FROM pg_dist_shard JOIN pg_dist_shard_placement USING (shardid)
+WHERE logicalrelid = 'citus_mx_schema_for_xacts.objects_for_xacts'::regclass;
+
+\c - - - :master_port
+SET citus.shard_replication_factor TO 1;
+SET citus.replication_model TO streaming;
+
+-- now show that we can rollback on creating mx table, but shards remain.... 
+BEGIN;
+CREATE SCHEMA IF NOT EXISTS citus_mx_schema_for_xacts;
+SET search_path TO citus_mx_schema_for_xacts;
+SET citus.shard_replication_factor TO 1;
+SET citus.shard_count TO 2;
+
+CREATE TABLE objects_for_xacts2 (
+    id bigint PRIMARY KEY,
+    name text NOT NULL
+);
+SELECT create_distributed_table('objects_for_xacts2', 'id');
+
+ROLLBACK;
+
+-- show that the table not exists on the schema node
+SELECT count(*) FROM pg_tables WHERE tablename = 'objects_for_xacts2' and schemaname = 'citus_mx_schema_for_xacts';
+
+\c - - - :worker_1_port
+
+-- the distributed table not exists on the worker node
+SELECT count(*) FROM pg_tables WHERE tablename = 'objects_for_xacts2' and schemaname = 'citus_mx_schema_for_xacts';
+-- but the shard exists since we do not create shards in a transaction
+SELECT count(*) FROM pg_tables WHERE tablename LIKE 'objects_for_xacts2_%' and schemaname = 'citus_mx_schema_for_xacts';
+
+-- make sure that master_drop_all_shards does not work from the worker nodes
+SELECT master_drop_all_shards('citus_mx_schema_for_xacts.objects_for_xacts'::regclass, 'citus_mx_schema_for_xacts', 'objects_for_xacts');
+
+-- Ensure pg_dist_transaction is empty for test
+SELECT recover_prepared_transactions();
+
+-- Create some "fake" prepared transactions to recover
+\c - - - :worker_1_port
+
+BEGIN;
+CREATE TABLE should_abort (value int);
+PREPARE TRANSACTION 'citus_0_should_abort';
+
+BEGIN;
+CREATE TABLE should_commit (value int);
+PREPARE TRANSACTION 'citus_0_should_commit';
+
+BEGIN;
+CREATE TABLE should_be_sorted_into_middle (value int);
+PREPARE TRANSACTION 'citus_0_should_be_sorted_into_middle';
+
+\c - - - :master_port
+-- Add "fake" pg_dist_transaction records and run recovery
+INSERT INTO pg_dist_transaction VALUES (14, 'citus_0_should_commit');
+INSERT INTO pg_dist_transaction VALUES (14, 'citus_0_should_be_forgotten');
+
+SELECT recover_prepared_transactions();
+SELECT count(*) FROM pg_dist_transaction;
+
+-- Confirm that transactions were correctly rolled forward
+\c - - - :worker_1_port
+SELECT count(*) FROM pg_tables WHERE tablename = 'should_abort';
+SELECT count(*) FROM pg_tables WHERE tablename = 'should_commit';
