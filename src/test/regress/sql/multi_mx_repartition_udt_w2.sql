2536c77ef09361561eac68c7fd896d40dbb7f31d
Eren Basak <aamederen@gmail.com>
2017-01-12 10:43:53 +0300

    Add Regression Tests For Querying MX Tables from Workers

---
+++ src/test/regress/sql/multi_mx_repartition_udt_w2.sql
@@ -0,0 +1,20 @@
+--
+-- MULTI_MX_REPARTITION_W2_UDT
+--
+
+\c - - - :worker_2_port
+SET client_min_messages = LOG;
+-- Query that should result in a repartition join on UDT column.
+SET citus.large_table_shard_count = 1;
+SET citus.task_executor_type = 'task-tracker';
+SET citus.log_multi_join_order = true;
+
+-- Query that should result in a repartition join on int column, and be empty.
+SELECT * FROM repartition_udt JOIN repartition_udt_other
+    ON repartition_udt.pk = repartition_udt_other.pk
+	WHERE repartition_udt.pk > 1;
+
+SELECT * FROM repartition_udt JOIN repartition_udt_other
+    ON repartition_udt.udtcol = repartition_udt_other.udtcol
+	WHERE repartition_udt.pk > 1
+	ORDER BY repartition_udt.pk;
