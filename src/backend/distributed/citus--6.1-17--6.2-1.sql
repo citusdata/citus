aed36acfeb60a4f38ce4ed7293222c9ecdb98b64
Brian Cloutier <github@briancloutier.com>
2017-03-07 11:59:26 +0300

    Remove unused master_stage_shard_{placement_,}row

--- src/backend/distributed/citus--6.1-17--6.2-1.sql
+++ src/backend/distributed/citus--6.1-17--6.2-1.sql
@@ -5,4 +5,7 @@ SET search_path = 'pg_catalog';
 DROP FUNCTION IF EXISTS master_get_local_first_candidate_nodes();
 DROP FUNCTION IF EXISTS master_get_round_robin_candidate_nodes();
 
+DROP FUNCTION IF EXISTS master_stage_shard_row();
+DROP FUNCTION IF EXISTS master_stage_shard_placement_row();
+
 RESET search_path;
