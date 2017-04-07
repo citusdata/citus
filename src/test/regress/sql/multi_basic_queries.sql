0f63edc5b4b7d0150d74aa2787a03fe3eae11c0f
Marco Slot <marco.slot@gmail.com>
2017-04-07 12:18:34 +0200

    Add basic read-only transaction tests

--- src/test/regress/sql/multi_basic_queries.sql
+++ src/test/regress/sql/multi_basic_queries.sql
@@ -15,5 +15,11 @@ SELECT sum(l_extendedprice) FROM lineitem;
 
 SELECT avg(l_extendedprice) FROM lineitem;
 
+-- Verify that we can do queries in read-only mode
+BEGIN;
+SET TRANSACTION READ ONLY;
+SELECT count(*) FROM lineitem;
+COMMIT;
+
 -- Verify temp tables which are used for final result aggregation don't persist.
 SELECT count(*) FROM pg_class WHERE relname LIKE 'pg_merge_job_%' AND relkind = 'r';
