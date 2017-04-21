d2d1d47150e5fe666d4cc2d983bf22b6cd3d9c70
Jason Petersen <jason@citusdata.com>
2017-04-20 19:53:52 -0600

    Handle DoCopy changes
    Requires a non-nULL ParseState in PostgreSQL 10, so we make one.

--- src/backend/distributed/worker/worker_merge_protocol.c
+++ src/backend/distributed/worker/worker_merge_protocol.c
@@ -532,7 +532,18 @@ CopyTaskFilesFromDirectory(StringInfo schemaName, StringInfo relationName,
 			copyStatement->options = list_make1(copyOption);
 		}
 
+#if (PG_VERSION_NUM >= 100000)
+		{
+			ParseState *pstate = make_parsestate(NULL);
+			pstate->p_sourcetext = queryString;
+
+			DoCopy(pstate, copyStatement, -1, -1, &copiedRowCount);
+
+			free_parsestate(pstate);
+		}
+#else
 		DoCopy(copyStatement, queryString, &copiedRowCount);
+#endif
 		copiedRowTotal += copiedRowCount;
 		CommandCounterIncrement();
 	}
