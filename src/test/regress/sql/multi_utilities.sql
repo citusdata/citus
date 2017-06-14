0ae265c436d80c7d7b00c7479109d3948e06596b
Marco Slot <marco.slot@gmail.com>
2017-06-14 10:32:41 +0200

    Add citus_create_restore_point for distributed snapshots

--- src/test/regress/sql/multi_utilities.sql
+++ src/test/regress/sql/multi_utilities.sql
@@ -224,6 +224,9 @@ SELECT worker_hash('(1, 2)'::test_composite_type);
 
 SELECT citus_truncate_trigger();
 
+-- confirm that citus_create_restore_point works
+SELECT 1 FROM citus_create_restore_point('regression-test');
+
 -- TODO: support VERBOSE
 -- VACUUM VERBOSE dustbunnies;
 -- VACUUM (FULL, VERBOSE) dustbunnies;
