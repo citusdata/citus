dc9b63504e3fdefa5e35b121ab22761d50c9a6b0
Onder Kalaci <onder@citusdata.com>
2017-06-18 20:16:18 +0300

    Add some utility functions for partitioned tables
    This commit is intended to be a base for supporting declarative partitioning
    on distributed tables. Here we add the following utility functions and their
    unit tests:
    
      * Very basic functions including differnentiating partitioned tables and
        partitions, listing the partitions
      * Generating the PARTITION BY (expr) and adding this to the DDL events
        of partitioned tables
      * Ability to generate text representations of the ranges for partitions
      * Ability to generate the `ALTER TABLE parent_table ATTACH PARTITION
        partition_table FOR VALUES value_range`
      * Ability to apply add shard ids to the above command using
        `worker_apply_inter_shard_ddl_command()`
      * Ability to generate `ALTER TABLE parent_table DETACH PARTITION`

--- src/backend/distributed/relay/relay_event_utility.c
+++ src/backend/distributed/relay/relay_event_utility.c
@@ -437,25 +437,42 @@ RelayEventExtendNamesForInterShardCommands(Node *parseTree, uint64 leftShardId,
 			foreach(commandCell, commandList)
 			{
 				AlterTableCmd *command = (AlterTableCmd *) lfirst(commandCell);
+				char **referencedTableName = NULL;
+				char **relationSchemaName = NULL;
+
 				if (command->subtype == AT_AddConstraint)
 				{
 					Constraint *constraint = (Constraint *) command->def;
 					if (constraint->contype == CONSTR_FOREIGN)
 					{
-						char **referencedTableName = &(constraint->pktable->relname);
-						char **relationSchemaName = &(constraint->pktable->schemaname);
-
-						/* prefix with schema name if it is not added already */
-						SetSchemaNameIfNotExist(relationSchemaName, rightShardSchemaName);
-
-						/*
-						 * We will not append shard id to referencing table name or
-						 * constraint name. They will be handled when we drop into
-						 * RelayEventExtendNames.
-						 */
-						AppendShardIdToName(referencedTableName, rightShardId);
+						referencedTableName = &(constraint->pktable->relname);
+						relationSchemaName = &(constraint->pktable->schemaname);
 					}
 				}
+#if (PG_VERSION_NUM >= 100000)
+				else if (command->subtype == AT_AttachPartition || command->subtype ==
+						 AT_DetachPartition)
+				{
+					PartitionCmd *partitionCommand = (PartitionCmd *) command->def;
+
+					referencedTableName = &(partitionCommand->name->relname);
+					relationSchemaName = &(partitionCommand->name->schemaname);
+				}
+#endif
+				else
+				{
+					continue;
+				}
+
+				/* prefix with schema name if it is not added already */
+				SetSchemaNameIfNotExist(relationSchemaName, rightShardSchemaName);
+
+				/*
+				 * We will not append shard id to referencing table name or
+				 * constraint name. They will be handled when we drop into
+				 * RelayEventExtendNames.
+				 */
+				AppendShardIdToName(referencedTableName, rightShardId);
 			}
 
 			/* drop into RelayEventExtendNames for non-inter table commands */
@@ -604,7 +621,6 @@ AppendShardIdToName(char **name, uint64 shardId)
 	{
 		snprintf(extendedName, NAMEDATALEN, "%s%s", (*name), shardIdAndSeparator);
 	}
-
 	/*
 	 * Otherwise, we need to truncate the name further to accommodate
 	 * a sufficient hash value. The resulting name will avoid collision
