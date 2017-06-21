98eb817568540bbb6ab011ec79304b59bb192570
Marco Slot <marco.slot@gmail.com>
2017-06-21 12:38:19 +0200

    Allow choosing a connection for multiple placements with GetPlacementListConnection

--- src/include/distributed/placement_connection.h
+++ src/include/distributed/placement_connection.h
@@ -15,12 +15,43 @@
 /* forward declare, to avoid dependency on ShardPlacement definition */
 struct ShardPlacement;
 
+/* represents the way in which a placement is accessed */
+typedef enum ShardPlacementAccessType
+{
+	/* read from placement */
+	PLACEMENT_ACCESS_SELECT,
+
+	/* modify rows in placement */
+	PLACEMENT_ACCESS_DML,
+
+	/* modify placement schema */
+	PLACEMENT_ACCESS_DDL
+} ShardPlacementAccessType;
+
+/* represents access to a placement */
+typedef struct ShardPlacementAccess
+{
+	/* placement that is accessed */
+	struct ShardPlacement *placement;
+
+	/* the way in which the placement is accessed */
+	ShardPlacementAccessType accessType;
+} ShardPlacementAccess;
+
+
 extern MultiConnection * GetPlacementConnection(uint32 flags,
 												struct ShardPlacement *placement,
 												const char *userName);
 extern MultiConnection * StartPlacementConnection(uint32 flags,
 												  struct ShardPlacement *placement,
 												  const char *userName);
+
+extern MultiConnection * GetPlacementListConnection(uint32 flags,
+													List *placementAccessList,
+													const char *userName);
+extern MultiConnection * StartPlacementListConnection(uint32 flags,
+													  List *placementAccessList,
+													  const char *userName);
 
 extern void ResetPlacementConnectionManagement(void);
 extern void MarkFailedShardPlacements(void);
