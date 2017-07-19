953df34d221f37767fb52ef27d47b7c48be9ef76
Hadi Moshayedi <hadi@citusdata.com>
2017-07-19 11:41:59 -0400

    Explicit switch/case fall-throughs to avoid compiler warnings.
    GCC 7 added `-Wimplicit-fallthrough` to warn for not explicitly specified switch/case fall-throughs.
    
    According to https://gcc.gnu.org/gcc-7/changes.html, to suppress that warning we could either use `__attribute__(fallthrough)`, which didn't seem to work for earlier GCC versions, or a `/* fallthrough */` comment just before the following `case`.
    
    Previously Citus code had the fall-through comments inside the brackets, which didn't seem to suppress the warning. Putting a `/* fallthrough */` comment outside the brackets and right before the `case` fixes the problem.

--- src/backend/distributed/utils/ruleutils_96.c
+++ src/backend/distributed/utils/ruleutils_96.c
@@ -4448,9 +4448,9 @@ isSimpleNode(Node *node, Node *parentNode, int prettyFlags)
 					return false;
 				}
 				/* else do the same stuff as for T_SubLink et al. */
-				/* FALL THROUGH */
 			}
 
+		/* fallthrough */
 		case T_SubLink:
 		case T_NullTest:
 		case T_BooleanTest:
