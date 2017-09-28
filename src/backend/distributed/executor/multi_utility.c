075ebde471f109e97a900168609abb47217945a4
Marco Slot <marco@citusdata.com>
2017-09-27 20:55:35 -0700

    Execute transmit commands as superuser during task-tracker queries

--- src/backend/distributed/executor/multi_utility.c
+++ src/backend/distributed/executor/multi_utility.c
@@ -637,6 +637,8 @@ VerifyTransmitStmt(CopyStmt *copyStatement)
 {
 	char *fileName = NULL;
 
+	EnsureSuperUser();
+
 	/* do some minimal option verification */
 	if (copyStatement->relation == NULL ||
 		copyStatement->relation->relname == NULL)
