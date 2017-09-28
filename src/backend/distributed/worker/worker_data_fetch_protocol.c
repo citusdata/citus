ea52bee891e0809975cd2adf679073967dab67f0
Marco Slot <marco@citusdata.com>
2017-09-28 13:45:18 +0200

    Allow read-only users to run task-tracker queries

--- src/backend/distributed/worker/worker_data_fetch_protocol.c
+++ src/backend/distributed/worker/worker_data_fetch_protocol.c
@@ -861,8 +861,6 @@ FetchRegularTable(const char *nodeName, uint32 nodePort, const char *tableName)
 		CommandCounterIncrement();
 	}
 
-	SetUserIdAndSecContext(savedUserId, savedSecurityContext);
-
 	/*
 	 * Copy local file into the relation. We call ProcessUtility() instead of
 	 * directly calling DoCopy() because some extensions (e.g. cstore_fdw) hook
@@ -880,6 +878,8 @@ FetchRegularTable(const char *nodeName, uint32 nodePort, const char *tableName)
 
 	/* finally delete the temporary file we created */
 	DeleteFile(localFilePath->data);
+
+	SetUserIdAndSecContext(savedUserId, savedSecurityContext);
 
 	return true;
 }
