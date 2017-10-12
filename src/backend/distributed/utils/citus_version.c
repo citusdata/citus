8544878c4b2e752d2b03df47f5ee5f58a3b6bc5b
Jason Petersen <jason@citusdata.com>
2017-10-12 10:49:26 -0600

    Add citus_version(), analogous to PG's version()
    This will provide the full project name (i.e. Citus/Citus Enterprise),
    and the host system, compiler, and architecture word size.
    
    I wanted to limit the number of copied files in 'config', so I added
    only config.guess and call it manually, rather than using the macro
    AC_CANONICAL_HOST, which requires several other files.

---
+++ src/backend/distributed/utils/citus_version.c
@@ -0,0 +1,26 @@
+/*-------------------------------------------------------------------------
+ *
+ * citus_version.c
+ *
+ * This file contains functions for displaying the Citus version string
+ *
+ * Copyright (c) 2017, Citus Data, Inc.
+ *
+ *-------------------------------------------------------------------------
+ */
+
+#include "postgres.h"
+
+#include "citus_version.h"
+#include "utils/builtins.h"
+
+
+/* exports for SQL callable functions */
+PG_FUNCTION_INFO_V1(citus_version);
+
+
+Datum
+citus_version(PG_FUNCTION_ARGS)
+{
+	PG_RETURN_TEXT_P(cstring_to_text(CITUS_VERSION_STR));
+}
