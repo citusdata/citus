e5b489b5f412afbd6ec597d2b8d07d630ee5ca24
Hadi Moshayedi <hadi@moshayedi.net>
2017-10-12 12:43:45 -0400

    grant select privilidges to pg_dist_metadata.

--- src/backend/distributed/citus--7.0-15--7.0-16.sql
+++ src/backend/distributed/citus--7.0-15--7.0-16.sql
@@ -5,5 +5,6 @@ CREATE TABLE citus.pg_dist_metadata(
     value text NOT NULL
 );
 ALTER TABLE citus.pg_dist_metadata SET SCHEMA pg_catalog;
+GRANT SELECT ON pg_catalog.pg_dist_metadata TO public;
 
 INSERT INTO pg_dist_metadata VALUES ('server_id', (random() * 1e18)::bigint::text);
