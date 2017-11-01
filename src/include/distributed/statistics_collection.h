85b94a8aa11cfdbe74f96ae2ce3a2c50cf9b3485
Hadi Moshayedi <hadi@moshayedi.net>
2017-10-31 21:23:31 -0400

    Check for Citus updates.
    Sends a request to /v1/releases/latest?flavor=$CITUS_EDITION once a day,
    which returns a response similar to {"version": "7.1.0", "major": 7,
    "minor": 1, "patch": 0}. Then compares it with current Citus version,
    and if the latest release is newer, logs a LOG message.

--- src/include/distributed/statistics_collection.h
+++ src/include/distributed/statistics_collection.h
@@ -33,6 +33,7 @@ extern bool EnableStatisticsCollection;
 
 extern void WarnIfSyncDNS(void);
 extern bool CollectBasicUsageStatistics(void);
+extern void CheckForUpdates(void);
 
 #endif /* HAVE_LIBCURL */
 
