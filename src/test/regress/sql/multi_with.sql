636d34051080b72dab28b47a7a3e1bdb217f38b3
mehmet furkan şahin <sahinffurkan@gmail.com>
2017-12-04 16:16:17 +0300

    some more cte tests about colocation and with

--- src/test/regress/sql/multi_with.sql
+++ src/test/regress/sql/multi_with.sql
@@ -1,4 +1,7 @@
 
+CREATE TABLE local_users (user_id int, event_type int);
+INSERT INTO local_users VALUES (0, 0), (1, 4), (1, 7), (2, 1), (3, 3), (5, 4), (6, 2), (10, 7);
+
 WITH users_events AS (
   SELECT 
       users_table.user_id as user_id,
@@ -131,6 +134,307 @@ FROM
   users_events
 GROUP BY
   1;
+
+
+-- Nested CTEs - joined with local table. Not supported yet.
+WITH users_events AS (
+  -- router select query
+  WITH users_events_1 AS (
+    SELECT
+      *
+    FROM
+      users_table
+    WHERE
+      user_id = 1
+  ),
+  -- real-time select query
+  users_events_2_3 AS (
+    SELECT
+      *
+    FROM
+      users_table
+    WHERE
+      user_id = 2
+      OR
+      user_id = 3
+  ),
+  -- router select query
+  -- sub CTE is a real-time executor query but the top level is router select
+  users_events_4 AS (
+    WITH users_events_4_5 AS (
+    SELECT
+      *
+    FROM
+      users_table
+    WHERE
+      user_id = 4
+      OR
+      user_id = 5
+    )
+    SELECT
+        *
+    FROM
+        users_events_4_5
+    WHERE
+        user_id = 4
+  ),
+  -- merge all the results from CTEs
+  merged_users AS (
+      SELECT
+        *
+      FROM
+        users_events_1
+    UNION
+      SELECT
+        *
+      FROM
+        users_events_2_3
+    UNION 
+      SELECT
+        *
+      FROM
+        users_events_4
+  ),
+  -- join with a local table 
+  local_users AS (
+    SELECT
+      *
+    FROM
+      (SELECT user_id as uid
+      FROM local_users) l_users
+      join
+      merged_users
+      on (l_users.uid = merged_users.user_id)
+  )
+  SELECT
+    *
+  FROM
+    local_users
+)
+SELECT 
+  * 
+FROM
+  users_events
+ORDER BY
+  1, 2, 3, 4, 5, 6
+LIMIT
+  20;
+
+
+-- Nested CTEs
+WITH users_events AS (
+  -- router select query
+  WITH users_events_1 AS (
+    SELECT
+      *
+    FROM
+      users_table
+    WHERE
+      user_id = 1
+  ),
+  -- real-time select query
+  users_events_2_3 AS (
+    SELECT
+      *
+    FROM
+      users_table
+    WHERE
+      user_id = 2
+      OR
+      user_id = 3
+  ),
+  -- router select query
+  -- sub CTE is a real-time executor query but the top level is router select
+  users_events_4 AS (
+    WITH users_events_4_5 AS (
+    SELECT
+      *
+    FROM
+      users_table
+    WHERE
+      user_id = 4
+      OR
+      user_id = 5
+    )
+    SELECT
+        *
+    FROM
+        users_events_4_5
+    WHERE
+        user_id = 4
+  ),
+  -- merge all the results from CTEs
+  merged_users AS (
+      SELECT
+        *
+      FROM
+        users_events_1
+    UNION
+      SELECT
+        *
+      FROM
+        users_events_2_3
+    UNION 
+      SELECT
+        *
+      FROM
+        users_events_4
+  )
+  SELECT
+    *
+  FROM
+    merged_users
+)
+SELECT 
+  * 
+FROM
+  users_events
+ORDER BY
+  1, 2, 3, 4, 5, 6
+LIMIT
+  20;
+
+
+SET citus.task_executor_type TO 'task-tracker';
+-- Co-location tests
+
+-- Two non-colocated CTE under a co-located join 
+WITH non_colocated_1 AS (
+  SELECT 
+    users_table.user_id
+  FROM
+    users_table, events_table
+  WHERE
+    users_table.user_id = events_table.value_2 AND event_type IN (1, 2, 3)
+),
+non_colocated_2 AS (
+  SELECT 
+    users_table.user_id
+  FROM
+    users_table, events_table
+  WHERE
+    users_table.user_id = events_table.value_2 AND event_type IN (4, 5, 6)
+)
+
+SELECT non_colocated_1.user_id, count(*) 
+FROM
+  non_colocated_1, non_colocated_2
+WHERE
+  non_colocated_1.user_id = non_colocated_2.user_id
+GROUP BY
+  1
+ORDER BY
+  2 DESC, 1;
+
+SET citus.task_executor_type TO 'real-time';
+-- Two colocated CTE under a non-colocated join 
+WITH colocated_1 AS (
+  SELECT 
+    users_table.user_id, events_table.value_2
+  FROM
+    users_table, events_table
+  WHERE
+    users_table.user_id = events_table.user_id AND event_type IN (1, 2, 3)
+),
+colocated_2 AS (
+  SELECT 
+    users_table.user_id, events_table.value_2
+  FROM
+    users_table, events_table
+  WHERE
+    users_table.user_id = events_table.user_id AND event_type IN (4, 5, 6)
+)
+SELECT colocated_1.user_id, count(*) 
+FROM
+  colocated_1, colocated_2
+WHERE
+  colocated_1.value_2 = colocated_2.value_2
+GROUP BY
+  1
+ORDER BY
+  2 DESC, 1;
+
+
+-- CTE in WHERE basic
+SELECT
+  count(*)
+FROM
+  events_table
+WHERE
+  event_type
+IN
+  (WITH events AS (
+    SELECT 
+      event_type 
+    FROM 
+      events_table 
+    WHERE user_id < 5 
+    GROUP BY 
+      1 
+    ORDER BY 
+      1)
+    SELECT * FROM events LIMIT 10
+  );
+
+SET citus.task_executor_type = 'task-tracker';
+-- CTE with non-colocated join in WHERE
+SELECT
+  count(*)
+FROM
+  events_table
+WHERE
+  user_id IN
+    (WITH users AS
+      (SELECT
+          events_table.user_id
+        FROM
+          events_table, users_table
+        WHERE
+          events_table.value_2 = users_table.value_2
+        GROUP BY 
+          1
+        ORDER BY
+          1
+      )
+      SELECT * FROM users LIMIT 10
+    );
+
+-- this query doesn't make much sense though but tests colocated join and non-colocated join together
+WITH users_events AS (
+  WITH colocated_join AS (
+    SELECT
+      users_table.user_id as uid, events_table.event_type
+    FROM
+        users_table
+      join
+        events_table
+      on (users_table.user_id = events_table.user_id)
+    WHERE
+      users_table.value_2 IN (1, 2)
+  ),
+
+  non_colocated_join AS (
+    SELECT
+      users_table.user_id as n_uid, events_table.event_type
+    FROM
+        users_table
+      join
+        events_table
+      on (users_table.value_2 = events_table.value_2)
+  )
+  SELECT
+    uid, colocated_join.event_type
+  FROM
+      colocated_join
+    INNER join
+      non_colocated_join
+    on (colocated_join.event_type = non_colocated_join.event_type)
+)
+SELECT
+  sum(uid), sum(event_type)
+FROM
+  users_events;
+SET citus.task_executor_type = 'real-time';
 
 
 -- prepared statements
