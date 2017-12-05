1d19be564c84657d7ed0d74ef14e9f3ce4bf22fe
mehmet furkan şahin <sahinffurkan@gmail.com>
2017-12-05 14:37:48 +0300

    some more tests are added and expected results are changed

--- src/test/regress/sql/multi_with_where.sql
+++ src/test/regress/sql/multi_with_where.sql
@@ -1,4 +1,115 @@
 -- CTE in WHERE basic
+WITH events AS (
+  SELECT 
+    event_type 
+  FROM 
+    events_table 
+  WHERE user_id < 5 
+  GROUP BY 
+    1 
+  ORDER BY 
+    1
+  LIMIT 10
+)
+SELECT
+  count(*)
+FROM
+  events_table
+WHERE
+  event_type
+IN
+  (SELECT
+    *
+  FROM
+    events
+  );
+
+
+WITH users AS (
+    SELECT
+      events_table.user_id
+    FROM
+      events_table, users_table
+    WHERE
+      events_table.user_id = users_table.user_id
+    GROUP BY 
+      1
+    ORDER BY
+      1
+    LIMIT 10
+) 
+SELECT
+  count(*)
+FROM
+  events_table
+WHERE
+  user_id IN
+    (
+      SELECT
+        *
+      FROM
+        users
+    );
+
+
+SET citus.task_executor_type = 'task-tracker';
+-- THIS AND THE NEXT QUERY DOES NOT RUN WITH TASK-TRACKER
+WITH users AS (
+    SELECT
+      events_table.user_id
+    FROM
+      events_table, users_table
+    WHERE
+      events_table.user_id = users_table.user_id
+    GROUP BY 
+      1
+    ORDER BY
+      1
+    LIMIT 10
+) 
+SELECT
+  count(*)
+FROM
+  events_table
+WHERE
+  user_id IN
+    (
+      SELECT
+        *
+      FROM
+        users
+    );
+
+
+-- CTE with non-colocated join in WHERE
+WITH users AS (
+    SELECT
+      events_table.user_id
+    FROM
+      events_table, users_table
+    WHERE
+      events_table.value_2 = users_table.value_2
+    GROUP BY 
+      1
+    ORDER BY
+      1
+    LIMIT 10
+) 
+SELECT
+  count(*)
+FROM
+  events_table
+WHERE
+  user_id IN
+    (
+      SELECT
+        *
+      FROM
+        users
+    );
+
+SET citus.task_executor_type = 'real-time';
+-- CTE in WHERE basic
 SELECT
   count(*)
 FROM
