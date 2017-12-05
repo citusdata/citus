e9f5d5dd400421b800d0c8824a5aeaa7e3f45121
Marco Slot <marco.slot@gmail.com>
2017-12-05 17:26:04 +0100

    Use proper schema in read_intermediate_result signature

--- src/backend/distributed/citus--7.2-1--7.2-2.sql
+++ src/backend/distributed/citus--7.2-1--7.2-2.sql
@@ -1,12 +1,12 @@
 /* citus--7.2-1--7.2-2 */
 
-CREATE TYPE citus.copy_format AS ENUM ('csv', 'binary', 'text');
+CREATE TYPE pg_catalog.citus_copy_format AS ENUM ('csv', 'binary', 'text');
 
-CREATE OR REPLACE FUNCTION pg_catalog.read_intermediate_result(result_id text, format citus.copy_format default 'csv')
-    RETURNS record
+CREATE OR REPLACE FUNCTION pg_catalog.read_intermediate_result(result_id text, format pg_catalog.citus_copy_format default 'csv')
+    RETURNS SETOF record
     LANGUAGE C STRICT VOLATILE PARALLEL SAFE
     AS 'MODULE_PATHNAME', $$read_intermediate_result$$;
-COMMENT ON FUNCTION pg_catalog.read_intermediate_result(text,citus.copy_format)
+COMMENT ON FUNCTION pg_catalog.read_intermediate_result(text,pg_catalog.citus_copy_format)
     IS 'read a file and return it as a set of records';
 
 CREATE OR REPLACE FUNCTION pg_catalog.create_intermediate_result(result_id text, query text)
