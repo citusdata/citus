207029eb9bcf23e647008d9f0cb216a6cb09dd52
mehmet furkan şahin <sahinffurkan@gmail.com>
2017-12-06 14:16:26 +0300

    FROM related tests are added

--- src/test/regress/sql/multi_with_basics.sql
+++ src/test/regress/sql/multi_with_basics.sql
@@ -54,6 +54,18 @@ LIMIT
 	10;
 
 
+-- CTE in SELECT level should not be allowed
+SELECT user_id = (
+	WITH users_done_event_3 AS (
+		SELECT users_table.user_id FROM users_table, events_table WHERE users_table.user_id=events_table.user_id AND event_type=3
+	)
+	SELECT * FROM users_done_event_3 WHERE user_id < 4
+)
+FROM users_table 
+ORDER BY 1
+LIMIT 10;
+
+
 -- the same CTEs in FROM and WHERE
 SELECT * FROM 
 (
@@ -103,3 +115,39 @@ ORDER BY
 	1, 2
 LIMIT
 	10;
+
+
+-- CTEs in FROM should work
+WITH cte AS (
+	SELECT user_id, value_2 from users_table WHERE user_id = 1 ORDER BY 2 LIMIT 5
+)
+SELECT * FROM cte;
+
+-- create and use CTE in FROM should work
+SELECT * FROM (WITH cte AS (
+		SELECT user_id, value_2 from users_table WHERE user_id = 1 ORDER BY 2 LIMIT 5
+	)
+SELECT * FROM cte) a;
+
+--CTEs in FROM in a subquery should work
+SELECT * FROM (WITH cte AS (
+		SELECT user_id, value_2 from users_table WHERE user_id = 1 ORDER BY 2 LIMIT 5
+	)
+SELECT * FROM cte) b;
+
+--CTEs in FROM in a subquery should work even when we create it there
+SELECT * FROM (
+	SELECT * FROM (WITH cte AS (
+		SELECT user_id, value_2 from users_table WHERE user_id = 1 ORDER BY 2 LIMIT 5
+		)
+		SELECT * FROM cte
+	) a) b;
+
+-- CTEs in FROM and as a subquery in FROM should work
+WITH users_events AS (
+	WITH users_done_2_3 AS (
+		SELECT users_table.user_id, users_table.value_2 FROM users_table, events_table WHERE users_table.user_id = events_table.user_id AND event_type IN (2, 3)
+	)
+	SELECT users_done_2_3.user_id, users_done_2_3.value_2 as value_2_3 FROM users_done_2_3
+)
+SELECT * FROM (SELECT * FROM users_events WHERE value_2_3 IN (2, 5)) a ORDER BY 1, 2 LIMIT 10;
