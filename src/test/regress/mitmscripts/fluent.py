3e309e3fefbd5b23ec3ed2fa0d11a7bacf234185
Brian Cloutier <github@briancloutier.com>
2018-03-20 18:51:09 -0700

    network proxy-based failure testing
    - Lots of detail is in src/test/regress/mitmscripts/README
    - Create a new target, make check-failure, which runs tests
    - Tells travis how to install everything and run the tests

---
+++ src/test/regress/mitmscripts/fluent.py
@@ -0,0 +1,425 @@
+import re
+import os
+import pprint
+import signal
+import socket
+import struct
+import threading
+import time
+import traceback
+import queue
+
+from mitmproxy import ctx
+from mitmproxy.utils import strutils
+from mitmproxy.proxy.protocol import TlsLayer, RawTCPLayer
+
+import structs
+
+# I. Command Strings
+
+class Stop(Exception):
+    pass
+
+class Handler:
+    '''
+    This class hierarchy serves two purposes:
+    1. Allow command strings to be evaluated. Once evaluated you'll have a Handler you can
+       pass packets to
+    2. Process packets as they come in and decide what to do with them.
+
+    Subclasses which want to change how packets are handled should override _handle.
+    '''
+    def __init__(self, root=None):
+        # all packets are first sent to the root handler to be processed
+        self.root = root if root else self
+        # all handlers keep track of the next handler so they know where to send packets
+        self.next = None
+
+    def _accept(self, flow, message):
+        result = self._handle(flow, message)
+
+        if result == 'pass':
+            # defer to our child
+            if not self.next:
+                raise Exception("we don't know what to do!")
+
+            try:
+                self.next._accept(flow, message)
+            except Stop:
+                if self.root is not self:
+                    raise
+                self.next = KillHandler(self)
+                flow.kill()
+        elif result == 'done':
+            # stop processing this packet, move on to the next one
+            return
+        elif result == 'stop':
+            # from now on kill all connections
+            raise Stop()
+
+    def _handle(self, flow, message):
+        '''
+        Handlers can return one of three things:
+        - "done" tells the parent to stop processing. This performs the default action,
+          which is to allow the packet to be sent.
+        - "pass" means to delegate to self.next and do whatever it wants
+        - "stop" means all processing will stop, and all connections will be killed
+        '''
+        # subclasses must implement this
+        raise NotImplementedError()
+
+class FilterableMixin:
+    def contains(self, pattern):
+        self.next = Contains(self.root, pattern)
+        return self.next
+
+    def matches(self, pattern):
+        self.next = Matches(self.root, pattern)
+        return self.next
+
+    def after(self, times):
+        self.next = After(self.root, times)
+        return self.next
+
+    def __getattr__(self, attr):
+        '''
+        Methods such as .onQuery trigger when a packet with that name is intercepted
+
+        Adds support for commands such as:
+          conn.onQuery(query="COPY")
+
+        Returns a function because the above command is resolved in two steps:
+          conn.onQuery becomes conn.__getattr__("onQuery")
+          conn.onQuery(query="COPY") becomes conn.__getattr__("onQuery")(query="COPY")
+        '''
+        if attr.startswith('on'):
+            def doit(**kwargs):
+                self.next = OnPacket(self.root, attr[2:], kwargs)
+                return self.next
+            return doit
+        raise AttributeError
+
+class ActionsMixin:
+    def kill(self):
+        self.next = KillHandler(self.root)
+        return self.next
+
+    def allow(self):
+        self.next = AcceptHandler(self.root)
+        return self.next
+
+    def killall(self):
+        self.next = KillAllHandler(self.root)
+        return self.next
+
+    def reset(self):
+        self.next = ResetHandler(self.root)
+        return self.next
+
+    def cancel(self, pid):
+        self.next = CancelHandler(self.root, pid)
+        return self.next
+
+class AcceptHandler(Handler):
+    def __init__(self, root):
+        super().__init__(root)
+    def _handle(self, flow, message):
+        return 'done'
+
+class KillHandler(Handler):
+    def __init__(self, root):
+        super().__init__(root)
+    def _handle(self, flow, message):
+        flow.kill()
+        return 'done'
+
+class KillAllHandler(Handler):
+    def __init__(self, root):
+        super().__init__(root)
+    def _handle(self, flow, message):
+        return 'stop'
+
+class ResetHandler(Handler):
+    # try to force a RST to be sent, something went very wrong!
+    def __init__(self, root):
+        super().__init__(root)
+    def _handle(self, flow, message):
+        flow.kill() # tell mitmproxy this connection should be closed
+
+        # this is a mitmproxy.connections.ClientConnection(mitmproxy.tcp.BaseHandler)
+        client_conn = flow.client_conn
+        # this is a regular socket object
+        conn = client_conn.connection
+
+        # cause linux to send a RST
+        LINGER_ON, LINGER_TIMEOUT = 1, 0
+        conn.setsockopt(
+            socket.SOL_SOCKET, socket.SO_LINGER,
+            struct.pack('ii', LINGER_ON, LINGER_TIMEOUT)
+        )
+        conn.close()
+
+        # closing the connection isn't ideal, this thread later crashes when mitmproxy
+        # tries to call conn.shutdown(), but there's nothing else to clean up so that's
+        # maybe okay
+
+        return 'done'
+
+class CancelHandler(Handler):
+    'Send a SIGINT to the process'
+    def __init__(self, root, pid):
+        super().__init__(root)
+        self.pid = pid
+    def _handle(self, flow, message):
+        os.kill(self.pid, signal.SIGINT)
+        # give the signal a chance to be received before we let the packet through
+        time.sleep(0.1)
+        return 'done'
+
+class Contains(Handler, ActionsMixin, FilterableMixin):
+    def __init__(self, root, pattern):
+        super().__init__(root)
+        self.pattern = pattern
+
+    def _handle(self, flow, message):
+        if self.pattern in message.content:
+            return 'pass'
+        return 'done'
+
+class Matches(Handler, ActionsMixin, FilterableMixin):
+    def __init__(self, root, pattern):
+        super().__init__(root)
+        self.pattern = re.compile(pattern)
+
+    def _handle(self, flow, message):
+        if self.pattern.search(message.content):
+            return 'pass'
+        return 'done'
+
+class After(Handler, ActionsMixin, FilterableMixin):
+    "Don't pass execution to our child until we've handled 'times' messages"
+    def __init__(self, root, times):
+        super().__init__(root)
+        self.target = times
+
+    def _handle(self, flow, message):
+        if not hasattr(flow, '_after_count'):
+            flow._after_count = 0
+
+        if flow._after_count >= self.target:
+            return 'pass'
+
+        flow._after_count += 1
+        return 'done'
+
+class OnPacket(Handler, ActionsMixin, FilterableMixin):
+    '''Triggers when a packet of the specified kind comes around'''
+    def __init__(self, root, packet_kind, kwargs):
+        super().__init__(root)
+        self.packet_kind = packet_kind
+        self.filters = kwargs
+    def _handle(self, flow, message):
+        if not message.parsed:
+            # if this is the first message in the connection we just skip it
+            return 'done'
+        for msg in message.parsed:
+            typ = structs.message_type(msg, from_frontend=message.from_client)
+            if typ == self.packet_kind:
+                matches = structs.message_matches(msg, self.filters, message.from_client)
+                if matches:
+                    return 'pass'
+        return 'done'
+
+class RootHandler(Handler, ActionsMixin, FilterableMixin):
+    def _handle(self, flow, message):
+        # do whatever the next Handler tells us to do
+        return 'pass'
+
+class RecorderCommand:
+    def __init__(self):
+        self.root = self
+        self.command = None
+
+    def dump(self):
+        # When the user calls dump() we return everything we've captured
+        self.command = 'dump'
+        return self
+
+    def reset(self):
+        # If the user calls reset() we dump all captured packets without returning them
+        self.command = 'reset'
+        return self
+
+# II. Utilities for interfacing with mitmproxy
+
+def build_handler(spec):
+    'Turns a command string into a RootHandler ready to accept packets'
+    root = RootHandler()
+    recorder = RecorderCommand()
+    handler = eval(spec, {'__builtins__': {}}, {'conn': root, 'recorder': recorder})
+    return handler.root
+
+# a bunch of globals
+
+handler = None  # the current handler used to process packets
+command_thread = None  # sits on the fifo and waits for new commands to come in
+command_queue = queue.Queue()  # we poll this from the main thread and apply commands
+response_queue = queue.Queue()  # the main thread uses this to reply to command_thread
+captured_messages = queue.Queue()  # where we store messages used for recorder.dump()
+connection_count = 0  # so we can give connections ids in recorder.dump()
+
+def listen_for_commands(fifoname):
+
+    def emit_row(conn, from_client, message):
+        # we're using the COPY text format. It requires us to escape backslashes
+        cleaned = message.replace('\\', '\\\\')
+        source = 'coordinator' if from_client else 'worker'
+        return '{}\t{}\t{}'.format(conn, source, cleaned)
+
+    def emit_message(message):
+        if message.is_initial:
+            return emit_row(
+                message.connection_id, message.from_client, '[initial message]'
+            )
+
+        pretty = structs.print(message.parsed)
+        return emit_row(message.connection_id, message.from_client, pretty)
+
+    def handle_recorder(recorder):
+        global connection_count
+        result = ''
+
+        if recorder.command is 'reset':
+            result = ''
+            connection_count = 0
+        elif recorder.command is not 'dump':
+            # this should never happen
+            raise Exception('Unrecognized command: {}'.format(recorder.command))
+
+        try:
+            results = []
+            while True:
+                message = captured_messages.get(block=False)
+                if recorder.command is 'reset':
+                    continue
+                results.append(emit_message(message))
+        except queue.Empty:
+            pass
+        result = '\n'.join(results)
+
+        with open(fifoname, mode='w') as fifo:
+            fifo.write('{}'.format(result))
+
+    while True:
+        with open(fifoname, mode='r') as fifo:
+            slug = fifo.read()
+
+        try:
+            handler = build_handler(slug)
+            if isinstance(handler, RecorderCommand):
+                handle_recorder(handler)
+                continue
+        except Exception as e:
+            traceback.print_exc()
+            result = str(e)
+        else:
+            result = None
+
+        if not result:
+            command_queue.put(slug)
+            result = response_queue.get()
+
+        with open(fifoname, mode='w') as fifo:
+            fifo.write('{}\n'.format(result))
+
+def create_thread(fifoname):
+    global command_thread
+
+    if not fifoname:
+        return
+    if not len(fifoname):
+        return
+
+    if command_thread:
+        print('cannot change the fifo path once mitmproxy has started');
+        return
+
+    command_thread = threading.Thread(target=listen_for_commands, args=(fifoname,), daemon=True)
+    command_thread.start()
+
+# III. mitmproxy callbacks
+
+def load(loader):
+    loader.add_option('slug', str, 'conn.allow()', "A script to run")
+    loader.add_option('fifo', str, '', "Which fifo to listen on for commands")
+
+
+def tick():
+    # we do this crazy dance because ctx isn't threadsafe, it is only useable while a
+    # callback (such as this one) is being called.
+    try:
+        slug = command_queue.get_nowait()
+    except queue.Empty:
+        return
+
+    try:
+        ctx.options.update(slug=slug)
+    except Exception as e:
+        response_queue.put(str(e))
+    else:
+        response_queue.put('')
+
+
+def configure(updated):
+    global handler
+
+    if 'slug' in updated:
+        text = ctx.options.slug
+        handler = build_handler(text)
+
+    if 'fifo' in updated:
+        fifoname = ctx.options.fifo
+        create_thread(fifoname)
+
+
+def next_layer(layer):
+    '''
+    mitmproxy wasn't really meant for intercepting raw tcp streams, it tries to wrap the
+    upsteam connection (the one to the worker) in a tls stream. This hook intercepts the
+    part where it creates the TlsLayer (it happens in root_context.py) and instead creates
+    a RawTCPLayer. That's the layer which calls our tcp_message hook
+    '''
+    if isinstance(layer, TlsLayer):
+        replacement = RawTCPLayer(layer.ctx)
+        layer.reply.send(replacement)
+
+
+def tcp_message(flow):
+    '''
+    This callback is hit every time mitmproxy receives a packet. It's the main entrypoint
+    into this script.
+    '''
+    global connection_count
+
+    tcp_msg = flow.messages[-1]
+
+    # Keep track of all the different connections, assign a unique id to each
+    if not hasattr(flow, 'connection_id'):
+        flow.connection_id = connection_count
+        connection_count += 1  # this is not thread safe but I think that's fine
+    tcp_msg.connection_id = flow.connection_id
+
+    # The first packet the frontend sends shounld be parsed differently
+    tcp_msg.is_initial = len(flow.messages) == 1
+
+    if tcp_msg.is_initial:
+        # skip parsing initial messages for now, they're not important
+        tcp_msg.parsed = None
+    else:
+        tcp_msg.parsed = structs.parse(tcp_msg.content, from_frontend=tcp_msg.from_client)
+
+    # record the message, for debugging purposes
+    captured_messages.put(tcp_msg)
+
+    # okay, finally, give the packet to the command the user wants us to use
+    handler._accept(flow, tcp_msg)
---
+++ src/test/regress/mitmscripts/structs.py
@@ -0,0 +1,410 @@
+from construct import (
+    Struct,
+    Int8ub, Int16ub, Int32ub, Int16sb, Int32sb,
+    Bytes, CString, Computed, Switch, Seek, this, Pointer,
+    GreedyRange, Enum, Byte, Probe, FixedSized, RestreamData, GreedyBytes, Array
+)
+import construct.lib as cl
+
+import re
+
+class MessageMeta(type):
+    def __init__(cls, name, bases, namespace):
+        '''
+        __init__ is called every time a subclass of MessageMeta is declared
+        '''
+        if not hasattr(cls, "_msgtypes"):
+            raise Exception("classes which use MessageMeta must have a '_msgtypes' field")
+
+        if not hasattr(cls, "_classes"):
+            raise Exception("classes which use MessageMeta must have a '_classes' field")
+
+        if not hasattr(cls, "struct"):
+            # This is one of the direct subclasses
+            return
+
+        if cls.__name__ in cls._classes:
+            raise Exception("You've already made a class called {}".format( cls.__name__))
+        cls._classes[cls.__name__] = cls
+
+        # add a _type field to the struct so we can identify it while printing structs
+        cls.struct = cls.struct + ("_type" / Computed(name))
+
+        if not hasattr(cls, "key"):
+            return
+
+        # register the type, so we can tell the parser about it
+        key = cls.key
+        if key in cls._msgtypes:
+            raise Exception('key {} is already assigned to {}'.format(
+                key, cls._msgtypes[key].__name__)
+            )
+        cls._msgtypes[key] = cls
+
+class Message:
+    'Do not subclass this object directly. Instead, subclass of one of the below types'
+
+    def print(message):
+        'Define this on subclasses you want to change the representation of'
+        raise NotImplementedError
+
+    def typeof(message):
+        'Define this on subclasses you want to change the expressed type of'
+        return message._type
+
+    @classmethod
+    def _default_print(cls, name, msg):
+        recur = cls.print_message
+        return "{}({})".format(name, ",".join(
+            "{}={}".format(key, recur(value)) for key, value in msg.items()
+            if not key.startswith('_')
+        ))
+
+    @classmethod
+    def find_typeof(cls, msg):
+        if not hasattr(cls, "_msgtypes"):
+            raise Exception('Do not call this method on Message, call it on a subclass')
+        if isinstance(msg, cl.ListContainer):
+            return ValueError("do not call this on a list of messages")
+        if not isinstance(msg, cl.Container):
+            return ValueError("must call this on a parsed message")
+        if not hasattr(msg, "_type"):
+            return "Anonymous"
+        if msg._type and msg._type not in cls._classes:
+            return msg._type
+        return cls._classes[msg._type].typeof(msg)
+
+    @classmethod
+    def print_message(cls, msg):
+        if not hasattr(cls, "_msgtypes"):
+            raise Exception('Do not call this method on Message, call it on a subclass')
+
+        if isinstance(msg, cl.ListContainer):
+            return repr([cls.print_message(message) for message in msg])
+
+        if not isinstance(msg, cl.Container):
+            return msg
+
+        if not hasattr(msg, "_type"):
+            return cls._default_print("Anonymous", msg)
+
+        if msg._type and msg._type not in cls._classes:
+            return cls._default_print(msg._type, msg)
+
+        try:
+            return cls._classes[msg._type].print(msg)
+        except NotImplementedError:
+            return cls._default_print(msg._type, msg)
+
+    @classmethod
+    def name_to_struct(cls):
+        return {
+            _class.__name__: _class.struct
+            for _class in cls._msgtypes.values()
+        }
+
+    @classmethod
+    def name_to_key(cls):
+        return {
+            _class.__name__ : ord(key)
+            for key, _class in cls._msgtypes.items()
+        }
+
+class SharedMessage(Message, metaclass=MessageMeta):
+    'A message which could be sent by either the frontend or the backend'
+    _msgtypes = dict()
+    _classes = dict()
+
+class FrontendMessage(Message, metaclass=MessageMeta):
+    'A message which will only be sent be a backend'
+    _msgtypes = dict()
+    _classes = dict()
+
+class BackendMessage(Message, metaclass=MessageMeta):
+    'A message which will only be sent be a frontend'
+    _msgtypes = dict()
+    _classes = dict()
+
+class Query(FrontendMessage):
+    key = 'Q'
+    struct = Struct(
+        "query" / CString("ascii")
+    )
+
+    @staticmethod
+    def print(message):
+        query = message.query
+        query = Query.normalize_shards(query)
+        query = Query.normalize_timestamps(query)
+        query = Query.normalize_assign_txn_id(query)
+        return "Query(query={})".format(query)
+
+    @staticmethod
+    def normalize_shards(content):
+        '''
+        For example:
+        >>> normalize_shards(
+        >>>   'COPY public.copy_test_120340 (key, value) FROM STDIN WITH (FORMAT BINARY))'
+        >>> )
+        'COPY public.copy_test_XXXXXX (key, value) FROM STDIN WITH (FORMAT BINARY))'
+        '''
+        result = content
+        pattern = re.compile('public\.[a-z_]+(?P<shardid>[0-9]+)')
+        for match in pattern.finditer(content):
+            span = match.span('shardid')
+            replacement = 'X'*( span[1] - span[0] )
+            result = result[:span[0]] + replacement + result[span[1]:]
+        return result
+
+    @staticmethod
+    def normalize_timestamps(content):
+        '''
+        For example:
+        >>> normalize_timestamps('2018-06-07 05:18:19.388992-07')
+        'XXXX-XX-XX XX:XX:XX.XXXXXX-XX'
+        >>> normalize_timestamps('2018-06-11 05:30:43.01382-07')
+        'XXXX-XX-XX XX:XX:XX.XXXXXX-XX'
+        '''
+
+        pattern = re.compile(
+            '[0-9]{4}-[0-9]{2}-[0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2}.[0-9]{2,6}-[0-9]{2}'
+        )
+
+        return re.sub(pattern, 'XXXX-XX-XX XX:XX:XX.XXXXXX-XX', content)
+
+    @staticmethod
+    def normalize_assign_txn_id(content):
+        '''
+        For example:
+        >>> normalize_assign_txn_id('SELECT assign_distributed_transaction_id(0, 52, ...')
+        'SELECT assign_distributed_transaction_id(0, XX, ...'
+        '''
+
+        pattern = re.compile(
+            'assign_distributed_transaction_id\s*\('  # a method call
+            '\s*[0-9]+\s*,'  # an integer first parameter
+            '\s*(?P<transaction_id>[0-9]+)'  # an integer second parameter
+        )
+        result = content
+        for match in pattern.finditer(content):
+            span = match.span('transaction_id')
+            result = result[:span[0]] + 'XX' + result[span[1]:]
+        return result
+
+class Terminate(FrontendMessage):
+    key = 'X'
+    struct = Struct()
+
+class CopyData(SharedMessage):
+    key = 'd'
+    struct = Struct(
+        'data' / GreedyBytes  # reads all of the data left in this substream
+    )
+
+class CopyDone(SharedMessage):
+    key = 'c'
+    struct = Struct()
+
+class EmptyQueryResponse(BackendMessage):
+    key = 'I'
+    struct = Struct()
+
+class CopyOutResponse(BackendMessage):
+    key = 'H'
+    struct = Struct(
+        "format" / Int8ub,
+        "columncount" / Int16ub,
+        "columns" / Array(this.columncount, Struct(
+            "format" / Int16ub
+        ))
+    )
+
+class ReadyForQuery(BackendMessage):
+    key='Z'
+    struct = Struct("state"/Enum(Byte,
+        idle=ord('I'),
+        in_transaction_block=ord('T'),
+        in_failed_transaction_block=ord('E')
+    ))
+
+class CommandComplete(BackendMessage):
+    key = 'C'
+    struct = Struct(
+        "command" / CString("ascii")
+    )
+
+class RowDescription(BackendMessage):
+    key = 'T'
+    struct = Struct(
+        "fieldcount" / Int16ub,
+        "fields" / Array(this.fieldcount, Struct(
+            "_type" / Computed("F"),
+            "name" / CString("ascii"),
+            "tableoid" / Int32ub,
+            "colattrnum" / Int16ub,
+            "typoid" / Int32ub,
+            "typlen" / Int16sb,
+            "typmod" / Int32sb,
+            "format_code" / Int16ub,
+        ))
+    )
+
+class DataRow(BackendMessage):
+    key = 'D'
+    struct = Struct(
+        "_type" / Computed("data_row"),
+        "columncount" / Int16ub,
+        "columns" / Array(this.columncount, Struct(
+            "_type" / Computed("C"),
+            "length" / Int16sb,
+            "value" / Bytes(this.length)
+        ))
+    )
+
+class AuthenticationOk(BackendMessage):
+    key = 'R'
+    struct = Struct()
+
+class ParameterStatus(BackendMessage):
+    key = 'S'
+    struct = Struct(
+        "name" / CString("ASCII"),
+        "value" / CString("ASCII"),
+    )
+
+    def print(message):
+        name, value = ParameterStatus.normalize(message.name, message.value)
+        return "ParameterStatus({}={})".format(name, value)
+
+    @staticmethod
+    def normalize(name, value):
+        if name in ('TimeZone', 'server_version'):
+            value = 'XXX'
+        return (name, value)
+
+class BackendKeyData(BackendMessage):
+    key = 'K'
+    struct = Struct(
+        "pid" / Int32ub,
+        "key" / Bytes(4)
+    )
+
+    def print(message):
+        # Both of these should be censored, for reproducible regression test output
+        return "BackendKeyData(XXX)"
+
+frontend_switch = Switch(
+    this.type,
+    { **FrontendMessage.name_to_struct(), **SharedMessage.name_to_struct() },
+    default=Bytes(this.length - 4)
+)
+
+backend_switch = Switch(
+    this.type,
+    {**BackendMessage.name_to_struct(), **SharedMessage.name_to_struct()},
+    default=Bytes(this.length - 4)
+)
+
+frontend_msgtypes = Enum(Byte, **{
+    **FrontendMessage.name_to_key(),
+    **SharedMessage.name_to_key()
+})
+
+backend_msgtypes = Enum(Byte, **{
+    **BackendMessage.name_to_key(),
+    **SharedMessage.name_to_key()
+})
+
+# It might seem a little circuitous to say a frontend message is a kind of frontend
+# message but this lets us easily customize how they're printed
+
+class Frontend(FrontendMessage):
+    struct = Struct(
+        "type" / frontend_msgtypes,
+        "length" / Int32ub,  # "32-bit unsigned big-endian"
+        "raw_body" / Bytes(this.length - 4),
+        # try to parse the body into something more structured than raw bytes
+        "body" / RestreamData(this.raw_body, frontend_switch),  
+    )
+
+    def print(message):
+        if isinstance(message.body, bytes):
+            return "Frontend(type={},body={})".format(
+                chr(message.type), message.body
+            )
+        return FrontendMessage.print_message(message.body)
+
+    def typeof(message):
+        if isinstance(message.body, bytes):
+            return "Unknown"
+        return message.body._type
+
+class Backend(BackendMessage):
+    struct = Struct(
+        "type" / backend_msgtypes,
+        "length" / Int32ub,  # "32-bit unsigned big-endian"
+        "raw_body" / Bytes(this.length - 4),
+        # try to parse the body into something more structured than raw bytes
+        "body" / RestreamData(this.raw_body, backend_switch),  
+    )
+
+    def print(message):
+        if isinstance(message.body, bytes):
+            return "Backend(type={},body={})".format(
+                chr(message.type), message.body
+            )
+        return BackendMessage.print_message(message.body)
+
+    def typeof(message):
+        if isinstance(message.body, bytes):
+            return "Unknown"
+        return message.body._type
+
+# GreedyRange keeps reading messages until we hit EOF
+frontend_messages = GreedyRange(Frontend.struct)
+backend_messages = GreedyRange(Backend.struct)
+
+def parse(message, from_frontend=True):
+    if from_frontend:
+        message = frontend_messages.parse(message)
+    else:
+        message = backend_messages.parse(message)
+    message.from_frontend = from_frontend
+
+    return message
+
+def print(message):
+    if message.from_frontend:
+        return FrontendMessage.print_message(message)
+    return BackendMessage.print_message(message)
+
+def message_type(message, from_frontend):
+    if from_frontend:
+        return FrontendMessage.find_typeof(message)
+    return BackendMessage.find_typeof(message)
+
+def message_matches(message, filters, from_frontend):
+    '''
+    Message is something like Backend(Query)) and fiters is something like query="COPY".
+
+    For now we only support strings, and treat them like a regex, which is matched against
+    the content of the wrapped message
+    '''
+    if message._type != 'Backend' and message._type != 'Frontend':
+        raise ValueError("can't handle {}".format(message._type))
+
+    wrapped = message.body
+    if isinstance(wrapped, bytes):
+        # we don't know which kind of message this is, so we can't match against it
+        return False
+
+    for key, value in filters.items():
+        if not isinstance(value, str):
+            raise ValueError("don't yet know how to handle {}".format(type(value)))
+
+        actual = getattr(wrapped, key)
+
+        if not re.search(value, actual):
+            return False
+
+    return True
