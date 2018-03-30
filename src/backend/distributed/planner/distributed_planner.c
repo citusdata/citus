8274680f5b722623a4911eaf8f578cd29d16ba88
Jason Petersen <jason@citusdata.com>
2018-03-30 12:23:15 -0600

    Attempt to address planner context crashes
    Both of these are a bit of a shot in the dark. In one case, we noticed
    a stack trace where a caller received a null pointer and attempted to
    dereference the memory context field (at 0x010). In the other, I saw
    that any error thrown from within AdjustParseTree could keep the stack
    from being cleaned up (presumably if we push we should always pop).
    
    Both stack traces were collected during times of high memory pressure
    and locally reproducing the problem locally or otherwise has been very
    tricky (i.e. it hasn't been reproduced reliably at all).

--- src/backend/distributed/planner/distributed_planner.c
+++ src/backend/distributed/planner/distributed_planner.c
@@ -147,6 +147,10 @@ distributed_planner(Query *parse, int cursorOptions, ParamListInfo boundParams)
 
 			result = CreateDistributedPlannedStmt(planId, result, originalQuery, parse,
 												  boundParams, plannerRestrictionContext);
+
+			setPartitionedTablesInherited = true;
+			AdjustPartitioningForDistributedPlanning(parse,
+													 setPartitionedTablesInherited);
 		}
 	}
 	PG_CATCH();
@@ -155,13 +159,6 @@ distributed_planner(Query *parse, int cursorOptions, ParamListInfo boundParams)
 		PG_RE_THROW();
 	}
 	PG_END_TRY();
-
-	if (needsDistributedPlanning)
-	{
-		setPartitionedTablesInherited = true;
-
-		AdjustPartitioningForDistributedPlanning(parse, setPartitionedTablesInherited);
-	}
 
 	/* remove the context from the context list */
 	PopPlannerRestrictionContext();
@@ -1445,6 +1442,13 @@ CurrentPlannerRestrictionContext(void)
 
 	plannerRestrictionContext =
 		(PlannerRestrictionContext *) linitial(plannerRestrictionContextList);
+
+	if (plannerRestrictionContext == NULL)
+	{
+		ereport(ERROR, (errcode(ERRCODE_INTERNAL_ERROR),
+						errmsg("planner restriction context stack was empty"),
+						errdetail("Please report this to the Citus core team.")));
+	}
 
 	return plannerRestrictionContext;
 }
