c7b4193ad51826449fcb82c9d855d3bae8b573b7
Hadi Moshayedi <hadi@moshayedi.net>
2018-05-17 22:46:44 -0400

    Add the missing sql definitions.

--- src/backend/distributed/citus--7.4-2--7.4-3.sql
+++ src/backend/distributed/citus--7.4-2--7.4-3.sql
@@ -11,3 +11,65 @@ COMMENT ON FUNCTION worker_hash_partition_table(bigint, integer, text, text, oid
     IS 'hash partition query results';
 
 RESET search_path;
+
+CREATE FUNCTION citus_connections_hash()
+RETURNS TABLE (
+    hostname TEXT,
+    port INT,
+    "user" TEXT,
+    database TEXT,
+    socket INT,
+    sessionLifespan BOOLEAN,
+    claimedExclusively BOOLEAN,
+    connectionStart Timestamp WITH TIME ZONE,
+    transactionState TEXT,
+    transactionCritical BOOLEAN,
+    transactionFailed BOOLEAN,
+    preparedName TEXT,
+    dontKill BOOLEAN
+)
+AS 'MODULE_PATHNAME', 'citus_connections_hash'
+LANGUAGE C STRICT;
+
+CREATE FUNCTION citus_zombie_connections()
+RETURNS TABLE (
+    hostname TEXT,
+    port INT,
+    "user" TEXT,
+    database TEXT,
+    socket INT,
+    sessionLifespan BOOLEAN,
+    claimedExclusively BOOLEAN,
+    connectionStart Timestamp WITH TIME ZONE,
+    transactionState TEXT,
+    transactionCritical BOOLEAN,
+    transactionFailed BOOLEAN,
+    preparedName TEXT,
+    dontKill BOOLEAN
+)
+AS 'MODULE_PATHNAME', 'citus_zombie_connections'
+LANGUAGE C STRICT;
+
+CREATE FUNCTION dont_kill_multiconnection(INTEGER) RETURNS VOID
+AS 'MODULE_PATHNAME', 'dont_kill_multiconnection'
+LANGUAGE C STRICT;
+
+CREATE FUNCTION cleanup_zombie_connections() RETURNS VOID
+AS 'MODULE_PATHNAME', 'cleanup_zombie_connections'
+LANGUAGE C STRICT;
+
+
+CREATE FUNCTION citus_connection_placement_hash()
+RETURNS TABLE (
+    placementId BIGINT,
+    failed BOOLEAN,
+    hasSecondary BOOLEAN,
+    username TEXT,
+    connection_socket INT,
+    hadDML BOOLEAN,
+    hadDDL BOOLEAN,
+    colocationGroupId INT,
+    representativeValue INT
+)
+AS 'MODULE_PATHNAME', 'citus_connection_placement_hash'
+LANGUAGE C STRICT;
