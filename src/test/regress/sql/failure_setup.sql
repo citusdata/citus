5f9fb9a263077ae4d992e5e49b0801c9c05f8009
Brian Cloutier <github@briancloutier.com>
2018-07-25 16:40:55 -0700

    Actually use our new port in the tests

--- src/test/regress/sql/failure_setup.sql
+++ src/test/regress/sql/failure_setup.sql
@@ -2,4 +2,4 @@ SELECT citus.mitmproxy('conn.allow()');
 
 -- add the workers
 SELECT master_add_node('localhost', :worker_1_port);  -- the second worker
-SELECT master_add_node('localhost', :worker_2_port + 2);  -- the first worker, behind a mitmproxy
+SELECT master_add_node('localhost', :worker_2_proxy_port);  -- the first worker, behind a mitmproxy
