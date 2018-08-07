95141fef42548fa0bdafb490aeb6933458d0b3a9
Jason Petersen <jason@citusdata.com>
2018-08-06 20:49:03 -0600

    Add test showing poolinfo validation works
    In other words, that it errors out.

--- src/test/regress/sql/multi_utility_warnings.sql
+++ src/test/regress/sql/multi_utility_warnings.sql
@@ -16,3 +16,8 @@ CREATE ROLE new_role;
 CREATE USER new_user;
 
 INSERT INTO pg_dist_authinfo VALUES (0, 'new_user', 'password=1234');
+
+BEGIN;
+INSERT INTO pg_dist_node VALUES (1234567890, 1234567890, 'localhost', 5432);
+INSERT INTO pg_dist_poolinfo VALUES (1234567890, 'port=1234');
+ROLLBACK;
