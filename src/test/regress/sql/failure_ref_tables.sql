1cb48416eb7475d29b6a968aa820ddae516dcd8d
Jason Petersen <jason@citusdata.com>
2018-10-04 09:58:58 -0600

    Add reference table failure tests
    Fairly straightforward; verified that modifications fail atomically if
    a worker is down or fails mid-transaction (i.e. all workers need to ack
    modifications to reference tables in order to persist changes).

---
+++ src/test/regress/sql/failure_ref_tables.sql
@@ -0,0 +1,47 @@
+SET citus.next_shard_id TO 100500;
+
+SELECT citus.mitmproxy('conn.allow()');
+
+CREATE TABLE ref_table (key int, value int);
+SELECT create_reference_table('ref_table');
+
+\copy ref_table FROM stdin delimiter ',';
+1,2
+2,3
+3,4
+4,5
+\.
+
+SELECT citus.clear_network_traffic();
+
+SELECT COUNT(*) FROM ref_table;
+
+-- verify behavior of single INSERT; should fail to execute
+SELECT citus.mitmproxy('conn.onQuery(query="^INSERT").kill()');
+INSERT INTO ref_table VALUES (5, 6);
+
+SELECT COUNT(*) FROM ref_table WHERE key=5;
+
+-- verify behavior of UPDATE ... RETURNING; should not execute
+SELECT citus.mitmproxy('conn.onQuery(query="^UPDATE").kill()');
+UPDATE ref_table SET key=7 RETURNING value;
+
+SELECT COUNT(*) FROM ref_table WHERE key=7;
+
+-- verify fix to #2214; should raise error and fail to execute
+SELECT citus.mitmproxy('conn.onQuery(query="^UPDATE").kill()');
+
+BEGIN;
+DELETE FROM ref_table WHERE key=5;
+UPDATE ref_table SET key=value;
+COMMIT;
+
+SELECT COUNT(*) FROM ref_table WHERE key=value;
+
+-- all shards should still be healthy
+SELECT COUNT(*) FROM pg_dist_shard_placement WHERE shardstate = 3;
+
+-- ==== Clean up, we're done here ====
+
+SELECT citus.mitmproxy('conn.allow()');
+DROP TABLE ref_table;
