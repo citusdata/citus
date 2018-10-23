c22cbb7a13384b84f55db35513b7220b72278499
Hadi Moshayedi <hadi@moshayedi.net>
2018-10-22 23:51:39 -0400

    Don't throw error for DROP DATABASE IF EXISTS

--- src/test/regress/sql/multi_utility_statements.sql
+++ src/test/regress/sql/multi_utility_statements.sql
@@ -146,3 +146,7 @@ FETCH ABSOLUTE 5 FROM noHoldCursor;
 FETCH BACKWARD noHoldCursor;
 COMMIT;
 FETCH ABSOLUTE 5 FROM noHoldCursor;
+
+-- Test we don't throw an error for DROP IF EXISTS
+DROP DATABASE IF EXISTS not_existing_database;
+DROP TABLE IF EXISTS not_existing_table;
