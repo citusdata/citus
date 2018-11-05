add7093b075bc4e66fb89ca96d8951e8a018ddec
Hadi Moshayedi <hadi@moshayedi.net>
2018-11-05 14:11:08 -0500

    Fix tests

--- src/test/regress/sql/multi_metadata_attributes.sql
+++ src/test/regress/sql/multi_metadata_attributes.sql
@@ -1,4 +1,3 @@
-CREATE EXTENSION citus;
 
 -- if the output of following query changes, we might need to change
 -- some heap_getattr() calls to heap_deform_tuple(). This errors out in
@@ -7,5 +6,3 @@ SELECT attrelid::regclass, attname, atthasmissing, attmissingval
 FROM pg_attribute
 WHERE atthasmissing
 ORDER BY attrelid, attname;
-
-DROP EXTENSION citus;
