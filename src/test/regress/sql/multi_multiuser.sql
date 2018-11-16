18acd00553b15fba532c562b0fdd98007228cb23
Marco Slot <marco.slot@gmail.com>
2018-11-16 23:53:37 +0100

    Check permissions in lock_relation_if_exists

--- src/test/regress/sql/multi_multiuser.sql
+++ src/test/regress/sql/multi_multiuser.sql
@@ -106,6 +106,12 @@ SELECT count(*) FROM test a JOIN test b ON (a.val = b.val) WHERE a.id = 1 AND b.
 -- should not be able to transmit directly
 COPY "postgresql.conf" TO STDOUT WITH (format transmit);
 
+-- should not be allowed to take aggressive locks on table
+BEGIN;
+SELECT lock_relation_if_exists('test', 'ACCESS SHARE');
+SELECT lock_relation_if_exists('test', 'EXCLUSIVE');
+ABORT;
+
 SET citus.task_executor_type TO 'real-time';
 
 -- check no permission
