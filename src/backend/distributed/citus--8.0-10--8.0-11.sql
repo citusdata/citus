1ec5b6c890f2c2da08f49ba227b3fe9307b555ed
Marco Slot <marco.slot@gmail.com>
2018-11-24 03:40:09 +0100

    Remove old worker_hash_partition_table API

--- src/backend/distributed/citus--8.0-10--8.0-11.sql
+++ src/backend/distributed/citus--8.0-10--8.0-11.sql
@@ -2,6 +2,7 @@
 SET search_path = 'pg_catalog';
 
 -- Deprecated functions
+DROP FUNCTION IF EXISTS worker_hash_partition_table(bigint,integer,text,text,oid,integer);
 DROP FUNCTION IF EXISTS worker_foreign_file_path(text);
 DROP FUNCTION IF EXISTS worker_find_block_local_path(bigint,text[]);
 DROP FUNCTION IF EXISTS worker_fetch_query_results_file(bigint,integer,integer,text,integer);
