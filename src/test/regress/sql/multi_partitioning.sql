18c9badff572c84722657ad2c7abd5d0c88978af
Onder Kalaci <onder@citusdata.com>
2018-11-30 13:58:27 +0300

    Make sure the explain output for partition wise join is stable
    We disable bunch of planning options on the workers. This might be
    risky if any concurrent test relies on EXPLAIN OUTPUT as well. Still,
    we want to keep this test, so we should try to not parallelize this
    test with such test.

--- src/test/regress/sql/multi_partitioning.sql
+++ src/test/regress/sql/multi_partitioning.sql
@@ -986,7 +986,6 @@ ORDER BY
 COMMIT;
 
 -- test partition-wise join
-
 CREATE TABLE partitioning_hash_join_test(id int, subid int) PARTITION BY HASH(subid);
 CREATE TABLE partitioning_hash_join_test_0 PARTITION OF partitioning_hash_join_test FOR VALUES WITH (MODULUS 3, REMAINDER 0);
 CREATE TABLE partitioning_hash_join_test_1 PARTITION OF partitioning_hash_join_test FOR VALUES WITH (MODULUS 3, REMAINDER 1);
@@ -994,8 +993,14 @@ CREATE TABLE partitioning_hash_join_test_2 PARTITION OF partitioning_hash_join_t
 
 SELECT create_distributed_table('partitioning_hash_join_test', 'id');
 
--- see the query plan without partition-wise join
-EXPLAIN
+SELECT success FROM run_command_on_workers('alter system set enable_mergejoin to off');
+SELECT success FROM run_command_on_workers('alter system set enable_nestloop to off');
+SELECT success FROM run_command_on_workers('alter system set enable_indexscan to off');
+SELECT success FROM run_command_on_workers('alter system set enable_indexonlyscan to off');
+SELECT success FROM run_command_on_workers('alter system set enable_partitionwise_join to off');
+SELECT success FROM run_command_on_workers('select pg_reload_conf()');
+
+EXPLAIN (COSTS OFF)
 SELECT * FROM partitioning_hash_test JOIN partitioning_hash_join_test USING (id, subid);
 
 -- set partition-wise join on
@@ -1004,18 +1009,21 @@ SELECT success FROM run_command_on_workers('select pg_reload_conf()');
 
 SET enable_partitionwise_join TO on;
 
--- see the new query plan
-EXPLAIN
+EXPLAIN (COSTS OFF)
 SELECT * FROM partitioning_hash_test JOIN partitioning_hash_join_test USING (id, subid);
 
 -- note that partition-wise joins only work when partition key is in the join
 -- following join does not have that, therefore join will not be pushed down to
 -- partitions
-EXPLAIN
+EXPLAIN (COSTS OFF)
 SELECT * FROM partitioning_hash_test JOIN partitioning_hash_join_test USING (id);
 
 -- reset partition-wise join
-SELECT success FROM run_command_on_workers('reset enable_partitionwise_join');
+SELECT success FROM run_command_on_workers('alter system reset enable_partitionwise_join');
+SELECT success FROM run_command_on_workers('alter system reset enable_mergejoin');
+SELECT success FROM run_command_on_workers('alter system reset enable_nestloop');
+SELECT success FROM run_command_on_workers('alter system reset enable_indexscan');
+SELECT success FROM run_command_on_workers('alter system reset enable_indexonlyscan');
 SELECT success FROM run_command_on_workers('select pg_reload_conf()');
 
 RESET enable_partitionwise_join;
