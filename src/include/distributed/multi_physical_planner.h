89f985ec4e6b8505a87740c061d8783c8e89e1b9
Onder Kalaci <onder@citusdata.com>
2018-11-30 16:31:38 +0300

    Sort task list for multi-task explain outputs
    This is purely for ensuring that regression tests do not randomly fail.

--- src/include/distributed/multi_physical_planner.h
+++ src/include/distributed/multi_physical_planner.h
@@ -351,6 +351,7 @@ extern List * TaskListDifference(const List *list1, const List *list2);
 extern List * AssignAnchorShardTaskList(List *taskList);
 extern List * FirstReplicaAssignTaskList(List *taskList);
 extern List * RoundRobinAssignTaskList(List *taskList);
+extern int CompareTasksByTaskId(const void *leftElement, const void *rightElement);
 
 /* function declaration for creating Task */
 extern List * QueryPushdownSqlTaskList(Query *query, uint64 jobId,
