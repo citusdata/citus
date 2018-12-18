5877ee303062c1282dab3b0f51d708d3c8d6be1d
Hanefi Onaldi <hanefionaldi@gmail.com>
2018-12-18 16:00:51 +0300

    Bump version to 8.1.0

--- windows/include/citus_version.h
+++ windows/include/citus_version.h
@@ -5,19 +5,19 @@
 #define CITUS_EDITION "community"
 
 /* Extension version expected by this Citus build */
-#define CITUS_EXTENSIONVERSION "8.0-1"
+#define CITUS_EXTENSIONVERSION "8.1-1"
 
 /* Citus major version as a string */
-#define CITUS_MAJORVERSION "8.0"
+#define CITUS_MAJORVERSION "8.1"
 
 /* Citus version as a string */
-#define CITUS_VERSION "8.0devel"
+#define CITUS_VERSION "8.1.0"
 
 /* Citus version as a number */
-#define CITUS_VERSION_NUM 80000
+#define CITUS_VERSION_NUM 80100
 
 /* A string containing the version number, platform, and C compiler */
-#define CITUS_VERSION_STR "Citus 8.0devel on x86_64-windows, compiled by Visual Studio"
+#define CITUS_VERSION_STR "Citus 8.1.0 on x86_64-windows, compiled by Visual Studio"
 
 /* Define to 1 if you have the `curl' library (-lcurl). */
 #define HAVE_LIBCURL 0
