cd0af91fdc8bf890b3022c4b6b514fb3deb2f32d
Marco Slot <marco.slot@gmail.com>
2019-03-29 00:22:44 +0100

    Latest changes

--- src/backend/distributed/commands/utility_hook.c
+++ src/backend/distributed/commands/utility_hook.c
@@ -561,15 +561,7 @@ ExecuteDistributedDDLJob(DDLJob *ddlJob)
 			SendCommandToWorkers(WORKERS_WITH_METADATA, (char *) ddlJob->commandString);
 		}
 
-		if (MultiShardConnectionType == SEQUENTIAL_CONNECTION ||
-			ddlJob->executeSequentially)
-		{
-			ExecuteModifyTasksSequentiallyWithoutResults(ddlJob->taskList, CMD_UTILITY);
-		}
-		else
-		{
-			ExecuteModifyTasksWithoutResults(ddlJob->taskList);
-		}
+		ExecuteTaskList(CMD_UTILITY, ddlJob->taskList);
 	}
 	else
 	{
@@ -580,7 +572,7 @@ ExecuteDistributedDDLJob(DDLJob *ddlJob)
 
 		PG_TRY();
 		{
-			ExecuteModifyTasksWithoutResults(ddlJob->taskList);
+			ExecuteTaskList(CMD_UTILITY, ddlJob->taskList);
 
 			if (shouldSyncMetadata)
 			{
