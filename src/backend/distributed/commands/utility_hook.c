f788f9dda05e174ec847204a398407ac78cacb26
Onder Kalaci <onder@citusdata.com>
2019-04-24 13:11:45 +0300

    Ensure that unified executor doesn't ignore sequential execution of DDLJob's
    Certain DDL commands, mainly creating foreign keys to reference tables,
    should be executed sequentially. Otherwise, we'd end up with a self
    distributed deadlock.
    
    To overcome this situaiton, we set a flag `DDLJob->executeSequentially`
    and execute it sequentially. Note that we have to do this because
    the command might not be called within a transaction block, and
    we cannot call `SetLocalMultiShardModifyModeToSequential()`.
    
    This fixes at least two test: multi_insert_select_on_conflit.sql and
    multi_foreign_key.sql
    
    Also, I wouldn't mind scattering local `targetPoolSize` variables within
    the code. The reason is that we'll soon have a GUC (or a global
    variable based on a GUC) that'd set the pool size. In that case, we'd
    simply replace `targetPoolSize` with the global variables.

--- src/backend/distributed/commands/utility_hook.c
+++ src/backend/distributed/commands/utility_hook.c
@@ -537,6 +537,7 @@ static void
 ExecuteDistributedDDLJob(DDLJob *ddlJob)
 {
 	bool shouldSyncMetadata = ShouldSyncTableMetadata(ddlJob->targetRelationId);
+	int targetPoolSize = ddlJob->executeSequentially ? 1 : DEFAULT_POOL_SIZE;
 
 	EnsureCoordinator();
 	EnsurePartitionTableNotReplicated(ddlJob->targetRelationId);
@@ -561,7 +562,7 @@ ExecuteDistributedDDLJob(DDLJob *ddlJob)
 			SendCommandToWorkers(WORKERS_WITH_METADATA, (char *) ddlJob->commandString);
 		}
 
-		ExecuteTaskList(CMD_UTILITY, ddlJob->taskList);
+		ExecuteTaskList(CMD_UTILITY, ddlJob->taskList, targetPoolSize);
 	}
 	else
 	{
@@ -572,7 +573,7 @@ ExecuteDistributedDDLJob(DDLJob *ddlJob)
 
 		PG_TRY();
 		{
-			ExecuteTaskList(CMD_UTILITY, ddlJob->taskList);
+			ExecuteTaskList(CMD_UTILITY, ddlJob->taskList, targetPoolSize);
 
 			if (shouldSyncMetadata)
 			{
