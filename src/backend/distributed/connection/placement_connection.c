b03b3b8acf026e5e502ad82852f321a040e52319
Onder Kalaci <onder@citusdata.com>
2019-04-26 12:57:48 +0300

    Support foreign keys to reference tables in unified executor
    The idea for supporting foreign keys to reference tables is simple:
    Keep track of the relation accesses within a transaction block.
        - If a parallel access happens on a distributed table which
          has a foreign key to a reference table, one cannot modify
          the reference table in the same transaction. Otherwise,
          we're very likely to end-up with a self-distributed deadlock.
        - If an access to a reference table happens, and then a parallel
          access to a distributed table (which has a fkey to the reference
          table) happens, we switch to sequential mode.
    
    Unified executor misses the function calls that marks the relation
    accesses during the execution. Thus, simply add the necessary calls
    and let the logic kick in.

--- src/backend/distributed/connection/placement_connection.c
+++ src/backend/distributed/connection/placement_connection.c
@@ -451,6 +451,9 @@ AssignPlacementListToConnection(List *placementAccessList, MultiConnection *conn
 		{
 			placementConnection->hadDML = true;
 		}
+
+		/* record the relation access mapping */
+		AssociatePlacementAccessWithRelation(placement, accessType);
 	}
 }
 
