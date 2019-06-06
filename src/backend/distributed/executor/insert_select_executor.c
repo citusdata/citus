a22a7cbd4e3033ad174f9af0b16d641660663274
Hadi Moshayedi <hadi@moshayedi.net>
2019-06-06 14:44:33 -0700

    Sort output of INSERT ... SELECT ... RETURNING

--- src/backend/distributed/executor/insert_select_executor.c
+++ src/backend/distributed/executor/insert_select_executor.c
@@ -139,6 +139,11 @@ CoordinatorInsertSelectExecScan(CustomScanState *node)
 				ExecuteTaskListExtended(CMD_INSERT, prunedTaskList,
 										tupleDescriptor, scanState->tuplestorestate,
 									    hasReturning, DEFAULT_POOL_SIZE);
+
+				if (SortReturning && hasReturning)
+				{
+					SortTupleStore(scanState);
+				}
 			}
 		}
 		else
