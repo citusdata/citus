76d5a7b689e39a64ceee9d18b62c0df0f6923546
Nils Dijk <nils@citusdata.com>
2019-09-10 18:14:52 +0200

    make base test for retaining data when types are overwritten

---
+++ src/test/regress/sql/distributed_types_conflict.sql
@@ -0,0 +1,32 @@
+SET citus.next_shard_id TO 20020000;
+
+CREATE SCHEMA type_conflict;
+SELECT run_command_on_workers($$CREATE SCHEMA type_conflict;$$);
+
+-- create a type on a worker that should not cause data loss once overwritten with a type
+-- from the coordinator
+\c - - - :worker_1_port
+SET citus.enable_ddl_propagation TO off;
+SET search_path TO type_conflict;
+CREATE TYPE my_precious_type AS (secret text, should bool);
+CREATE TABLE local_table (a int, b my_precious_type);
+INSERT INTO local_table VALUES (42, ('always bring a towel', true)::my_precious_type);
+\c - - - :master_port
+SET search_path TO type_conflict;
+
+-- overwrite the type on the worker from the coordinator. The type should be over written
+-- but the data should not have been destroyed
+--CREATE TYPE my_precious_type AS (scatterd_secret text);
+
+-- verify the data is retained
+\c - - - :worker_1_port
+SET search_path TO type_conflict;
+\d+ local_table
+SELECT * FROM local_table;
+
+\c - - - :master_port
+SET search_path TO type_conflict;
+
+-- hide cascades
+SET client_min_messages TO error;
+DROP SCHEMA type_conflict CASCADE;
