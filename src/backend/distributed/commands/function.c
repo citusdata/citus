27b79495a9f8e59a24f49d06e7408d53b5273c45
Hanefi Onaldi <hanefi@citusdata.com>
2019-09-12 14:39:09 +0300

    Introduce create_distributed_function UDF
    This commit aims to add the minimal set of changes required to start
    distributing functions. We now can issue such a query to distribute a
    function:
    
      SELECT create_distributed_function('add(int,int)');
    
    The function definition should include the param types to properly
    identify the correct function that we wish to distribute

---
+++ src/backend/distributed/commands/function.c
@@ -0,0 +1,61 @@
+/*-------------------------------------------------------------------------
+ *
+ * function.c
+ *    Commands for FUNCTION statements.
+ *    Currently the following will be supported in Citus:
+ * 	  -
+ *
+ * Copyright (c) 2019, Citus Data, Inc.
+ *
+ *-------------------------------------------------------------------------
+ */
+
+#include "postgres.h"
+
+#include "catalog/pg_proc.h"
+#include "distributed/metadata_sync.h"
+#include "distributed/metadata/distobject.h"
+#include "distributed/worker_transaction.h"
+#include "utils/fmgrprotos.h"
+#include "utils/builtins.h"
+
+/* forward declaration for helper functions*/
+static const char * GetFunctionDDLCommand(Oid funcOid);
+
+PG_FUNCTION_INFO_V1(create_distributed_function);
+
+
+/*
+ * create_distributed_function gets a function or procedure name with their list of
+ * argument types in parantheses, then it creates a new distributed function.
+ */
+Datum
+create_distributed_function(PG_FUNCTION_ARGS)
+{
+	RegProcedure funcOid = PG_GETARG_OID(0);
+	const char *ddlCommand = NULL;
+	ObjectAddress functionAddress = { 0 };
+	ObjectAddressSet(functionAddress, ProcedureRelationId, funcOid);
+
+	EnsureDependenciesExistsOnAllNodes(&functionAddress);
+
+	ddlCommand = GetFunctionDDLCommand(funcOid);
+
+	/* EnsureSequentialModeForTypeDDL(); */
+	SendCommandToWorkersAsUser(ALL_WORKERS, DISABLE_DDL_PROPAGATION, NULL);
+	SendCommandToWorkersAsUser(ALL_WORKERS, ddlCommand, NULL);
+
+	MarkObjectDistributed(&functionAddress);
+
+	PG_RETURN_VOID();
+}
+
+
+static const char *
+GetFunctionDDLCommand(Oid funcOid)
+{
+	Datum sqlTextDatum = DirectFunctionCall1(pg_get_functiondef,
+											 ObjectIdGetDatum(funcOid));
+	const char *sql = TextDatumGetCString(sqlTextDatum);
+	return sql;
+}
