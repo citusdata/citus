114eaca3ef2ecd7654bf75f0b5bed75a34ccf606
Hanefi Onaldi <hanefi@citusdata.com>
2019-09-13 16:17:23 +0300

    Enforce sequential mode on create_distributed_function calls

--- src/backend/distributed/commands/function.c
+++ src/backend/distributed/commands/function.c
@@ -20,15 +20,18 @@
 #include "catalog/pg_proc.h"
 #include "distributed/metadata_sync.h"
 #include "distributed/metadata/distobject.h"
+#include "distributed/multi_executor.h"
+#include "distributed/relation_access_tracking.h"
 #include "distributed/worker_transaction.h"
-#include "utils/fmgrprotos.h"
+#include "server/access/xact.h"
 #include "utils/builtins.h"
+#include "utils/fmgrprotos.h"
 
 /* forward declaration for helper functions*/
 static const char * GetFunctionDDLCommand(Oid funcOid);
+static void EnsureSequentialModeForFunctionDDL(void);
 
 PG_FUNCTION_INFO_V1(create_distributed_function);
-
 
 /*
  * create_distributed_function gets a function or procedure name with their list of
@@ -53,6 +56,12 @@ create_distributed_function(PG_FUNCTION_ARGS)
 
 	ObjectAddressSet(functionAddress, ProcedureRelationId, funcOid);
 
+	/*
+	 * when we allow propagation within a transaction block we should make sure to only
+	 * allow this in sequential mode
+	 */
+	EnsureSequentialModeForFunctionDDL();
+
 	EnsureDependenciesExistsOnAllNodes(&functionAddress);
 
 	ddlCommand = GetFunctionDDLCommand(funcOid);
@@ -75,4 +84,46 @@ GetFunctionDDLCommand(RegProcedure funcOid)
 											 ObjectIdGetDatum(funcOid));
 	const char *sql = TextDatumGetCString(sqlTextDatum);
 	return sql;
+}
+
+
+/*
+ * EnsureSequentialModeForFunctionDDL makes sure that the current transaction is already in
+ * sequential mode, or can still safely be put in sequential mode, it errors if that is
+ * not possible. The error contains information for the user to retry the transaction with
+ * sequential mode set from the beginnig.
+ *
+ * As functions are node scoped objects there exists only 1 instance of the function used by
+ * potentially multiple shards. To make sure all shards in the transaction can interact
+ * with the function the function needs to be visible on all connections used by the transaction,
+ * meaning we can only use 1 connection per node.
+ */
+static void
+EnsureSequentialModeForFunctionDDL(void)
+{
+	if (!IsTransactionBlock())
+	{
+		/* we do not need to switch to sequential mode if we are not in a transaction */
+		return;
+	}
+
+	if (ParallelQueryExecutedInTransaction())
+	{
+		ereport(ERROR, (errmsg("cannot create function because there was a "
+							   "parallel operation on a distributed table in the "
+							   "transaction"),
+						errdetail("When creating a distributed function, Citus needs to "
+								  "perform all operations over a single connection per "
+								  "node to ensure consistency."),
+						errhint("Try re-running the transaction with "
+								"\"SET LOCAL citus.multi_shard_modify_mode TO "
+								"\'sequential\';\"")));
+	}
+
+	ereport(DEBUG1, (errmsg("switching to sequential query execution mode"),
+					 errdetail(
+						 "A distributed function is created. To make sure subsequent "
+						 "commands see the type correctly we need to make sure to "
+						 "use only one connection for all future commands")));
+	SetLocalMultiShardModifyModeToSequential();
 }
