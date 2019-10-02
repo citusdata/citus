1201fe91be19ea14fb824354aea22a7002832fe3
Jelte Fennema <github-tech@jeltef.nl>
2019-10-02 16:02:01 +0200

    Add more upgrade tests

---
+++ src/test/regress/sql/upgrade_type_after.sql
@@ -0,0 +1,11 @@
+SET search_path TO upgrade_type, public;
+BEGIN;
+SET LOCAL citus.multi_shard_modify_mode TO 'sequential';
+
+-- test distributed type
+INSERT INTO tt VALUES (1, (2,3)::type1);
+SELECT * FROM tt;
+ALTER TYPE type1 RENAME TO type1_newname;
+INSERT INTO tt VALUES (3, (4,5)::type1_newname);
+
+ROLLBACK;
