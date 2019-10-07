7390ed5e89176166221104d031a4ada367dabb57
SaitTalhaNisanci <s.talhanisanci@gmail.com>
2019-10-07 22:03:56 +0300

    use current code instead of master for upgrade

--- src/test/regress/upgrade/generate_citus_tarballs.sh
+++ src/test/regress/upgrade/generate_citus_tarballs.sh
@@ -1,27 +1,19 @@
 #!/bin/bash
 
 
-citus_versions=( $1 master )
+citus_old_version=$1
 
 base="$(pwd)"
 
-build_ext() {
-  citus_version="$1"
-  basedir="${base}/${citus_version}"
-  
-  mkdir -p ${basedir}
-  cd ${basedir}
-  citus_dir=${basedir}/citus_$citus_version
-  git clone --branch $citus_version https://github.com/citusdata/citus.git --depth 1 citus_$citus_version  
-  builddir="${basedir}/build"
 
+install_citus_and_tar() {
   # do everything in a subdirectory to avoid clutter in current directory
-  mkdir -p "${builddir}" && cd "${builddir}"
+  mkdir -p "${builddir}" && cd "${builddir}" || exit
 
   "${citus_dir}/configure" --without-libcurl
 
   installdir="${builddir}/install"
-  make && mkdir -p "${installdir}" && make DESTDIR="${installdir}" install
+  make "-j$(nproc)" && mkdir -p "${installdir}" && make DESTDIR="${installdir}" install
 
   cd "${installdir}" && find . -type f -print > "${builddir}/files.lst"
   tar cvf "${basedir}/install-citus${citus_version}.tar" `cat ${builddir}/files.lst`
@@ -29,7 +21,34 @@ build_ext() {
 
   cd "${builddir}" && rm -rf install files.lst && make clean
 }
-for citus_version in "${citus_versions[@]}"
-do 
-  build_ext "${citus_version}"
-done
+
+build_current() {
+  citus_version="$1"
+  basedir="${base}/${citus_version}"
+  
+  mkdir -p "${basedir}"
+  cd "${basedir}" || exit
+  citus_dir="${base}/../../../.."  
+  builddir="${basedir}/build"
+
+  make -C "${citus_dir}" clean
+
+  install_citus_and_tar
+
+}
+
+build_ext() {
+  citus_version="$1"
+  basedir="${base}/${citus_version}"
+  
+  mkdir -p "${basedir}"
+  cd "${basedir}" || exit
+  citus_dir=${basedir}/citus_$citus_version
+  git clone --branch "$citus_version" https://github.com/citusdata/citus.git --depth 1 citus_"$citus_version"  
+  builddir="${basedir}/build"
+
+  install_citus_and_tar
+}
+
+build_current "master"
+build_ext "${citus_old_version}"
