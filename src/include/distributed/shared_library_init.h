dadbe86af1b29c39dc3337b668d57ad445559fa9
SaitTalhaNisanci <s.talhanisanci@gmail.com>
2019-10-30 10:35:39 +0300

    refactor some of hard coded values in citus gucs (#3137)
    * refactor some of hard coded values in citus gucs
    
    * rename GUC_ALLOW_ALL to GUC_STANDARD

--- src/include/distributed/shared_library_init.h
+++ src/include/distributed/shared_library_init.h
@@ -11,6 +11,10 @@
 #ifndef SHARED_LIBRARY_INIT_H
 #define SHARED_LIBRARY_INIT_H
 
+#define GUC_STANDARD 0
+#define MAX_SHARD_COUNT 64000
+#define MAX_SHARD_REPLICATION_FACTOR 100
+
 extern void StartupCitusBackend(void);
 
 #endif /* SHARED_LIBRARY_INIT_H */
