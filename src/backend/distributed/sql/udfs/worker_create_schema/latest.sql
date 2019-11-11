cfc879d888dbfd6b583813d67c7292acccf77ea2
SaitTalhaNisanci <s.talhanisanci@gmail.com>
2019-11-11 13:33:59 +0300

    do not change the search path while creating a udf

--- src/backend/distributed/sql/udfs/worker_create_schema/latest.sql
+++ src/backend/distributed/sql/udfs/worker_create_schema/latest.sql
@@ -1,7 +1,6 @@
-SET search_path = 'pg_catalog';
-CREATE FUNCTION worker_create_schema(bigint)
+CREATE FUNCTION pg_catalog.worker_create_schema(bigint)
     RETURNS void
     LANGUAGE C STRICT
     AS 'MODULE_PATHNAME', $$worker_create_schema$$;
-COMMENT ON FUNCTION worker_create_schema(bigint)
+COMMENT ON FUNCTION pg_catalog.worker_create_schema(bigint)
     IS 'create schema in remote node';   
