b3b0d147f88d1b4a0533524cee39091264bdd7b4
Nils Dijk <nils@citusdata.com>
2019-11-15 19:26:27 +0100

    q18

--- src/test/regress/sql/ch_bench_join_expr.sql
+++ src/test/regress/sql/ch_bench_join_expr.sql
@@ -594,6 +594,36 @@ WHERE ol_i_id = t.i_id;
 -- influence on how the query gets planned so I removed the clause
 --AND ol_quantity < t.a;
 
+-- Query 18
+SELECT
+    c_last,
+    c_id o_id,
+    o_entry_d,
+    o_ol_cnt,
+    sum(ol_amount)
+FROM
+    customer,
+    oorder,
+    order_line
+WHERE c_id = o_c_id
+  AND c_w_id = o_w_id
+  AND c_d_id = o_d_id
+  AND ol_w_id = o_w_id
+  AND ol_d_id = o_d_id
+  AND ol_o_id = o_id
+GROUP BY
+    o_id,
+    o_w_id,
+    o_d_id,
+    c_id,
+    c_last,
+    o_entry_d,
+    o_ol_cnt
+HAVING sum(ol_amount) > 5 -- was 200, but thats too big for the dataset
+ORDER BY
+    sum(ol_amount) DESC,
+    o_entry_d;
+
 -- Query 20
 SELECT
     su_name,
