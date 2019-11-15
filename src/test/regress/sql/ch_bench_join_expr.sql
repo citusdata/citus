53154f7e93f266b7c3a5acb520fca5afda3260bb
Nils Dijk <nils@citusdata.com>
2019-11-15 19:18:44 +0100

    q16

--- src/test/regress/sql/ch_bench_join_expr.sql
+++ src/test/regress/sql/ch_bench_join_expr.sql
@@ -554,6 +554,27 @@ WHERE su_suppkey = supplier_no
   AND total_revenue = (SELECT max(total_revenue) FROM revenue)
 ORDER BY su_suppkey;
 
+--Q16
+SELECT
+    i_name,
+    substr(i_data, 1, 3) AS brand,
+    i_price,
+    count(DISTINCT (mod((s_w_id * s_i_id),10000))) AS supplier_cnt
+FROM
+    stock,
+    item
+WHERE i_id = s_i_id
+  AND i_data NOT LIKE 'zz%'
+  AND (mod((s_w_id * s_i_id),10000) NOT IN
+       (SELECT su_suppkey
+        FROM supplier
+        WHERE su_comment LIKE '%bad%'))
+GROUP BY
+    i_name,
+    substr(i_data, 1, 3),
+    i_price
+ORDER BY supplier_cnt DESC;
+
 -- Query 20
 SELECT
     su_name,
