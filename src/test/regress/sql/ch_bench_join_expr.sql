09186bd1f0035751e6a0d4883f889473c409199d
Nils Dijk <nils@citusdata.com>
2019-11-15 19:01:50 +0100

    q5

--- src/test/regress/sql/ch_bench_join_expr.sql
+++ src/test/regress/sql/ch_bench_join_expr.sql
@@ -288,6 +288,39 @@ WHERE o_entry_d >= '2007-01-02 00:00:00.000000'
 GROUP BY o_ol_cnt
 ORDER BY o_ol_cnt;
 
+-- Query 5
+SELECT
+    n_name,
+    sum(ol_amount) AS revenue
+FROM
+    customer,
+    oorder,
+    order_line,
+    stock,
+    supplier,
+    nation,
+    region
+WHERE c_id = o_c_id
+  AND c_w_id = o_w_id
+  AND c_d_id = o_d_id
+  AND ol_o_id = o_id
+  AND ol_w_id = o_w_id
+  AND ol_d_id=o_d_id
+  AND ol_w_id = s_w_id
+  AND ol_i_id = s_i_id
+  AND mod((s_w_id * s_i_id),10000) = su_suppkey
+-- our dataset does not have the supplier in the same nation as the customer causing this
+-- join to filter out all the data. We verify later on that we can actually perform an
+-- ascii(substr(c_state,1,1)) == reference table column join later on so it should not
+-- matter we skip this filter here.
+--AND ascii(substr(c_state,1,1)) = su_nationkey
+  AND su_nationkey = n_nationkey
+  AND n_regionkey = r_regionkey
+  AND r_name = 'Europe'
+  AND o_entry_d >= '2007-01-02 00:00:00.000000'
+GROUP BY n_name
+ORDER BY revenue DESC;
+
 -- Query 7
 SELECT
     su_nationkey as supp_nation,
