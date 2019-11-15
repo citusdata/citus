f5f03dbb97d8fb99e440999d22fd6241a3afc2fe
Nils Dijk <nils@citusdata.com>
2019-11-15 19:29:11 +0100

    q19

--- src/test/regress/sql/ch_bench_join_expr.sql
+++ src/test/regress/sql/ch_bench_join_expr.sql
@@ -624,6 +624,32 @@ ORDER BY
     sum(ol_amount) DESC,
     o_entry_d;
 
+-- Query 19
+SELECT
+    sum(ol_amount) AS revenue
+FROM
+    order_line,
+     item
+WHERE (     ol_i_id = i_id
+        AND i_data LIKE '%a'
+        AND ol_quantity >= 1
+        AND ol_quantity <= 10
+        AND i_price BETWEEN 1 AND 400000
+        AND ol_w_id IN (1,2,3))
+   OR (     ol_i_id = i_id
+        AND i_data LIKE '%b'
+        AND ol_quantity >= 1
+        AND ol_quantity <= 10
+        AND i_price BETWEEN 1 AND 400000
+        AND ol_w_id IN (1,2,4))
+   OR (     ol_i_id = i_id
+        AND i_data LIKE '%c'
+        AND ol_quantity >= 1
+        AND ol_quantity <= 10
+        AND i_price BETWEEN 1 AND 400000
+        AND ol_w_id IN (1,5,3));
+
+
 -- Query 20
 SELECT
     su_name,
