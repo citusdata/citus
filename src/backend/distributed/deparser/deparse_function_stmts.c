60b741927fff6fe5e38fa63b1f38c3c8231e1c62
Marco Slot <marco.slot@gmail.com>
2019-11-24 06:04:22 +0100

    Add missing include to deparse_function_stmts.c

--- src/backend/distributed/deparser/deparse_function_stmts.c
+++ src/backend/distributed/deparser/deparse_function_stmts.c
@@ -20,6 +20,7 @@
 #include "access/htup_details.h"
 #include "catalog/namespace.h"
 #include "catalog/pg_proc.h"
+#include "catalog/pg_type.h"
 #include "commands/defrem.h"
 #include "distributed/citus_ruleutils.h"
 #include "distributed/commands.h"
