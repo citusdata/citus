5b40eb5e0a89806ead4300099113fa62443af7b9
Hanefi Onaldi <hanefionaldi@gmail.com>
2020-01-16 13:43:53 +0300

    Test case for locally executing a query that used to run on a round-robin worker

--- src/test/regress/sql/multi_task_assignment_policy.sql
+++ src/test/regress/sql/multi_task_assignment_policy.sql
@@ -239,9 +239,30 @@ SELECT create_distributed_table('task_assignment_test_table_2', 'test_id');
 SET citus.task_assignment_policy TO 'round-robin';
 
 -- Run the query two times to make sure that it hits two different workers
+<<<<<<< HEAD
 -- on consecutive runs
 -- prevent PG 11 - PG 12 outputs to diverge
 SET citus.enable_cte_inlining TO false;
+=======
+-- on consecutive runs only when local execution is not enabled
+INSERT INTO explain_outputs
+SELECT parse_explain_output($cmd$
+EXPLAIN WITH q1 AS (SELECT * FROM task_assignment_test_table_2) SELECT * FROM q1
+$cmd$, 'task_assignment_test_table_2');
+
+INSERT INTO explain_outputs
+SELECT parse_explain_output($cmd$
+EXPLAIN WITH q1 AS (SELECT * FROM task_assignment_test_table_2) SELECT * FROM q1
+$cmd$, 'task_assignment_test_table_2');
+
+-- There should be one record for local execution since the intermediate results
+-- are processed only on coordinator
+SELECT DISTINCT value FROM explain_outputs;
+TRUNCATE explain_outputs;
+
+-- Disable local execution and make sure it hits two different workers
+SET citus.enable_local_execution_planning TO FALSE;
+>>>>>>> 49ab67564... Test case for locally executing a query that used to run on a round-robin worker
 INSERT INTO explain_outputs
 SELECT parse_explain_output($cmd$
 EXPLAIN WITH q1 AS (SELECT * FROM task_assignment_test_table_2) SELECT * FROM q1
@@ -256,6 +277,7 @@ $cmd$, 'task_assignment_test_table_2');
 -- different workers
 SELECT count(DISTINCT value) FROM explain_outputs;
 
+RESET citus.enable_local_execution_planning;
 RESET citus.task_assignment_policy;
 RESET client_min_messages;
 
