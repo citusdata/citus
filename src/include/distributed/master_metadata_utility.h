7efbbcfb8335c9b7ca7c2712d4bf298d055b535e
Onder Kalaci <onderkalaci@gmail.com>
2020-01-22 18:16:04 +0100

    Update shardPlacement->nodeId to uint
    As the source of the shardPlacement->nodeId is always workerNode->nodeId,
    and that is uint32.
    
    We had this hack because of: https://github.com/citusdata/citus/pull/2633/commits/0ea4e52df512acd678ed7ad0cb128638221a3a5a#r266421409
    
    And, that is gone with: https://github.com/citusdata/citus/commit/90056f7d3cd9f2035dd90d38c658a7211151f3c9#diff-c532177d74c72d3f0e7cd10e448ab3c6L1123
    
    So, we're safe to do it now.

--- src/include/distributed/master_metadata_utility.h
+++ src/include/distributed/master_metadata_utility.h
@@ -81,7 +81,7 @@ typedef struct ShardPlacement
 	/* the rest of the fields aren't from pg_dist_placement */
 	char *nodeName;
 	uint32 nodePort;
-	int nodeId;
+	uint32 nodeId;
 	char partitionMethod;
 	uint32 colocationGroupId;
 	uint32 representativeValue;
