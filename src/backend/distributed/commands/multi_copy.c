78767c9a7d408ce1de637ade7c6ba2567b159eac
Philip Dubé <gunkmute@gmail.com>
2020-02-27 22:38:34 +0000

    Fix create_distributed_table on a table using GENERATED ALWAYS AS
    If the generated column does not come at the end of the column list,
    columnNameList doesn't line up with the column indexes. Seek past
    
    CREATE TABLE test_table (
        test_id int PRIMARY KEY,
        gen_n int GENERATED ALWAYS AS (1) STORED,
        created_at TIMESTAMPTZ NOT NULL DEFAULT now()
    );
    SELECT create_distributed_table('test_table', 'test_id');
    
    Would raise ERROR: cannot cast 23 to 1184
    
    (cherry picked from commit 34f241af16ad772f75571196b7a59619b0ce2b79)

--- src/backend/distributed/commands/multi_copy.c
+++ src/backend/distributed/commands/multi_copy.c
@@ -1490,8 +1490,8 @@ TypeForColumnName(Oid relationId, TupleDesc tupleDescriptor, char *columnName)
 
 
 /*
- * Walks a TupleDesc and returns an array of the types of each attribute. Will return
- * InvalidOid in the place of dropped attributes.
+ * Walks a TupleDesc and returns an array of the types of each attribute.
+ * Returns InvalidOid in the place of dropped or generated attributes.
  */
 static Oid *
 TypeArrayFromTupleDescriptor(TupleDesc tupleDescriptor)
@@ -1503,7 +1503,11 @@ TypeArrayFromTupleDescriptor(TupleDesc tupleDescriptor)
 	for (columnIndex = 0; columnIndex < columnCount; columnIndex++)
 	{
 		Form_pg_attribute attr = TupleDescAttr(tupleDescriptor, columnIndex);
-		if (attr->attisdropped)
+		if (attr->attisdropped
+#if PG_VERSION_NUM >= 120000
+			|| attr->attgenerated == ATTRIBUTE_GENERATED_STORED
+#endif
+			)
 		{
 			typeArray[columnIndex] = InvalidOid;
 		}
@@ -1541,7 +1545,7 @@ ColumnCoercionPaths(TupleDesc destTupleDescriptor, TupleDesc inputTupleDescripto
 
 		if (inputTupleType == InvalidOid)
 		{
-			/* this was a dropped column and will not be in the incoming tuples */
+			/* TypeArrayFromTupleDescriptor decided to skip this column */
 			continue;
 		}
 
@@ -1582,9 +1586,9 @@ TypeOutputFunctions(uint32 columnCount, Oid *typeIdArray, bool binaryFormat)
 		bool typeVariableLength = false;
 		Oid outputFunctionId = InvalidOid;
 
-		/* If there are any dropped columns it'll show up as a NULL */
 		if (columnTypeId == InvalidOid)
 		{
+			/* TypeArrayFromTupleDescriptor decided to skip this column */
 			continue;
 		}
 		else if (binaryFormat)
