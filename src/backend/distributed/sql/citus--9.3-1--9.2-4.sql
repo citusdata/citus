e63435def5748ed2c5ee9604709148ed964fdfa4
Önder Kalacı <onder@citusdata.com>
2020-03-31 09:18:25 +0200

    Downgrade from 9.3-1 to 9.2-4 AND upgrade from 9.2-2 to 9.2-4 (#3673)
    * Downgrade from 9.3-1 to 9.2-4 AND upgrade from 9.2-2 to 9.2-4
    
    We have accidentally tagged master branch with v9.2-3. However master branch
    was already bumped to v9.3-1, with this commit, we are undoing the catalog
    changes that have happened between 9.2-2 to 9.3-1, and making 9.2-4 as the
    release that we can use as the continuation of 9.2 series.
    
    Similarly, we have a path to upgrade from 9.2-2 to 9.2-4

---
+++ src/backend/distributed/sql/citus--9.3-1--9.2-4.sql
@@ -0,0 +1,18 @@
+/* citus--9.3-1--9.2-4 */
+/* this is an unusual upgrade path, we are doing it because */
+/* we have accidentally tagged master branch with v9.2-3 */
+/* however master branch was already bumped to v9.3-1 */
+/* with this file, we are undoing the catalog changes that */
+/* have happened between 9.2-2 to 9.3-1, and making 9.2-4 */
+/* as the release that we can */
+
+-- undo the changes for citus_extradata_container that happened on citus 9.3
+DROP FUNCTION IF EXISTS pg_catalog.citus_extradata_container(INTERNAL);
+CREATE FUNCTION pg_catalog.citus_extradata_container(INTERNAL)
+    RETURNS void
+    LANGUAGE C
+    AS 'MODULE_PATHNAME', $$citus_extradata_container$$;
+COMMENT ON FUNCTION pg_catalog.citus_extradata_container(INTERNAL)
+    IS 'placeholder function to store additional data in postgres node trees';
+
+DROP FUNCTION IF EXISTS pg_catalog.update_distributed_table_colocation(regclass, text);
