79465b775b1e5b44dd0ef613bbd72c9d107fa8ce
Onur Tirtir <onurcantirtir@gmail.com>
2020-04-07 14:24:56 +0300

    Implement GetQueryLockMode helper
    If we want to get necessary lockmode for a relation RangeVar within
    a query, we can get the lockmode easily from the RangeVar itself (if
    pg version >= 12).
    
    However, if we want to decide the lockmode appropriate for the
    "query", we can derive this information by using GetQueryLockMode
    according to the code comment from RangeTblEntry->rellockmode.

--- src/include/distributed/distributed_planner.h
+++ src/include/distributed/distributed_planner.h
@@ -203,6 +203,7 @@ extern Node * ResolveExternalParams(Node *inputNode, ParamListInfo boundParams);
 extern bool IsMultiTaskPlan(struct DistributedPlan *distributedPlan);
 extern RangeTblEntry * RemoteScanRangeTableEntry(List *columnNameList);
 extern int GetRTEIdentity(RangeTblEntry *rte);
+extern LOCKMODE GetQueryLockMode(Query *query);
 extern int32 BlessRecordExpression(Expr *expr);
 extern void DissuadePlannerFromUsingPlan(PlannedStmt *plan);
 extern PlannedStmt * FinalizePlan(PlannedStmt *localPlan,
