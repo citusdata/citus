00f0cdabb653ceaea0010ee05c830a24af3baeff
Nils Dijk <nils@citusdata.com>
2020-04-08 13:28:33 +0200

    Add alter role set tests back

--- src/test/regress/sql/alter_role_propagation.sql
+++ src/test/regress/sql/alter_role_propagation.sql
@@ -16,4 +16,39 @@ SELECT 1 FROM master_add_node('localhost', :worker_1_port);
 SELECT row(rolname, rolsuper, rolinherit,  rolcreaterole, rolcreatedb, rolcanlogin, rolreplication, rolbypassrls, rolconnlimit, rolpassword, EXTRACT (year FROM rolvaliduntil)) FROM pg_authid WHERE rolname = 'alter_role_1';
 SELECT run_command_on_workers($$SELECT row(rolname, rolsuper, rolinherit,  rolcreaterole, rolcreatedb, rolcanlogin, rolreplication, rolbypassrls, rolconnlimit, rolpassword, EXTRACT (year FROM rolvaliduntil)) FROM pg_authid WHERE rolname = current_user$$);
 
+-- check user, database and postgres wide SET settings.
+-- pre check
+SELECT run_command_on_workers('SHOW enable_hashjoin');
+SELECT run_command_on_workers('SHOW enable_hashagg');
+SELECT run_command_on_workers('SHOW enable_indexonlyscan');
+
+-- change a setting for all users
+ALTER ROLE ALL SET enable_hashjoin TO FALSE;
+SELECT run_command_on_workers('SHOW enable_hashjoin');
+ALTER ROLE ALL IN DATABASE regression SET enable_indexonlyscan TO FALSE;
+SELECT run_command_on_workers('SHOW enable_indexonlyscan');
+
+-- alter configuration_parameter defaults for a user
+ALTER ROLE CURRENT_USER SET enable_hashagg TO FALSE;
+SELECT run_command_on_workers('SHOW enable_hashagg');
+
+-- reset to default values
+ALTER ROLE CURRENT_USER RESET enable_hashagg;
+SELECT run_command_on_workers('SHOW enable_hashagg');
+
+-- RESET ALL with IN DATABASE clause
+ALTER ROLE ALL RESET ALL;
+
+-- post check 1 - should have settings reset except for database specific settings
+SELECT run_command_on_workers('SHOW enable_hashjoin');
+SELECT run_command_on_workers('SHOW enable_indexonlyscan');
+SELECT run_command_on_workers('SHOW enable_hashagg');
+
+ALTER ROLE ALL IN DATABASE regression RESET ALL;
+
+-- post check 2 - should have all settings reset
+SELECT run_command_on_workers('SHOW enable_hashjoin');
+SELECT run_command_on_workers('SHOW enable_indexonlyscan');
+SELECT run_command_on_workers('SHOW enable_hashagg');
+
 DROP SCHEMA alter_role CASCADE;
