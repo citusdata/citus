0ac3d97baecac2e6beffaae88fbc834389e94268
SaitTalhaNisanci <s.talhanisanci@gmail.com>
2020-04-08 17:08:01 +0300

    remove all repartition schemas in case of error
    This solution is not an ideal one but it seems to be doing the job.
    We should have a more generic solution for the cleanup but it seems that
    putting the cleanup in the abort handler is dangerous and it was
    crashing.

--- src/backend/distributed/transaction/transaction_management.c
+++ src/backend/distributed/transaction/transaction_management.c
@@ -28,6 +28,7 @@
 #include "distributed/listutils.h"
 #include "distributed/local_executor.h"
 #include "distributed/multi_executor.h"
+#include "distributed/repartition_join_execution.h"
 #include "distributed/transaction_management.h"
 #include "distributed/placement_connection.h"
 #include "distributed/subplan_execution.h"
