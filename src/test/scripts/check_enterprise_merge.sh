b3ec6fbe7a4a59be0384473676aec38846e2a469
Jelte Fennema <github-tech@jeltef.nl>
2020-06-19 12:45:36 +0200

    Make check_enterprise_merge script stricter (#3918)
    We've had two issues with merge conflicts to enterprise in the last week, that
    suddenly happened. Because of this CI check this actually blocks all community
    PRs from being merged.
    
    This PR tries to improve on the previous script we had, by putting tougher
    constraints on when a merge is allowed.
    
    Previously the check would pass in two cases:
    1. This PR be merged without conflicts into `enterprise-master`
    2. A branch exists with the same name as this PR on enterprise and that can be
       merged into `enterprise-master`.
    
    The first case stays the same, but I've changed the second case to require the
    following instead:
    1. A branch exists on enterprise with the same name as this PR
    2. **NEW: This branch contains the the last commit of the community PR branch**
    3. This branch can be merged into enterprise-master
    
    This makes sure the enterprise branch is actually up to date and not forgotten about.
    
    If we still get problems with this change, future improvements could be:
    1. Check that the PR on enterprise passes CI
    2. Check that the PR on enterprise has been approved
    3. Require the enterprise PR branch to be merged before merging community.

--- src/test/scripts/check_enterprise_merge.sh
+++ src/test/scripts/check_enterprise_merge.sh
@@ -1,55 +1,100 @@
 #!/bin/bash
 
+# Testing this script locally requires you to set the following environment
+# variables:
+# CIRCLE_BRANCH, GIT_USERNAME and GIT_TOKEN
+
 # fail if trying to reference a variable that is not set.
 set -u
 # exit immediately if a command fails
 set -e
+# Fail on pipe failures
+set -o pipefail
 
-# try_merge sees if we can merge "from" branch to "to" branch
-# it will exit with nonzero code if the merge fails because of conflicts.
-try_merge() {
-    to=$1
-    from=$2
-    git checkout "${to}"
-    # this will exit since -e option is set and it will return non-zero code on conflicts.
-    git merge --no-ff --no-commit "${from}"
+PR_BRANCH="${CIRCLE_BRANCH}"
+ENTERPRISE_REMOTE="https://${GIT_USERNAME}:${GIT_TOKEN}@github.com/citusdata/citus-enterprise"
 
+# For echo commands "set -x" would show the message effectively twice. Once as
+# part of the echo command shown by "set -x" and once because of the output of
+# the echo command. We do not want "set -x" to show the echo command. We only
+# want to see the actual message in the output of echo itself. This function is
+# a trick to do so. Read the StackOverflow post below to understand why this
+# works and what this works around.
+# Source: https://superuser.com/a/1141026/242593
+shopt -s expand_aliases
+alias echo='{ save_flags="$-"; set +x;} 2> /dev/null; echo_and_restore'
+echo_and_restore() {
+        builtin echo "$*"
+        #shellcheck disable=SC2154
+        case "$save_flags" in
+         (*x*)  set -x
+        esac
 }
 
-cd /tmp
-if [ ! -d citus-enterprise ]; then
-    git clone https://${GIT_USERNAME}:${GIT_TOKEN}@github.com/citusdata/citus-enterprise
-fi
+# Make sure that on a failing exit we show a useful message
+hint_on_fail() {
+    exit_code=$?
+    if [ $exit_code != 0 ]; then
+        echo HINT: To solve this failure look here: https://github.com/citusdata/citus/blob/master/src/test/scripts/README.md#check-merge-to-enterprise-job
+    fi
+    exit $exit_code
+}
+trap hint_on_fail EXIT
 
-cd citus-enterprise
+
+# List executed commands. This is done so debugging this script is easier when
+# it fails. It's explicitly done after git remote add so username and password
+# are not shown in CI output (even though it's also filtered out by CircleCI)
+set -x
+
+# Clone current git repo (which should be community) to a temporary working
+# directory and go there
+GIT_DIR_ROOT="$(git rev-parse --show-toplevel)"
+TMP_GIT_DIR="$(mktemp --directory -t citus-merge-check.XXXXXXXXX)"
+git clone "$GIT_DIR_ROOT" "$TMP_GIT_DIR"
+cd "$TMP_GIT_DIR"
+
+# Fails in CI without this
 git config user.email "citus-bot@microsoft.com"
 git config user.name "citus bot"
 
-# reset repository into usable state if script ran before
-git fetch origin
-git reset --hard
-git checkout enterprise-master
-git reset --hard origin/enterprise-master
+# Disable "set -x" temporarily, because $ENTERPRISE_REMOTE contains passwords
+{ set +x ; } 2> /dev/null
+git remote add enterprise "$ENTERPRISE_REMOTE"
+set -x
 
-branch_name="${CIRCLE_BRANCH}"
+git remote set-url --push enterprise no-pushing
 
-# check if the branch on community exists on enterprise
-# the output will not be empty if it does
-if [ `git branch -r --list origin/$branch_name` ]
-then
-    try_merge enterprise-master origin/$branch_name
-else
-    # add community as a remote if not already added
-    set +e
-    if ! git ls-remote community > /dev/null; then
-        set -e
-        git remote add --no-tags community git@github.com:citusdata/citus.git
-    fi
-    set -e
+# Fetch enterprise-master
+git fetch enterprise enterprise-master
 
-    # prevent pushes to community and update branch we care about
-    git remote set-url --push community no-pushing
-    git fetch community $branch_name
 
-    try_merge enterprise-master community/$branch_name
+git checkout "enterprise/enterprise-master"
+
+if git merge --no-commit "origin/$PR_BRANCH"; then
+    echo "INFO: community PR branch could be merged into enterprise-master, so everything is good"
+    exit 0
 fi
+
+# undo partial merge
+git merge --abort
+
+if ! git fetch enterprise "$PR_BRANCH" ; then
+    echo "ERROR: enterprise/$PR_BRANCH was not found and community PR branch could not be merged into enterprise-master"
+    exit 1
+fi
+
+# Show the top commit of the enterprise PR branch to make debugging easier
+git log -n 1 "enterprise/$PR_BRANCH"
+
+# Check that this branch contains the top commit of the current community PR
+# branch. If it does not it means it's not up to date with the current PR, so
+# the enterprise branch should be updated.
+if ! git merge-base --is-ancestor "origin/$PR_BRANCH" "enterprise/$PR_BRANCH" ; then
+    echo "ERROR: enterprise/$PR_BRANCH is not up to date with community PR branch"
+    exit 1
+fi
+
+# Now check if we can merge the enterprise PR into enterprise-master without
+# issues.
+git merge --no-commit "enterprise/$PR_BRANCH"
