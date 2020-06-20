00633165fcf2178783cac3bee5a59b4e32715651
Sait Talha Nisanci <s.talhanisanci@gmail.com>
2020-06-20 21:06:16 +0300

    Changelog: Test unicode escapes
    Unicode escapes work as expected, related tests are added.
    
    Changelog entry on PG13:
    Allow Unicode escapes, e.g., E'\u####', U&'\####', to specify any character available in the database encoding, even when the database encoding is not UTF-8 (Tom Lane)

--- src/test/regress/sql/pg13.sql
+++ src/test/regress/sql/pg13.sql
@@ -43,6 +43,7 @@ INSERT INTO ab SELECT i, 2 * i FROM generate_series(1,20)i;
 SELECT * FROM ab WHERE (ROW(a,b)).f1 > (ROW(10,30)).f1 ORDER BY 1,2;
 SELECT * FROM ab WHERE (ROW(a,b)).f2 > (ROW(0,38)).f2 ORDER BY 1,2;
 
+-- test normalized
 CREATE TABLE text_table (name text);
 SELECT create_distributed_table('text_table', 'name');
 INSERT INTO text_table VALUES ('abc');
@@ -52,5 +53,12 @@ SELECT name IS NORMALIZED FROM text_table ORDER BY 1;
 SELECT is_normalized(name) FROM text_table ORDER BY 1;
 SELECT normalize(name) FROM text_table ORDER BY 1;
 INSERT INTO text_table VALUES (normalize(U&'\0061\0308bc', NFC));
+
+-- test unicode escape
+-- insert the word 'data' with unicode escapes
+INSERT INTO text_table VALUES(U&'d\0061t\+000061');
+-- insert the word слон
+INSERT INTO text_table VALUES(U&'\0441\043B\043E\043D');
+SELECT * FROM text_table ORDER BY 1;
 
 drop schema test_pg13 cascade;
