citusdata/citus

commit f4791fcb106114143134d25c8a1abe745750ae6e

Remove SwallowErrors by using PathNameDeleteTemporaryDir (#3893)
This is a different version of #3634. It also removes SwallowErrors, but
instead of modifying our own functions to not throw errors, it uses the
postgres built in `PathNameDeleteTemporaryDir` function. This function
does not throw errors.

Since this change is for a bugfix, I tried to minimize the changes.

PRs with the following changes would be good to do separately from this
PR:
1. Use PathName(Create|Open|Delete)Temporary(File|Dir) to open and
   remove all files/dirs instead of our own custom file functions.
2. Prefix our outmost files/directories with `PG_TEMP_FILE_PREFIX` so
   that they are identified by Postgres as temporary files, which will be
   removed at postmaster start. This way we do not have to do this cleanup
   ourselves.
3. Store the files in the temporary table space if it exists.

Fixes #3634
Fixes #3618

src/backend/distributed/executor/intermediate_results.c
@@ -688,7 +688,7 @@ RemoveIntermediateResultsDirectory(void)
 {
 	if (CreatedResultsDirectory)
 	{
-		CitusRemoveDirectory(IntermediateResultsDirectory());
+		PathNameDeleteTemporaryDir(IntermediateResultsDirectory());
 
 		CreatedResultsDirectory = false;
 	}

src/backend/distributed/transaction/transaction_management.c
@@ -112,7 +112,6 @@ static void ResetShardPlacementTransactionState(void);
 static void AdjustMaxPreparedTransactions(void);
 static void PushSubXact(SubTransactionId subId);
 static void PopSubXact(SubTransactionId subId);
-static void SwallowErrors(void (*func)());
 static bool MaybeExecutingUDF(void);
 static void ResetGlobalVariables(void);
 
@@ -297,7 +296,7 @@ CoordinatedTransactionCallback(XactEvent event, void *arg)
 				 * RemoveIntermediateResultsDirectory.
 				 */
 				AtEOXact_Files(false);
-				SwallowErrors(RemoveIntermediateResultsDirectory);
+				RemoveIntermediateResultsDirectory();
 			}
 			ResetShardPlacementTransactionState();
 
@@ -666,51 +665,6 @@ ActiveSubXactContexts(void)
 	}
 
 	return reversedSubXactStates;
-}
-
-
-/*
- * If an ERROR is thrown while processing a transaction the ABORT handler is called.
- * ERRORS thrown during ABORT are not treated any differently, the ABORT handler is also
- * called during processing of those. If an ERROR was raised the first time through it's
- * unlikely that the second try will succeed; more likely that an ERROR will be thrown
- * again. This loop continues until Postgres notices and PANICs, complaining about a stack
- * overflow.
- *
- * Instead of looping and crashing, SwallowErrors lets us attempt to continue running the
- * ABORT logic. This wouldn't be safe in most other parts of the codebase, in
- * approximately none of the places where we emit ERROR do we first clean up after
- * ourselves! It's fine inside the ABORT handler though; Postgres is going to clean
- * everything up before control passes back to us.
- */
-static void
-SwallowErrors(void (*func)())
-{
-	MemoryContext savedContext = CurrentMemoryContext;
-
-	PG_TRY();
-	{
-		func();
-	}
-	PG_CATCH();
-	{
-		ErrorData *edata = CopyErrorData();
-
-		/* don't try to intercept PANIC or FATAL, let those breeze past us */
-		if (edata->elevel != ERROR)
-		{
-			PG_RE_THROW();
-		}
-
-		/* turn the ERROR into a WARNING and emit it */
-		edata->elevel = WARNING;
-		ThrowErrorData(edata);
-
-		/* leave the error handling system */
-		FlushErrorState();
-		MemoryContextSwitchTo(savedContext);
-	}
-	PG_END_TRY();
 }
 
 

commit a98226842d218720094459b41a9ac774cd85ecea

Use rename to make sure no files are inserted while deleting (#3912)
As suggested by @marcocitus in https://github.com/citusdata/citus/pull/3911#issuecomment-643978531, there was
a regression in #3893. If another backend would write a file during deletion of
the intermediate results directory, this file would not necessarily be deleted.

The approach used in `CitusRemoveDirectory` is to try recursive removal of the
directory again if it has failed. This does not work here, since when a file
can not be removed for other reasons (e.g. `EPERM`) it will not throw an error
anymore. So then we would get into an infinite removal loop. Instead I now
`rename` the directory before removing it. That way other backends will not
write files to it anymore.

src/backend/distributed/executor/intermediate_results.c
@@ -701,7 +701,33 @@ RemoveIntermediateResultsDirectory(void)
 {
 	if (CreatedResultsDirectory)
 	{
-		PathNameDeleteTemporaryDir(IntermediateResultsDirectory());
+		/*
+		 * The shared directory is renamed before deleting it. Otherwise it
+		 * would be possible for another backend to write a file, while we are
+		 * deleting the directory. Since rename is atomic by POSIX standards
+		 * that's not possible. The current PID is included in the new
+		 * filename, so there can be no collisions with other backends.
+		 */
+		char *sharedName = IntermediateResultsDirectory();
+		StringInfo privateName = makeStringInfo();
+		appendStringInfo(privateName, "%s.removed-by-%d", sharedName, MyProcPid);
+		if (rename(sharedName, privateName->data))
+		{
+			ereport(LOG,
+					(errcode_for_file_access(),
+					 errmsg(
+						 "could not rename intermediate results directory \"%s\" to \"%s\": %m",
+						 sharedName, privateName->data)));
+
+			/* rename failed for some reason, we do a best effort removal of
+			 * the shared directory */
+
+			PathNameDeleteTemporaryDir(sharedName);
+		}
+		else
+		{
+			PathNameDeleteTemporaryDir(privateName->data);
+		}
 
 		CreatedResultsDirectory = false;
 	}