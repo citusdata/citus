c62b1dc98cb267e7f6c9578b7cdffb1ce63aff9d
Jelte Fennema <github-tech@jeltef.nl>
2020-06-25 12:03:26 +0200

    Use the right hashtable

--- src/backend/distributed/metadata/metadata_cache.c
+++ src/backend/distributed/metadata/metadata_cache.c
@@ -776,7 +776,7 @@ LookupShardIdCacheEntry(int64 shardId)
 	InitializeCaches();
 
 	ShardIdCacheEntry *shardEntry =
-		hash_search(DistTableCacheHash, &shardId, HASH_FIND, &foundInCache);
+		hash_search(ShardIdCacheHash, &shardId, HASH_FIND, &foundInCache);
 
 	if (!foundInCache)
 	{
