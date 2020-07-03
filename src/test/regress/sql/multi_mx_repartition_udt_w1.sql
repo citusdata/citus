453d2d2b6651496e34e4fe5377717d1af669ff36
Sait Talha Nisanci <s.talhanisanci@gmail.com>
2020-07-03 10:46:24 +0300

    test parallel repartition joins with adaptive

--- src/test/regress/sql/multi_mx_repartition_udt_w1.sql
+++ src/test/regress/sql/multi_mx_repartition_udt_w1.sql
@@ -5,7 +5,9 @@
 \c - - - :worker_1_port
 SET client_min_messages = LOG;
 -- Query that should result in a repartition join on UDT column.
-SET citus.task_executor_type = 'task-tracker';
+SET citus.max_adaptive_executor_pool_size TO 2;
+SET citus.enable_repartition_joins to ON;
+SET citus.task_executor_type = 'adaptive';
 SET citus.log_multi_join_order = true;
 
 -- Query that should result in a repartition
