c3748e48e9f8a323ef24b01062e199ea78b1e4d3
Sait Talha Nisanci <s.talhanisanci@gmail.com>
2020-07-03 12:58:39 +0300

    disable other tests

--- src/test/regress/sql/multi_mx_repartition_udt_prepare.sql
+++ src/test/regress/sql/multi_mx_repartition_udt_prepare.sql
@@ -184,23 +184,23 @@ INSERT INTO repartition_udt_other values (12, '(2,3)'::test_udt, 'foo');
 SET client_min_messages = LOG;
 SET citus.task_executor_type = 'task-tracker';
 
--- Query that should result in a repartition
--- join on int column, and be empty.
-SELECT * FROM repartition_udt JOIN repartition_udt_other
-    ON repartition_udt.pk = repartition_udt_other.pk;
+-- -- Query that should result in a repartition
+-- -- join on int column, and be empty.
+-- SELECT * FROM repartition_udt JOIN repartition_udt_other
+--     ON repartition_udt.pk = repartition_udt_other.pk;
 
--- Query that should result in a repartition join on UDT column.
-SET citus.log_multi_join_order = true;
+-- -- Query that should result in a repartition join on UDT column.
+-- SET citus.log_multi_join_order = true;
 
-EXPLAIN (COSTS OFF)
-SELECT * FROM repartition_udt JOIN repartition_udt_other
-    ON repartition_udt.udtcol = repartition_udt_other.udtcol
-    WHERE repartition_udt.pk > 1;
+-- EXPLAIN (COSTS OFF)
+-- SELECT * FROM repartition_udt JOIN repartition_udt_other
+--     ON repartition_udt.udtcol = repartition_udt_other.udtcol
+--     WHERE repartition_udt.pk > 1;
 
-SELECT * FROM repartition_udt JOIN repartition_udt_other
-    ON repartition_udt.udtcol = repartition_udt_other.udtcol
-    WHERE repartition_udt.pk > 1
-    ORDER BY repartition_udt.pk;
+-- SELECT * FROM repartition_udt JOIN repartition_udt_other
+--     ON repartition_udt.udtcol = repartition_udt_other.udtcol
+--     WHERE repartition_udt.pk > 1
+--     ORDER BY repartition_udt.pk;
 
 \c - - - :worker_1_port
 \c - - - :worker_2_port
