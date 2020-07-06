2b52aaecf97b0872ab59ebb582a86eebd87f2eb3
Onur Tirtir <onurcantirtir@gmail.com>
2020-07-07 01:32:41 +0300

    update sql def and add downgrade step

--- src/backend/distributed/sql/citus--9.4-1--9.5-1.sql
+++ src/backend/distributed/sql/citus--9.4-1--9.5-1.sql
@@ -1,4 +1,5 @@
 -- citus--9.4-1--9.5-1
 
 -- bump version to 9.5-1
+#include "udfs/create_citus_local_table/9.5-1.sql"
 
--- src/backend/distributed/sql/citus--9.5-1--9.4-1.sql
+++ src/backend/distributed/sql/citus--9.5-1--9.4-1.sql
@@ -1,2 +1,4 @@
 -- citus--9.5-1--9.4-1
--- this is an empty downgrade path since citus--9.4-1--9.5-1.sql is empty for now
+-- this is a downgrade path that will revert the changes made in citus--9.4-1--9.5-1.sql
+
+DROP FUNCTION pg_catalog.create_citus_local_table(table_name regclass);
