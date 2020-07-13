d77e386e9295698db10b1ac09b16d3e6853375c5
Nils Dijk <nils@citusdata.com>
2020-07-13 16:29:24 +0200

    force aliases in deparsing for queries with anonymous column references (#4011)
    DESCRIPTION: Force aliases in deparsing for queries with anonymous column references
    
    Fixes: #3985
    
    The root cause has todo with discrepancies in the query tree we create. I think in the future we should spend some time on categorising all changes we made to ruleutils and see if we can change the data structure `query` we pass to the deparser to have an actual valid postgres query for the deparser to render.
    
    For now the fix is to keep track, besides changing the names of the entries in the target list, also if we have a reference to an anonymous columns. If there are anonymous columns we set the `printaliases` flag to true which forces the deparser to add the aliases.
    (cherry picked from commit 449d1f0e91ba227b70c2b14e17d0f0f521aaca56)

---
+++ src/test/regress/sql/anonymous_columns.sql
@@ -0,0 +1,39 @@
+SET citus.next_shard_id TO 20080000;
+
+CREATE SCHEMA anonymous_columns;
+SET search_path TO anonymous_columns;
+
+CREATE TABLE t0 (a int PRIMARY KEY, b int, "?column?" text);
+SELECT create_distributed_table('t0', 'a');
+INSERT INTO t0 VALUES (1, 2, 'hello'), (2, 4, 'world');
+
+SELECT "?column?" FROM t0;
+
+WITH a AS (SELECT * FROM t0) SELECT "?column?" FROM a;
+WITH a AS (SELECT '' FROM t0) SELECT * FROM a;
+
+-- test CTE's that could be rewritten as subquery
+WITH a AS (SELECT '' FROM t0 GROUP BY a) SELECT * FROM a;
+WITH a AS (SELECT '' FROM t0 GROUP BY b) SELECT * FROM a;
+WITH a AS (SELECT '','' FROM t0 GROUP BY a) SELECT * FROM a;
+WITH a AS (SELECT '','' FROM t0 GROUP BY b) SELECT * FROM a;
+WITH a AS (SELECT 1, * FROM t0 WHERE a = 1) SELECT * FROM a;
+
+-- test CTE's that are referenced multiple times and hence need to stay CTE's
+WITH a AS (SELECT '' FROM t0 WHERE a = 1) SELECT * FROM a, a b;
+WITH a AS (SELECT '','' FROM t0 WHERE a = 42) SELECT * FROM a, a b;
+
+-- test with explicit subqueries
+SELECT * FROM (SELECT a, '' FROM t0 GROUP BY a ) as foo;
+SELECT * FROM (SELECT a, '', '' FROM t0 GROUP BY a ) as foo;
+SELECT * FROM (SELECT b, '' FROM t0 GROUP BY b ) as foo;
+SELECT * FROM (SELECT b, '', '' FROM t0 GROUP BY b ) as foo;
+
+-- some tests that follow very similar codeoaths
+SELECT a + 1 FROM t0;
+SELECT a + 1, a - 1 FROM t0;
+WITH cte1 AS (SELECT row_to_json(row(a))->'f1' FROM t0) SELECT * FROM cte1;
+
+-- clean up after test
+SET client_min_messages TO WARNING;
+DROP SCHEMA anonymous_columns CASCADE;
