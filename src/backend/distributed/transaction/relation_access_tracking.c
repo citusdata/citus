d39685adddc6aefb09fa21050fedebfba1b0a62a
Onur Tirtir <onurcantirtir@gmail.com>
2020-09-07 16:36:56 +0300

    see

--- src/backend/distributed/transaction/relation_access_tracking.c
+++ src/backend/distributed/transaction/relation_access_tracking.c
@@ -492,6 +492,17 @@ RecordParallelRelationAccess(Oid relationId, ShardPlacementAccessType placementA
 	/* act accordingly if it's a conflicting access */
 	CheckConflictingParallelRelationAccesses(relationId, placementAccess);
 
+	/*
+	 * CheckConflictingParallelRelationAccesses might switch to sequential
+	 * execution. If that's the case, no need to continue because the executor
+	 * would take the necessary actions to switch to sequential execution
+	 * immediately.
+	 */
+	if (MultiShardConnectionType == SEQUENTIAL_CONNECTION)
+	{
+		return;
+	}
+
 	/* If a relation is partitioned, record accesses to all of its partitions as well. */
 	if (PartitionedTable(relationId))
 	{
