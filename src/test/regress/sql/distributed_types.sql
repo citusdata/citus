0e2d9a7d26546f8b7c315108f92449a2acb24721
Onur Tirtir <onurcantirtir@gmail.com>
2020-10-13 17:19:47 +0300

    move test comment

--- src/test/regress/sql/distributed_types.sql
+++ src/test/regress/sql/distributed_types.sql
@@ -259,7 +259,6 @@ VALUES (0, 1, 'text1', 2), (3, 4, 'text1', 5);
 -- not supported (field indirection in update)
 UPDATE field_indirection_test_2 SET (ct2_col.text_1, ct1_col.int_2) = ('text2', 10) WHERE int_col=4;
 
--- not supported (field indirection to underlying composite type)
 CREATE TYPE two_ints as (if1 int, if2 int);
 CREATE DOMAIN domain AS two_ints CHECK ((VALUE).if1 > 0);
 -- citus does not propagate domain objects
@@ -270,6 +269,7 @@ $$);
 CREATE TABLE domain_indirection_test (f1 int, f3 domain);
 SELECT create_distributed_table('domain_indirection_test', 'f1');
 
+-- not supported (field indirection to underlying composite type)
 INSERT INTO domain_indirection_test (f1,f3.if1, f3.if2) values (0, 1, 2);
 INSERT INTO domain_indirection_test (f1,f3.if1) values (0, 1);
 
