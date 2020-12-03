2e79f212765d275fe3f23dec7002fc371558e85f
Marco Slot <marco.slot@gmail.com>
2020-12-03 22:03:25 +0100

    Make citus_nodes tolerant to node failure

--- src/backend/distributed/sql/udfs/citus_nodes/latest.sql
+++ src/backend/distributed/sql/udfs/citus_nodes/latest.sql
@@ -3,7 +3,7 @@ SELECT
   nodename AS "Host",
   nodeport AS "Port",
   CASE WHEN groupid = 0 THEN 'coordinator' ELSE 'worker' END AS "Node Type",
-  pg_size_pretty(citus_node_database_size(nodename, nodeport)) AS "Database Size",
+  pg_size_pretty(nullif(citus_node_database_size(nodename, nodeport),0)) AS "Database Size",
   (SELECT
      count(*)
    FROM
@@ -32,7 +32,7 @@ SELECT
      (shardid)
    WHERE p.groupid = n.groupid AND t.partmethod = 'n' AND t.repmodel = 't'
   ) AS "Reference Tables",
-  round(100 * (1. - (ds.available_disk_size::double precision / ds.total_disk_size))) || '%' AS "Disk Usage"
+  round(100 * (1. - (ds.available_disk_size::double precision / nullif(ds.total_disk_size, 0)))) || '%' AS "Disk Usage"
 FROM
   pg_dist_node n,
   citus_node_disk_space_stats(n.nodename, n.nodeport) ds
