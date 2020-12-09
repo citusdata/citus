b59a0c4f1e7c20ef956f90c79de092fbe80750a0
Hadi Moshayedi <hadi@moshayedi.net>
2020-12-09 13:24:37 -0800

    Make zstd & lz4 tests more consistent

--- src/test/regress/sql/am_zstd.sql
+++ src/test/regress/sql/am_zstd.sql
@@ -10,34 +10,43 @@ SET search_path TO am_zstd;
 SET columnar.compression TO 'zstd';
 CREATE TABLE test_zstd (a int, b text, c int) USING columnar;
 
-INSERT INTO test_zstd SELECT floor(i / 1000), floor(i / 10)::text, 4 FROM generate_series(1, 10000) i;
+INSERT INTO test_zstd SELECT i % 1000, (i % 10)::text, 4 FROM generate_series(1, 10000) i;
 SELECT count(*) FROM test_zstd;
 
 INSERT INTO test_zstd SELECT floor(i / 2), floor(i / 10)::text, 5 FROM generate_series(1000, 11000) i;
 SELECT count(*) FROM test_zstd;
 
-VACUUM VERBOSE test_zstd;
+CREATE TABLE test_none (LIKE test_zstd) USING columnar;
+INSERT INTO test_none SELECT * FROM test_zstd;
 
 SELECT DISTINCT * FROM test_zstd ORDER BY a, b, c LIMIT 5;
 
+VACUUM FULL test_zstd;
+
+SELECT pg_relation_size('test_zstd') AS size_comp_level_default \gset
+
 -- change compression level
--- for this particular usecase, higher compression levels
--- don't improve compression ratio
 SELECT alter_columnar_table_set('test_zstd', compression_level => 19);
 VACUUM FULL test_zstd;
-VACUUM VERBOSE test_zstd;
+
+SELECT pg_relation_size('test_zstd') AS size_comp_level_19 \gset
+
+-- verify that higher compression level compressed better
+SELECT :size_comp_level_default > :size_comp_level_19 AS size_changed;
 
 -- compare compression rate to pglz
 SET columnar.compression TO 'pglz';
 CREATE TABLE test_pglz (LIKE test_zstd) USING columnar;
 INSERT INTO test_pglz SELECT * FROM test_zstd;
 
-VACUUM VERBOSE test_pglz;
+SELECT pg_relation_size('test_pglz') AS size_pglz \gset
+
+-- verify that zstd compressed better than pglz
+SELECT :size_pglz > :size_comp_level_default;
 
 -- Other operations
-VACUUM FULL test_zstd;
-ANALYZE test_zstd;
 
+ANALYZE test_zstd;
 SELECT count(DISTINCT test_zstd.*) FROM test_zstd;
 
 TRUNCATE test_zstd;
