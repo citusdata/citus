9ac1de3856ed3eae155994041fb6fd0a73528634
Jeff Davis <jdavis@postgresql.org>
2020-12-08 23:37:24 -0800

    add deletes to test

--- src/test/regress/sql/am_update_delete.sql
+++ src/test/regress/sql/am_update_delete.sql
@@ -7,6 +7,8 @@ INSERT INTO columnar_update VALUES (3, 30);
 
 -- should fail
 UPDATE columnar_update SET j = j+1 WHERE i = 2;
+-- should fail
+DELETE FROM columnar_update WHERE i = 2;
 
 -- should succeed because there's no target
 INSERT INTO columnar_update VALUES
@@ -46,26 +48,36 @@ CREATE TABLE p2 PARTITION OF parent
 INSERT INTO parent VALUES('2020-01-15', 10, 100, 'one thousand'); -- columnar
 INSERT INTO parent VALUES('2020-02-15', 20, 200, 'two thousand'); -- columnar
 INSERT INTO parent VALUES('2020-03-15', 30, 300, 'three thousand'); -- row
+INSERT INTO parent VALUES('2020-03-21', 31, 301, 'three thousand and one'); -- row
+INSERT INTO parent VALUES('2020-03-22', 32, 302, 'three thousand and two'); -- row
+INSERT INTO parent VALUES('2020-03-23', 33, 303, 'three thousand and three'); -- row
 
 SELECT * FROM parent;
 
 -- update on specific row partition should succeed
 UPDATE p2 SET i = i+1 WHERE ts = '2020-03-15';
+DELETE FROM p2 WHERE ts = '2020-03-21';
 
 -- update on specific columnar partition should fail
 UPDATE p1 SET i = i+1 WHERE ts = '2020-02-15';
+DELETE FROM p1 WHERE ts = '2020-02-15';
 
 -- partitioned updates that affect only row tables
 -- should succeed
 UPDATE parent SET i = i+1 WHERE ts = '2020-03-15';
+DELETE FROM parent WHERE ts = '2020-03-22';
 
 -- partitioned updates that affect columnar tables
 -- should fail
 UPDATE parent SET i = i+1 WHERE ts > '2020-02-15';
+DELETE FROM parent WHERE ts > '2020-02-15';
 
 -- non-partitioned update should fail, even if it
 -- only affects a row partition
 UPDATE parent SET i = i+1 WHERE n = 300;
+DELETE FROM parent WHERE n = 303;
+
+SELECT * FROM parent;
 
 DROP TABLE parent;
 
