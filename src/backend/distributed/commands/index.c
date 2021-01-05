f27649754bd4cf8c9317fe3a3bc6254b746db64a
Ahmet Gedemenli <afgedemenli@gmail.com>
2021-01-05 13:23:11 +0300

    Add alter index set statistics support (#4455)
    * Add alter index set statistics support
    
    * Use attNum instead of attName

--- src/backend/distributed/commands/index.c
+++ src/backend/distributed/commands/index.c
@@ -785,6 +785,7 @@ PostprocessIndexStmt(Node *node, const char *queryString)
  *
  * ALTER INDEX SET ()
  * ALTER INDEX RESET ()
+ * ALTER INDEX ALTER COLUMN SET STATISTICS
  */
 void
 ErrorIfUnsupportedAlterIndexStmt(AlterTableStmt *alterTableStatement)
@@ -801,6 +802,7 @@ ErrorIfUnsupportedAlterIndexStmt(AlterTableStmt *alterTableStatement)
 			case AT_SetRelOptions:  /* SET (...) */
 			case AT_ResetRelOptions:    /* RESET (...) */
 			case AT_ReplaceRelOptions:  /* replace entire option list */
+			case AT_SetStatistics:  /* SET STATISTICS */
 			{
 				/* this command is supported by Citus */
 				break;
@@ -814,8 +816,8 @@ ErrorIfUnsupportedAlterIndexStmt(AlterTableStmt *alterTableStatement)
 						(errcode(ERRCODE_FEATURE_NOT_SUPPORTED),
 						 errmsg("alter index ... set tablespace ... "
 								"is currently unsupported"),
-						 errdetail("Only RENAME TO, SET (), and RESET () "
-								   "are supported.")));
+						 errdetail("Only RENAME TO, SET (), RESET () "
+								   "and SET STATISTICS are supported.")));
 				return; /* keep compiler happy */
 			}
 		}
