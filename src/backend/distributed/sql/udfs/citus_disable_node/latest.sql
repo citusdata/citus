5a7df80c4ca1ec362a81199d538e04bbd285673b
Ahmet Gedemenli <afgedemenli@gmail.com>
2021-01-13 11:14:10 +0300

    Add missing revoke statement for citus_disable_node

--- src/backend/distributed/sql/udfs/citus_disable_node/latest.sql
+++ src/backend/distributed/sql/udfs/citus_disable_node/latest.sql
@@ -4,3 +4,5 @@ CREATE FUNCTION pg_catalog.citus_disable_node(nodename text, nodeport integer)
 	AS 'MODULE_PATHNAME', $$citus_disable_node$$;
 COMMENT ON FUNCTION pg_catalog.citus_disable_node(nodename text, nodeport integer)
 	IS 'removes node from the cluster temporarily';
+
+REVOKE ALL ON FUNCTION pg_catalog.citus_disable_node(text,int) FROM PUBLIC;
