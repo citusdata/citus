c0f2817b70d9130f0338028f78777d1334b19717
Onur Tirtir <onurcantirtir@gmail.com>
2021-02-03 19:33:54 +0300

    Disallow using alter_table udfs with tables having any identity cols (#4635)
    pg_get_tableschemadef_string doesn't know how to deparse identity
    columns so we cannot reflect those columns when creating table
    from scratch. For this reason, we don't allow using alter_table udfs
    with tables having any identity cols.

--- src/backend/distributed/commands/alter_table.c
+++ src/backend/distributed/commands/alter_table.c
@@ -909,6 +909,20 @@ CreateTableConversion(TableConversionParameters *params)
 		ereport(ERROR, (errmsg("cannot complete operation "
 							   "because no such table exists")));
 	}
+
+	TupleDesc relationDesc = RelationGetDescr(relation);
+	if (RelationUsesIdentityColumns(relationDesc))
+	{
+		/*
+		 * pg_get_tableschemadef_string doesn't know how to deparse identity
+		 * columns so we cannot reflect those columns when creating table
+		 * from scratch. For this reason, error out here.
+		 */
+		ereport(ERROR, (errmsg("cannot complete command because relation "
+							   "%s has identity column",
+							   generate_qualified_relation_name(con->relationId)),
+						errhint("Drop the identity columns and re-try the command")));
+	}
 	relation_close(relation, NoLock);
 	con->distributionKey =
 		BuildDistributionKeyFromColumnName(relation, con->distributionColumn);
