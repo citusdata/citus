5ebd4eac7f8448c2bee77ce35db7557741f67095
Naisila Puka <37271756+naisila@users.noreply.github.com>
2021-02-25 19:52:47 +0300

    Preserve colocation with procedures in alter_distributed_table (#4743)

--- src/test/regress/sql/multi_mx_alter_distributed_table.sql
+++ src/test/regress/sql/multi_mx_alter_distributed_table.sql
@@ -1,6 +1,8 @@
 CREATE SCHEMA mx_alter_distributed_table;
 SET search_path TO mx_alter_distributed_table;
 SET citus.shard_replication_factor TO 1;
+ALTER SEQUENCE pg_catalog.pg_dist_colocationid_seq RESTART 1410000;
+SET citus.replication_model TO 'streaming';
 
 -- test alter_distributed_table UDF
 CREATE TABLE adt_table (a INT, b INT);
@@ -47,6 +49,115 @@ SELECT COUNT(*) FROM adt_table;
 END;
 
 SELECT table_name, citus_table_type, distribution_column, shard_count FROM public.citus_tables WHERE table_name::text = 'adt_table';
+
+-- test procedure colocation is preserved with alter_distributed_table
+CREATE TABLE test_proc_colocation_0 (a float8);
+SELECT create_distributed_table('test_proc_colocation_0', 'a');
+
+CREATE OR REPLACE procedure proc_0(dist_key float8)
+LANGUAGE plpgsql
+AS $$
+DECLARE
+    res INT := 0;
+BEGIN
+    INSERT INTO test_proc_colocation_0 VALUES (dist_key);
+    SELECT count(*) INTO res FROM test_proc_colocation_0;
+    RAISE NOTICE 'Res: %', res;
+    COMMIT;
+END;$$;
+SELECT create_distributed_function('proc_0(float8)', 'dist_key', 'test_proc_colocation_0' );
+
+SELECT logicalrelid, colocationid FROM pg_dist_partition WHERE logicalrelid::regclass::text IN ('test_proc_colocation_0');
+SELECT proname, colocationid FROM pg_proc JOIN citus.pg_dist_object ON pg_proc.oid = citus.pg_dist_object.objid WHERE proname IN ('proc_0');
+
+SET client_min_messages TO DEBUG1;
+CALL proc_0(1.0);
+RESET client_min_messages;
+
+-- shardCount is not null && list_length(colocatedTableList) = 1
+SELECT alter_distributed_table('test_proc_colocation_0', shard_count:= 8);
+
+SET client_min_messages TO DEBUG1;
+CALL proc_0(1.0);
+RESET client_min_messages;
+
+SELECT logicalrelid, colocationid FROM pg_dist_partition WHERE logicalrelid::regclass::text IN ('test_proc_colocation_0');
+SELECT proname, colocationid FROM pg_proc JOIN citus.pg_dist_object ON pg_proc.oid = citus.pg_dist_object.objid WHERE proname IN ('proc_0');
+
+-- colocatewith is not null && list_length(colocatedTableList) = 1
+SELECT alter_distributed_table('test_proc_colocation_0', shard_count:= 4);
+CREATE TABLE test_proc_colocation_1 (a float8);
+SELECT create_distributed_table('test_proc_colocation_1', 'a', colocate_with := 'none');
+SELECT alter_distributed_table('test_proc_colocation_0', colocate_with := 'test_proc_colocation_1');
+
+SET client_min_messages TO DEBUG1;
+CALL proc_0(1.0);
+RESET client_min_messages;
+
+SELECT logicalrelid, colocationid FROM pg_dist_partition WHERE logicalrelid::regclass::text IN ('test_proc_colocation_0');
+SELECT proname, colocationid FROM pg_proc JOIN citus.pg_dist_object ON pg_proc.oid = citus.pg_dist_object.objid WHERE proname IN ('proc_0');
+
+-- shardCount is not null && cascade_to_colocated is true
+SELECT alter_distributed_table('test_proc_colocation_0', shard_count:= 8, cascade_to_colocated := true);
+
+SET client_min_messages TO DEBUG1;
+CALL proc_0(1.0);
+RESET client_min_messages;
+
+SELECT logicalrelid, colocationid FROM pg_dist_partition WHERE logicalrelid::regclass::text IN ('test_proc_colocation_0');
+SELECT proname, colocationid FROM pg_proc JOIN citus.pg_dist_object ON pg_proc.oid = citus.pg_dist_object.objid WHERE proname IN ('proc_0');
+
+-- colocatewith is not null && cascade_to_colocated is true
+SELECT alter_distributed_table('test_proc_colocation_0', shard_count:= 4, cascade_to_colocated := true);
+CREATE TABLE test_proc_colocation_2 (a float8);
+SELECT create_distributed_table('test_proc_colocation_2', 'a', colocate_with := 'none');
+SELECT alter_distributed_table('test_proc_colocation_0', colocate_with := 'test_proc_colocation_2', cascade_to_colocated := true);
+
+SET client_min_messages TO DEBUG1;
+CALL proc_0(1.0);
+RESET client_min_messages;
+
+SELECT logicalrelid, colocationid FROM pg_dist_partition WHERE logicalrelid::regclass::text IN ('test_proc_colocation_0');
+SELECT proname, colocationid FROM pg_proc JOIN citus.pg_dist_object ON pg_proc.oid = citus.pg_dist_object.objid WHERE proname IN ('proc_0');
+
+-- try a case with more than one procedure
+CREATE OR REPLACE procedure proc_1(dist_key float8)
+LANGUAGE plpgsql
+AS $$
+DECLARE
+    res INT := 0;
+BEGIN
+    INSERT INTO test_proc_colocation_0 VALUES (dist_key);
+    SELECT count(*) INTO res FROM test_proc_colocation_0;
+    RAISE NOTICE 'Res: %', res;
+    COMMIT;
+END;$$;
+SELECT create_distributed_function('proc_1(float8)', 'dist_key', 'test_proc_colocation_0' );
+
+SELECT logicalrelid, colocationid FROM pg_dist_partition WHERE logicalrelid::regclass::text IN ('test_proc_colocation_0');
+SELECT proname, colocationid FROM pg_proc JOIN citus.pg_dist_object ON pg_proc.oid = citus.pg_dist_object.objid WHERE proname IN ('proc_0', 'proc_1') ORDER BY proname;
+
+SET client_min_messages TO DEBUG1;
+CALL proc_0(1.0);
+CALL proc_1(2.0);
+RESET client_min_messages;
+
+SELECT alter_distributed_table('test_proc_colocation_0', shard_count:= 8, cascade_to_colocated := true);
+
+SET client_min_messages TO DEBUG1;
+CALL proc_0(1.0);
+CALL proc_1(2.0);
+RESET client_min_messages;
+
+SELECT logicalrelid, colocationid FROM pg_dist_partition WHERE logicalrelid::regclass::text IN ('test_proc_colocation_0');
+SELECT proname, colocationid FROM pg_proc JOIN citus.pg_dist_object ON pg_proc.oid = citus.pg_dist_object.objid WHERE proname IN ('proc_0', 'proc_1') ORDER BY proname;
+
+-- case which shouldn't preserve colocation for now
+-- shardCount is not null && cascade_to_colocated is false
+SELECT alter_distributed_table('test_proc_colocation_0', shard_count:= 18, cascade_to_colocated := false);
+
+SELECT logicalrelid, colocationid FROM pg_dist_partition WHERE logicalrelid::regclass::text IN ('test_proc_colocation_0');
+SELECT proname, colocationid FROM pg_proc JOIN citus.pg_dist_object ON pg_proc.oid = citus.pg_dist_object.objid WHERE proname IN ('proc_0', 'proc_1') ORDER BY proname;
 
 SET client_min_messages TO WARNING;
 DROP SCHEMA mx_alter_distributed_table CASCADE;
