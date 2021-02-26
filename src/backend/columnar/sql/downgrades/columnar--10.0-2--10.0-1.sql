54ac924bef5df2cba4c0a1d7f90073f68523a478
Onur Tirtir <onurcantirtir@gmail.com>
2021-02-26 11:39:19 +0300

    Grant read access for columnar metadata tables to unprivileged user

---
+++ src/backend/columnar/sql/downgrades/columnar--10.0-2--10.0-1.sql
@@ -0,0 +1,5 @@
+/* columnar--10.0-2--10.0-1.sql */
+
+-- revoke read access for columnar metadata tables from unprivileged user
+REVOKE USAGE ON SCHEMA columnar FROM PUBLIC;
+REVOKE SELECT ON ALL tables IN SCHEMA columnar FROM PUBLIC;
