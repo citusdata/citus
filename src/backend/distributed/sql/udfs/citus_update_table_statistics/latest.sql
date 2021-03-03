445291d94bd48afaebdc6f07435b384be0bdfd34
Naisila Puka <37271756+naisila@users.noreply.github.com>
2021-03-03 04:12:30 +0300

    Reimplement citus_update_table_statistics to detect dist. deadlocks (#4752)
    * Reimplement citus_update_table_statistics
    
    * Update stats for the given table not colocation group
    
    * Add tests for reimplemented citus_update_table_statistics
    
    * Use coordinated transaction, merge with citus_shard_sizes functions
    
    * Update the old master_update_table_statistics as well
    
    (cherry picked from commit 2f30614fe343103f2c90c804f7c6e4c9d1cbfc71)

--- src/backend/distributed/sql/udfs/citus_update_table_statistics/latest.sql
+++ src/backend/distributed/sql/udfs/citus_update_table_statistics/latest.sql
@@ -1,17 +1,6 @@
 CREATE FUNCTION pg_catalog.citus_update_table_statistics(relation regclass)
-RETURNS VOID AS $$
-DECLARE
-	colocated_tables regclass[];
-BEGIN
-	SELECT get_colocated_table_array(relation) INTO colocated_tables;
-
-	PERFORM
-		master_update_shard_statistics(shardid)
-	FROM
-		pg_dist_shard
-	WHERE
-		logicalrelid = ANY (colocated_tables);
-END;
-$$ LANGUAGE 'plpgsql';
+	RETURNS VOID
+    LANGUAGE C STRICT
+    AS 'MODULE_PATHNAME', $$citus_update_table_statistics$$;
 COMMENT ON FUNCTION pg_catalog.citus_update_table_statistics(regclass)
-	IS 'updates shard statistics of the given table and its colocated tables';
+	IS 'updates shard statistics of the given table';
