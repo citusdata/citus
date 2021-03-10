18a2120258dede6d651d10f562b6fac1c18ee44b
naisila <nicypp@gmail.com>
2021-03-10 20:56:21 +0300

    Fix upgrade and downgrade paths for master/citus_update_table_statistics

---
+++ src/backend/distributed/sql/citus--10.0-2--10.0-3.sql
@@ -0,0 +1,10 @@
+-- citus--10.0-2--10.0-3
+
+#include "udfs/citus_update_table_statistics/10.0-3.sql"
+
+CREATE OR REPLACE FUNCTION master_update_table_statistics(relation regclass)
+RETURNS VOID
+    LANGUAGE C STRICT
+    AS 'MODULE_PATHNAME', $$citus_update_table_statistics$$;
+COMMENT ON FUNCTION pg_catalog.master_update_table_statistics(regclass)
+	IS 'updates shard statistics of the given table';
---
+++ src/backend/distributed/sql/downgrades/citus--10.1-1--10.0-3.sql
@@ -0,0 +1,3 @@
+-- citus--10.1-1--10.0-2
+-- this is an empty downgrade path since citus--10.0-3--10.1-1.sql is empty for now
+
