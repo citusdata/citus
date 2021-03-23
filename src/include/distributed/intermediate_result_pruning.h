a2bf4dec6591ddaa2d2ec8f15150d5950a3d627f
Nils Dijk <nils@citusdata.com>
2021-03-23 14:24:37 +0100

    LOCAL_HOST_NAME is still used in some specific cases

--- src/include/distributed/intermediate_result_pruning.h
+++ src/include/distributed/intermediate_result_pruning.h
@@ -18,6 +18,13 @@
  */
 #define LOCAL_NODE_ID UINT32_MAX
 
+/*
+ * If you want to connect to the current node use `LocalHostName`, which is a GUC, instead
+ * of the hardcoded loopback hostname. Only if you really need the loopback hostname use
+ * this define.
+ */
+#define LOCAL_HOST_NAME "localhost"
+
 extern bool LogIntermediateResults;
 
 extern List * FindSubPlanUsages(DistributedPlan *plan);
