01dd0d901f4904396b890919e8cfd43ce3ab5f6a
Onur Tirtir <onurcantirtir@gmail.com>
2021-04-29 01:18:20 +0300

    Add more tests for metapage + stripe metadata changes

--- src/test/regress/sql/upgrade_columnar_metapage_after.sql
+++ src/test/regress/sql/upgrade_columnar_metapage_after.sql
@@ -21,10 +21,29 @@ CREATE OR REPLACE FUNCTION columnar_storage_info(
 STRICT
 LANGUAGE c AS 'citus', 'columnar_storage_info';
 
+CREATE VIEW columnar_table_stripe_info AS
+SELECT columnar_table_storageids.relname relname,
+       columnar.stripe.stripe_num stripe_num,
+       columnar.stripe.row_count row_count,
+       columnar.stripe.first_row_number first_row_number
+FROM columnar.stripe,
+(
+  SELECT c.oid relid, c.relname relname, (columnar_storage_info(c.oid)).storage_id relstorageid
+  FROM pg_class c, pg_am a
+  WHERE c.relam = a.oid AND amname = 'columnar'
+) columnar_table_storageids
+WHERE relstorageid = columnar.stripe.storage_id;
+
 SET search_path TO upgrade_columnar_metapage, public;
+
+-- show that first_row_number values are equal to MAX(row_count) * stripe_num + COLUMNAR_FIRST_ROW_NUMBER
+SELECT * FROM columnar_table_stripe_info ORDER BY relname, stripe_num;
 
 -- should work since we upgrade metapages when upgrading schema version
 INSERT INTO columnar_table_1 VALUES (3);
+
+-- state of stripe metadata for columnar_table_1 after post-upgrade insert
+SELECT * FROM columnar_table_stripe_info WHERE relname = 'columnar_table_1' ORDER BY stripe_num;
 
 -- show that all columnar relation's metapage's are upgraded to "2.0"
 SELECT count(*)=0
@@ -37,6 +56,9 @@ WHERE t.version_major != 2 and t.version_minor != 0;
 SELECT columnar_storage_info('columnar_table_1');
 SELECT columnar_storage_info('columnar_table_2');
 
+-- show that no_data_columnar_table also has metapage after upgrade
+SELECT columnar_storage_info('no_data_columnar_table');
+
 -- table is already upgraded, make sure that upgrade_columnar_metapage is no-op
 SELECT citus_internal.upgrade_columnar_storage(c.oid)
 FROM pg_class c, pg_am a
@@ -44,16 +66,8 @@ WHERE c.relam = a.oid AND amname = 'columnar' and relname = 'columnar_table_2';
 
 SELECT columnar_storage_info('columnar_table_2');
 
--- show that we populated first_row_number column when ugrading citus
-SELECT columnar_table_storageids.relname,
-       columnar.stripe.stripe_num,
-       columnar.stripe.row_count,
-       columnar.stripe.first_row_number
-FROM columnar.stripe,
-(
-  SELECT c.oid relid, c.relname relname, (columnar_storage_info(c.oid)).storage_id relstorageid
-  FROM pg_class c, pg_am a
-  WHERE c.relam = a.oid AND amname = 'columnar'
-) columnar_table_storageids
-WHERE relstorageid = columnar.stripe.storage_id
-ORDER BY 1,2;
+VACUUM FULL columnar_table_2;
+
+-- print metapage and stripe metadata after post-upgrade vacuum full
+SELECT columnar_storage_info('columnar_table_2');
+SELECT * FROM columnar_table_stripe_info WHERE relname = 'columnar_table_2' ORDER BY stripe_num;
