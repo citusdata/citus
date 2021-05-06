16c740c93a2e258b49ec8f4d01e5d34431998a8c
jeff-davis <Jeffrey.Davis@microsoft.com>
2021-05-05 22:36:12 -0700

    Columnnar: metapage changes. (#4907)
    * Columnar: introduce columnar storage API.
    
    This new API is responsible for the low-level storage details of
    columnar; translating large reads and writes into individual block
    reads and writes that respect the page headers and emit WAL. It's also
    responsible for the columnar metapage, resource reservations (stripe
    IDs, row numbers, and data), and truncation.
    
    This new API is not used yet, but will be used in subsequent
    forthcoming commits.
    
    * Columnar: add columnar_storage_info() for debugging purposes.
    
    * Columnar: expose ColumnarMetadataNewStorageId().
    
    * Columnar: always initialize metapage at creation time.
    
    This avoids the complexity of dealing with tables where the metapage
    has not yet been initialized.
    
    * Columnar: columnar storage upgrade/downgrade UDFs.
    
    Necessary upgrade/downgrade step so that new code doesn't see an old
    metapage.
    
    * Columnar: improve metadata.c comment.
    
    * Columnar: make ColumnarMetapage internal to the storage API.
    
    Callers should not have or need direct access to the metapage.
    
    * Columnar: perform resource reservation using storage API.
    
    * Columnar: implement truncate using storage API.
    
    * Columnar: implement read/write paths with storage API.
    
    * Columnar: add storage tests.
    
    * Revert "Columnar: don't include stripe reservation locks in lock graph."
    
    This reverts commit c3dcd6b9f8ebf9aa2f4b054c07a31fa821bf66d4.
    
    No longer needed because the columnar storage API takes care of
    concurrency for resource reservation.
    
    * Columnar: remove unnecessary lock when reserving.
    
    No longer necessary because the columnar storage API takes care of
    concurrent resource reservation.
    
    * Add simple upgrade tests for storage/ branch
    
    * fix multi_extension.out
    
    Co-authored-by: Onur Tirtir <onurcantirtir@gmail.com>

---
+++ src/include/columnar/columnar_storage.h
@@ -0,0 +1,54 @@
+/*-------------------------------------------------------------------------
+ *
+ * columnar_storage.h
+ *
+ * Type and function declarations for storage of columnar data in blocks.
+ *
+ * Copyright (c) Citus Data, Inc.
+ *
+ *-------------------------------------------------------------------------
+ */
+
+#ifndef COLUMNAR_STORAGE_H
+#define COLUMNAR_STORAGE_H
+
+#include "postgres.h"
+
+#include "storage/smgr.h"
+#include "utils/rel.h"
+
+
+/*
+ * Logical offsets never fall on the first two physical pages. See
+ * comments in columnar_storage.c.
+ */
+#define ColumnarInvalidLogicalOffset 0
+#define ColumnarFirstLogicalOffset ((BLCKSZ - SizeOfPageHeaderData) * 2)
+#define ColumnarLogicalOffsetIsValid(X) ((X) >= ColumnarFirstLogicalOffset)
+
+
+extern void ColumnarStorageInit(SMgrRelation srel, uint64 storageId);
+extern bool ColumnarStorageIsCurrent(Relation rel);
+extern void ColumnarStorageUpdateCurrent(Relation rel, bool upgrade,
+										 uint64 reservedStripeId,
+										 uint64 reservedRowNumber,
+										 uint64 reservedOffset);
+
+extern uint64 ColumnarStorageGetVersionMajor(Relation rel, bool force);
+extern uint64 ColumnarStorageGetVersionMinor(Relation rel, bool force);
+extern uint64 ColumnarStorageGetStorageId(Relation rel, bool force);
+extern uint64 ColumnarStorageGetReservedStripeId(Relation rel, bool force);
+extern uint64 ColumnarStorageGetReservedRowNumber(Relation rel, bool force);
+extern uint64 ColumnarStorageGetReservedOffset(Relation rel, bool force);
+
+extern uint64 ColumnarStorageReserveData(Relation rel, uint64 amount);
+extern uint64 ColumnarStorageReserveStripe(Relation rel, uint64 nrows,
+										   uint64 *firstRowNumber);
+
+extern void ColumnarStorageRead(Relation rel, uint64 logicalOffset,
+								char *data, uint32 amount);
+extern void ColumnarStorageWrite(Relation rel, uint64 logicalOffset,
+								 char *data, uint32 amount);
+extern bool ColumnarStorageTruncate(Relation rel, uint64 newDataReservation);
+
+#endif /* COLUMNAR_STORAGE_H */
