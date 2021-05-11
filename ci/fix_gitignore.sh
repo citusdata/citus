6b2c9d3567a63a7ed41147691f97643f3c412317
Hanefi Onaldi <Hanefi.Onaldi@microsoft.com>
2021-05-11 11:17:55 +0300

    Remove ignored files from git tree

--- ci/fix_gitignore.sh
+++ ci/fix_gitignore.sh
@@ -18,3 +18,17 @@ ls -1 src/test/regress/input | sed -E "s#(.*)\.source#/\1.sql#" > src/test/regre
 
 # e.g. multi_copy.source -> /multi_copy.out
 ls -1 src/test/regress/output | sed -E "s#(.*)\.source#/\1.out#" > src/test/regress/expected/.gitignore
+
+# Remove all the ignored files from git tree, and error out
+# find all ignored files in git tree, and use quotation marks to prevent word splitting on filenames with spaces in them
+ignored_lines_in_git_tree=$(git ls-files --ignored --exclude-standard | sed 's/.*/"&"/')
+
+if [[ -n $ignored_lines_in_git_tree ]]
+then
+    echo "Ignored files should not be in git tree!"
+    echo "${ignored_lines_in_git_tree}"
+
+    echo "Removing these files from git tree, please review and commit"
+    echo "$ignored_lines_in_git_tree" | xargs git rm -r --cached
+    exit 1
+fi
