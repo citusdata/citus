be7a312dccdd90c28f40cdaa83e8d9f780e64682
Ahmet Gedemenli <afgedemenli@gmail.com>
2021-06-16 17:23:19 +0300

    Set table size to zero if no size is read (#5049)
    * Set table size to zero if no size is read
    
    * Add comment to relation size bug fix
    
    (cherry picked from commit 5115100db01c1cfdb2548e7b7e2466b6719b48d1)

--- src/backend/distributed/metadata/metadata_utility.c
+++ src/backend/distributed/metadata/metadata_utility.c
@@ -644,7 +644,19 @@ DistributedTableSizeOnWorker(WorkerNode *workerNode, Oid relationId,
 	StringInfo tableSizeStringInfo = (StringInfo) linitial(sizeList);
 	char *tableSizeString = tableSizeStringInfo->data;
 
-	*tableSize = SafeStringToUint64(tableSizeString);
+	if (strlen(tableSizeString) > 0)
+	{
+		*tableSize = SafeStringToUint64(tableSizeString);
+	}
+	else
+	{
+		/*
+		 * This means the shard is moved or dropped while citus_total_relation_size is
+		 * being executed. For this case we get an empty string as table size.
+		 * We can take that as zero to prevent any unnecessary errors.
+		 */
+		*tableSize = 0;
+	}
 
 	PQclear(result);
 	ClearResults(connection, failOnError);
