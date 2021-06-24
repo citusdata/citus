f27e960c35ffb9cd1aafb1d79e965c142d028c88
Ahmet Gedemenli <afgedemenli@gmail.com>
2021-06-24 15:13:00 +0300

    Add test for distributed table with custom type

--- src/test/regress/sql/start_stop_metadata_sync.sql
+++ src/test/regress/sql/start_stop_metadata_sync.sql
@@ -5,7 +5,9 @@ SET client_min_messages TO WARNING;
 SET citus.shard_count TO 4;
 SET citus.shard_replication_factor TO 1;
 
-CREATE TABLE distributed_table_1(col int unique);
+CREATE TYPE tt2 AS ENUM ('a', 'b');
+
+CREATE TABLE distributed_table_1(col int unique, b tt2);
 CREATE TABLE "distributed_table_2'! ?._"(col int unique);
 CREATE TABLE distributed_table_3(col int);
 CREATE TABLE reference_table_1(col int unique);
@@ -31,6 +33,7 @@ SELECT start_metadata_sync_to_node('localhost', :worker_1_port);
 
 \c - - - :worker_1_port
 SET search_path TO "start_stop_metadata_sync";
+SELECT * FROM distributed_table_1;
 CREATE VIEW test_view AS SELECT COUNT(*) FROM distributed_table_3;
 CREATE MATERIALIZED VIEW test_matview AS SELECT COUNT(*) FROM distributed_table_3;
 SELECT * FROM test_view;
@@ -41,6 +44,7 @@ SELECT count(*) > 0 FROM pg_class WHERE relname LIKE 'distributed_table__' AND r
 SELECT count(*) > 0 FROM pg_class WHERE relname LIKE 'reference_table__' AND relnamespace IN (SELECT oid FROM pg_namespace WHERE nspname = 'start_stop_metadata_sync');
 \c - - - :master_port
 SET search_path TO "start_stop_metadata_sync";
+SELECT * FROM distributed_table_1;
 SELECT stop_metadata_sync_to_node('localhost', :worker_1_port);
 SELECT * FROM test_view;
 SELECT * FROM test_matview;
@@ -50,6 +54,7 @@ SELECT count(*) > 0 FROM pg_class WHERE relname LIKE 'distributed_table__' AND r
 SELECT count(*) > 0 FROM pg_class WHERE relname LIKE 'reference_table__' AND relnamespace IN (SELECT oid FROM pg_namespace WHERE nspname = 'start_stop_metadata_sync');
 \c - - - :worker_1_port
 SET search_path TO "start_stop_metadata_sync";
+SELECT * FROM distributed_table_1;
 SELECT * FROM test_view;
 SELECT * FROM test_matview;
 SELECT count(*) > 0 FROM pg_dist_node;
@@ -57,6 +62,8 @@ SELECT count(*) > 0 FROM pg_dist_shard;
 SELECT count(*) > 0 FROM pg_class WHERE relname LIKE 'distributed_table__' AND relnamespace IN (SELECT oid FROM pg_namespace WHERE nspname = 'start_stop_metadata_sync');
 SELECT count(*) > 0 FROM pg_class WHERE relname LIKE 'reference_table__' AND relnamespace IN (SELECT oid FROM pg_namespace WHERE nspname = 'start_stop_metadata_sync');
 \c - - - :master_port
+SET search_path TO "start_stop_metadata_sync";
+SELECT * FROM distributed_table_1;
 
 --cleanup
 DROP SCHEMA start_stop_metadata_sync CASCADE;
