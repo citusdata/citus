3dac89ba36a4db276ba9d22fd8a526ea8d00aea3
Ahmet Gedemenli <afgedemenli@gmail.com>
2021-06-24 12:33:24 +0300

    Add test with name that needs to be quoted

--- src/test/regress/sql/start_stop_metadata_sync.sql
+++ src/test/regress/sql/start_stop_metadata_sync.sql
@@ -6,19 +6,19 @@ SET citus.shard_count TO 4;
 SET citus.shard_replication_factor TO 1;
 
 CREATE TABLE distributed_table_1(col int unique);
-CREATE TABLE distributed_table_2(col int unique);
+CREATE TABLE "distributed_table_2'! ?._"(col int unique);
 CREATE TABLE reference_table_1(col int unique);
 CREATE TABLE reference_table_2(col int unique);
 CREATE TABLE local_table(col int unique);
 
-ALTER TABLE distributed_table_1 ADD CONSTRAINT fkey_1 FOREIGN KEY (col) REFERENCES distributed_table_2(col);
-ALTER TABLE distributed_table_2 ADD CONSTRAINT fkey_1 FOREIGN KEY (col) REFERENCES reference_table_1(col);
+ALTER TABLE distributed_table_1 ADD CONSTRAINT fkey_1 FOREIGN KEY (col) REFERENCES "distributed_table_2'! ?._"(col);
+ALTER TABLE "distributed_table_2'! ?._" ADD CONSTRAINT fkey_1 FOREIGN KEY (col) REFERENCES reference_table_1(col);
 ALTER TABLE reference_table_1 ADD CONSTRAINT fkey_1 FOREIGN KEY (col) REFERENCES reference_table_2(col);
 ALTER TABLE local_table ADD CONSTRAINT fkey_1 FOREIGN KEY (col) REFERENCES reference_table_1(col);
 
 SELECT create_reference_table('reference_table_2');
 SELECT create_reference_table('reference_table_1');
-SELECT create_distributed_table('distributed_table_2', 'col');
+SELECT create_distributed_table('"distributed_table_2''! ?._"', 'col');
 SELECT create_distributed_table('distributed_table_1', 'col');
 
 SELECT start_metadata_sync_to_node('localhost', :worker_1_port);
