d3417a5e34b9e94a6b0f249099b170c961b9f801
Marco Slot <marco.slot@gmail.com>
2021-06-25 15:57:04 +0200

    Fix PG upgrade scripts for 9.5

---
+++ src/backend/distributed/sql/citus--9.5-2--9.5-1.sql
@@ -0,0 +1,9 @@
+--
+-- 9.5-1--9.5-2 was added later as a patch to fix a bug in our PG upgrade functions
+--
+-- This script brings users who installed the patch released back to the 9.5-1
+-- upgrade path. We do this via a semantical downgrade since there has already been
+-- introduced new changes in the schema from 9.5-1 to 10.0-1. To make sure we include all
+-- changes made during that version change we decide to use the existing upgrade path from
+-- our later introduced 9.5-1 version.
+--
