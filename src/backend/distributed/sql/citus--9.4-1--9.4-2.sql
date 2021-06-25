bee202aa39d37c2887a33c2c6940a1fc791e722f
Marco Slot <marco.slot@gmail.com>
2021-06-25 16:28:57 +0200

    Fix PG upgrade scripts for 9.4

---
+++ src/backend/distributed/sql/citus--9.4-1--9.4-2.sql
@@ -0,0 +1,3 @@
+-- 9.4-1--9.4-2 was added later as a patch to fix a bug in our PG upgrade functions
+#include "udfs/citus_prepare_pg_upgrade/9.4-2.sql"
+#include "udfs/citus_finish_pg_upgrade/9.4-2.sql"
