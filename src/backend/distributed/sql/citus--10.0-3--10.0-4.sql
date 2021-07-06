d7b90e0804a054ac199fda9d715b60fa54193921
Hanefi Onaldi <Hanefi.Onaldi@microsoft.com>
2021-07-06 17:06:29 +0300

    Remove public schema dependency for 10.0 upgrades
    This commit contains a subset of the changes that should be cherry
    picked to 10.0 releases.
    
    (cherry picked from commit 8e9cc229ffb0b6be25348ecc69795f0e821fa1cd)

--- src/backend/distributed/sql/citus--10.0-3--10.0-4.sql
+++ src/backend/distributed/sql/citus--10.0-3--10.0-4.sql
@@ -1,5 +1,16 @@
+-- citus--10.0-3--10.0-4
+
+-- This migration file aims to fix 2 issues with upgrades on clusters
+
+-- 1. a bug in public schema dependency for citus_tables view.
 --
--- 10.0-3--10.0-4 was added later as a patch to fix a bug in our PG upgrade functions
+-- Users who do not have public schema in their clusters were unable to upgrade
+-- to Citus 10.x due to the citus_tables view that used to be created in public
+-- schema
+
+#include "udfs/citus_tables/10.0-4.sql"
+
+-- 2. a bug in our PG upgrade functions
 --
 -- Users who took the 9.5-2--10.0-1 upgrade path already have the fix, but users
 -- who took the 9.5-1--10.0-1 upgrade path do not. Hence, we repeat the CREATE OR
