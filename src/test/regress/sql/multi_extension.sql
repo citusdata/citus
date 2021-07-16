34f3e0894f9bbbf250fe959f627e51d68183016a
Jelte Fennema <github-tech@jeltef.nl>
2021-07-16 14:31:35 +0200

    Add upgrade and dowgrade tests for Citus 10.2
    It seems we forgot to add this when starting 10.2 development.

--- src/test/regress/sql/multi_extension.sql
+++ src/test/regress/sql/multi_extension.sql
@@ -259,6 +259,16 @@ SELECT * FROM multi_extension.print_extension_changes();
 ALTER EXTENSION citus UPDATE TO '10.1-1';
 SELECT * FROM multi_extension.print_extension_changes();
 
+-- Test downgrade to 10.1-1 from 10.2-1
+ALTER EXTENSION citus UPDATE TO '10.2-1';
+ALTER EXTENSION citus UPDATE TO '10.1-1';
+-- Should be empty result since upgrade+downgrade should be a no-op
+SELECT * FROM multi_extension.print_extension_changes();
+
+-- Snapshot of state at 10.2-1
+ALTER EXTENSION citus UPDATE TO '10.2-1';
+SELECT * FROM multi_extension.print_extension_changes();
+
 DROP TABLE multi_extension.prev_objects, multi_extension.extension_diff;
 
 -- show running version
