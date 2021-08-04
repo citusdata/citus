499cb8c522ac95ce74d0dd7b992a188e353679d4
Ahmet Gedemenli <afgedemenli@gmail.com>
2021-08-04 15:47:41 +0300

    Add the new script to readme

--- ci/check_gucs_are_alphabetically_sorted.sh
+++ ci/check_gucs_are_alphabetically_sorted.sh
@@ -1,3 +1,9 @@
+#!/bin/bash
+
 set -euo pipefail
+# shellcheck disable=SC1091
+source ci/ci_helpers.sh
+
+# extract citus gucs in the form of "citus.X"
 grep -o -E "(\.*\"citus.\w+\")," src/backend/distributed/shared_library_init.c > gucs.out
 sort -c gucs.out
