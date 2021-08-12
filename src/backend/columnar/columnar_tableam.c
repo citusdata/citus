66606843b38332cb96a95a80bd8b0f9fde6be3c4
Halil Ozan Akgul <hozanakgul@gmail.com>
2021-08-12 16:03:58 +0300

    Introduces macros for vacuum options
    VacOptTernaryValue enum is renamed to VacOptValue.
    In the enum there were three values, VACOPT_TERNARY_DEFAULT, VACOPT_TERNARY_DISABLED, and VACOPT_TERNARY_ENABLED
    Now there are four values VACOPTVALUE_UNSPECIFIED, VACOPTVALUE_AUTO, VACOPTVALUE_DISABLED, and VACOPTVALUE_ENABLED
    
    New macros are VacOptValue_compat, VACOPTVALUE_UNSPECIFIED_COMPAT, VACOPTVALUE_DISABLED_COMPAT, and VACOPTVALUE_ENABLED_COMPAT
    The VACOPTVALUE_UNSPECIFIED_COMPAT matches VACOPT_TERNARY_DEFAULT and VACOPTVALUE_UNSPECIFIED. And there are no macro for VACOPTVALUE_AUTO.
    
    Relevant PG commit:
    3499df0dee8c4ea51d264a674df5b5e31991319a

--- src/backend/columnar/columnar_tableam.c
+++ src/backend/columnar/columnar_tableam.c
@@ -883,7 +883,7 @@ columnar_vacuum_rel(Relation rel, VacuumParams *params,
 	int elevel = (params->options & VACOPT_VERBOSE) ? INFO : DEBUG2;
 
 	/* this should have been resolved by vacuum.c until now */
-	Assert(params->truncate != VACOPT_TERNARY_DEFAULT);
+	Assert(params->truncate != VACOPTVALUE_UNSPECIFIED_COMPAT);
 
 	LogRelationStats(rel, elevel);
 
@@ -891,7 +891,7 @@ columnar_vacuum_rel(Relation rel, VacuumParams *params,
 	 * We don't have updates, deletes, or concurrent updates, so all we
 	 * care for now is truncating the unused space at the end of storage.
 	 */
-	if (params->truncate == VACOPT_TERNARY_ENABLED)
+	if (params->truncate == VACOPTVALUE_ENABLED_COMPAT)
 	{
 		TruncateColumnar(rel, elevel);
 	}
