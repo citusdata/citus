c19faedce66be0c40d0d7ad8e09ec137644fdcad
Halil Ozan Akgul <hozanakgul@gmail.com>
2021-08-13 10:42:00 +0300

    Introduces IsReindexWithParam_compat macro
    In ReindexStmt concurrent field is moved to options and then options are converted to params list.
    This macro uses previous fields for previous versions and the new params list with a new function named IsReindexWithParam for PG14
    
    Relevant PG commits:
    844c05abc3f1c1703bf17cf44ab66351ed9711d2
    b5913f6120792465f4394b93c15c2e2ac0c08376

--- src/backend/distributed/commands/index.c
+++ src/backend/distributed/commands/index.c
@@ -529,8 +529,8 @@ PreprocessReindexStmt(Node *node, const char *reindexCommand,
 	{
 		Relation relation = NULL;
 		Oid relationId = InvalidOid;
-		LOCKMODE lockmode = reindexStatement->concurrent ? ShareUpdateExclusiveLock :
-							AccessExclusiveLock;
+		LOCKMODE lockmode = IsReindexWithParam_compat(reindexStatement, "concurrently") ?
+							ShareUpdateExclusiveLock : AccessExclusiveLock;
 		MemoryContext relationContext = NULL;
 
 		Assert(reindexStatement->kind == REINDEX_OBJECT_INDEX ||
@@ -539,7 +539,8 @@ PreprocessReindexStmt(Node *node, const char *reindexCommand,
 		if (reindexStatement->kind == REINDEX_OBJECT_INDEX)
 		{
 			struct ReindexIndexCallbackState state;
-			state.concurrent = reindexStatement->concurrent;
+			state.concurrent = IsReindexWithParam_compat(reindexStatement,
+														 "concurrently");
 			state.locked_table_oid = InvalidOid;
 
 			Oid indOid = RangeVarGetRelidExtended(reindexStatement->relation,
@@ -590,8 +591,10 @@ PreprocessReindexStmt(Node *node, const char *reindexCommand,
 		{
 			DDLJob *ddlJob = palloc0(sizeof(DDLJob));
 			ddlJob->targetRelationId = relationId;
-			ddlJob->concurrentIndexCmd = reindexStatement->concurrent;
-			ddlJob->startNewTransaction = reindexStatement->concurrent;
+			ddlJob->concurrentIndexCmd = IsReindexWithParam_compat(reindexStatement,
+																   "concurrently");
+			ddlJob->startNewTransaction = IsReindexWithParam_compat(reindexStatement,
+																	"concurrently");
 			ddlJob->commandString = reindexCommand;
 			ddlJob->taskList = CreateReindexTaskList(relationId, reindexStatement);
 
