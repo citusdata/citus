f5378a8c9df26b51b8933537e42344644c2e9227
Ahmet Gedemenli <afgedemenli@gmail.com>
2021-08-16 18:27:44 +0300

    CreateCitusLocalTable if the parent is a citus local table

--- src/backend/distributed/commands/table.c
+++ src/backend/distributed/commands/table.c
@@ -360,6 +360,13 @@ PostprocessCreateTableStmtPartitionOf(CreateStmt *createStatement, const
 	 */
 	if (IsCitusTable(parentRelationId))
 	{
+		if(IsCitusTableType(parentRelationId, CITUS_LOCAL_TABLE))
+		{
+			/* if it's a citus local table, we don't need distribution column */
+			CreateCitusLocalTable(relationId, false);
+			return;
+		}
+
 		Var *parentDistributionColumn = DistPartitionKeyOrError(parentRelationId);
 		char parentDistributionMethod = DISTRIBUTE_BY_HASH;
 		char *parentRelationName = generate_qualified_relation_name(parentRelationId);
