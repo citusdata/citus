5442882209de79954113162a68dfbd76d4c7b861
Sait Talha Nisanci <s.talhanisanci@gmail.com>
2021-08-19 15:03:49 +0300

    Add shard relation to pState in localCopy
    CopyFrom expects an AccessShareLock on the table. Instead of adding an
    explicit lock, we do it how postgres is doing, which is to use
    addRangeTableEntryForRelation.
    
    Commit:
    c532d15dddff14b01fe9ef1d465013cb8ef186df

--- src/backend/distributed/commands/local_multi_copy.c
+++ src/backend/distributed/commands/local_multi_copy.c
@@ -209,10 +209,8 @@ DoLocalCopy(StringInfo buffer, Oid relationId, int64 shardId, CopyStmt *copyStat
 	Oid shardOid = GetTableLocalShardOid(relationId, shardId);
 	Relation shard = table_open(shardOid, RowExclusiveLock);
 	ParseState *pState = make_parsestate(NULL);
-
-	/* p_rtable of pState is set so that we can check constraints. */
-	pState->p_rtable = CreateRangeTable(shard, ACL_INSERT);
-
+	(void) addRangeTableEntryForRelation(pState, shard, AccessShareLock,
+										 NULL, false, false);
 	CopyFromState_compat cstate = BeginCopyFrom_compat(pState, shard, NULL, NULL, false,
 													   ReadFromLocalBufferCallback,
 													   copyStatement->attlist,
