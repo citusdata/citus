4517afc97aa662b5cdabfc43ff6d27a22522e948
Sait Talha Nisanci <s.talhanisanci@gmail.com>
2021-08-27 09:46:08 +0300

    Fix style

--- src/test/regress/sql/tablespace.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLESPACE test_tablespace LOCATION '/home/talha/citus/src/test/regress/data/ts0';
-\c - - - :worker_1_port
-CREATE TABLESPACE test_tablespace LOCATION '/home/talha/citus/src/test/regress/data/ts1';
-\c - - - :worker_2_port
-CREATE TABLESPACE test_tablespace LOCATION '/home/talha/citus/src/test/regress/data/ts2';
