e09c90e2ca8f51e53438173f5b3e8c2b8ab17af7
Onder Kalaci <onderkalaci@gmail.com>
2021-09-01 14:35:41 +0200

    Support OUT parameters in procedure pushdown delegation
    In PG 14, procedures can have OUT parameters. In Citus' procedure
    delegation framework, we need to adjust the function expression
    to get the outargs parameters.
    
    Releven PG change:
    https://github.com/postgres/postgres/commit/e56bce5d43789cce95d099554ae9593ada92b3b7

--- src/backend/distributed/commands/call.c
+++ src/backend/distributed/commands/call.c
@@ -11,6 +11,9 @@
  */
 
 #include "postgres.h"
+#include "funcapi.h"
+
+#include "distributed/pg_version_constants.h"
 
 #include "catalog/pg_proc.h"
 #include "commands/defrem.h"
@@ -46,6 +49,9 @@
 static bool CallFuncExprRemotely(CallStmt *callStmt,
 								 DistObjectCacheEntry *procedure,
 								 FuncExpr *funcExpr, DestReceiver *dest);
+#if PG_VERSION_NUM >= PG_VERSION_14
+static bool FunctionHasOutOnlyParameter(Oid functionOid);
+#endif
 
 /*
  * CallDistributedProcedureRemotely calls a stored procedure on the worker if possible.
@@ -142,12 +148,26 @@ CallFuncExprRemotely(CallStmt *callStmt, DistObjectCacheEntry *procedure,
 		return false;
 	}
 
+
+#if PG_VERSION_NUM >= PG_VERSION_14
+
+	/*
+	 * We might need to add outargs to the funcExpr->args so that they can
+	 * be pushed down. We can implement in the future.
+	 */
+	if (FunctionHasOutOnlyParameter(funcExpr->funcid))
+	{
+		ereport(DEBUG1, (errmsg("not pushing down procedures with OUT parameters")));
+		return false;
+	}
+#endif
+
 	ereport(DEBUG1, (errmsg("pushing down the procedure")));
 
 	/* build remote command with fully qualified names */
 	StringInfo callCommand = makeStringInfo();
-	appendStringInfo(callCommand, "CALL %s", pg_get_rule_expr((Node *) funcExpr));
 
+	appendStringInfo(callCommand, "CALL %s", pg_get_rule_expr((Node *) funcExpr));
 	{
 		Tuplestorestate *tupleStore = tuplestore_begin_heap(true, false, work_mem);
 		TupleDesc tupleDesc = CallStmtResultDesc(callStmt);
@@ -207,3 +227,53 @@ CallFuncExprRemotely(CallStmt *callStmt, DistObjectCacheEntry *procedure,
 
 	return true;
 }
+
+
+#if PG_VERSION_NUM >= PG_VERSION_14
+
+/*
+ * FunctionHasOutOnlyParameter is a helper function which takes
+ * a function Oid and returns true if the input function has at least
+ * one OUT parameter.
+ */
+static bool
+FunctionHasOutOnlyParameter(Oid functionOid)
+{
+	Oid *argTypes = NULL;
+	char **argNames = NULL;
+	char *argModes = NULL;
+
+	HeapTuple proctup = SearchSysCache1(PROCOID, ObjectIdGetDatum(functionOid));
+	if (!HeapTupleIsValid(proctup))
+	{
+		elog(ERROR, "cache lookup failed for function %u", functionOid);
+	}
+
+	int numberOfArgs = get_func_arg_info(proctup, &argTypes, &argNames, &argModes);
+
+	if (argModes == NULL)
+	{
+		/* short circuit, all arguments are IN */
+		ReleaseSysCache(proctup);
+
+		return false;
+	}
+
+	int argIndex = 0;
+	for (; argIndex < numberOfArgs; ++argIndex)
+	{
+		if (argModes[argIndex] == PROARGMODE_OUT)
+		{
+			ReleaseSysCache(proctup);
+
+			return true;
+		}
+	}
+
+	ReleaseSysCache(proctup);
+
+	return false;
+}
+
+
+#endif
