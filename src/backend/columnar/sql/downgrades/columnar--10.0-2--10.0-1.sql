e5e5228675396e2c4af920632f7edc75ae7e9e7a
Hanefi Onaldi <Hanefi.Onaldi@microsoft.com>
2021-09-09 11:05:37 +0300

    Check columnar sql files for C comments

--- src/backend/columnar/sql/downgrades/columnar--10.0-2--10.0-1.sql
+++ src/backend/columnar/sql/downgrades/columnar--10.0-2--10.0-1.sql
@@ -1,4 +1,4 @@
-/* columnar--10.0-2--10.0-1.sql */
+-- columnar--10.0-2--10.0-1.sql
 
 -- revoke read access for columnar metadata tables from unprivileged user
 REVOKE USAGE ON SCHEMA columnar FROM PUBLIC;
