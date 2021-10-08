ebb401e838da5e6d16f5920a5dd01ffd0b2f8c5c
Jelte Fennema <github-tech@jeltef.nl>
2021-10-08 10:59:17 +0200

    Add a bit more tests

--- src/test/regress/sql/create_test.sql
+++ src/test/regress/sql/create_test.sql
@@ -4,5 +4,8 @@ SET search_path to "te;'st", public;
 CREATE TABLE dist(a int, b int);
 SELECT create_distributed_table('dist', 'a');
 
+CREATE TABLE dist2(a int, b int);
+SELECT create_distributed_table('dist2', 'a');
+
 CREATE table ref(a int, b int);
 SELECT create_reference_table('ref');
