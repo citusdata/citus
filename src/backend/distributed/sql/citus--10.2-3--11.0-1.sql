9cbb33a4488f611798a8a3f0a0bd2cd52dabb12c
Ahmet Gedemenli <afgedemenli@gmail.com>
2021-10-18 20:18:00 +0300

    Move new column to version 11

--- src/backend/distributed/sql/citus--10.2-3--11.0-1.sql
+++ src/backend/distributed/sql/citus--10.2-3--11.0-1.sql
@@ -6,5 +6,6 @@
 #include "udfs/fix_all_partition_shard_index_names/11.0-1.sql"
 #include "udfs/worker_fix_partition_shard_index_names/11.0-1.sql"
 
+ALTER TABLE pg_catalog.pg_dist_partition ADD COLUMN autoconverted boolean;
 DROP FUNCTION IF EXISTS pg_catalog.master_apply_delete_command(text);
 DROP FUNCTION pg_catalog.master_get_table_metadata(text);
