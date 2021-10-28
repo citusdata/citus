e9c9e181f885fb5d6983d58cf5099171f37f0ccc
Ahmet Gedemenli <afgedemenli@gmail.com>
2021-10-28 11:51:00 +0300

    Remove alternative output

--- src/test/regress/sql/upgrade_basic_after.sql
+++ src/test/regress/sql/upgrade_basic_after.sql
@@ -1,7 +1,7 @@
 SET search_path TO upgrade_basic, public, pg_catalog;
 BEGIN;
 
-SELECT * FROM pg_indexes WHERE schemaname = 'upgrade_basic' ORDER BY tablename;
+SELECT * FROM pg_indexes WHERE schemaname = 'upgrade_basic' and tablename NOT LIKE "r_%" ORDER BY tablename;
 
 SELECT nextval('pg_dist_shardid_seq') = MAX(shardid)+1 FROM pg_dist_shard;
 SELECT nextval('pg_dist_placement_placementid_seq') = MAX(placementid)+1 FROM pg_dist_placement;
