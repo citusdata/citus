19f28eabae41a42e295391b0ece35ee732f9548c
Talha Nisanci <s.talhanisanci@gmail.com>
2021-11-03 16:17:36 +0300

    Fix citus upgrade local run issues (#5414)
    This PR is fixing 2 separate issues related to the local run of citus upgrade tests.
    
    https://github.com/citusdata/citus/pull/5414/commits/d3e7c825ab1f62837dfcff288696cbb82f16bc42 fixes the issue that, with our new testing infrastructure, we moved/renamed some of existing folders. This created a problem for local runs of citus upgrade tests since some paths were sensitive to such changes. This commit tries to make it more generic so that this issue is less likely to happen in the future, while also fixing the current issue.
    
    https://github.com/citusdata/citus/pull/5414/commits/93de6b60c35d79e99076de60f81e2bab41e32333 we are fixing an issue that a new environment variable was added for citus upgrade tests, which is defined in the CI. https://github.com/citusdata/citus/blob/0cb51f8c37f57039c69512963e90e4be3f7ddee6/.circleci/config.yml#L294
    This environment variable wasn't set in our local runs hence it would create problems. Instead of defining this environment variable in the local run, we change the citus_upgrade run command to use an existing env variable, which is now also set in the CI.

--- src/test/regress/citus_tests/upgrade/generate_citus_tarballs.sh
+++ src/test/regress/citus_tests/upgrade/generate_citus_tarballs.sh
@@ -6,56 +6,56 @@ citus_old_version=$1
 
 base="$(pwd)"
 
-
 install_citus_and_tar() {
-  # do everything in a subdirectory to avoid clutter in current directory
-  mkdir -p "${builddir}" && cd "${builddir}"
+    # do everything in a subdirectory to avoid clutter in current directory
+    mkdir -p "${builddir}" && cd "${builddir}"
 
-  "${citus_dir}/configure" --without-libcurl
+    "${citus_dir}/configure" --without-libcurl
 
-  installdir="${builddir}/install"
-  make "-j$(nproc)" && mkdir -p "${installdir}" && make DESTDIR="${installdir}" install
+    installdir="${builddir}/install"
+    make "-j$(nproc)" && mkdir -p "${installdir}" && make DESTDIR="${installdir}" install
 
-  cd "${installdir}" && find . -type f -print > "${builddir}/files.lst"
+    cd "${installdir}" && find . -type f -print >"${builddir}/files.lst"
 
-  tar cvf "${basedir}/install-citus${citus_version}.tar" $(cat "${builddir}"/files.lst)
-  mv "${basedir}/install-citus${citus_version}.tar" "${base}/install-citus${citus_version}.tar"
+    tar cvf "${basedir}/install-citus${citus_version}.tar" $(cat "${builddir}"/files.lst)
+    mv "${basedir}/install-citus${citus_version}.tar" "${base}/install-citus${citus_version}.tar"
 
-  cd "${builddir}" && rm -rf install files.lst && make clean
+    cd "${builddir}" && rm -rf install files.lst && make clean
 }
 
 build_current() {
-  citus_version="$1"
-  basedir="${base}/${citus_version}"
+    citus_version="$1"
+    basedir="${base}/${citus_version}"
 
-  mkdir -p "${basedir}"
-  citus_repo="${base}/../../../.."
+    mkdir -p "${basedir}"
+    citus_repo=$(git rev-parse --show-toplevel)
 
-  cd "$citus_repo" && cp -R . /tmp/citus_copy
-  mv /tmp/citus_copy "${basedir}/citus_${citus_version}"
-  builddir="${basedir}/build"
-  cd "${basedir}"
+    cd "$citus_repo" && cp -R . /tmp/citus_copy
+    # https://stackoverflow.com/questions/957928/is-there-a-way-to-get-the-git-root-directory-in-one-command
+    mv /tmp/citus_copy "${basedir}/citus_${citus_version}"
+    builddir="${basedir}/build"
+    cd "${basedir}"
 
-  citus_dir=${basedir}/citus_$citus_version
+    citus_dir=${basedir}/citus_$citus_version
 
-  make -C "${citus_dir}" clean
-  cd "${citus_dir}"
-  ./configure --without-libcurl
+    make -C "${citus_dir}" clean
+    cd "${citus_dir}"
+    ./configure --without-libcurl
 
-  install_citus_and_tar
+    install_citus_and_tar
 }
 
 build_ext() {
-  citus_version="$1"
-  basedir="${base}/${citus_version}"
+    citus_version="$1"
+    basedir="${base}/${citus_version}"
 
-  mkdir -p "${basedir}"
-  cd "${basedir}"
-  citus_dir=${basedir}/citus_$citus_version
-  git clone --branch "$citus_version" https://github.com/citusdata/citus.git --depth 1 citus_"$citus_version"
-  builddir="${basedir}/build"
+    mkdir -p "${basedir}"
+    cd "${basedir}"
+    citus_dir=${basedir}/citus_$citus_version
+    git clone --branch "$citus_version" https://github.com/citusdata/citus.git --depth 1 citus_"$citus_version"
+    builddir="${basedir}/build"
 
-  install_citus_and_tar
+    install_citus_and_tar
 }
 
 build_current "master"
