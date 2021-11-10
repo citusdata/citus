37623bceba43f26bf8932bb59675de622d0d0832
Hanefi Onaldi <Hanefi.Onaldi@microsoft.com>
2021-11-10 22:03:36 +0300

    Update migration paths from 10.2 to 11.0
    We recently introduced a set of patches to 10.2, and introduced 10.2-4
    migration version. This migration version only resides on `release-10.2`
    branch, and is missing on our default branch. This creates a problem
    because we do not have a valid migration path from 10.2 to latest 11.0.
    
    To remedy this issue, I copied the relevant migration files from
    `release-10.2` branch, and renamed some of our migration files on
    default branch to make sure we have a linear upgrade path.

---
+++ src/backend/distributed/sql/citus--10.2-3--10.2-4.sql
@@ -0,0 +1,7 @@
+-- citus--10.2-3--10.2-4
+
+-- bump version to 10.2-4
+
+#include "udfs/fix_partition_shard_index_names/10.2-4.sql"
+#include "udfs/fix_all_partition_shard_index_names/10.2-4.sql"
+#include "udfs/worker_fix_partition_shard_index_names/10.2-4.sql"
