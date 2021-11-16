042d45b263af66d35f707a274c5f02c667f2c36f
Ahmet Gedemenli <afgedemenli@gmail.com>
2021-11-16 20:44:21 +0300

    Propagate foreign server ops

---
+++ src/backend/distributed/commands/foreign_server.c
@@ -0,0 +1,364 @@
+/*-------------------------------------------------------------------------
+ *
+ * foreign_server.c
+ *    Commands for FOREIGN SERVER statements.
+ *
+ * Copyright (c) Citus Data, Inc.
+ *
+ *-------------------------------------------------------------------------
+ */
+
+#include "postgres.h"
+
+#include "catalog/pg_foreign_server.h"
+#include "distributed/commands/utility_hook.h"
+#include "distributed/commands.h"
+#include "distributed/deparser.h"
+#include "distributed/listutils.h"
+#include "distributed/metadata/distobject.h"
+#include "distributed/metadata_sync.h"
+#include "distributed/worker_transaction.h"
+#include "foreign/foreign.h"
+#include "nodes/makefuncs.h"
+#include "nodes/parsenodes.h"
+#include "nodes/primnodes.h"
+
+static Node * RecreateForeignServerStmt(Oid serverId);
+static bool NameListHasDistributedServer(List *serverNames);
+static ObjectAddress GetObjectAddressByServerName(char *serverName, bool missing_ok);
+
+
+/*
+ * PreprocessCreateForeignServerStmt is called during the planning phase for
+ * CREATE SERVER.
+ */
+List *
+PreprocessCreateForeignServerStmt(Node *node, const char *queryString,
+								  ProcessUtilityContext processUtilityContext)
+{
+	if (!ShouldPropagate())
+	{
+		return NIL;
+	}
+
+	EnsureCoordinator();
+
+	char *sql = DeparseTreeNode(node);
+
+	/* to prevent recursion with mx we disable ddl propagation */
+	List *commands = list_make3(DISABLE_DDL_PROPAGATION,
+								(void *) sql,
+								ENABLE_DDL_PROPAGATION);
+
+	return NodeDDLTaskList(NON_COORDINATOR_NODES, commands);
+}
+
+
+/*
+ * PreprocessAlterForeignServerStmt is called during the planning phase for
+ * ALTER SERVER .. OPTIONS ..
+ */
+List *
+PreprocessAlterForeignServerStmt(Node *node, const char *queryString,
+								 ProcessUtilityContext processUtilityContext)
+{
+	AlterForeignServerStmt *stmt = castNode(AlterForeignServerStmt, node);
+
+	ObjectAddress address = GetObjectAddressByServerName(stmt->servername, false);
+
+	if (!ShouldPropagateObject(&address))
+	{
+		return NIL;
+	}
+
+	EnsureCoordinator();
+
+	char *sql = DeparseTreeNode(node);
+
+	/* to prevent recursion with mx we disable ddl propagation */
+	List *commands = list_make3(DISABLE_DDL_PROPAGATION,
+								(void *) sql,
+								ENABLE_DDL_PROPAGATION);
+
+	return NodeDDLTaskList(NON_COORDINATOR_NODES, commands);
+}
+
+
+/*
+ * PreprocessRenameForeignServerStmt is called during the planning phase for
+ * ALTER SERVER RENAME.
+ */
+List *
+PreprocessRenameForeignServerStmt(Node *node, const char *queryString,
+								  ProcessUtilityContext processUtilityContext)
+{
+	RenameStmt *stmt = castNode(RenameStmt, node);
+	Assert(stmt->renameType == OBJECT_FOREIGN_SERVER);
+
+	ObjectAddress address = GetObjectAddressByServerName(strVal(stmt->object), false);
+
+	/* filter distributed servers */
+	if (!ShouldPropagateObject(&address))
+	{
+		return NIL;
+	}
+
+	EnsureCoordinator();
+
+	char *sql = DeparseTreeNode(node);
+
+	/* to prevent recursion with mx we disable ddl propagation */
+	List *commands = list_make3(DISABLE_DDL_PROPAGATION,
+								(void *) sql,
+								ENABLE_DDL_PROPAGATION);
+
+	return NodeDDLTaskList(NON_COORDINATOR_NODES, commands);
+}
+
+
+/*
+ * PreprocessAlterForeignServerOwnerStmt is called during the planning phase for
+ * ALTER SERVER .. OWNER TO.
+ */
+List *
+PreprocessAlterForeignServerOwnerStmt(Node *node, const char *queryString,
+									  ProcessUtilityContext processUtilityContext)
+{
+	AlterOwnerStmt *stmt = castNode(AlterOwnerStmt, node);
+	Assert(stmt->objectType == OBJECT_FOREIGN_SERVER);
+
+	ObjectAddress address = GetObjectAddressByServerName(strVal(stmt->object), false);
+
+	/* filter distributed servers */
+	if (!ShouldPropagateObject(&address))
+	{
+		return NIL;
+	}
+
+	EnsureCoordinator();
+
+	char *sql = DeparseTreeNode(node);
+
+	/* to prevent recursion with mx we disable ddl propagation */
+	List *commands = list_make3(DISABLE_DDL_PROPAGATION,
+								(void *) sql,
+								ENABLE_DDL_PROPAGATION);
+
+	return NodeDDLTaskList(NON_COORDINATOR_NODES, commands);
+}
+
+
+/*
+ * PreprocessDropForeignServerStmt is called during the planning phase for
+ * DROP SERVER.
+ */
+List *
+PreprocessDropForeignServerStmt(Node *node, const char *queryString,
+								ProcessUtilityContext processUtilityContext)
+{
+	DropStmt *stmt = castNode(DropStmt, node);
+	Assert(stmt->removeType == OBJECT_FOREIGN_SERVER);
+
+	bool includesDistributedServer = NameListHasDistributedServer(stmt->objects);
+
+	if (!includesDistributedServer)
+	{
+		return NIL;
+	}
+
+	if (list_length(stmt->objects) > 1)
+	{
+		ereport(ERROR, (errcode(ERRCODE_FEATURE_NOT_SUPPORTED),
+						errmsg("cannot drop distributed server with other servers"),
+						errhint("Try dropping each object in a separate DROP command")));
+	}
+
+	if (!ShouldPropagate())
+	{
+		return NIL;
+	}
+
+	EnsureCoordinator();
+
+	Assert(list_length(stmt->objects) == 1);
+
+	Value *serverValue = linitial(stmt->objects);
+	ObjectAddress address = GetObjectAddressByServerName(strVal(serverValue), false);
+
+	/* unmark distributed server */
+	UnmarkObjectDistributed(&address);
+
+	const char *deparsedStmt = DeparseTreeNode((Node *) stmt);
+
+	/*
+	 * To prevent recursive propagation in mx architecture, we disable ddl
+	 * propagation before sending the command to workers.
+	 */
+	List *commands = list_make3(DISABLE_DDL_PROPAGATION,
+								(void *) deparsedStmt,
+								ENABLE_DDL_PROPAGATION);
+
+	return NodeDDLTaskList(NON_COORDINATOR_NODES, commands);
+}
+
+
+/*
+ * PostprocessCreateForeignServerStmt is called after a CREATE SERVER command has
+ * been executed by standard process utility.
+ */
+List *
+PostprocessCreateForeignServerStmt(Node *node, const char *queryString)
+{
+	bool missingOk = false;
+	ObjectAddress address = GetObjectAddressFromParseTree(node, missingOk);
+	EnsureDependenciesExistOnAllNodes(&address);
+
+	return NIL;
+}
+
+
+/*
+ * PostprocessAlterForeignServerOwnerStmt is called after a ALTER SERVER OWNER command
+ * has been executed by standard process utility.
+ */
+List *
+PostprocessAlterForeignServerOwnerStmt(Node *node, const char *queryString)
+{
+	bool missingOk = false;
+	ObjectAddress address = GetObjectAddressFromParseTree(node, missingOk);
+	EnsureDependenciesExistOnAllNodes(&address);
+
+	return NIL;
+}
+
+
+/*
+ * CreateForeignServerStmtObjectAddress finds the ObjectAddress for the server
+ * that is created by given CreateForeignServerStmt. If missingOk is false and if
+ * the server does not exist, then it errors out.
+ *
+ * Never returns NULL, but the objid in the address can be invalid if missingOk
+ * was set to true.
+ */
+ObjectAddress
+CreateForeignServerStmtObjectAddress(Node *node, bool missing_ok)
+{
+	CreateForeignServerStmt *stmt = castNode(CreateForeignServerStmt, node);
+
+	return GetObjectAddressByServerName(stmt->servername, missing_ok);
+}
+
+
+/*
+ * AlterForeignServerOwnerStmtObjectAddress finds the ObjectAddress for the server
+ * given in AlterOwnerStmt. If missingOk is false and if
+ * the server does not exist, then it errors out.
+ *
+ * Never returns NULL, but the objid in the address can be invalid if missingOk
+ * was set to true.
+ */
+ObjectAddress
+AlterForeignServerOwnerStmtObjectAddress(Node *node, bool missing_ok)
+{
+	AlterOwnerStmt *stmt = castNode(AlterOwnerStmt, node);
+	char *serverName = strVal(stmt->object);
+
+	return GetObjectAddressByServerName(serverName, missing_ok);
+}
+
+
+/*
+ * GetForeignServerCreateDDLCommand returns a list that includes the CREATE SERVER
+ * command that would recreate the given server on a new node.
+ */
+List *
+GetForeignServerCreateDDLCommand(Oid serverId)
+{
+	/* generate a statement for creation of the server in "if not exists" construct */
+	Node *stmt = RecreateForeignServerStmt(serverId);
+
+	/* capture ddl command for the create statement */
+	const char *ddlCommand = DeparseTreeNode(stmt);
+
+	List *ddlCommands = list_make1((void *) ddlCommand);
+
+	return ddlCommands;
+}
+
+
+/*
+ * RecreateForeignServerStmt returns a parsetree for a CREATE SERVER statement
+ * that would recreate the given server on a new node.
+ */
+static Node *
+RecreateForeignServerStmt(Oid serverId)
+{
+	ForeignServer *server = GetForeignServer(serverId);
+
+	CreateForeignServerStmt *createStmt = makeNode(CreateForeignServerStmt);
+
+	/* set server name and if_not_exists fields */
+	createStmt->servername = pstrdup(server->servername);
+	createStmt->if_not_exists = true;
+
+	/* set foreign data wrapper */
+	ForeignDataWrapper *fdw = GetForeignDataWrapper(server->fdwid);
+	createStmt->fdwname = pstrdup(fdw->fdwname);
+
+	/* set all fields using the existing server */
+	if (server->servertype != NULL)
+	{
+		createStmt->servertype = pstrdup(server->servertype);
+	}
+
+	if (server->serverversion != NULL)
+	{
+		createStmt->version = pstrdup(server->serverversion);
+	}
+
+	createStmt->options = NIL;
+
+	int location = -1;
+	DefElem *option = NULL;
+	foreach_ptr(option, server->options)
+	{
+		DefElem *copyOption = makeDefElem(option->defname, option->arg, location);
+		createStmt->options = lappend(createStmt->options, copyOption);
+	}
+
+	return (Node *) createStmt;
+}
+
+
+/*
+ * NameListHasDistributedServer takes a namelist of servers and returns true if at least
+ * one of them is distributed. Returns false otherwise.
+ */
+static bool
+NameListHasDistributedServer(List *serverNames)
+{
+	Value *serverValue = NULL;
+	foreach_ptr(serverValue, serverNames)
+	{
+		ObjectAddress address = GetObjectAddressByServerName(strVal(serverValue), false);
+
+		if (IsObjectDistributed(&address))
+		{
+			return true;
+		}
+	}
+
+	return false;
+}
+
+
+static ObjectAddress
+GetObjectAddressByServerName(char *serverName, bool missing_ok)
+{
+	ForeignServer *server = GetForeignServerByName(serverName, missing_ok);
+	Oid serverOid = server->serverid;
+	ObjectAddress address = { 0 };
+	ObjectAddressSet(address, ForeignServerRelationId, serverOid);
+
+	return address;
+}
---
+++ src/backend/distributed/deparser/deparse_foreign_server_stmts.c
@@ -0,0 +1,277 @@
+/*-------------------------------------------------------------------------
+ *
+ * deparse_foreign_server_stmts.c
+ *	  All routines to deparse foreign server statements.
+ *
+ * Copyright (c) Citus Data, Inc.
+ *
+ *-------------------------------------------------------------------------
+ */
+#include "postgres.h"
+
+#include "commands/defrem.h"
+#include "distributed/citus_ruleutils.h"
+#include "distributed/deparser.h"
+#include "distributed/listutils.h"
+#include "distributed/relay_utility.h"
+#include "lib/stringinfo.h"
+#include "nodes/nodes.h"
+#include "utils/builtins.h"
+
+static void AppendCreateForeignServerStmt(StringInfo buf, CreateForeignServerStmt *stmt);
+static void AppendAlterForeignServerStmt(StringInfo buf, AlterForeignServerStmt *stmt);
+static void AppendAlterForeignServerOptions(StringInfo buf, AlterForeignServerStmt *stmt);
+static void AppendAlterForeignServerRenameStmt(StringInfo buf, RenameStmt *stmt);
+static void AppendAlterForeignServerOwnerStmt(StringInfo buf, AlterOwnerStmt *stmt);
+static void AppendDropForeignServerStmt(StringInfo buf, DropStmt *stmt);
+static void AppendServerNames(StringInfo buf, DropStmt *stmt);
+static void AppendBehavior(StringInfo buf, DropStmt *stmt);
+static char * GetDefElemActionString(DefElemAction action);
+
+char *
+DeparseCreateForeignServerStmt(Node *node)
+{
+	CreateForeignServerStmt *stmt = castNode(CreateForeignServerStmt, node);
+
+	StringInfoData str;
+	initStringInfo(&str);
+
+	AppendCreateForeignServerStmt(&str, stmt);
+
+	return str.data;
+}
+
+
+char *
+DeparseAlterForeignServerStmt(Node *node)
+{
+	AlterForeignServerStmt *stmt = castNode(AlterForeignServerStmt, node);
+
+	StringInfoData str;
+	initStringInfo(&str);
+
+	AppendAlterForeignServerStmt(&str, stmt);
+
+	return str.data;
+}
+
+
+char *
+DeparseAlterForeignServerRenameStmt(Node *node)
+{
+	RenameStmt *stmt = castNode(RenameStmt, node);
+
+	Assert(stmt->renameType == OBJECT_FOREIGN_SERVER);
+
+	StringInfoData str;
+	initStringInfo(&str);
+
+	AppendAlterForeignServerRenameStmt(&str, stmt);
+
+	return str.data;
+}
+
+
+char *
+DeparseAlterForeignServerOwnerStmt(Node *node)
+{
+	AlterOwnerStmt *stmt = castNode(AlterOwnerStmt, node);
+
+	Assert(stmt->objectType == OBJECT_FOREIGN_SERVER);
+
+	StringInfoData str;
+	initStringInfo(&str);
+
+	AppendAlterForeignServerOwnerStmt(&str, stmt);
+
+	return str.data;
+}
+
+
+char *
+DeparseDropForeignServerStmt(Node *node)
+{
+	DropStmt *stmt = castNode(DropStmt, node);
+
+	Assert(stmt->removeType == OBJECT_FOREIGN_SERVER);
+
+	StringInfoData str;
+	initStringInfo(&str);
+
+	AppendDropForeignServerStmt(&str, stmt);
+
+	return str.data;
+}
+
+
+static void
+AppendCreateForeignServerStmt(StringInfo buf, CreateForeignServerStmt *stmt)
+{
+	appendStringInfoString(buf, "CREATE SERVER ");
+
+	if (stmt->if_not_exists)
+	{
+		appendStringInfoString(buf, "IF NOT EXISTS ");
+	}
+
+	appendStringInfo(buf, "%s ", quote_identifier(stmt->servername));
+
+	if (stmt->servertype)
+	{
+		appendStringInfo(buf, "TYPE %s ", quote_literal_cstr(stmt->servertype));
+	}
+
+	if (stmt->version)
+	{
+		appendStringInfo(buf, "VERSION %s ", quote_literal_cstr(stmt->version));
+	}
+
+	appendStringInfo(buf, "FOREIGN DATA WRAPPER %s ", quote_identifier(stmt->fdwname));
+
+	AppendOptionListToString(buf, stmt->options);
+}
+
+
+static void
+AppendAlterForeignServerStmt(StringInfo buf, AlterForeignServerStmt *stmt)
+{
+	appendStringInfo(buf, "ALTER SERVER %s ", quote_identifier(stmt->servername));
+
+	if (stmt->has_version)
+	{
+		appendStringInfo(buf, "VERSION %s ", quote_literal_cstr(stmt->version));
+	}
+
+	AppendAlterForeignServerOptions(buf, stmt);
+}
+
+
+static void
+AppendAlterForeignServerOptions(StringInfo buf, AlterForeignServerStmt *stmt)
+{
+	if (list_length(stmt->options) <= 0)
+	{
+		return;
+	}
+
+	appendStringInfoString(buf, "OPTIONS (");
+
+	DefElemAction action = DEFELEM_UNSPEC;
+	DefElem *def = NULL;
+	foreach_ptr(def, stmt->options)
+	{
+		if (def->defaction != DEFELEM_UNSPEC)
+		{
+			action = def->defaction;
+			char *actionString = GetDefElemActionString(action);
+			appendStringInfo(buf, "%s ", actionString);
+		}
+
+		appendStringInfo(buf, "%s", quote_identifier(def->defname));
+
+		if (action != DEFELEM_DROP)
+		{
+			const char *value = quote_literal_cstr(defGetString(def));
+			appendStringInfo(buf, " %s", value);
+		}
+
+		if (def != llast(stmt->options))
+		{
+			appendStringInfoString(buf, ", ");
+		}
+	}
+
+	appendStringInfoString(buf, ")");
+}
+
+
+static void
+AppendAlterForeignServerRenameStmt(StringInfo buf, RenameStmt *stmt)
+{
+	appendStringInfo(buf, "ALTER SERVER %s RENAME TO %s",
+					 quote_identifier(strVal(stmt->object)),
+					 quote_identifier(stmt->newname));
+}
+
+
+static void
+AppendAlterForeignServerOwnerStmt(StringInfo buf, AlterOwnerStmt *stmt)
+{
+	const char *servername = quote_identifier(strVal(stmt->object));
+	appendStringInfo(buf, "ALTER SERVER %s OWNER TO ", servername);
+
+	appendStringInfo(buf, "%s", RoleSpecString(stmt->newowner, true));
+}
+
+
+static void
+AppendDropForeignServerStmt(StringInfo buf, DropStmt *stmt)
+{
+	appendStringInfoString(buf, "DROP SERVER ");
+
+	if (stmt->missing_ok)
+	{
+		appendStringInfoString(buf, "IF EXISTS ");
+	}
+
+	AppendServerNames(buf, stmt);
+
+	AppendBehavior(buf, stmt);
+}
+
+
+static void
+AppendServerNames(StringInfo buf, DropStmt *stmt)
+{
+	Value *serverValue = NULL;
+	foreach_ptr(serverValue, stmt->objects)
+	{
+		const char *serverString = quote_identifier(strVal(serverValue));
+		appendStringInfo(buf, "%s", serverString);
+
+		if (serverValue != llast(stmt->objects))
+		{
+			appendStringInfoString(buf, ", ");
+		}
+	}
+}
+
+
+static void
+AppendBehavior(StringInfo buf, DropStmt *stmt)
+{
+	if (stmt->behavior == DROP_CASCADE)
+	{
+		appendStringInfoString(buf, " CASCADE");
+	}
+	else if (stmt->behavior == DROP_RESTRICT)
+	{
+		appendStringInfoString(buf, " RESTRICT");
+	}
+}
+
+
+static char *
+GetDefElemActionString(DefElemAction action)
+{
+	switch (action)
+	{
+		case DEFELEM_ADD:
+		{
+			return "ADD";
+		}
+
+		case DEFELEM_SET:
+		{
+			return "SET";
+		}
+
+		case DEFELEM_DROP:
+		{
+			return "DROP";
+		}
+
+		default:
+			return "";
+	}
+}
