5f0eb460f419c60f8cb278d7263b4beb43770ba5
Halil Ozan Akgul <hozanakgul@gmail.com>
2021-12-15 16:57:45 +0300

    Remove flaky citus.dump_network_traffic

--- src/test/regress/sql/failure_connection_establishment.sql
+++ src/test/regress/sql/failure_connection_establishment.sql
@@ -80,9 +80,6 @@ SELECT citus.mitmproxy('conn.delay(500)');
 SELECT count(*) FROM products;
 SELECT count(*) FROM products;
 
--- use OFFSET 1 to prevent printing the line where source
--- is the worker, and LIMIT 1 in case there were multiple connections
-SELECT citus.dump_network_traffic() ORDER BY 1 LIMIT 1 OFFSET 1;
 
 SELECT citus.mitmproxy('conn.allow()');
 SET citus.shard_replication_factor TO 1;
