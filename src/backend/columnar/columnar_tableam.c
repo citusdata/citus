79055a3bad9b40dbb99c13637d3247a82f6bfb2c
Jeff Davis <jeff@j-davis.com>
2022-02-02 15:31:28 -0800

    remove header

--- src/backend/columnar/columnar_tableam.c
+++ src/backend/columnar/columnar_tableam.c
@@ -20,6 +20,9 @@
 #include "access/xact.h"
 #include "catalog/catalog.h"
 #include "catalog/index.h"
+#if PG_VERSION_NUM < 140000
+#include "catalog/indexing.h"
+#endif
 #include "catalog/objectaccess.h"
 #include "catalog/pg_am.h"
 #include "catalog/pg_publication.h"
@@ -54,8 +57,10 @@
 #include "columnar/columnar_storage.h"
 #include "columnar/columnar_tableam.h"
 #include "columnar/columnar_version_compat.h"
-#include "distributed/coordinator_protocol.h"
 #include "distributed/listutils.h"
+
+extern bool CheckCitusVersion(int elevel);
+extern void EnsureTableOwner(Oid relationId);
 
 /*
  * Timing parameters for truncate locking heuristics.
