bcb00e33189f454169a5a42d2102c70c37deac96
Onder Kalaci <onderkalaci@gmail.com>
2022-02-03 10:13:05 +0100

    remove not used files

--- src/test/regress/sql/turn_mx_off.sql
+++
@@ -1,5 +0,0 @@
-ALTER SYSTEM SET citus.enable_metadata_sync TO OFF;
-SELECT pg_reload_conf();
-
-SET client_min_messages TO ERROR;
-SELECT stop_metadata_sync_to_node(nodename, nodeport) FROM pg_dist_node WHERE isactive = 't' and noderole = 'primary';
