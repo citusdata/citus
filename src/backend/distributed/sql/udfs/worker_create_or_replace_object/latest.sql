f94f87a7d5368e64cedfd6697edf918c30394313
Nils Dijk <nils@citusdata.com>
2022-02-10 13:42:42 +0100

    update udf sql

--- src/backend/distributed/sql/udfs/worker_create_or_replace_object/latest.sql
+++ src/backend/distributed/sql/udfs/worker_create_or_replace_object/latest.sql
@@ -6,10 +6,10 @@ CREATE OR REPLACE FUNCTION pg_catalog.worker_create_or_replace_object(statement 
 COMMENT ON FUNCTION pg_catalog.worker_create_or_replace_object(statement text)
     IS 'takes a sql CREATE statement, before executing the create it will check if an object with that name already exists and safely replaces that named object with the new object';
 
-CREATE OR REPLACE FUNCTION pg_catalog.worker_create_or_replace_object(statement []text)
+CREATE OR REPLACE FUNCTION pg_catalog.worker_create_or_replace_object(statement text[])
   RETURNS bool
   LANGUAGE C STRICT
   AS 'MODULE_PATHNAME', $$worker_create_or_replace_object$$;
 
-COMMENT ON FUNCTION pg_catalog.worker_create_or_replace_object(statement []text)
+COMMENT ON FUNCTION pg_catalog.worker_create_or_replace_object(statement text[])
     IS 'takes a lost of sql statements, before executing these it will check if the object already exists in that exact state otherwise replaces that named object with the new object';
