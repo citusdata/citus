61bdf9f834351f2d9779490c43c2a11745899553
Ahmet Gedemenli <afgedemenli@gmail.com>
2022-03-08 17:26:31 +0300

    update tests

--- src/test/regress/sql/sequences.sql
+++ src/test/regress/sql/sequences.sql
@@ -11,18 +11,10 @@ SELECT create_distributed_table('seq_test_0','x');
 
 INSERT INTO seq_test_0 SELECT 1, s FROM generate_series(1, 50) s;
 INSERT INTO seq_test_0_local_table SELECT 1, s FROM generate_series(1, 50) s;
-ALTER TABLE seq_test_0 ADD COLUMN z int DEFAULT nextval('seq_0');
--- follow hint
-ALTER TABLE seq_test_0 ADD COLUMN z int;
-ALTER TABLE seq_test_0 ALTER COLUMN z SET DEFAULT nextval('seq_0');
 SELECT * FROM seq_test_0 ORDER BY 1, 2 LIMIT 5;
 
 ALTER TABLE seq_test_0_local_table ADD COLUMN z int;
 ALTER TABLE seq_test_0_local_table ALTER COLUMN z SET DEFAULT nextval('seq_0_local_table');
 SELECT * FROM seq_test_0_local_table ORDER BY 1, 2 LIMIT 5;
-
--- cannot alter a sequence used in a distributed table
--- since the metadata is synced to workers
-ALTER SEQUENCE seq_0 AS bigint;
 
 DROP SCHEMA sequences_schema CASCADE;
