8a0e5725db4bc4e835e8a4e25b3b1f2547674095
Ahmet Gedemenli <afgedemenli@gmail.com>
2022-03-09 14:46:53 +0300

    fix output

--- src/test/regress/sql/sequences.sql
+++ src/test/regress/sql/sequences.sql
@@ -2,9 +2,5 @@ SET search_path TO sequences_schema;
 
 INSERT INTO seq_test_0 VALUES (1,2);
 
-ALTER SEQUENCE seq_0 RENAME TO sequence_0;
-
 -- see the renamed sequence object
-select count(*) from pg_sequence where seqrelid = 'sequence_0'::regclass;
-
-DROP SCHEMA sequences_schema CASCADE;
+select count(*) from pg_sequence where seqrelid = 'seq_0'::regclass;
