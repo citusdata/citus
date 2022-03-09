483926dfb8bbb70451818ddd7cbe105c6909f562
Ahmet Gedemenli <afgedemenli@gmail.com>
2022-03-09 16:12:21 +0300

    add more tests

--- src/test/regress/sql/sequences.sql
+++ src/test/regress/sql/sequences.sql
@@ -1,9 +1,9 @@
 SET search_path TO sequences_schema;
 
--- see the sequence object
-select count(*) from pg_sequence where seqrelid = 'seq_0'::regclass;
+-- see the renamed sequence object
+select count(*) from pg_sequence where seqrelid = 'renamed_seq'::regclass;
 
 INSERT INTO seq_test_0 VALUES (1,2);
 
 -- verify that sequence works properly
-select max(z)<nextval('seq_0') as check_sanity from seq_test_0 ;
+select max(z)<nextval('renamed_seq') as check_sanity from seq_test_0 ;
