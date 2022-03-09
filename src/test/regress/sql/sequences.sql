a98700f5da30da7e26dd5858f581cc626f1e85ed
Ahmet Gedemenli <afgedemenli@gmail.com>
2022-03-09 15:03:31 +0300

    add sanity check for seq

--- src/test/regress/sql/sequences.sql
+++ src/test/regress/sql/sequences.sql
@@ -1,6 +1,9 @@
 SET search_path TO sequences_schema;
 
+-- see the sequence object
+select count(*) from pg_sequence where seqrelid = 'seq_0'::regclass;
+
 INSERT INTO seq_test_0 VALUES (1,2);
 
--- see the renamed sequence object
-select count(*) from pg_sequence where seqrelid = 'seq_0'::regclass;
+-- verify that sequence works properly
+select max(z)<nextval('seq_0') as check_sanity from seq_test_0 ;
