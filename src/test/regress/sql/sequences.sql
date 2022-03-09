eafafeb23fef277988bdbc4908b027a99f530b86
Ahmet Gedemenli <afgedemenli@gmail.com>
2022-03-09 14:41:03 +0300

    remove unnecessary table

--- src/test/regress/sql/sequences.sql
+++ src/test/regress/sql/sequences.sql
@@ -1,13 +1,10 @@
 SET search_path TO sequences_schema;
 
 INSERT INTO seq_test_0 VALUES (1,2);
-INSERT INTO seq_test_0_local_table VALUES (1,2);
 
 ALTER SEQUENCE seq_0 RENAME TO sequence_0;
-ALTER SEQUENCE seq_0_local_table RENAME TO sequence_0_local_table;
 
--- see the renamed sequence objects
+-- see the renamed sequence object
 select count(*) from pg_sequence where seqrelid = 'sequence_0'::regclass;
-select count(*) from pg_sequence where seqrelid = 'sequence_0_local_table'::regclass;
 
 DROP SCHEMA sequences_schema CASCADE;
