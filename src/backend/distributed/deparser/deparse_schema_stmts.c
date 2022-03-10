a32b05602fcc25903cddc25947184a18b5435cd9
Ahmet Gedemenli <afgedemenli@gmail.com>
2022-03-10 12:33:47 +0300

    Support CREATE SCHEMA without name

--- src/backend/distributed/deparser/deparse_schema_stmts.c
+++ src/backend/distributed/deparser/deparse_schema_stmts.c
@@ -92,11 +92,6 @@ AppendCreateSchemaStmt(StringInfo buf, CreateSchemaStmt *stmt)
 		elog(ERROR, "schema creating is not supported with other create commands");
 	}
 
-	if (stmt->schemaname == NULL)
-	{
-		elog(ERROR, "schema name should be specified");
-	}
-
 	appendStringInfoString(buf, "CREATE SCHEMA ");
 
 	if (stmt->if_not_exists)
@@ -104,7 +99,19 @@ AppendCreateSchemaStmt(StringInfo buf, CreateSchemaStmt *stmt)
 		appendStringInfoString(buf, "IF NOT EXISTS ");
 	}
 
-	appendStringInfo(buf, "%s ", quote_identifier(stmt->schemaname));
+	if (stmt->schemaname != NULL)
+	{
+		appendStringInfo(buf, "%s ", quote_identifier(stmt->schemaname));
+	}
+	else
+	{
+		/*
+		 * If the schema name is not provided, the schema will be created
+		 * with the name of the authorizated user.
+		 */
+		Assert(stmt->authrole != NULL);
+		appendStringInfo(buf, "%s ", RoleSpecString(stmt->authrole, true));
+	}
 
 	if (stmt->authrole != NULL)
 	{
