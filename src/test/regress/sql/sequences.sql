69057b765f4f2ddce53862d17f0032e98a893c2f
Ahmet Gedemenli <afgedemenli@gmail.com>
2022-03-11 15:09:31 +0300

    add one more seq

--- src/test/regress/sql/sequences.sql
+++ src/test/regress/sql/sequences.sql
@@ -3,7 +3,24 @@ SET search_path TO sequences_schema;
 -- see the renamed sequence object
 select count(*) from pg_sequence where seqrelid = 'renamed_seq'::regclass;
 
-INSERT INTO seq_test_0 VALUES (1,2);
+TRUNCATE seq_test_0;
+INSERT INTO seq_test_0 VALUES (1);
 
 -- verify that sequence works properly
 select max(z)<nextval('renamed_seq') as check_sanity from seq_test_0 ;
+select max(y)<nextval('seq_1') as check_sanity from seq_test_0 ;
+
+TRUNCATE seq_test_0;
+BEGIN;
+    INSERT INTO seq_test_0 VALUES (199999, DEFAULT, DEFAULT);
+    SELECT 1 from (select setval('renamed_seq', max(z)) FROM seq_test_0) as setvalue;
+    SELECT currval('renamed_seq') = max(z) FROM seq_test_0;
+COMMIT;
+
+TRUNCATE seq_test_0;
+BEGIN;
+    INSERT INTO seq_test_0 VALUES (2);
+    -- verify that sequence works properly
+    select max(z)<nextval('renamed_seq') as check_sanity from seq_test_0 ;
+    select max(y)<nextval('seq_1') as check_sanity from seq_test_0 ;
+COMMIT;
--- src/test/regress/sql/sequences_create.sql
+++ src/test/regress/sql/sequences_create.sql
@@ -4,6 +4,9 @@ SET search_path TO sequences_schema;
 CREATE SEQUENCE seq_0;
 ALTER SEQUENCE seq_0 AS smallint;
 
+CREATE SEQUENCE seq_1;
+ALTER SEQUENCE seq_1 AS bigint;
+
 CREATE TABLE seq_test_0 (x bigint, y bigint);
 SELECT create_distributed_table('seq_test_0','x');
 
@@ -13,5 +16,6 @@ SELECT * FROM seq_test_0 ORDER BY 1, 2 LIMIT 5;
 
 ALTER TABLE seq_test_0 ADD COLUMN z bigint;
 ALTER TABLE seq_test_0 ALTER COLUMN z SET DEFAULT nextval('seq_0');
+ALTER TABLE seq_test_0 ALTER COLUMN y SET DEFAULT nextval('seq_1');
 
 ALTER SEQUENCE seq_0 RENAME TO renamed_seq;
