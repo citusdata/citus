fb2ca54e0c0960d80b4a410b9c70d3e24b9e98a5
Ahmet Gedemenli <afgedemenli@gmail.com>
2022-03-14 16:12:43 +0300

    more precise nextval tests

--- src/test/regress/sql/sequences.sql
+++ src/test/regress/sql/sequences.sql
@@ -7,8 +7,8 @@ TRUNCATE seq_test_0;
 INSERT INTO seq_test_0 VALUES (1);
 
 -- verify that sequence works properly
-select max(z)<nextval('renamed_seq') as check_sanity from seq_test_0 ;
-select max(y)<nextval('seq_1') as check_sanity from seq_test_0 ;
+select max(z)+1=nextval('renamed_seq') as check_sanity from seq_test_0 ;
+select max(y)+1=nextval('seq_1') as check_sanity from seq_test_0 ;
 TRUNCATE seq_test_0;
 BEGIN;
     INSERT INTO seq_test_0 VALUES (199999, DEFAULT, DEFAULT);
@@ -19,6 +19,6 @@ TRUNCATE seq_test_0;
 BEGIN;
     INSERT INTO seq_test_0 VALUES (2);
     -- verify that sequence works properly
-    select max(z)<nextval('renamed_seq') as check_sanity from seq_test_0 ;
-    select max(y)<nextval('seq_1') as check_sanity from seq_test_0 ;
+    select max(z)+1=nextval('renamed_seq') as check_sanity from seq_test_0 ;
+    select max(y)+1=nextval('seq_1') as check_sanity from seq_test_0 ;
 COMMIT;
