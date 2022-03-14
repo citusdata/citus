6a693028943202c24a87ea0de3ab659949772da8
Ahmet Gedemenli <afgedemenli@gmail.com>
2022-03-14 16:25:50 +0300

    currval out of tx

--- src/test/regress/sql/sequences.sql
+++ src/test/regress/sql/sequences.sql
@@ -10,10 +10,8 @@ INSERT INTO seq_test_0 VALUES (1);
 select max(z)+1=nextval('renamed_seq') as check_sanity from seq_test_0 ;
 select max(y)+1=nextval('seq_1') as check_sanity from seq_test_0 ;
 TRUNCATE seq_test_0;
-BEGIN;
-    INSERT INTO seq_test_0 VALUES (199999, DEFAULT, DEFAULT);
-    SELECT currval('renamed_seq') = max(z) FROM seq_test_0;
-COMMIT;
+INSERT INTO seq_test_0 VALUES (199999, DEFAULT, DEFAULT);
+SELECT currval('renamed_seq') = max(z) FROM seq_test_0;
 
 TRUNCATE seq_test_0;
 BEGIN;
