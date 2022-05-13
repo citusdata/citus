c20732142e71abd877be64b45646184a66533369
Marco Slot <marco.slot@gmail.com>
2022-05-13 12:15:43 +0200

    Add a run_command_on_coordinator function

--- src/backend/distributed/sql/downgrades/citus--11.0-2--11.0-1.sql
+++ src/backend/distributed/sql/downgrades/citus--11.0-2--11.0-1.sql
@@ -1,3 +1,4 @@
 #include "../udfs/citus_shards_on_worker/11.0-1.sql"
 #include "../udfs/citus_shard_indexes_on_worker/11.0-1.sql"
 DROP FUNCTION pg_catalog.citus_is_coordinator();
+DROP FUNCTION pg_catalog.run_command_on_coordinator(text,boolean);
--- src/test/regress/sql/run_command_on_all_nodes.sql
+++ src/test/regress/sql/run_command_on_all_nodes.sql
@@ -30,12 +30,39 @@ SELECT tablename FROM pg_tables WHERE schemaname = 'run_command_on_all_nodes';
 SELECT result FROM run_command_on_all_nodes('SELECT tablename FROM pg_tables WHERE schemaname = ''run_command_on_all_nodes'';');
 
 -- break a node and check messages
+BEGIN;
 SELECT nodeid AS worker_1_nodeid FROM pg_dist_node WHERE nodeport = :worker_1_port \gset
 UPDATE pg_dist_node SET nodeport = 0 WHERE nodeid = :worker_1_nodeid;
 
 SELECT nodeid = :worker_1_nodeid AS "Is Worker 1", success, result FROM run_command_on_all_nodes('SELECT 1') ORDER BY 1;
 SELECT nodeid = :worker_1_nodeid AS "Is Worker 1", success, result FROM run_command_on_all_nodes('SELECT 1', give_warning_for_connection_errors:=true) ORDER BY 1;
 
-UPDATE pg_dist_node SET nodeport = :worker_1_port WHERE nodeid = :worker_1_nodeid;
+ROLLBACK;
+
+-- break connection to localhost
+BEGIN;
+UPDATE pg_dist_node SET nodeport = 0 WHERE groupid = 0;
+
+SELECT success, result
+FROM run_command_on_coordinator('SELECT inet_server_port()') ORDER BY 1;
+
+SELECT success, result
+FROM run_command_on_coordinator('SELECT inet_server_port()', give_warning_for_connection_errors:=true) ORDER BY 1;
+
+ROLLBACK;
+
+-- we cannot use run_command_on_coordinator from workers if coordinator is not in the metadata
+SELECT success, result FROM run_command_on_all_nodes($$select result from run_command_on_coordinator('select inet_server_port()')$$);
+
+-- we can use run_command_on_coordinator from any node if the coordinator is in the metadata
+SELECT citus_set_coordinator_host('localhost');
+SELECT success, result FROM run_command_on_all_nodes($$select result from run_command_on_coordinator('select inet_server_port()')$$);
+SELECT citus_remove_node(nodename, nodeport) FROM pg_dist_node WHERE groupid = 0;
+
+-- check that we fail when pg_dist_node is empty
+BEGIN;
+DELETE FROM pg_dist_node;
+SELECT success, result FROM run_command_on_coordinator('select inet_server_port()');
+ROLLBACK;
 
 DROP SCHEMA run_command_on_all_nodes CASCADE;
