81564452d80fb7fa969f9dab4c0c95507adda739
Nils Dijk <nils@citusdata.com>
2022-05-13 14:54:54 +0200

    Fix CI on release branch 9.4 (#5942)

---
+++ ci/build-citus.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+# make bash behave
+set -euo pipefail
+IFS=$'\n\t'
+
+# shellcheck disable=SC1091
+source ci/ci_helpers.sh
+
+# read pg major version, error if not provided
+PG_MAJOR=${PG_MAJOR:?please provide the postgres major version}
+
+# get codename from release file
+. /etc/os-release
+codename=${VERSION#*(}
+codename=${codename%)*}
+
+# get project from argument
+project="${CIRCLE_PROJECT_REPONAME}"
+
+# we'll do everything with absolute paths
+basedir="$(pwd)"
+
+# get the project and clear out the git repo (reduce workspace size
+rm -rf "${basedir}/.git"
+
+build_ext() {
+  pg_major="$1"
+
+  builddir="${basedir}/build-${pg_major}"
+  echo "Beginning build of ${project} for PostgreSQL ${pg_major}..." >&2
+
+  # do everything in a subdirectory to avoid clutter in current directory
+  mkdir -p "${builddir}" && cd "${builddir}"
+
+  CFLAGS=-Werror "${basedir}/configure" PG_CONFIG="/usr/lib/postgresql/${pg_major}/bin/pg_config" --enable-coverage
+
+  installdir="${builddir}/install"
+  make -j$(nproc) && mkdir -p "${installdir}" && { make DESTDIR="${installdir}" install-all || make DESTDIR="${installdir}" install ; }
+
+  cd "${installdir}" && find . -type f -print > "${builddir}/files.lst"
+  tar cvf "${basedir}/install-${pg_major}.tar" `cat ${builddir}/files.lst`
+
+  cd "${builddir}" && rm -rf install files.lst && make clean
+}
+
+build_ext "${PG_MAJOR}"
---
+++ ci/check_sql_snapshots.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -euo pipefail
+
+# shellcheck disable=SC1091
+source ci/ci_helpers.sh
+
+for udf_dir in src/backend/distributed/sql/udfs/*; do
+    # We want to find the last snapshotted sql file, to make sure it's the same
+    # as "latest.sql". This is done by:
+    # 1. Getting the filenames in the UDF directory (using find instead of ls, to keep shellcheck happy)
+    # 2. Filter out latest.sql
+    # 3. Sort using "version sort"
+    # 4. Get the last one using tail
+    latest_snapshot=$(\
+        find "$udf_dir" -iname "*.sql" -exec basename {} \; \
+        | { grep --invert-match latest.sql || true; } \
+        | sort --version-sort \
+        | tail --lines 1);
+    diff --unified --color=auto "$udf_dir/latest.sql" "$udf_dir/$latest_snapshot"; \
+done
