0c99c9951e0ab0069bfd695de73aaa30a40b48a8
Ahmet Gedemenli <afgedemenli@gmail.com>
2022-05-23 17:55:08 +0300

    Remove matview check

--- src/backend/distributed/metadata/metadata_sync.c
+++ src/backend/distributed/metadata/metadata_sync.c
@@ -345,12 +345,6 @@ CreateDependentViewsOnWorkers(Oid relationId)
 	Oid viewOid = InvalidOid;
 	foreach_oid(viewOid, views)
 	{
-		if (get_rel_relkind(viewOid) == RELKIND_MATVIEW)
-		{
-			/* do not propagate materialized views */
-			continue;
-		}
-
 		if (!ShouldMarkRelationDistributed(viewOid))
 		{
 			continue;
