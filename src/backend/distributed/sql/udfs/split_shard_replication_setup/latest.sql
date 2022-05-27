cbc4a4a11cc3715a5cd08b8187ea81ef2a2f7484
Sameer Awasekar <saawasek@microsoft.com>
2022-05-27 12:57:15 +0530

    Change datatype from integer to bigint

--- src/backend/distributed/sql/udfs/split_shard_replication_setup/latest.sql
+++ src/backend/distributed/sql/udfs/split_shard_replication_setup/latest.sql
@@ -1,7 +1,7 @@
 CREATE OR REPLACE FUNCTION pg_catalog.split_shard_replication_setup(
-    shardInfo integer[][])
+    shardInfo bigint[][])
 RETURNS bigint
 LANGUAGE C STRICT
 AS 'MODULE_PATHNAME', $$split_shard_replication_setup$$;
-COMMENT ON FUNCTION pg_catalog.split_shard_replication_setup(shardInfo integer[][])
+COMMENT ON FUNCTION pg_catalog.split_shard_replication_setup(shardInfo bigint[][])
     IS 'Replication setup for splitting a shard'
