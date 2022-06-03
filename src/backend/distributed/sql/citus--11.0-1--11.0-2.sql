4bcffce036e1553bf6bf0c864241997c7c22fe91
Marco Slot <marco.slot@gmail.com>
2022-06-03 17:49:57 +0200

    Introduce a citus_finish_citus_upgrade() function

--- src/backend/distributed/sql/citus--11.0-1--11.0-2.sql
+++ src/backend/distributed/sql/citus--11.0-1--11.0-2.sql
@@ -5,3 +5,4 @@
 #include "udfs/run_command_on_coordinator/11.0-2.sql"
 #include "udfs/start_metadata_sync_to_all_nodes/11.0-2.sql"
 #include "udfs/citus_finalize_upgrade_to_citus11/11.0-2.sql"
+#include "udfs/citus_finish_citus_upgrade/11.0-2.sql"
