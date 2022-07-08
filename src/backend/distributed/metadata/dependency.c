169bef5477f108f26b3c2f5f8801b6008bd9ef8b
Ahmet Gedemenli <afgedemenli@gmail.com>
2022-07-08 12:15:20 +0300

    Add MATVIEW as a supported dep by Citus

--- src/backend/distributed/metadata/dependency.c
+++ src/backend/distributed/metadata/dependency.c
@@ -784,7 +784,8 @@ SupportedDependencyByCitus(const ObjectAddress *address)
 				relKind == RELKIND_SEQUENCE ||
 				relKind == RELKIND_INDEX ||
 				relKind == RELKIND_PARTITIONED_INDEX ||
-				relKind == RELKIND_VIEW)
+				relKind == RELKIND_VIEW ||
+				relKind == RELKIND_MATVIEW)
 			{
 				return true;
 			}
@@ -1615,7 +1616,7 @@ ExpandCitusSupportedTypes(ObjectAddressCollector *collector, ObjectAddress targe
 			 * rule and that rule has dependencies to other objects.
 			 */
 			char relKind = get_rel_relkind(relationId);
-			if (relKind == RELKIND_VIEW)
+			if (relKind == RELKIND_VIEW || relKind == RELKIND_MATVIEW)
 			{
 				List *ruleRefDepList = GetViewRuleReferenceDependencyList(relationId);
 				result = list_concat(result, ruleRefDepList);
