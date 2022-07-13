06d6ffbb6e5cbb3b15fd92521bb2d94f2d3d3dd0
Onder Kalaci <onderkalaci@gmail.com>
2022-07-13 14:08:49 +0200

    LOCK COMMAND does not require primaries at the start
    (cherry picked from commit 8ab696f7e2ce9d479370cbaa7c547b5e9424d81a)

--- src/backend/distributed/metadata/metadata_sync.c
+++ src/backend/distributed/metadata/metadata_sync.c
@@ -2503,7 +2503,7 @@ SchemaOwnerName(Oid objectId)
 static bool
 HasMetadataWorkers(void)
 {
-	List *workerNodeList = ActivePrimaryNonCoordinatorNodeList(NoLock);
+	List *workerNodeList = ActiveReadableNonCoordinatorNodeList();
 
 	WorkerNode *workerNode = NULL;
 	foreach_ptr(workerNode, workerNodeList)
