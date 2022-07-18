5e7a4edd64a031f7d8d5d4b53c34b7da2e6b984b
Nils Dijk <nils@citusdata.com>
2022-07-18 14:18:48 +0200

    add simple test for background job

--- src/backend/distributed/sql/udfs/citus_wait_for_rebalance_job/latest.sql
+++ src/backend/distributed/sql/udfs/citus_wait_for_rebalance_job/latest.sql
@@ -2,7 +2,7 @@ CREATE FUNCTION pg_catalog.citus_wait_for_rebalance_job(jobid bigint)
     RETURNS VOID
     LANGUAGE C STRICT
     AS 'MODULE_PATHNAME',$$citus_wait_for_rebalance_job$$;
-COMMENT ON FUNCTION pg_catalog.citus_wait_for_rebalance_job()
+COMMENT ON FUNCTION pg_catalog.citus_wait_for_rebalance_job(jobid bigint)
     IS 'blocks till the job identified by jobid is at a terminal state, errors if no such job exists';
 
-GRANT EXECUTE ON FUNCTION pg_catalog.citus_wait_for_rebalance_job() TO PUBLIC;
+GRANT EXECUTE ON FUNCTION pg_catalog.citus_wait_for_rebalance_job(jobid bigint) TO PUBLIC;
