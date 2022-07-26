751008e39f259fc6a50b3c119fb69aa9f1b3aa2b
Onder Kalaci <onderkalaci@gmail.com>
2022-07-26 11:48:08 +0200

    WIP

--- src/backend/columnar/sql/downgrades/citus--11.0-4--11.0-3.sql
+++ src/backend/columnar/sql/downgrades/citus--11.0-4--11.0-3.sql
@@ -1,3 +1,1 @@
-DROP PROCEDURE pg_catalog.citus_finalize_upgrade_to_citus11(enforce_version_check bool);
-#include "../udfs/citus_finalize_upgrade_to_citus11/11.0-3.sql"
-#include "../udfs/citus_finish_citus_upgrade/11.0-2.sql"
+-- no changes needed
