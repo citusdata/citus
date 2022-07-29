715022d382e223c668b29d7f32c766c8d95bf940
Ahmet Gedemenli <afgedemenli@gmail.com>
2022-07-29 14:08:35 +0300

    Do not create truncate triggers on foreign tables

--- src/test/regress/sql/multi_extension.sql
+++ src/test/regress/sql/multi_extension.sql
@@ -479,6 +479,14 @@ SELECT * FROM multi_extension.print_extension_changes();
 ALTER EXTENSION citus UPDATE TO '11.0-3';
 SELECT * FROM multi_extension.print_extension_changes();
 
+-- Snapshot of state at 11.0-4
+ALTER EXTENSION citus UPDATE TO '11.0-4';
+SELECT * FROM multi_extension.print_extension_changes();
+
+-- Test downgrade script (result should be empty)
+ALTER EXTENSION citus UPDATE TO '11.0-3';
+ALTER EXTENSION citus UPDATE TO '11.0-4';
+
 DROP TABLE multi_extension.prev_objects, multi_extension.extension_diff;
 
 -- show running version
