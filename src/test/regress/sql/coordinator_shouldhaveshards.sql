88f8a8b8d7d421245528884521d7037addbc4d8b
naisila <nicypp@gmail.com>
2022-08-02 10:33:00 +0300

    Add alternative test outputs for change in Insert Select display
    coordinator_shouldhaveshards.sql
    multi_insert_select.sql
    multi_insert_select_conflict.sql
    single_node.sql
    
    Relevant PG commit:
    a8d8445a7b2f80f6d0bfe97b19f90bd2cbef8759

--- src/test/regress/sql/coordinator_shouldhaveshards.sql
+++ src/test/regress/sql/coordinator_shouldhaveshards.sql
@@ -1,4 +1,12 @@
+--
+-- COORDINATOR_SHOULDHAVESHARDS
+--
 -- Test queries on a distributed table with shards on the coordinator
+--
+-- This test file has an alternative output because of the change in the
+-- display of SQL-standard function's arguments in INSERT/SELECT in PG15.
+-- The alternative output can be deleted when we drop support for PG14
+--
 
 CREATE SCHEMA coordinator_shouldhaveshards;
 SET search_path TO coordinator_shouldhaveshards;
