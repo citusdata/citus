8866d9ac32ace5ad7a6debe7105588fd42da3279
Jelte Fennema <github-tech@jeltef.nl>
2022-08-02 16:58:59 +0200

    Reduce setup time of check-minimal and check-minimal-mx (#6117)
    This change reduces the setup time of our minimal schedules in two ways:
    1. Don't run `multi_cluster_managament`, but instead run a much smaller
       sql file with almost the same results. `multi_cluster_management`
       adds and removes lots of nodes and tests all kinds of failure
       scenarios. This is not needed for the minimal schedules. The only
       reason we were using it there was to get a working cluster of the
       layout that the tests expected. The new `minimal_cluster_management`
       test achieves this with much less work, going from ~2s to ~0.5s.
    2. Parallelize a bit more of the helper tests.

---
+++ src/test/regress/sql/minimal_cluster_management.sql
@@ -0,0 +1,40 @@
+SET citus.next_shard_id TO 1220000;
+ALTER SEQUENCE pg_catalog.pg_dist_colocationid_seq RESTART 1390000;
+ALTER SEQUENCE pg_catalog.pg_dist_groupid_seq RESTART 1;
+
+-- Tests functions related to cluster membership
+
+-- add the nodes to the cluster with the same nodeids and groupids that
+-- multi_cluster_management.sql creates
+ALTER SEQUENCE pg_catalog.pg_dist_node_nodeid_seq RESTART 18;
+ALTER SEQUENCE pg_catalog.pg_dist_groupid_seq RESTART 16;
+SELECT 1 FROM master_add_node('localhost', :worker_2_port);
+ALTER SEQUENCE pg_catalog.pg_dist_node_nodeid_seq RESTART 16;
+ALTER SEQUENCE pg_catalog.pg_dist_groupid_seq RESTART 14;
+SELECT 1 FROM master_add_node('localhost', :worker_1_port);
+
+-- Create the same colocation groups as multi_cluster_management.sql
+SET citus.shard_count TO 16;
+SET citus.shard_replication_factor TO 1;
+CREATE TABLE cluster_management_test (col_1 text, col_2 int);
+SELECT create_distributed_table('cluster_management_test', 'col_1', 'hash');
+DROP TABLE cluster_management_test;
+
+CREATE TABLE test_reference_table (y int primary key, name text);
+SELECT create_reference_table('test_reference_table');
+DROP TABLE test_reference_table;
+
+SET citus.shard_count TO 4;
+SET citus.shard_replication_factor TO 2;
+CREATE TABLE cluster_management_test (col_1 text, col_2 int);
+SELECT create_distributed_table('cluster_management_test', 'col_1', 'hash');
+DROP TABLE cluster_management_test;
+
+SET citus.shard_count TO 4;
+SET citus.shard_replication_factor TO 1;
+CREATE TABLE test_dist (x int, y int);
+SELECT create_distributed_table('test_dist', 'x');
+DROP TABLE test_dist;
+ALTER SEQUENCE pg_catalog.pg_dist_node_nodeid_seq RESTART 30;
+ALTER SEQUENCE pg_catalog.pg_dist_groupid_seq RESTART 18;
+ALTER SEQUENCE pg_catalog.pg_dist_placement_placementid_seq RESTART 83;
