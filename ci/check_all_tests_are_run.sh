072a51d506427bab87105171c6dcfc994ac222ce
Hanefi Onaldi <Hanefi.Onaldi@microsoft.com>
2022-08-03 18:40:58 +0300

    Fix CI checks that reference source files

--- ci/check_all_tests_are_run.sh
+++ ci/check_all_tests_are_run.sh
@@ -7,13 +7,12 @@ source ci/ci_helpers.sh
 
 cd src/test/regress
 
-# 1. Find all *.sql *.spec and *.source files in the sql, spec and input
-#    directories
+# 1. Find all *.sql and *.spec files in the sql, and spec directories
 # 2. Strip the extension and the directory
 # 3. Ignore names that end with .include, those files are meant to be in an C
 #    preprocessor #include statement. They should not be in schedules.
 test_names=$(
-    find sql spec input -iname "*.sql" -o -iname "*.spec" -o -iname "*.source" |
+    find sql spec -iname "*.sql" -o -iname "*.spec" |
     sed -E 's#^\w+/([^/]+)\.[^.]+$#\1#g' |
     grep -v '.include$'
 )
