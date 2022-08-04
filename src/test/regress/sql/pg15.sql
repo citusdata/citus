7496b2143c2c4219f499dd1be2975af623a3450d
naisila <nicypp@gmail.com>
2022-08-04 16:47:57 +0300

    Fixes tests for ALTER TRIGGER RENAME consistency for part. tables
    Relevant PG commit:
    80ba4bb383538a2ee846fece6a7b8da9518b6866

--- src/test/regress/sql/pg15.sql
+++ src/test/regress/sql/pg15.sql
@@ -58,5 +58,61 @@ SELECT result FROM run_command_on_all_nodes('
     SELECT colliculocale FROM pg_collation WHERE collname = ''default_provider'';
 ');
 
+--
+-- In PG15, Renaming triggers on partitioned tables had two problems
+-- recurses to renaming the triggers on the partitions as well.
+-- Here we test that distributed triggers behave the same way.
+-- Relevant PG commit:
+-- 80ba4bb383538a2ee846fece6a7b8da9518b6866
+--
+
+SET citus.enable_unsafe_triggers TO true;
+
+CREATE TABLE sale(
+    sale_date date not null,
+    state_code text,
+    product_sku text,
+    units integer)
+    PARTITION BY list (state_code);
+
+ALTER TABLE sale ADD CONSTRAINT sale_pk PRIMARY KEY (state_code, sale_date);
+
+CREATE TABLE sale_newyork PARTITION OF sale FOR VALUES IN ('NY');
+CREATE TABLE sale_california PARTITION OF sale FOR VALUES IN ('CA');
+
+CREATE TABLE record_sale(
+    operation_type text not null,
+    product_sku text,
+    state_code text,
+    units integer,
+    PRIMARY KEY(state_code, product_sku, operation_type, units));
+
+SELECT create_distributed_table('sale', 'state_code');
+SELECT create_distributed_table('record_sale', 'state_code', colocate_with := 'sale');
+
+CREATE OR REPLACE FUNCTION record_sale()
+RETURNS trigger
+AS $$
+BEGIN
+    INSERT INTO pg15.record_sale(operation_type, product_sku, state_code, units)
+    VALUES (TG_OP, NEW.product_sku, NEW.state_code, NEW.units);
+    RETURN NULL;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER record_sale_trigger
+AFTER INSERT OR UPDATE OR DELETE ON sale
+FOR EACH ROW EXECUTE FUNCTION pg15.record_sale();
+
+CREATE VIEW sale_triggers AS
+    SELECT tgname, tgrelid::regclass, tgenabled
+    FROM pg_trigger
+    WHERE tgrelid::regclass::text like 'sale%'
+    ORDER BY 1, 2;
+
+SELECT * FROM sale_triggers ORDER BY 1, 2;
+ALTER TRIGGER "record_sale_trigger" ON "pg15"."sale" RENAME TO "new_record_sale_trigger";
+SELECT * FROM sale_triggers ORDER BY 1, 2;
+
 -- Clean up
 DROP SCHEMA pg15 CASCADE;
