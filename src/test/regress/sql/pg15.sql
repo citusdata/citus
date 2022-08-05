f511b291dc5e5d8899e2f3a07445029b208d9d26
naisila <nicypp@gmail.com>
2022-08-05 12:12:36 +0300

    Fix tests for generated columns dependency changes
    In PG15, For GENERATED columns, all dependencies of the generation
    expression are recorded as NORMAL dependencies of the column itself.
    This requires CASCADE to drop generated cols with the original col.
    PRE PG15, dependencies were recorded as AUTO, with which
    generated columns are silently dropped with the original column.
    
    Relevant PG commit:
    cb02fcb4c95bae08adaca1202c2081cfc81a28b5

--- src/test/regress/sql/pg15.sql
+++ src/test/regress/sql/pg15.sql
@@ -114,5 +114,44 @@ SELECT * FROM sale_triggers ORDER BY 1, 2;
 ALTER TRIGGER "record_sale_trigger" ON "pg15"."sale" RENAME TO "new_record_sale_trigger";
 SELECT * FROM sale_triggers ORDER BY 1, 2;
 
+--
+-- In PG15, For GENERATED columns, all dependencies of the generation
+-- expression are recorded as NORMAL dependencies of the column itself.
+-- This requires CASCADE to drop generated cols with the original col.
+-- Test this behavior in distributed table, specifically with
+-- undistribute_table within a transaction.
+-- Relevant PG Commit: cb02fcb4c95bae08adaca1202c2081cfc81a28b5
+--
+
+CREATE TABLE generated_stored_ref (
+  col_1 int,
+  col_2 int,
+  col_3 int generated always as (col_1+col_2) stored,
+  col_4 int,
+  col_5 int generated always as (col_4*2-col_1) stored
+);
+
+SELECT create_reference_table ('generated_stored_ref');
+
+-- populate the table
+INSERT INTO generated_stored_ref (col_1, col_4) VALUES (1,2), (11,12);
+INSERT INTO generated_stored_ref (col_1, col_2, col_4) VALUES (100,101,102), (200,201,202);
+SELECT * FROM generated_stored_ref ORDER BY 1,2,3,4,5;
+
+-- fails, CASCADE must be specified
+-- will test CASCADE inside the transcation
+ALTER TABLE generated_stored_ref DROP COLUMN col_1;
+
+BEGIN;
+  -- drops col_1, col_3, col_5
+  ALTER TABLE generated_stored_ref DROP COLUMN col_1 CASCADE;
+  ALTER TABLE generated_stored_ref DROP COLUMN col_4;
+
+  -- show that undistribute_table works fine
+  SELECT undistribute_table('generated_stored_ref');
+  INSERT INTO generated_stored_ref VALUES (5);
+  SELECT * FROM generated_stored_REF ORDER BY 1;
+ROLLBACK;
+
 -- Clean up
 DROP SCHEMA pg15 CASCADE;
