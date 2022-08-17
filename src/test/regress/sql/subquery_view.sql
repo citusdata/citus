2aa07e37a4c91ded59bb0f8694b37ebc8560f4df
naisila <nicypp@gmail.com>
2022-08-17 13:15:02 +0300

    Handles EXPLAIN output diffs in PG15, Hash Agg/Join leverage
    To handle differences in usage of GroupAggregate vs HashAggregate
    or Merge Join vs Hash join in cases where this detail doesn't
    seem to matter, we use coordinator_plan().
    - coordinator_plan() is updated to remove "Result" lines
    
    There are some cases where we have subplans so we add a new
    function that prints all Task Count lines as well
    - coordinator_plan_with_subplans()
    
    Still not sure of the relevant PG commit
    Could be db0d67db2401eb6238ccc04c6407a4fd4f985832
    but disabling enable_group_by_reordering didn't help.

--- src/test/regress/sql/subquery_view.sql
+++ src/test/regress/sql/subquery_view.sql
@@ -427,17 +427,21 @@ SET client_min_messages TO DEFAULT;
 CREATE TABLE reference_table (text_col text, int_col int);
 SELECT create_reference_table('reference_table');
 
+SELECT public.coordinator_plan_with_subplans($Q$
 EXPLAIN (COSTS OFF) WITH cte AS (
   SELECT application_name AS text_col
   FROM pg_stat_activity
 ) SELECT * FROM reference_table JOIN cte USING (text_col);
+$Q$);
 
 CREATE OR REPLACE VIEW view_on_views AS SELECT pg_stat_activity.application_name, pg_locks.pid FROM pg_stat_activity, pg_locks;
 
+SELECT public.coordinator_plan_with_subplans($Q$
 EXPLAIN (COSTS OFF) WITH cte AS (
   SELECT application_name AS text_col
   FROM view_on_views
 ) SELECT * FROM reference_table JOIN cte USING (text_col);
+$Q$);
 
 DROP SCHEMA subquery_view CASCADE;
 SET search_path TO public;
