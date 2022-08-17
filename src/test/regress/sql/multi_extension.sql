408768bfaed3ca565613dac55ead3ca48d6eefa5
Ahmet Gedemenli <afgedemenli@gmail.com>
2022-08-17 17:17:53 +0300

    Fix multi extension order

--- src/test/regress/sql/multi_extension.sql
+++ src/test/regress/sql/multi_extension.sql
@@ -492,14 +492,17 @@ SELECT * FROM multi_extension.print_extension_changes();
 ALTER EXTENSION citus UPDATE TO '11.0-3';
 ALTER EXTENSION citus UPDATE TO '11.0-4';
 
--- Test downgrade to 11.0-4 from 11.1-1
-ALTER EXTENSION citus UPDATE TO '11.1-1';
-ALTER EXTENSION citus UPDATE TO '11.0-4';
 -- Should be empty result since upgrade+downgrade should be a no-op
 SELECT * FROM multi_extension.print_extension_changes();
 
 -- Snapshot of state at 11.1-1
 ALTER EXTENSION citus UPDATE TO '11.1-1';
+SELECT * FROM multi_extension.print_extension_changes();
+
+-- Test downgrade to 11.0-4 from 11.1-1
+ALTER EXTENSION citus UPDATE TO '11.0-4';
+ALTER EXTENSION citus UPDATE TO '11.1-1';
+-- Should be empty result since upgrade+downgrade should be a no-op
 SELECT * FROM multi_extension.print_extension_changes();
 
 DROP TABLE multi_extension.prev_objects, multi_extension.extension_diff;
