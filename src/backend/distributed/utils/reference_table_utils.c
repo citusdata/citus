28330cc0438ae1fe47eb6efe9a5d4683f9af93b3
Marco Slot <marco.slot@gmail.com>
2022-08-23 23:11:15 +0200

    Set application_name to citus_rebalancer when copying reference tables

--- src/backend/distributed/utils/reference_table_utils.c
+++ src/backend/distributed/utils/reference_table_utils.c
@@ -244,6 +244,16 @@ EnsureReferenceTablesExistOnAllNodesExtended(char transferMode)
 				CopyShardPlacementToWorkerNodeQuery(sourceShardPlacement,
 													newWorkerNode,
 													transferMode);
+
+			/*
+			 * The placement copy command uses distributed execution to create
+			 * indexes. This is allowed when indicating that the backend is a
+			 * rebalancer backend.
+			 */
+			ExecuteCriticalRemoteCommand(connection,
+										 "SET LOCAL application_name TO "
+										 CITUS_REBALANCER_NAME);
+
 			ExecuteCriticalRemoteCommand(connection, placementCopyCommand->data);
 			RemoteTransactionCommit(connection);
 		}
