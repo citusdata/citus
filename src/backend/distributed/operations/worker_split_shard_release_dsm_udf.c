4df8eca77f87f342f08c5963d9b7c100f8053fa2
Sameer Awasekar <82865256+saawasek@users.noreply.github.com>
2022-08-26 18:27:32 +0530

    Add worker_split_shard_release_dsm udf to release dynamic shared memory (#6248)
    The code introduces worker_split_shard_release_dsm udf to release the dynamic shared memory segment allocated during non-blocking split workflow.

---
+++ src/backend/distributed/operations/worker_split_shard_release_dsm_udf.c
@@ -0,0 +1,24 @@
+/*-------------------------------------------------------------------------
+ *
+ * worker_split_shard_release_dsm.c
+ *    This file contains functions to release dynamic shared memory segment
+ *    allocated during split workflow.
+ *
+ * Copyright (c) Citus Data, Inc.
+ *
+ *-------------------------------------------------------------------------
+ */
+#include "postgres.h"
+#include "distributed/shardinterval_utils.h"
+#include "distributed/shardsplit_shared_memory.h"
+
+
+/* declarations for dynamic loading */
+PG_FUNCTION_INFO_V1(worker_split_shard_release_dsm);
+
+Datum
+worker_split_shard_release_dsm(PG_FUNCTION_ARGS)
+{
+	ReleaseSharedMemoryOfShardSplitInfo();
+	PG_RETURN_VOID();
+}
