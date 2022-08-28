3b231b70d851454bb5cae871e5922a4f085e2653
Nitish Upreti <niupre@microsoft.com>
2022-08-27 23:04:11 -0700

    multi_tenant_isolation test changes

--- src/test/regress/sql/multi_tenant_isolation.sql
+++ src/test/regress/sql/multi_tenant_isolation.sql
@@ -393,6 +393,10 @@ RESET citus.enable_metadata_sync;
 CREATE EVENT TRIGGER abort_drop ON sql_drop
    EXECUTE PROCEDURE abort_drop_command();
 
+\c :master_port
+-- Disable deferred drop otherwise we will skip the drop and operation will succeed instead of failing.
+ALTER SYSTEM SET citus.defer_drop_after_shard_split TO false;
+SELECT pg_reload_conf();
 \c - mx_isolation_role_ent - :master_port
 SET search_path to "Tenant Isolation";
 
