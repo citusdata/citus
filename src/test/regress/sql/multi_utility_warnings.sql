29162b851c6157f2afdc251207048db74ec0880d
Hanefi Onaldi <Hanefi.Onaldi@microsoft.com>
2022-09-26 22:20:36 +0300

    Cleanup multi_utility_warnings test
    This test used to contain some utility commands that Citus did not
    support. However we added support for most of the commands, and this
    test got outdated.
    
    We used to error out on community when user attempted to use pooler
    options. Now that we open sourced all enterprise features, the test can
    now be removed.

--- src/test/regress/sql/multi_utility_warnings.sql
+++ src/test/regress/sql/multi_utility_warnings.sql
@@ -3,15 +3,6 @@
 --
 
 -- Tests to check if we inform the user about potential caveats of creating new
--- databases, schemas.
-
-
-SET citus.next_shard_id TO 1010000;
-
+-- databases.
 
 CREATE DATABASE new_database;
-
-BEGIN;
-INSERT INTO pg_dist_node VALUES (1234567890, 1234567890, 'localhost', 5432);
-INSERT INTO pg_dist_poolinfo VALUES (1234567890, 'port=1234');
-ROLLBACK;
