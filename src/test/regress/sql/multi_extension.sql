628908e9904ea709336b5fe72f2178f23206fc57
Hanefi Onaldi <Hanefi.Onaldi@microsoft.com>
2022-09-28 18:52:24 +0300

    Document failing downgrades from 10.2-4 to 10.2-2
    (cherry picked from commit 5ddd4754a2fcad425e187c4b16da63b3c0622ce2)

--- src/test/regress/sql/multi_extension.sql
+++ src/test/regress/sql/multi_extension.sql
@@ -395,6 +395,12 @@ SELECT * FROM multi_extension.print_extension_changes();
 ALTER EXTENSION citus UPDATE TO '10.2-4';
 SELECT * FROM multi_extension.print_extension_changes();
 
+-- There was a bug when downgrading to 10.2-2 from 10.2-4
+-- Test that we do not have any issues with this particular downgrade
+ALTER EXTENSION citus UPDATE TO '10.2-2';
+ALTER EXTENSION citus UPDATE TO '10.2-4';
+SELECT * FROM multi_extension.print_extension_changes();
+
 -- Test downgrade to 10.2-4 from 10.2-5
 ALTER EXTENSION citus UPDATE TO '10.2-5';
 ALTER EXTENSION citus UPDATE TO '10.2-4';
