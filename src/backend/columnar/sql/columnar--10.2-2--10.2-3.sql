8fd0aff620dfdd03943e1f54f261d2741edafbc0
Hanefi Onaldi <Hanefi.Onaldi@microsoft.com>
2022-09-28 18:22:53 +0300

    Ensure no dependencies to index before drop
    (cherry picked from commit 11a9a3771fcab97447a6e910aaae27d19e92f5ce)

--- src/backend/columnar/sql/columnar--10.2-2--10.2-3.sql
+++ src/backend/columnar/sql/columnar--10.2-2--10.2-3.sql
@@ -10,6 +10,17 @@
 --
 -- To do that, drop stripe_first_row_number_idx and create a unique
 -- constraint with the same name to keep the code change at minimum.
+--
+-- If we have a pg_depend entry for this index, we can not drop it as
+-- the extension depends on it. Remove the pg_depend entry if it exists.
+DELETE FROM pg_depend
+WHERE classid = 'pg_am'::regclass::oid
+    AND objid IN (select oid from pg_am where amname = 'columnar')
+    AND objsubid = 0
+    AND refclassid = 'pg_class'::regclass::oid
+    AND refobjid = 'columnar.stripe_first_row_number_idx'::regclass::oid
+    AND refobjsubid = 0
+    AND deptype = 'n';
 DROP INDEX columnar.stripe_first_row_number_idx;
 ALTER TABLE columnar.stripe ADD CONSTRAINT stripe_first_row_number_idx
 UNIQUE (storage_id, first_row_number);
