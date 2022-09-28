84e43424fc11f0a7d9f33ea112f8e56ba786d69c
Hanefi Onaldi <Hanefi.Onaldi@microsoft.com>
2022-09-28 18:22:53 +0300

    Ensure no dependencies to index before drop
    (cherry picked from commit 11a9a3771fcab97447a6e910aaae27d19e92f5ce)

--- src/backend/columnar/sql/downgrades/columnar--10.2-3--10.2-2.sql
+++ src/backend/columnar/sql/downgrades/columnar--10.2-3--10.2-2.sql
@@ -1,4 +1,14 @@
 -- columnar--10.2-3--10.2-2.sql
-
+--
+-- If we have a pg_depend entry for this index, we can not drop it as
+-- the extension depends on it. Remove the pg_depend entry if it exists.
+DELETE FROM pg_depend
+WHERE classid = 'pg_am'::regclass::oid
+    AND objid IN (select oid from pg_am where amname = 'columnar')
+    AND objsubid = 0
+    AND refclassid = 'pg_class'::regclass::oid
+    AND refobjid = 'columnar.stripe_first_row_number_idx'::regclass::oid
+    AND refobjsubid = 0
+    AND deptype = 'n';
 ALTER TABLE columnar.stripe DROP CONSTRAINT stripe_first_row_number_idx;
 CREATE INDEX stripe_first_row_number_idx ON columnar.stripe USING BTREE(storage_id, first_row_number);
