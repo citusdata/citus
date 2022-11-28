3157c3760a9c6dfa60650fd8de5187e70aff313b
Onder Kalaci <onderkalaci@gmail.com>
2022-11-28 20:33:32 +0100

    Drop partitioned tables saner while re-syncing metadata

--- src/backend/distributed/metadata/node_metadata.c
+++ src/backend/distributed/metadata/node_metadata.c
@@ -811,12 +811,10 @@ SyncDistributedObjectsCommandList(WorkerNode *workerNode)
 	 */
 	commandList = list_concat(commandList, PropagateNodeWideObjectsCommandList());
 
-	/*
-	 * Detach partitions, break dependencies between sequences and table then
-	 * remove shell tables first.
-	 */
-	commandList = list_concat(commandList, DetachPartitionCommandList());
 	commandList = lappend(commandList, BREAK_CITUS_TABLE_SEQUENCE_DEPENDENCY_COMMAND);
+
+	/* first remove partitioned tables to avoid any need for detaching partitions */
+	commandList = lappend(commandList, REMOVE_PARTITIONED_SHELL_TABLES_COMMAND);
 	commandList = lappend(commandList, REMOVE_ALL_SHELL_TABLES_COMMAND);
 
 	/*
