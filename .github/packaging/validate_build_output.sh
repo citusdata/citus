d1e695948cdb58f5ac211a2d8dbd658ed7973bb2
gindibay <gurkanindibay@gmail.com>
2022-12-01 15:40:12 +0300

    Fixes review comments

---
+++ .github/packaging/validate_build_output.sh
@@ -0,0 +1,6 @@
+package_type=${1}
+git clone -b v0.8.23 --depth=1  https://github.com/citusdata/tools.git tools
+python3 -m pip install -r tools/packaging_automation/requirements.txt
+python3 -m tools.packaging_automation.validate_build_output --output_file output.log \
+                                                            --ignore_file .github/packaging/packaging_ignore.yml \
+                                                            --package_type ${package_type}
