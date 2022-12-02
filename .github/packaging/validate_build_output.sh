a781f543a9ae01f178c7ad0a928a8b51ac73b9d8
gindibay <gurkanindibay@gmail.com>
2022-12-02 22:00:25 +0300

    Fixes ol7 errors for tools

--- .github/packaging/validate_build_output.sh
+++ .github/packaging/validate_build_output.sh
@@ -1,5 +1,5 @@
 package_type=${1}
-git clone -b develop --depth=1  https://github.com/citusdata/tools.git tools
+git clone -b fix_ol7_errors --depth=1  https://github.com/citusdata/tools.git tools
 python3 -m pip install -r tools/packaging_automation/requirements.txt
 python3 -m tools.packaging_automation.validate_build_output --output_file output.log \
                                                             --ignore_file .github/packaging/packaging_ignore.yml \
