e0d3fd3d7295d63521be86e28780ef7729edc96a
Nils Dijk <nils@citusdata.com>
2022-12-12 16:01:18 +0100

    fix include path in downgrade

--- src/backend/distributed/sql/downgrades/citus--11.3-1--11.2-1.sql
+++ src/backend/distributed/sql/downgrades/citus--11.3-1--11.2-1.sql
@@ -6,7 +6,7 @@ DROP SEQUENCE citus.pg_dist_shardgroupid_seq;
 ALTER TABLE pg_catalog.pg_dist_shard DROP COLUMN shardgroupid;
 
 DROP FUNCTION pg_catalog.citus_internal_add_shard_metadata(regclass, bigint, "char", text, text, bigint);
-#include "udfs/citus_internal_add_shard_metadata/10.2-1.sql"
+#include "../udfs/citus_internal_add_shard_metadata/10.2-1.sql"
 
 DROP FUNCTION pg_catalog.citus_internal_add_shardgroup_metadata(bigint, integer, text, text);
 DROP FUNCTION FUNCTION pg_catalog.citus_internal_delete_shardgroup_metadata(bigint, int);
