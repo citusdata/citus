d959d5932d3f63f029f7ccd57022cb615b6d680a
Gurkan Indibay <gindibay@microsoft.com>
2022-12-12 14:06:49 +0300

    Fixes typos in sql file

--- src/test/regress/sql/columnar_alter_table_set_access_method_view.sql
+++ src/test/regress/sql/columnar_alter_table_set_access_method_view.sql
@@ -1,9 +1,9 @@
-CREATE SCHEMA view_acceess_method_test;
-SET search_path TO view_acceess_method_test;
+CREATE SCHEMA view_access_method_test;
+SET search_path TO view_access_method_test;
 
 --create the view to test alter table set access method on it
 CREATE TABLE view_test_table (id int, val int, flag bool, kind int);
-SELECT create_distributed_table('view_acceess_method_test.view_test_table','id');
+SELECT create_distributed_table('view_access_method_test.view_test_table','id');
 INSERT INTO view_test_table VALUES (1, 1, true, 99), (2, 2, false, 99), (2, 3, true, 88);
 CREATE VIEW view_test_view AS SELECT * FROM view_test_table;
 
@@ -11,4 +11,4 @@ CREATE VIEW view_test_view AS SELECT * FROM view_test_table;
 select alter_table_set_access_method('view_test_view','columnar');
 
 --clean environment
-DROP SCHEMA view_acceess_method_test CASCADE;
+DROP SCHEMA view_access_method_test CASCADE;
