0a69128e753a1ef5a86700902cc4981565537d7f
Ahmet Gedemenli <afgedemenli@gmail.com>
2022-12-15 10:51:47 +0300

    Add upgrade script to insert cleanup records for orphaned shards

--- src/backend/distributed/sql/citus--11.1-1--11.2-1.sql
+++ src/backend/distributed/sql/citus--11.1-1--11.2-1.sql
@@ -13,3 +13,12 @@ DROP FUNCTION pg_catalog.worker_append_table_to_shard(text, text, text, integer)
 #include "udfs/citus_task_wait/11.2-1.sql"
 #include "udfs/citus_prepare_pg_upgrade/11.2-1.sql"
 #include "udfs/citus_finish_pg_upgrade/11.2-1.sql"
+
+-- drop orphaned shards after inserting records for them into pg_dist_cleanup
+INSERT INTO pg_dist_cleanup
+    SELECT 0, 0, 0, shard_name(sh.logicalrelid, sh.shardid) AS object_name, plc.groupid AS node_group_id, 0
+        FROM pg_dist_placement plc
+        JOIN pg_dist_shard sh ON sh.shardid = plc.shardid
+        WHERE plc.shardstate = 4;
+
+DELETE FROM pg_dist_placement WHERE shardstate = 4;
