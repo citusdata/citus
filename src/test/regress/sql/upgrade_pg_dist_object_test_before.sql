bc2cfd37aca928b910b3bcef2ab7bccb2e43e142
Hanefi Onaldi <Hanefi.Onaldi@microsoft.com>
2022-12-20 17:53:42 +0300

    Remove tests that are intended for versions earler than Citus 9.5

--- src/test/regress/sql/upgrade_pg_dist_object_test_before.sql
+++ src/test/regress/sql/upgrade_pg_dist_object_test_before.sql
@@ -8,22 +8,6 @@ AS upgrade_test_old_citus_version_lt_10_0;
 \q
 \endif
 
--- create some objects that we just included into distributed object
--- infrastructure in 9.1 versions but not included in 9.0.2
-
--- extension propagation --
-
--- create an extension on all nodes and a table that depends on it
-CREATE EXTENSION isn;
-SELECT run_command_on_workers($$CREATE EXTENSION isn;$$);
-
-CREATE TABLE isn_dist_table (key int, value issn);
-SELECT create_reference_table('isn_dist_table');
-
--- create an extension on all nodes, but do not create a table depending on it
-CREATE EXTENSION seg;
-SELECT run_command_on_workers($$CREATE EXTENSION seg;$$);
-
 -- schema propagation --
 
 -- public schema
