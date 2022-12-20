1c3bfb456346f870ca50edd01854bd4f340cdfe6
Ahmet Gedemenli <afgedemenli@gmail.com>
2022-12-20 19:37:41 +0300

    Fix nonblocking split test

--- src/test/regress/sql/citus_non_blocking_split_shards.sql
+++ src/test/regress/sql/citus_non_blocking_split_shards.sql
@@ -316,4 +316,5 @@ SELECT run_command_on_workers($$SELECT count(*) FROM pg_subscription$$);
 ALTER SYSTEM RESET citus.defer_shard_delete_interval;
 SELECT pg_reload_conf();
 DROP SCHEMA "citus_split_test_schema" CASCADE;
+DROP ROLE test_shard_split_role;
 --END : Cleanup
