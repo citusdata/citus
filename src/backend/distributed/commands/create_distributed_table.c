b445cff37848c9c0018d536725dcf954610ffb0d
Gokhan Gulbiz <ggulbiz@gmail.com>
2023-01-12 09:55:24 +0300

    Remove unneccesarry function

--- src/backend/distributed/commands/create_distributed_table.c
+++ src/backend/distributed/commands/create_distributed_table.c
@@ -146,8 +146,6 @@ static bool is_valid_numeric_typmod(int32 typmod);
 
 static bool DistributionColumnUsesGeneratedStoredColumn(TupleDesc relationDesc,
 														Var *distributionColumn);
-static bool DistributionColumnUsesGeneratedIdentityColumn(TupleDesc relationDesc,
-														  Var *distributionColumn);
 static bool CanUseExclusiveConnections(Oid relationId, bool localTableEmpty);
 static void DoCopyFromLocalTableIntoShards(Relation distributedRelation,
 										   DestReceiver *copyDest,
@@ -1630,13 +1628,10 @@ EnsureRelationCanBeDistributed(Oid relationId, Var *distributionColumn,
 	ErrorIfTableIsACatalogTable(relation);
 
 
-	/* verify target relation is not distributed by a generated columns
-	 * except generated identity
+	/* verify target relation is not distributed by a generated stored column
 	 */
 	if (distributionMethod != DISTRIBUTE_BY_NONE &&
-		DistributionColumnUsesGeneratedStoredColumn(relationDesc, distributionColumn) &&
-		!DistributionColumnUsesGeneratedIdentityColumn(relationDesc, distributionColumn)
-		)
+		DistributionColumnUsesGeneratedStoredColumn(relationDesc, distributionColumn))
 	{
 		ereport(ERROR, (errcode(ERRCODE_FEATURE_NOT_SUPPORTED),
 						errmsg("cannot distribute relation: %s", relationName),
@@ -2434,28 +2429,6 @@ DistributionColumnUsesGeneratedStoredColumn(TupleDesc relationDesc,
 
 	return false;
 }
-
-
-/*
- * DistributionColumnUsesGeneratedIdentityColumn returns whether a given relation uses
- * GENERATED (ALWAYS|BY DEFAULT) AS (...) IDENTITY on distribution column
- */
-static bool
-DistributionColumnUsesGeneratedIdentityColumn(TupleDesc relationDesc,
-											  Var *distributionColumn)
-{
-	Form_pg_attribute attributeForm = TupleDescAttr(relationDesc,
-													distributionColumn->varattno - 1);
-
-	if (attributeForm->attgenerated == ATTRIBUTE_IDENTITY_ALWAYS ||
-		attributeForm->attgenerated == ATTRIBUTE_IDENTITY_BY_DEFAULT)
-	{
-		return true;
-	}
-
-	return false;
-}
-
 
 /*
  * ErrorIfForeignTable errors out if the relation with given relationOid
