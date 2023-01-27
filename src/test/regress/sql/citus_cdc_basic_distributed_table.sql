ee23f30422440c9b2ebb5c238942175f76369a32
Rajesh Kumar <rthandapani@microsoft.com>
2023-01-27 14:56:53 +0530

    Fixed style.

--- src/test/regress/sql/citus_cdc_basic_distributed_table.sql
+++ src/test/regress/sql/citus_cdc_basic_distributed_table.sql
@@ -4,7 +4,7 @@ Here is a high level overview of test plan:
  1. Co-ordinator node: Create a table 'sensors' and add indexes and statistics on this table, create distributed table from sesnsors table.
  2. CDC node: Create a table 'sensors' and add indexes and statistics on this table, create subscription to connect to co-ordinator node.
  3. Co-ordinator node: Connect to co-ordinator node and populate the distributed table and store the current LSN values of worker nodes in variables.
- 4. CDC node: Wait for the data to be replicated, select the count of rows to sensors table 
+ 4. CDC node: Wait for the data to be replicated, select the count of rows to sensors table
     and match it with the number of rows inserted in the distributed table.
  5. Clean up all the resources created.
 */
@@ -36,7 +36,7 @@ SET citus.shard_replication_factor TO 1;
 
 SELECT 1 FROM citus_set_coordinator_host('localhost', :master_port);
 SELECT 1 FROM master_add_node('localhost', :worker_1_port);
-SELECT 1 from master_add_node('localhost', :worker_2_port);	
+SELECT 1 from master_add_node('localhost', :worker_2_port);
 
 CREATE TABLE sensors(
     measureid               integer,
@@ -46,7 +46,7 @@ CREATE TABLE sensors(
     measure_status          char(1),
 	measure_comment         varchar(44),
     PRIMARY KEY (measureid, eventdatetime, measure_data));
-	
+
 CREATE INDEX index_on_sensors ON sensors(lower(measureid::text));
 ALTER INDEX index_on_sensors ALTER COLUMN 1 SET STATISTICS 1000;
 CREATE INDEX hash_index_on_sensors ON sensors USING HASH((measure_data->'IsFailed'));
@@ -107,7 +107,7 @@ CREATE SUBSCRIPTION cdc_subscription_1
                    slot_name='cdc_replication_slot',
                    copy_data=false);
 
--- Create subscriptions to receive events from worker nodes for the distributed table.				   
+-- Create subscriptions to receive events from worker nodes for the distributed table.
 CREATE SUBSCRIPTION cdc_subscription_2
         CONNECTION 'host=localhost port=57638 user=postgres dbname=regression'
         PUBLICATION cdc_publication
@@ -121,19 +121,19 @@ CREATE SUBSCRIPTION cdc_subscription_2
 -- BEGIN 3: connect to co-ordinator node and populate the distributed table and store the current LSN values of worker nodes in variables.
 \c - postgres - :master_port
 SET search_path TO "citus_cdc_test_schema";
-INSERT INTO sensors 
-	SELECT i, '2020-01-05', '{}', 11011.10, 'A', 'I <3 Citus' 
+INSERT INTO sensors
+	SELECT i, '2020-01-05', '{}', 11011.10, 'A', 'I <3 Citus'
 	FROM generate_series(0,1000)i;
-	
+
 SELECT COUNT(*) AS distribued_sensor_count FROM sensors \gset
-	
+
 \c - postgres - :worker_1_port
 SELECT pg_current_wal_lsn()-'0/0' AS remote_lsn_1 \gset
 
 \c - postgres - :worker_2_port
 SELECT pg_current_wal_lsn()-'0/0' AS remote_lsn_2 \gset
 -- END 3: connect to co-ordinator node and populate the distributed table and note down the LSN values of worker nodes in variables.
-	
+
 
 -- BEGIN 4: connect to CDC client node and wait for the subscriptions to catch up with the worker nodes.
 \c - postgres - :worker_3_port
@@ -150,7 +150,7 @@ BEGIN
 		EXECUTE FORMAT('SELECT min(latest_end_lsn)-''0/0'' FROM pg_stat_subscription WHERE subname = ''%s'';', subscription_name) INTO receivedLsn;
 		PERFORM pg_sleep(1);
     END LOOP;
-END$$ LANGUAGE plpgsql;	
+END$$ LANGUAGE plpgsql;
 
 SELECT wait_for_subscription_to_catchup('cdc_subscription_1',:remote_lsn_1);
 SELECT wait_for_subscription_to_catchup('cdc_subscription_2',:remote_lsn_2);
