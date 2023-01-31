960cb6b054bbf11b87a419a9250982b6631160b2
Rajesh Kumar <rthandapani@microsoft.com>
2023-01-31 16:11:00 +0530

    Added update and delete oprations to basic CDC test

--- src/test/regress/sql/citus_cdc_basic_distributed_table.sql
+++ src/test/regress/sql/citus_cdc_basic_distributed_table.sql
@@ -1,17 +1,18 @@
 /*
-Citus CDC basic Test to verify a distributed table's inserts are published correctly to a subscriber.
+Citus CDC basic Test to verify a distributed table's INSERT/UPDATE/DELETE operations are published correctly to a subscriber without duplication.
 Here is a high level overview of test plan:
  1. Co-ordinator node: Create a table 'sensors' and add indexes and statistics on this table, create distributed table from sesnsors table.
- 2. CDC node: Create a table 'sensors' and add indexes and statistics on this table, create subscription to connect to co-ordinator node.
- 3. Co-ordinator node: Connect to co-ordinator node and populate the distributed table and store the current LSN values of worker nodes in variables.
- 4. CDC node: Wait for the data to be replicated, select the count of rows to sensors table
-    and match it with the number of rows inserted in the distributed table.
- 5. Clean up all the resources created.
+ 2. CDC client node: Create tables to receive CDC events published from worker nodes holding the distributed table.
+ 3. Co-ordinator node: Insert 1001 rows in distributed sensors table and make sure those inserts are replicated in CDC client.
+ 4. Co-ordinator node: Update 1001 rows in distributed sensors table and make sure those updates are replicated in CDC client.
+ 5. Co-ordinator node: Delete half the rows in distributed sensors table and make sure those deletes are replicated in CDC client.
+ 6. Clean up all the resources created.
 */
 
-CREATE SCHEMA "citus_cdc_test_schema";
+-- BEGIN 1: Co-ordinator node: Create a table 'sensors' and add indexes and statistics on this table, create distributed table from sesnsors table.
 
 \c - postgres - :master_port
+CREATE SCHEMA "citus_cdc_test_schema";
 SET search_path TO "citus_cdc_test_schema";
 SET citus.next_shard_id TO 8981000;
 SET citus.next_placement_id TO 8610000;
@@ -24,6 +25,9 @@ SELECT 1 from master_add_node('localhost', :worker_2_port);
 
 SELECT nodeid AS worker_1_node FROM pg_dist_node WHERE nodeport=:worker_1_port \gset
 SELECT nodeid AS worker_2_node FROM pg_dist_node WHERE nodeport=:worker_2_port \gset
+
+-- Worker3 node will be used as the CDC client port for CDC tests.
+\set cdc_client_port :worker_3_port
 
 CREATE TABLE sensors(
     measureid               integer,
@@ -40,7 +44,7 @@ CREATE INDEX hash_index_on_sensors ON sensors USING HASH((measure_data->'IsFaile
 CREATE INDEX index_with_include_on_sensors ON sensors ((measure_data->'IsFailed')) INCLUDE (measure_data, eventdatetime, measure_status);
 CREATE STATISTICS stats_on_sensors (dependencies) ON measureid, eventdatetime FROM sensors;
 
--- Create the sensors distributed table.
+-- Create the sensors distributed table and create he publications and replication slots in all worker nodes.
 SELECT create_distributed_table('sensors', 'measureid', colocate_with:='none');
 
 \c - postgres - :worker_1_port
@@ -55,16 +59,12 @@ SELECT slot_name AS slot_name_2 FROM pg_create_logical_replication_slot(FORMAT('
 -- END 1:
 
 
--- BEGIN 2:	 Connect to the CDC client node and create table to receive events published from distributed table, indexes and statistics
-\c - postgres - :worker_3_port
+-- BEGIN 2:	 Connect to the CDC client node and create table to receive CDC events published from worker nodes holding the distributed table.
+\c - postgres - :cdc_client_port
 CREATE SCHEMA "citus_cdc_test_schema";
 SET search_path TO "citus_cdc_test_schema";
 
 -- Disable Deferred drop auto cleanup to avoid flaky tests.
-ALTER SYSTEM SET citus.defer_shard_delete_interval TO -1;
-ALTER SYSTEM SET citus.override_table_visibility to off;
-SELECT pg_reload_conf();
-
 CREATE TABLE sensors(
     measureid               integer,
     eventdatetime           date,
@@ -79,6 +79,18 @@ ALTER INDEX index_on_sensors ALTER COLUMN 1 SET STATISTICS 1000;
 CREATE INDEX hash_index_on_sensors ON sensors USING HASH((measure_data->'IsFailed'));
 CREATE INDEX index_with_include_on_sensors ON sensors ((measure_data->'IsFailed')) INCLUDE (measure_data, eventdatetime, measure_status);
 CREATE STATISTICS stats_on_sensors (dependencies) ON measureid, eventdatetime FROM sensors;
+
+-- This function waits for the subscription to catch up with the given remote node's LSN.
+CREATE OR REPLACE FUNCTION wait_for_subscription_to_catchup(subscription_name text, remoteLsn bigint) RETURNS void AS $$
+DECLARE
+	receivedLsn bigint;
+BEGIN
+    EXECUTE FORMAT('SELECT min(latest_end_lsn)-''0/0'' FROM pg_stat_subscription WHERE subname = ''%s'';', subscription_name) INTO receivedLsn;
+	WHILE receivedLsn IS NULL  OR remoteLsn > receivedLsn LOOP
+		EXECUTE FORMAT('SELECT min(latest_end_lsn)-''0/0'' FROM pg_stat_subscription WHERE subname = ''%s'';', subscription_name) INTO receivedLsn;
+		PERFORM pg_sleep(1);
+    END LOOP;
+END$$ LANGUAGE plpgsql;
 
 -- Create subscriptions to receive events from worker nodes for the distributed table.
 \set connection_string '\'user=postgres host=localhost port=' :worker_1_port ' dbname=regression\''
@@ -103,7 +115,8 @@ CREATE SUBSCRIPTION cdc_subscription_2
                    copy_data=false);
 -- END 2
 
--- BEGIN 3: connect to co-ordinator node and populate the distributed table and store the current LSN values of worker nodes in variables.
+-- BEGIN 3: Insert 1001 rows in distributed sensors table and make sure those inserts are replicated in CDC client.
+
 \c - postgres - :master_port
 SET search_path TO "citus_cdc_test_schema";
 INSERT INTO sensors
@@ -118,25 +131,52 @@ SELECT pg_current_wal_lsn()-'0/0' AS remote_lsn_1 \gset
 
 \c - postgres - :worker_2_port
 SELECT pg_current_wal_lsn()-'0/0' AS remote_lsn_2 \gset
--- END 3: connect to co-ordinator node and populate the distributed table and note down the LSN values of worker nodes in variables.
 
-
--- BEGIN 4: connect to CDC client node and wait for the subscriptions to catch up with the worker nodes.
-\c - postgres - :worker_3_port
+\c - postgres - :cdc_client_port
 -- Create subscription to connect to co-ordinator and replicate the sensors table.
 SET search_path TO "citus_cdc_test_schema";
 
--- This function waits for the subscription to catch up with the given remote node's LSN.
-CREATE OR REPLACE FUNCTION wait_for_subscription_to_catchup(subscription_name text, remoteLsn bigint) RETURNS void AS $$
-DECLARE
-	receivedLsn bigint;
-BEGIN
-    EXECUTE FORMAT('SELECT min(latest_end_lsn)-''0/0'' FROM pg_stat_subscription WHERE subname = ''%s'';', subscription_name) INTO receivedLsn;
-	WHILE receivedLsn IS NULL  OR remoteLsn > receivedLsn LOOP
-		EXECUTE FORMAT('SELECT min(latest_end_lsn)-''0/0'' FROM pg_stat_subscription WHERE subname = ''%s'';', subscription_name) INTO receivedLsn;
-		PERFORM pg_sleep(1);
-    END LOOP;
-END$$ LANGUAGE plpgsql;
+-- Wait for CDC subscriptions to catch up with publishers (worker nodes of distributed table)
+SELECT wait_for_subscription_to_catchup('cdc_subscription_1',:remote_lsn_1);
+SELECT wait_for_subscription_to_catchup('cdc_subscription_2',:remote_lsn_2);
+
+-- Export the local table created by CDC subscriptions into a CSV file.
+\COPY (select * from sensors ORDER BY measureid, eventdatetime, measure_data) TO 'results/sensors2.csv' DELIMITER ',' CSV HEADER;
+
+-- Compare the dumps from original distributed table with the dump from the CDC subscribed table and make sure there are no differences.
+\! diff results/sensors1.csv results/sensors2.csv
+
+-- END 3:
+
+
+-- BEGIN 4: Update 1001 rows in distributed sensors table and make sure those updates are replicated in CDC client.
+
+\c - postgres - :master_port
+
+SET search_path TO "citus_cdc_test_schema";
+UPDATE sensors
+	SET eventdatetime=NOW(),measure_data = jsonb_set(measure_data, '{val}', to_jsonb(measureid * 10)),
+	meaure_quantity = (measureid  * 10) + 5,
+	measure_status = CASE
+		WHEN measureid % 2 = 0
+			THEN 'y'
+			ELSE 'n'
+		END,
+	measure_comment= 'Comment:' || measureid::text;
+-- Export the source distributed table into a CSV file for comparison later with table forme	d using CDC subscriprions in CDC client node.
+\COPY (select * from sensors ORDER BY measureid, eventdatetime, measure_data) TO 'results/sensors1.csv' DELIMITER ',' CSV HEADER;
+
+\c - postgres - :worker_1_port
+SELECT pg_current_wal_lsn()-'0/0' AS remote_lsn_1 \gset
+
+\c - postgres - :worker_2_port
+SELECT pg_current_wal_lsn()-'0/0' AS remote_lsn_2 \gset
+
+
+-- Connect to the CDC client node and wait for the subscriptions to catch up with the worker nodes.
+\c - postgres - :cdc_client_port
+-- Create subscription to connect to co-ordinator and replicate the sensors table.
+SET search_path TO "citus_cdc_test_schema";
 
 --wait for CDC subscriptions to catch up with publishers (worker nodes of distributed table)
 SELECT wait_for_subscription_to_catchup('cdc_subscription_1',:remote_lsn_1);
@@ -147,10 +187,46 @@ SELECT wait_for_subscription_to_catchup('cdc_subscription_2',:remote_lsn_2);
 
 -- Compare the dumps from original distributed table with the dump from the CDC subscribed table and make sure there are no differences.
 \! diff results/sensors1.csv results/sensors2.csv
--- END 4: connect to CDC client node and wait for the subscriptions to catch up with the worker nodes.
 
---Begin 5: Cleanup all the resources created.
+-- END 4:
+
+-- BEGIN 5: Delete half the rows in distributed sensors table and make sure those deletes are replicated in CDC client.
+
+\c - postgres - :master_port
+
+SET search_path TO "citus_cdc_test_schema";
+
+DELETE FROM sensors	WHERE (measureid %2) == 0;
+
+-- Export the source distributed table into a CSV file for comparison later with table forme	d using CDC subscriprions in CDC client node.
+\COPY (select * from sensors ORDER BY measureid, eventdatetime, measure_data) TO 'results/sensors1.csv' DELIMITER ',' CSV HEADER;
+
+\c - postgres - :worker_1_port
+SELECT pg_current_wal_lsn()-'0/0' AS remote_lsn_1 \gset
+
+\c - postgres - :worker_2_port
+SELECT pg_current_wal_lsn()-'0/0' AS remote_lsn_2 \gset
+
+
+-- Connect to the CDC client node and wait for the subscriptions to catch up with the worker nodes.
+\c - postgres - :cdc_client_port
+-- Create subscription to connect to co-ordinator and replicate the sensors table.
+SET search_path TO "citus_cdc_test_schema";
+
+--wait for CDC subscriptions to catch up with publishers (worker nodes of distributed table)
+SELECT wait_for_subscription_to_catchup('cdc_subscription_1',:remote_lsn_1);
+SELECT wait_for_subscription_to_catchup('cdc_subscription_2',:remote_lsn_2);
+
+-- Export the local table created by CDC subscriptions into a CSV file.
+\COPY (select * from sensors ORDER BY measureid, eventdatetime, measure_data) TO 'results/sensors2.csv' DELIMITER ',' CSV HEADER;
+
+-- Compare the dumps from original distributed table with the dump from the CDC subscribed table and make sure there are no differences.
+\! diff results/sensors1.csv results/sensors2.csv
+
+-- END 5:
+
+--Begin 6: Cleanup all the resources created.
 DROP SCHEMA "citus_cdc_test_schema" CASCADE;
 \c - postgres - :master_port
 DROP SCHEMA "citus_cdc_test_schema" CASCADE;
---END : Cleanup
+--END 5 :
