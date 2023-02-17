01506e8a577d058370a3072b99a69ad738c50d3e
aykut-bozkurt <51649454+aykut-bozkurt@users.noreply.github.com>
2023-02-17 14:15:09 +0300

    fix single tuple result memory leak (#6724)
    We should not omit to free PGResult when we receive single tuple result
    from an internal backend.
    Single tuple results are normally freed by our ReceiveResults for
    `tupleDescriptor != NULL` flow but not for those with `tupleDescriptor
    == NULL`. See PR #6722 for details.
    
    DESCRIPTION: Fixes memory leak issue with query results that returns
    single row.
    
    (cherry picked from commit 9e69dd0e7fac2f8854618941c51ce0d7654ba4f3)

--- src/backend/distributed/executor/adaptive_executor.c
+++ src/backend/distributed/executor/adaptive_executor.c
@@ -4563,6 +4563,7 @@ ReceiveResults(WorkerSession *session, bool storeRows)
 		TupleDesc tupleDescriptor = tupleDest->tupleDescForQuery(tupleDest, queryIndex);
 		if (tupleDescriptor == NULL)
 		{
+			PQclear(result);
 			continue;
 		}
 
