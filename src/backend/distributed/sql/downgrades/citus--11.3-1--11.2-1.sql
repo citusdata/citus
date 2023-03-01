7701ca12e08765ef4a2a722af920330b27c01942
Gokhan Gulbiz <ggulbiz@gmail.com>
2023-03-01 11:13:24 +0300

    Update migration sql scripts.

--- src/backend/distributed/sql/downgrades/citus--11.3-1--11.2-1.sql
+++ src/backend/distributed/sql/downgrades/citus--11.3-1--11.2-1.sql
@@ -1,2 +1,2 @@
 -- citus--11.3-1--11.2-1
-DROP FUNCTION worker_modify_identity_columns(regclass);
+DROP FUNCTION IF EXISTS worker_modify_identity_columns(regclass);
---
+++ src/backend/distributed/sql/udfs/worker_modify_identity_columns/11.3-1.sql
@@ -0,0 +1,7 @@
+CREATE OR REPLACE FUNCTION worker_modify_identity_columns(regclass)
+    RETURNS VOID
+    LANGUAGE C STRICT
+    AS 'MODULE_PATHNAME', $$worker_modify_identity_columns$$;
+COMMENT ON FUNCTION worker_modify_identity_columns(regclass)
+    IS 'modify identity columns to produce globally unique values';
+
