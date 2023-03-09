2bf52adb357d865bf3cb40111e07b625af44b8bb
Halil Ozan Akgul <hozanakgul@gmail.com>
2023-03-09 11:02:28 +0300

    downgrade queries

--- src/backend/distributed/sql/downgrades/citus--11.3-1--11.2-1.sql
+++ src/backend/distributed/sql/downgrades/citus--11.3-1--11.2-1.sql
@@ -1,2 +1,8 @@
 -- citus--11.3-1--11.2-1
 -- this is an empty downgrade path since citus--11.2-1--11.3-1.sql is empty for now
+
+DROP VIEW pg_catalog.citus_stats_tenants;
+DROP VIEW pg_catalog.citus_stats_tenants_storage;
+
+DROP FUNCTION pg_catalog.citus_stats_tenants(boolean);
+DROP FUNCTION pg_catalog.citus_stats_tenants_storage;
