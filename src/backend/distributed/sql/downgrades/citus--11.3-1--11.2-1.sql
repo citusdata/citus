9f0139997ffa90ad7489fdf2db292cc585a2f1df
Rajesh Kumar <rthandapani@microsoft.com>
2023-03-16 15:47:24 +0530

    Fixed upgrade_list_citus_objects

--- src/backend/distributed/sql/downgrades/citus--11.3-1--11.2-1.sql
+++ src/backend/distributed/sql/downgrades/citus--11.3-1--11.2-1.sql
@@ -2,4 +2,3 @@
 DROP FUNCTION pg_catalog.citus_internal_start_replication_origin_tracking();
 DROP FUNCTION pg_catalog.citus_internal_stop_replication_origin_tracking();
 DROP FUNCTION pg_catalog.citus_internal_is_replication_origin_tracking_active();
--- this is an empty downgrade path since citus--11.2-1--11.3-1.sql is empty for now
