cdf54ff4b1ead8b4a77dc995f0ddd2695a4c2ff1
Ahmet Gedemenli <afgedemenli@gmail.com>
2023-03-21 12:24:16 +0300

    Add DDL support null-shard-key tables(#6778/#6784/#6787/#6859)
    Add tests for ddl coverage:
    * indexes
    * partitioned tables + indexes with long names
    * triggers
    * foreign keys
    * statistics
    * grant & revoke statements
    * truncate & vacuum
    * create/test/drop view that depends on a dist table with no shard key
    * policy & rls test
    
    * alter table add/drop/alter_type column (using sequences/different data
      types/identity columns)
    * alter table add constraint (not null, check, exclusion constraint)
    * alter table add column with a default value / set default / drop
      default
    * alter table set option (autovacuum)
    
    * indexes / constraints without names
    * multiple subcommands
    
    Adds support for
    * Creating new partitions after distributing (with null key) the parent
    table
    * Attaching partitions to a distributed table with null distribution key
    (and automatically distribute the new partition with null key as well)
    * Detaching partitions from it

--- src/test/regress/sql/create_null_dist_key.sql
+++ src/test/regress/sql/create_null_dist_key.sql
@@ -317,15 +317,23 @@ DROP TABLE null_dist_key_table_1, null_dist_key_table_2;
 -- create indexes before creating the null dist key tables
 
 -- .. for an initially empty table
-CREATE TABLE null_dist_key_table_1(a int);
+CREATE TABLE null_dist_key_table_1(a int, b int);
+CREATE STATISTICS s1 (dependencies) ON a, b FROM null_dist_key_table_1;
 CREATE INDEX null_dist_key_table_1_idx ON null_dist_key_table_1(a);
 SELECT create_distributed_table('null_dist_key_table_1', null, colocate_with=>'none');
+CREATE STATISTICS s2 (dependencies) ON a, b FROM null_dist_key_table_1;
 
 -- .. and for another table having data in it before creating null dist key table
 CREATE TABLE null_dist_key_table_2(a int);
 INSERT INTO null_dist_key_table_2 VALUES(1);
 CREATE INDEX null_dist_key_table_2_idx ON null_dist_key_table_2(a);
 SELECT create_distributed_table('null_dist_key_table_2', null, colocate_with=>'none');
+
+-- test create index concurrently, then reindex
+CREATE INDEX CONCURRENTLY ind_conc ON null_dist_key_table_2(a);
+REINDEX INDEX ind_conc;
+REINDEX INDEX CONCURRENTLY ind_conc;
+DROP INDEX ind_conc;
 
 SELECT * FROM null_dist_key_table_2 ORDER BY a;
 
@@ -347,11 +355,22 @@ BEGIN;
   CREATE POLICY table_policy ON null_dist_key_table_3 TO table_users
       USING (table_user = current_user);
 
+  GRANT ALL ON TABLE null_dist_key_table_3 TO table_users;
+  ALTER TABLE null_dist_key_table_3 OWNER TO table_users;
+
   SELECT create_distributed_table('null_dist_key_table_3', null, colocate_with=>'none');
+
+  REVOKE ALL ON TABLE null_dist_key_table_3 FROM table_users;
+  ALTER TABLE null_dist_key_table_3 OWNER TO postgres;
+  GRANT ALL ON TABLE null_dist_key_table_3 TO table_users;
 ROLLBACK;
 
+ALTER STATISTICS s2 SET STATISTICS 46;
+ALTER TABLE null_dist_key_table_1 SET SCHEMA public;
+DROP STATISTICS s1, s2;
+
 -- drop them for next tests
-DROP TABLE null_dist_key_table_1, null_dist_key_table_2, distributed_table;
+DROP TABLE public.null_dist_key_table_1, null_dist_key_table_2, distributed_table;
 
 -- tests for object names that should be escaped properly
 
@@ -437,6 +456,240 @@ CREATE TABLE sensors_2000 PARTITION OF sensors FOR VALUES FROM ('2000-01-01') TO
 SELECT create_distributed_table('sensors_2000', NULL, distribution_type=>null);
 
 SELECT create_distributed_table('sensors', NULL, distribution_type=>null);
+
+-- verify we can create new partitions after distributing the parent table
+CREATE TABLE sensors_2001 PARTITION OF sensors FOR VALUES FROM ('2001-01-01') TO ('2002-01-01');
+
+-- verify we can attach to a null dist key table
+CREATE TABLE sensors_2002 (measureid integer, eventdatetime date, measure_data jsonb, PRIMARY KEY (measureid, eventdatetime, measure_data));
+ALTER TABLE sensors ATTACH PARTITION sensors_2002 FOR VALUES FROM ('2002-01-01') TO ('2003-01-01');
+
+-- verify we can detach from a null dist key table
+ALTER TABLE sensors DETACH PARTITION sensors_2001;
+
+-- error out when attaching a noncolocated partition
+CREATE TABLE sensors_2003 (measureid integer, eventdatetime date, measure_data jsonb, PRIMARY KEY (measureid, eventdatetime, measure_data));
+SELECT create_distributed_table('sensors_2003', NULL, distribution_type=>null, colocate_with=>'none');
+ALTER TABLE sensors ATTACH PARTITION sensors_2003 FOR VALUES FROM ('2003-01-01') TO ('2004-01-01');
+DROP TABLE sensors_2003;
+
+-- verify we can attach after distributing, if the parent and partition are colocated
+CREATE TABLE sensors_2004 (measureid integer, eventdatetime date, measure_data jsonb, PRIMARY KEY (measureid, eventdatetime, measure_data));
+SELECT create_distributed_table('sensors_2004', NULL, distribution_type=>null, colocate_with=>'sensors');
+ALTER TABLE sensors ATTACH PARTITION sensors_2004 FOR VALUES FROM ('2004-01-01') TO ('2005-01-01');
+
+-- verify we can attach a citus local table
+CREATE TABLE sensors_2005 (measureid integer, eventdatetime date, measure_data jsonb, PRIMARY KEY (measureid, eventdatetime, measure_data));
+SELECT citus_add_local_table_to_metadata('sensors_2005');
+ALTER TABLE sensors ATTACH PARTITION sensors_2005 FOR VALUES FROM ('2005-01-01') TO ('2006-01-01');
+
+-- check metadata
+-- check all partitions and the parent on pg_dist_partition
+SELECT logicalrelid::text FROM pg_dist_partition WHERE logicalrelid::text IN ('sensors', 'sensors_2000', 'sensors_2001', 'sensors_2002', 'sensors_2004', 'sensors_2005') ORDER BY logicalrelid::text;
+-- verify they are all colocated
+SELECT COUNT(DISTINCT(colocationid)) FROM pg_dist_partition WHERE logicalrelid::text IN ('sensors', 'sensors_2000', 'sensors_2001', 'sensors_2002', 'sensors_2004', 'sensors_2005');
+-- verify all partitions are placed on the same node
+SELECT COUNT(DISTINCT(groupid)) FROM pg_dist_placement WHERE shardid IN
+    (SELECT shardid FROM pg_dist_shard WHERE logicalrelid::text IN ('sensors', 'sensors_2000', 'sensors_2001', 'sensors_2002', 'sensors_2004', 'sensors_2005'));
+
+-- verify the shard of sensors_2000 is attached to the parent shard, on the worker node
+SELECT COUNT(*) FROM run_command_on_workers($$
+    SELECT relpartbound FROM pg_class WHERE relname LIKE 'sensors_2000_1______';$$)
+    WHERE length(result) > 0;
+
+-- verify the shard of sensors_2001 is detached from the parent shard, on the worker node
+SELECT COUNT(*) FROM run_command_on_workers($$
+    SELECT relpartbound FROM pg_class WHERE relname LIKE 'sensors_2001_1______';$$)
+    WHERE length(result) > 0;
+
+-- verify the shard of sensors_2002 is attached to the parent shard, on the worker node
+SELECT COUNT(*) FROM run_command_on_workers($$
+    SELECT relpartbound FROM pg_class WHERE relname LIKE 'sensors_2002_1______';$$)
+    WHERE length(result) > 0;
+
+-- create a partitioned citus local table and verify we error out when attaching a partition with null dist key
+CREATE TABLE partitioned_citus_local_tbl(
+    measureid integer,
+    eventdatetime date,
+    measure_data jsonb,
+PRIMARY KEY (measureid, eventdatetime, measure_data))
+PARTITION BY RANGE(eventdatetime);
+SELECT citus_add_local_table_to_metadata('partitioned_citus_local_tbl');
+CREATE TABLE partition_with_null_key (measureid integer, eventdatetime date, measure_data jsonb, PRIMARY KEY (measureid, eventdatetime, measure_data));
+SELECT create_distributed_table('partition_with_null_key', NULL, distribution_type=>null);
+ALTER TABLE partitioned_citus_local_tbl ATTACH PARTITION partition_with_null_key FOR VALUES FROM ('2004-01-01') TO ('2005-01-01');
+
+-- test partitioned tables + indexes with long names
+CREATE TABLE "NULL_!_dist_key"."nullKeyTable.1!?!9012345678901234567890123456789012345678901234567890123456789"(
+  id int PRIMARY KEY,
+  "TeNANt_Id" int,
+  "jsondata" jsonb NOT NULL,
+  name text,
+  price numeric CHECK (price > 0),
+  serial_data bigserial, UNIQUE (id, price))
+  PARTITION BY LIST(id);
+
+CREATE TABLE "NULL_!_dist_key"."partition1_nullKeyTable.1!?!9012345678901234567890123456789012345678901234567890123456789"
+    PARTITION OF "NULL_!_dist_key"."nullKeyTable.1!?!9012345678901234567890123456789012345678901234567890123456789"
+    FOR VALUES IN (1);
+CREATE TABLE "NULL_!_dist_key"."partition2_nullKeyTable.1!?!9012345678901234567890123456789012345678901234567890123456789"
+    PARTITION OF "NULL_!_dist_key"."nullKeyTable.1!?!9012345678901234567890123456789012345678901234567890123456789"
+    FOR VALUES IN (2);
+CREATE TABLE "NULL_!_dist_key"."partition100_nullKeyTable.1!?!9012345678901234567890123456789012345678901234567890123456789"
+    PARTITION OF "NULL_!_dist_key"."nullKeyTable.1!?!9012345678901234567890123456789012345678901234567890123456789"
+    FOR VALUES IN (100);
+
+-- create some objects before create_distributed_table
+CREATE INDEX "my!Index1New" ON "NULL_!_dist_key"."nullKeyTable.1!?!9012345678901234567890123456789012345678901234567890123456789"(id) WITH ( fillfactor = 80 ) WHERE  id > 10;
+CREATE UNIQUE INDEX uniqueIndexNew ON "NULL_!_dist_key"."nullKeyTable.1!?!9012345678901234567890123456789012345678901234567890123456789" (id);
+
+-- ingest some data before create_distributed_table
+set client_min_messages to ERROR;
+INSERT INTO "NULL_!_dist_key"."nullKeyTable.1!?!9012345678901234567890123456789012345678901234567890123456789" VALUES (1, 1, row_to_json(row(1,1), true)),
+                                     (2, 1, row_to_json(row(2,2), 'false'));
+reset client_min_messages;
+-- create a replica identity before create_distributed_table
+ALTER TABLE "NULL_!_dist_key"."nullKeyTable.1!?!9012345678901234567890123456789012345678901234567890123456789" REPLICA IDENTITY USING INDEX uniqueIndexNew;
+
+-- test triggers
+SET client_min_messages TO ERROR;
+CREATE FUNCTION insert_id_100() RETURNS trigger AS $insert_100$
+BEGIN
+    INSERT INTO "NULL_!_dist_key"."nullKeyTable.1!?!9012345678901234567890123456789012345678901234567890123456789" VALUES (100, 1, row_to_json(row(1,1), true));
+    RETURN NEW;
+END;
+$insert_100$ LANGUAGE plpgsql;
+
+CREATE TABLE null_key_table_with_trigger(a INT);
+SELECT create_distributed_table('null_key_table_with_trigger', null);
+-- try to add a trigger after distributing the table, fails
+CREATE TRIGGER insert_100_trigger
+    AFTER UPDATE ON null_key_table_with_trigger
+    FOR EACH STATEMENT EXECUTE FUNCTION insert_id_100();
+
+-- now try to distribute a table that already has a trigger on it
+CREATE TRIGGER insert_100_trigger
+    AFTER UPDATE ON "NULL_!_dist_key"."nullKeyTable.1!?!9012345678901234567890123456789012345678901234567890123456789"
+    FOR EACH STATEMENT EXECUTE FUNCTION insert_id_100();
+
+-- error out because of the trigger
+SELECT create_distributed_table('"NULL_!_dist_key"."nullKeyTable.1!?!9012345678901234567890123456789012345678901234567890123456789"', null);
+
+SET citus.enable_unsafe_triggers TO ON;
+RESET client_min_messages;
+
+-- this shouldn't give any syntax errors
+SELECT create_distributed_table('"NULL_!_dist_key"."nullKeyTable.1!?!9012345678901234567890123456789012345678901234567890123456789"', null);
+
+-- now we can add triggers on distributed tables, because we set the GUC to on
+CREATE TRIGGER insert_100_trigger_2
+    AFTER UPDATE ON null_key_table_with_trigger
+    FOR EACH STATEMENT EXECUTE FUNCTION insert_id_100();
+
+SET client_min_messages TO ERROR;
+UPDATE "NULL_!_dist_key"."nullKeyTable.1!?!9012345678901234567890123456789012345678901234567890123456789" SET "TeNANt_Id"="TeNANt_Id"+1;
+-- we should see one row with id = 100
+SELECT COUNT(*) FROM "NULL_!_dist_key"."nullKeyTable.1!?!9012345678901234567890123456789012345678901234567890123456789" WHERE id = 100;
+
+-- create some objects after create_distributed_table
+CREATE INDEX "my!Index2New" ON "NULL_!_dist_key"."nullKeyTable.1!?!9012345678901234567890123456789012345678901234567890123456789"(id) WITH ( fillfactor = 90 ) WHERE id < 20;
+CREATE UNIQUE INDEX uniqueIndex2New ON "NULL_!_dist_key"."nullKeyTable.1!?!9012345678901234567890123456789012345678901234567890123456789"(id);
+
+-- error out for already existing, because of the unique index
+INSERT INTO "NULL_!_dist_key"."nullKeyTable.1!?!9012345678901234567890123456789012345678901234567890123456789" VALUES (1, 1, row_to_json(row(1,1), true));
+
+-- verify all 4 shard indexes are created on the same node
+SELECT result FROM run_command_on_workers($$
+    SELECT COUNT(*) FROM pg_indexes WHERE indexname LIKE '%my!Index_New_1%' OR indexname LIKE '%uniqueindex%new_1%';$$)
+    ORDER BY nodeport;
+
+-- foreign key to a ref table
+CREATE TABLE dummy_reference_table (a INT PRIMARY KEY);
+SELECT create_reference_table('dummy_reference_table');
+TRUNCATE "NULL_!_dist_key"."nullKeyTable.1!?!9012345678901234567890123456789012345678901234567890123456789";
+ALTER TABLE "NULL_!_dist_key"."nullKeyTable.1!?!9012345678901234567890123456789012345678901234567890123456789"
+    ADD CONSTRAINT fkey_to_dummy_ref FOREIGN KEY (id) REFERENCES dummy_reference_table(a);
+BEGIN; -- try to add the same fkey, reversed
+    ALTER TABLE dummy_reference_table
+        ADD CONSTRAINT fkey_to_dummy_ref FOREIGN KEY (a) REFERENCES "NULL_!_dist_key"."nullKeyTable.1!?!9012345678901234567890123456789012345678901234567890123456789"(id);
+ROLLBACK;
+
+-- errors out because of foreign key violation
+INSERT INTO "NULL_!_dist_key"."nullKeyTable.1!?!9012345678901234567890123456789012345678901234567890123456789" VALUES (100, 1, row_to_json(row(1,1), true));
+
+-- now inserts successfully
+INSERT INTO dummy_reference_table VALUES (100);
+INSERT INTO "NULL_!_dist_key"."nullKeyTable.1!?!9012345678901234567890123456789012345678901234567890123456789" VALUES (100, 1, row_to_json(row(1,1), true));
+DELETE FROM "NULL_!_dist_key"."nullKeyTable.1!?!9012345678901234567890123456789012345678901234567890123456789" WHERE id = 100;
+
+-- foreign key to a local table, errors out
+CREATE TABLE local_table_for_fkey (a INT PRIMARY KEY);
+ALTER TABLE "NULL_!_dist_key"."nullKeyTable.1!?!9012345678901234567890123456789012345678901234567890123456789"
+    ADD CONSTRAINT fkey_to_dummy_local FOREIGN KEY (id) REFERENCES local_table_for_fkey(a);
+
+-- Normally, we support foreign keys from Postgres tables to distributed
+-- tables assuming that the user will soon distribute the local table too
+-- anyway. However, this is not the case for null-shard-key tables before
+-- we improve SQL support.
+ALTER TABLE local_table_for_fkey
+    ADD CONSTRAINT fkey_from_dummy_local FOREIGN KEY (a) REFERENCES "NULL_!_dist_key"."nullKeyTable.1!?!9012345678901234567890123456789012345678901234567890123456789"(id);
+
+-- foreign key to a citus local table, errors out
+CREATE TABLE citus_local_table_for_fkey (a INT PRIMARY KEY);
+SELECT citus_add_local_table_to_metadata('citus_local_table_for_fkey');
+ALTER TABLE "NULL_!_dist_key"."nullKeyTable.1!?!9012345678901234567890123456789012345678901234567890123456789"
+    ADD CONSTRAINT fkey_to_dummy_citus_local FOREIGN KEY (id) REFERENCES citus_local_table_for_fkey(a);
+-- reversed, still fails
+ALTER TABLE citus_local_table_for_fkey
+    ADD CONSTRAINT fkey_from_dummy_citus_local FOREIGN KEY (a) REFERENCES "NULL_!_dist_key"."nullKeyTable.1!?!9012345678901234567890123456789012345678901234567890123456789"(id);
+
+-- foreign key to a distributed table, errors out because not colocated
+CREATE TABLE dist_table_for_fkey (a INT PRIMARY KEY);
+SELECT create_distributed_table('dist_table_for_fkey', 'a');
+ALTER TABLE "NULL_!_dist_key"."nullKeyTable.1!?!9012345678901234567890123456789012345678901234567890123456789"
+    ADD CONSTRAINT fkey_to_dummy_dist FOREIGN KEY (id) REFERENCES dist_table_for_fkey(a);
+-- reversed, still fails
+ALTER TABLE dist_table_for_fkey
+    ADD CONSTRAINT fkey_to_dummy_dist FOREIGN KEY (a) REFERENCES "NULL_!_dist_key"."nullKeyTable.1!?!9012345678901234567890123456789012345678901234567890123456789"(id);
+
+-- create a null key distributed table, not colocated with the partitioned table, and then try to create a fkey
+CREATE TABLE null_key_dist_not_colocated (a INT PRIMARY KEY);
+SELECT create_distributed_table('null_key_dist_not_colocated', null, colocate_with=>'none');
+ALTER TABLE "NULL_!_dist_key"."nullKeyTable.1!?!9012345678901234567890123456789012345678901234567890123456789"
+    ADD CONSTRAINT fkey_to_dummy_dist FOREIGN KEY (id) REFERENCES null_key_dist_not_colocated(a);
+
+-- create a null key distributed table, colocated with the partitioned table, and then create a fkey
+CREATE TABLE null_key_dist (a INT PRIMARY KEY);
+SELECT create_distributed_table('null_key_dist', null, colocate_with=>'"NULL_!_dist_key"."nullKeyTable.1!?!9012345678901234567890123456789012345678901234567890123456789"');
+ALTER TABLE "NULL_!_dist_key"."nullKeyTable.1!?!9012345678901234567890123456789012345678901234567890123456789"
+    ADD CONSTRAINT fkey_to_dummy_dist FOREIGN KEY (id) REFERENCES null_key_dist(a);
+
+-- check supported ON DELETE and ON UPDATE commands
+ALTER TABLE null_key_dist ADD CONSTRAINT fkey_add_test_1 FOREIGN KEY(a)
+    REFERENCES "NULL_!_dist_key"."nullKeyTable.1!?!9012345678901234567890123456789012345678901234567890123456789"(id) ON DELETE SET DEFAULT;
+ALTER TABLE null_key_dist ADD CONSTRAINT fkey_add_test_2 FOREIGN KEY(a)
+    REFERENCES "NULL_!_dist_key"."nullKeyTable.1!?!9012345678901234567890123456789012345678901234567890123456789"(id) ON UPDATE CASCADE;
+ALTER TABLE null_key_dist ADD CONSTRAINT fkey_add_test_3 FOREIGN KEY(a)
+    REFERENCES dummy_reference_table(a) ON DELETE SET DEFAULT;
+ALTER TABLE null_key_dist ADD CONSTRAINT fkey_add_test_4 FOREIGN KEY(a)
+    REFERENCES dummy_reference_table(a) ON UPDATE CASCADE;
+
+ALTER TABLE null_key_dist DROP CONSTRAINT fkey_add_test_1;
+ALTER TABLE null_key_dist DROP CONSTRAINT fkey_add_test_2;
+ALTER TABLE null_key_dist DROP CONSTRAINT fkey_add_test_3;
+ALTER TABLE null_key_dist DROP CONSTRAINT fkey_add_test_4;
+ALTER TABLE "NULL_!_dist_key"."nullKeyTable.1!?!9012345678901234567890123456789012345678901234567890123456789" DROP CONSTRAINT fkey_to_dummy_dist;
+
+-- create a view that depends on the null shard key table
+CREATE VIEW public.v1 AS SELECT * FROM null_key_dist;
+SELECT * FROM public.v1;
+
+DELETE FROM null_key_dist;
+VACUUM null_key_dist;
+TRUNCATE null_key_dist;
+DROP TABLE null_key_dist CASCADE;
+
+RESET client_min_messages;
 
 CREATE TABLE multi_level_partitioning_parent(
     measureid integer,
@@ -685,6 +938,15 @@ BEGIN;
   SELECT create_distributed_table('referencing_table', NULL, distribution_type=>null, colocate_with=>'referenced_table');
 ROLLBACK;
 
+BEGIN;
+  CREATE TABLE referenced_table(a int UNIQUE, b int);
+  SELECT create_distributed_table('referenced_table', NULL, distribution_type=>null);
+
+  CREATE TABLE referencing_table(a serial, b int);
+  SELECT create_distributed_table('referencing_table', NULL, distribution_type=>null, colocate_with=>'referenced_table');
+  ALTER TABLE referencing_table ADD CONSTRAINT fkey_to_dummy_ref_on_update FOREIGN KEY (a) REFERENCES referenced_table(a) ON UPDATE SET DEFAULT;
+ROLLBACK;
+
 -- to a non-colocated null dist key table
 BEGIN;
   CREATE TABLE referenced_table(a int UNIQUE, b int);
@@ -888,9 +1150,6 @@ FOR EACH ROW EXECUTE FUNCTION increment_value();
 
 SELECT create_distributed_table('trigger_table_1', NULL, distribution_type=>null);
 
-SET citus.enable_unsafe_triggers TO ON;
-SELECT create_distributed_table('trigger_table_1', NULL, distribution_type=>null);
-
 INSERT INTO trigger_table_1 VALUES(1), (2);
 SELECT * FROM trigger_table_1 ORDER BY value;
 
@@ -952,10 +1211,21 @@ SET client_min_messages TO NOTICE;
 TRUNCATE trigger_table_3;
 SET client_min_messages TO WARNING;
 
+-- test rename, disable and drop trigger
+ALTER TRIGGER trigger_4 ON trigger_table_3 RENAME TO trigger_new_name;
+ALTER TABLE trigger_table_3 DISABLE TRIGGER ALL;
+DROP TRIGGER trigger_new_name ON trigger_table_3;
+-- enable the remaining triggers
+ALTER TABLE trigger_table_3 ENABLE TRIGGER ALL;
+
 -- try a few simple queries at least to make sure that we don't crash
 BEGIN;
   INSERT INTO nullkey_c1_t1 SELECT * FROM nullkey_c2_t1;
 ROLLBACK;
+
+DROP TRIGGER IF EXISTS trigger_1 ON trigger_table_1;
+DROP TRIGGER trigger_2 ON trigger_table_2 CASCADE;
+DROP TRIGGER trigger_3 ON trigger_table_3 RESTRICT;
 
 -- cleanup at exit
 SET client_min_messages TO ERROR;
