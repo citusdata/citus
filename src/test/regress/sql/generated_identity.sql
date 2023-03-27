529d7da33d75178b2ed8d3899ab2440bc374d3e1
Gokhan Gulbiz <ggulbiz@gmail.com>
2023-03-27 20:58:01 +0300

    Add server version to test output

--- src/test/regress/sql/generated_identity.sql
+++ src/test/regress/sql/generated_identity.sql
@@ -1,4 +1,6 @@
 -- This test file has an alternative output because of error messages vary for PG13
+SHOW server_version \gset
+SELECT substring(:'server_version', '\d+')::int <= 13 AS server_version_le_13;
 
 CREATE SCHEMA generated_identities;
 SET search_path TO generated_identities;
