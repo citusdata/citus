39daf2c0b8287f79b5f85ba680cad9633e4295b9
Gokhan Gulbiz <ggulbiz@gmail.com>
2023-03-29 15:20:32 +0300

    Remove unnecessary log.

--- src/backend/distributed/utils/attribute.c
+++ src/backend/distributed/utils/attribute.c
@@ -240,14 +240,6 @@ AttributeTask(char *tenantId, int colocationId, CmdType commandType)
 	strncpy_s(attributeToTenant, MAX_TENANT_ATTRIBUTE_LENGTH, tenantId,
 			  MAX_TENANT_ATTRIBUTE_LENGTH - 1);
 	attributeCommandType = commandType;
-
-	if (MultiTenantMonitoringLogLevel != CITUS_LOG_LEVEL_OFF)
-	{
-		ereport(NOTICE, (errmsg(
-							 "attributing query to tenant: %s, colocationGroupId: %d",
-							 quote_literal_cstr(attributeToTenant),
-							 colocationGroupId)));
-	}
 }
 
 
