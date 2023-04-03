10a6197441e69720e8851958050d43d2d02b7234
Gokhan Gulbiz <ggulbiz@gmail.com>
2023-04-03 16:09:50 +0300

    Rename existing 11.3-1--11.2-1 upgrade script to 11.3-1--11.2-2.

--- src/backend/distributed/sql/downgrades/citus--11.3-1--11.2-2.sql
+++ src/backend/distributed/sql/downgrades/citus--11.3-1--11.2-2.sql
@@ -3,7 +3,6 @@
 DROP FUNCTION pg_catalog.citus_internal_start_replication_origin_tracking();
 DROP FUNCTION pg_catalog.citus_internal_stop_replication_origin_tracking();
 DROP FUNCTION pg_catalog.citus_internal_is_replication_origin_tracking_active();
-DROP FUNCTION IF EXISTS pg_catalog.worker_adjust_identity_column_seq_ranges(regclass);
 ALTER TABLE pg_catalog.pg_dist_authinfo REPLICA IDENTITY NOTHING;
 ALTER TABLE pg_catalog.pg_dist_partition REPLICA IDENTITY NOTHING;
 ALTER TABLE pg_catalog.pg_dist_placement REPLICA IDENTITY NOTHING;
