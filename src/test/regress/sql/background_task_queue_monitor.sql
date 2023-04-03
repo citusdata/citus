0983db382560c726af7e84617505cb982341244a
naisila <nicypp@gmail.com>
2023-04-03 16:37:00 +0300

    Add pg_cancel_backend test

--- src/test/regress/sql/background_task_queue_monitor.sql
+++ src/test/regress/sql/background_task_queue_monitor.sql
@@ -288,16 +288,15 @@ SELECT job_id, task_id, status FROM pg_dist_background_task
 -- output i.e. to avoid flakiness
 
 BEGIN;
-SELECT nodeid AS worker_1_node FROM pg_dist_node WHERE nodeport=:worker_1_port \gset
-INSERT INTO pg_dist_background_job (job_type, description) VALUES ('test_job', 'simple test to verify changing max parallel tasks per node on the fly') RETURNING job_id AS job_id1 \gset
-INSERT INTO pg_dist_background_task (job_id, command, nodes_involved) VALUES (:job_id1, $job$ SELECT pg_sleep(2); $job$, ARRAY [1, 2]) RETURNING task_id AS task_id1 \gset
-INSERT INTO pg_dist_background_task (job_id, command, nodes_involved) VALUES (:job_id1, $job$ SELECT pg_sleep(2); $job$, ARRAY [3, 4]) RETURNING task_id AS task_id2 \gset
-INSERT INTO pg_dist_background_task (job_id, command, nodes_involved) VALUES (:job_id1, $job$ SELECT pg_sleep(4); $job$, ARRAY [1, 2]) RETURNING task_id AS task_id3 \gset
-INSERT INTO pg_dist_background_task (job_id, command, nodes_involved) VALUES (:job_id1, $job$ SELECT pg_sleep(4); $job$, ARRAY [1, 3]) RETURNING task_id AS task_id4 \gset
-INSERT INTO pg_dist_background_task (job_id, command, nodes_involved) VALUES (:job_id1, $job$ SELECT pg_sleep(4); $job$, ARRAY [2, 4]) RETURNING task_id AS task_id5 \gset
-INSERT INTO pg_dist_background_task (job_id, command, nodes_involved) VALUES (:job_id1, $job$ SELECT pg_sleep(4); $job$, ARRAY [1, 2]) RETURNING task_id AS task_id6 \gset
-INSERT INTO pg_dist_background_task (job_id, command, nodes_involved) VALUES (:job_id1, $job$ SELECT pg_sleep(4); $job$, ARRAY [1, 3]) RETURNING task_id AS task_id7 \gset
-INSERT INTO pg_dist_background_task (job_id, command, nodes_involved) VALUES (:job_id1, $job$ SELECT pg_sleep(4); $job$, ARRAY [1, 4]) RETURNING task_id AS task_id8 \gset
+INSERT INTO pg_dist_background_job (job_type, description) VALUES ('test_job', 'simple test to verify changing max background task executors per node on the fly') RETURNING job_id AS job_id1 \gset
+INSERT INTO pg_dist_background_task (job_id, command, nodes_involved) VALUES (:job_id1, $job$ SELECT pg_sleep(1); $job$, ARRAY [1, 2]) RETURNING task_id AS task_id1 \gset
+INSERT INTO pg_dist_background_task (job_id, command, nodes_involved) VALUES (:job_id1, $job$ SELECT pg_sleep(1); $job$, ARRAY [3, 4]) RETURNING task_id AS task_id2 \gset
+INSERT INTO pg_dist_background_task (job_id, command, nodes_involved) VALUES (:job_id1, $job$ SELECT pg_sleep(2); $job$, ARRAY [1, 2]) RETURNING task_id AS task_id3 \gset
+INSERT INTO pg_dist_background_task (job_id, command, nodes_involved) VALUES (:job_id1, $job$ SELECT pg_sleep(2); $job$, ARRAY [1, 3]) RETURNING task_id AS task_id4 \gset
+INSERT INTO pg_dist_background_task (job_id, command, nodes_involved) VALUES (:job_id1, $job$ SELECT pg_sleep(2); $job$, ARRAY [2, 4]) RETURNING task_id AS task_id5 \gset
+INSERT INTO pg_dist_background_task (job_id, command, nodes_involved) VALUES (:job_id1, $job$ SELECT pg_sleep(3); $job$, ARRAY [1, 2]) RETURNING task_id AS task_id6 \gset
+INSERT INTO pg_dist_background_task (job_id, command, nodes_involved) VALUES (:job_id1, $job$ SELECT pg_sleep(3); $job$, ARRAY [1, 3]) RETURNING task_id AS task_id7 \gset
+INSERT INTO pg_dist_background_task (job_id, command, nodes_involved) VALUES (:job_id1, $job$ SELECT pg_sleep(3); $job$, ARRAY [1, 4]) RETURNING task_id AS task_id8 \gset
 INSERT INTO pg_dist_background_task (job_id, command, nodes_involved) VALUES (:job_id1, $job$ SELECT pg_sleep(4); $job$, ARRAY [1, 3]) RETURNING task_id AS task_id9 \gset
 INSERT INTO pg_dist_background_task (job_id, command, nodes_involved) VALUES (:job_id1, $job$ SELECT pg_sleep(4); $job$, ARRAY [1, 2]) RETURNING task_id AS task_id10 \gset
 INSERT INTO pg_dist_background_task (job_id, command, nodes_involved) VALUES (:job_id1, $job$ SELECT pg_sleep(4); $job$, ARRAY [1, 2]) RETURNING task_id AS task_id11 \gset
@@ -344,15 +343,32 @@ SELECT job_id, task_id, status, nodes_involved FROM pg_dist_background_task
 
 ALTER SYSTEM RESET citus.max_background_task_executors_per_node;
 SELECT pg_reload_conf();
-SELECT citus_task_wait(:task_id6, desired_status => 'done');
-SELECT citus_task_wait(:task_id7, desired_status => 'done');
-SELECT citus_task_wait(:task_id8, desired_status => 'done');
-SELECT citus_task_wait(:task_id9, desired_status => 'running');
 
+-- if pg_cancel_backend is called on one of the running task PIDs
+-- task doesn't restart because it's not allowed anymore by the limit.
+-- node with id 1 can be used only once, unless there are previously running tasks
+SELECT pid AS task_id8_pid FROM pg_dist_background_task WHERE task_id IN (:task_id8) \gset
+SELECT pg_cancel_backend(:task_id8_pid); -- cancel task_id8 process
+
+-- task goes to only runnable state, not running anymore.
+SELECT citus_task_wait(:task_id8, desired_status => 'runnable');
+
+-- show that cancelled task hasn't restarted because limit doesn't allow it
 SELECT job_id, task_id, status, nodes_involved FROM pg_dist_background_task
     WHERE task_id IN (:task_id1, :task_id2, :task_id3, :task_id4, :task_id5,
                       :task_id6, :task_id7, :task_id8, :task_id9, :task_id10, :task_id11)
-    ORDER BY job_id, task_id; -- show that at most one task per node is running
+    ORDER BY job_id, task_id;
+
+SELECT citus_task_wait(:task_id6, desired_status => 'done');
+SELECT citus_task_wait(:task_id7, desired_status => 'done');
+SELECT citus_task_wait(:task_id8, desired_status => 'running');
+
+-- show that the 8th task has restarted only after both 6 and 7 are done
+-- since we have a limit of 1 background task executor per node with id 1
+SELECT job_id, task_id, status, nodes_involved FROM pg_dist_background_task
+    WHERE task_id IN (:task_id1, :task_id2, :task_id3, :task_id4, :task_id5,
+                      :task_id6, :task_id7, :task_id8, :task_id9, :task_id10, :task_id11)
+    ORDER BY job_id, task_id;
 
 SELECT citus_job_cancel(:job_id1);
 SELECT citus_job_wait(:job_id1);
