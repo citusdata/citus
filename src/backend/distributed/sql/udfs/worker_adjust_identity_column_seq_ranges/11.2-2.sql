12c45bdc0123bd1a979065716617c022395fc39e
Gokhan Gulbiz <ggulbiz@gmail.com>
2023-04-04 15:21:12 +0300

    Include v11.3-1 udf definition for v11.2-2
    Co-authored-by: Hanefi Onaldi <Hanefi.Onaldi@microsoft.com>

--- src/backend/distributed/sql/udfs/worker_adjust_identity_column_seq_ranges/11.2-2.sql
+++ src/backend/distributed/sql/udfs/worker_adjust_identity_column_seq_ranges/11.2-2.sql
@@ -1,7 +1,3 @@
-CREATE OR REPLACE FUNCTION pg_catalog.worker_adjust_identity_column_seq_ranges(regclass)
-    RETURNS VOID
-    LANGUAGE C STRICT
-    AS 'MODULE_PATHNAME', $$worker_adjust_identity_column_seq_ranges$$;
-COMMENT ON FUNCTION pg_catalog.worker_adjust_identity_column_seq_ranges(regclass)
-    IS 'modify identity column seq ranges to produce globally unique values';
+-- Since we backported the UDF below from version 11.3, the definition is the same
+#include "udfs/worker_adjust_identity_column_seq_ranges/11.3-1.sql"
 
