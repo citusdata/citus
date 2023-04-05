1754ff26eda0c39910afaedb3f978d24af62a758
Rajesh Kumar <rthandapani@microsoft.com>
2023-04-05 21:55:55 +0530

    Refactored CDC build into CDC makefiles in temp build dirs.

--- src/test/cdc/t/016_cdc_wal2json.pl
+++ src/test/cdc/t/016_cdc_wal2json.pl
@@ -16,8 +16,6 @@ my $result = 0;
 ### Create the citus cluster with coordinator and two worker nodes
 our ($node_coordinator, @workers) = create_citus_cluster(1,"localhost",57636);
 
-our $node_cdc_client = create_node('cdc_client', 0, "localhost", 57639);
-
 print("coordinator port: " . $node_coordinator->port() . "\n");
 print("worker0 port:" . $workers[0]->port() . "\n");
 
@@ -29,16 +27,13 @@ my $initial_schema = "
 
 $node_coordinator->safe_psql('postgres',$initial_schema);
 $node_coordinator->safe_psql('postgres','ALTER TABLE data_100008 REPLICA IDENTITY FULL;');
-
-$node_cdc_client->safe_psql('postgres',$initial_schema);
-
 $node_coordinator->safe_psql('postgres',"SELECT pg_catalog.pg_create_logical_replication_slot('cdc_replication_slot','wal2json');");
 
 #insert data into the data_100008 table in the coordinator node before distributing the table.
 $node_coordinator->safe_psql('postgres',"
   	INSERT INTO data_100008
   SELECT i, 'my test data ' || i
-  FROM generate_series(0,0)i;");
+  FROM generate_series(-1,1)i;");
 
 my $output = $node_coordinator->safe_psql('postgres',"SELECT * FROM pg_logical_slot_get_changes('cdc_replication_slot', NULL, NULL);");
 print($output);
@@ -51,6 +46,6 @@ if ($output =~ /$change_string_expected/) {
 }
 
 is($result, 1, 'CDC create_distributed_table - wal2json test');
+my $output = $node_coordinator->safe_psql('postgres',"SELECT pg_drop_replication_slot('cdc_replication_slot');");
 
-#$node_cdc_client->safe_psql('postgres',"drop subscription cdc_subscription");
 done_testing();
