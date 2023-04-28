fc454535bee06459f1aab420ef563f9fbc5e5d68
onderkalaci <onderkalaci@gmail.com>
2023-04-28 14:21:24 +0300

    fix insert .. select pushdown

--- src/backend/distributed/planner/insert_select_planner.c
+++ src/backend/distributed/planner/insert_select_planner.c
@@ -909,7 +909,7 @@ RouterModifyTaskForShardInterval(Query *originalQuery,
 		 * Note that this is only the case with PG14 as the parameter doesn't exist
 		 * prior to that.
 		 */
-		shardRestrictionList = make_simple_restrictinfo(NULL,
+		shardRestrictionList = make_simple_restrictinfo(restriction->plannerInfo,
 														(Expr *) shardOpExpressions);
 		extendedBaseRestrictInfo = lappend(extendedBaseRestrictInfo,
 										   shardRestrictionList);
