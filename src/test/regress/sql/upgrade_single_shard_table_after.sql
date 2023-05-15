77fb0aeceb4ee6042a7aa5521699fe36dffa31d8
Onur Tirtir <onurcantirtir@gmail.com>
2023-05-15 12:21:02 +0300

    feedback

--- src/test/regress/sql/upgrade_single_shard_table_after.sql
+++ src/test/regress/sql/upgrade_single_shard_table_after.sql
@@ -3,12 +3,10 @@ SELECT 1 FROM pg_dist_partition
 WHERE logicalrelid = 'citus_schema.null_shard_key'::regclass AND
       partmethod = 'n' AND repmodel = 's' AND colocationid != 0;
 
-INSERT INTO citus_schema.null_shard_key (name) VALUES ('c');
-SELECT * FROM citus_schema.null_shard_key WHERE id <= 2 ORDER BY id;
-
--- Tests in after schedule are run twice, so we would insert a row with id=3
--- at the first run and a row with id=4 at the second run.
-SELECT COUNT(*) IN (1,2) FROM citus_schema.null_shard_key WHERE id IN (3,4);
+BEGIN;
+  INSERT INTO citus_schema.null_shard_key (name) VALUES ('c');
+  SELECT * FROM citus_schema.null_shard_key ORDER BY id;
+ROLLBACK;
 
 -- Check that we can create a distributed table with a single-shard
 -- after upgrade.
--- src/test/regress/sql/upgrade_single_shard_table_before.sql
+++ src/test/regress/sql/upgrade_single_shard_table_before.sql
@@ -1,3 +1,3 @@
-CREATE TABLE null_shard_key (id bigserial, name text);
+CREATE TABLE null_shard_key (id int, name text);
 SELECT create_distributed_table('null_shard_key', null);
-INSERT INTO null_shard_key (name) VALUES ('a'), ('b');
+INSERT INTO null_shard_key (id, name) VALUES (1, 'a'), (2, 'b');
