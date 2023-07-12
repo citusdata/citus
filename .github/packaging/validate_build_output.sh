41b9db020d9f05fdfefe92e9e16be5ca2038fec6
gindibay <gindibay@microsoft.com>
2023-07-12 20:38:18 +0300

    Fixes version errors

--- .github/packaging/validate_build_output.sh
+++ .github/packaging/validate_build_output.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-set -eo
+set -ex
 
 # Function to get the OS version
 get_os_version() {
     if [[ -f /etc/centos-release ]]; then
         cat /etc/centos-release | awk '{print $4}'
     elif [[ -f /etc/oracle-release ]]; then
-        cat /etc/oracle-release | awk '{print $4}'
+        cat /etc/oracle-release | awk '{print $5}'
     else
         echo "Unknown"
     fi
@@ -34,7 +34,7 @@ echo "OS version: $(get_os_version)"
  # if os version is centos 7 or oracle linux 7, then remove urllib3 with pip uninstall and install urllib3<2.0.0 with pip install
 if [[ ${package_type} == "rpm" && $(get_os_version) == 7* ]]; then
     python3 -m pip uninstall -y urllib3
-    python3 -m pip install urllib3<2.0.0
+    python3 -m pip install 'urllib3<2'
 fi
 
 python3 -m tools.packaging_automation.validate_build_output --output_file output.log \
