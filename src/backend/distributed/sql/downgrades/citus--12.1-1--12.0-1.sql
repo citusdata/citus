62c7ef04fb01178109ebe659dbe948fb8dbfeb25
Marco Slot <marco.slot@gmail.com>
2023-07-14 16:57:10 +0200

    Move database sharding functions into 12.1

--- src/backend/distributed/sql/downgrades/citus--12.1-1--12.0-1.sql
+++ src/backend/distributed/sql/downgrades/citus--12.1-1--12.0-1.sql
@@ -1,2 +1,11 @@
 -- citus--12.1-1--12.0-1
 -- this is an empty downgrade path since citus--12.0-1--12.1-1.sql is empty for now
+
+
+DROP SCHEMA citus_catalog CASCADE;
+DROP FUNCTION pg_catalog.execute_command_on_all_nodes(text);
+DROP FUNCTION pg_catalog.execute_command_on_other_nodes(text);
+DROP FUNCTION pg_catalog.citus_internal_database_command(text);
+DROP FUNCTION pg_catalog.citus_internal_add_database_shard(text,int);
+DROP FUNCTION pg_catalog.citus_internal_start_migration_monitor(text,text);
+DROP FUNCTION pg_catalog.database_move(text,int);
