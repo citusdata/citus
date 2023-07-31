13eb0336d3abccad10bd015b6e4155ed7fa073f4
onderkalaci <onderkalaci@gmail.com>
2023-07-31 15:57:27 +0300

    Improve failure handling of distributed execution
        Prior to this commit, the code would skip processing the
        errors happened for local commands.
    
        Prior to https://github.com/citusdata/citus/pull/5379, it might
        make sense to allow the execution continue. But, as of today,
        if a modification fails on any placement, we can safely fail
        the execution.
    
    (cherry picked from commit b4008bc872bf0db74fc1531bf11ef91fb53a2022)

--- src/backend/distributed/executor/adaptive_executor.c
+++ src/backend/distributed/executor/adaptive_executor.c
@@ -503,7 +503,9 @@ typedef enum TaskExecutionState
 /*
  * PlacementExecutionOrder indicates whether a command should be executed
  * on any replica, on all replicas sequentially (in order), or on all
- * replicas in parallel.
+ * replicas in parallel. In other words, EXECUTION_ORDER_ANY is used for
+ * SELECTs, EXECUTION_ORDER_SEQUENTIAL/EXECUTION_ORDER_PARALLEL is used for
+ * DML/DDL.
  */
 typedef enum PlacementExecutionOrder
 {
@@ -5062,6 +5064,10 @@ TaskExecutionStateMachine(ShardCommandExecution *shardCommandExecution)
 	}
 
 	if (failedPlacementCount == placementCount)
+	{
+		currentTaskExecutionState = TASK_EXECUTION_FAILED;
+	}
+	else if (executionOrder != EXECUTION_ORDER_ANY && failedPlacementCount > 0)
 	{
 		currentTaskExecutionState = TASK_EXECUTION_FAILED;
 	}
