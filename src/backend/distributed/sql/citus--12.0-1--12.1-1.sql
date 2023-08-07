3fbe5e48612df0651abd1e247b32d7a8c479290e
Gürkan İndibay <gindibay@microsoft.com>
2023-08-07 18:45:30 +0300

    Update src/backend/distributed/sql/citus--12.0-1--12.1-1.sql
    Co-authored-by: Hanefi Onaldi <Hanefi.Onaldi@microsoft.com>

--- src/backend/distributed/sql/citus--12.0-1--12.1-1.sql
+++ src/backend/distributed/sql/citus--12.0-1--12.1-1.sql
@@ -1,10 +1,7 @@
 -- citus--12.0-1--12.1-1
 
-#include "udfs/citus_pause_node_within_txn/12.1-1.sql"
-
-
-
 -- bump version to 12.1-1
 
+#include "udfs/citus_pause_node_within_txn/12.1-1.sql"
 #include "udfs/citus_prepare_pg_upgrade/12.1-1.sql"
 #include "udfs/citus_finish_pg_upgrade/12.1-1.sql"
