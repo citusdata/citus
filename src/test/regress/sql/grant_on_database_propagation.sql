f2e442fd7985d044b31aabe03b783066444e09b3
gindibay <gindibay@microsoft.com>
2023-08-13 11:45:47 +0300

    Adds detailed tests

--- src/test/regress/sql/grant_on_database_propagation.sql
+++ src/test/regress/sql/grant_on_database_propagation.sql
@@ -1,16 +1,55 @@
+
 create user myuser;
 
+-- test grant/revoke create on database
 grant create on database regression to myuser;
-
 set role myuser;
-create schema myschema;
 
-drop schema myschema;
+select has_database_privilege('gurkanindibay', 'CREATE');
+\c - - - :worker_1_port
+select has_database_privilege('gurkanindibay', 'CREATE');
+\c - - - :master_port
 
-set role postgres;
+RESET ROLE;
+
 revoke create on database regression from myuser;
+
+select has_database_privilege('gurkanindibay', 'CREATE');
+\c - - - :worker_1_port
+select has_database_privilege('gurkanindibay', 'CREATE');
+\c - - - :master_port
+
 drop user myuser;
---
+
+-- test grant/revoke connect on database
+
+create user myuser;
+
+-- test grant/revoke CONNECT on database
+grant connect on database regression to myuser;
+set role myuser;
+
+select has_database_privilege('gurkanindibay', 'CONNECT');
+\c - - - :worker_1_port
+select has_database_privilege('gurkanindibay', 'CONNECT');
+\c - - - :master_port
+
+RESET ROLE;
+
+revoke CONNECT on database regression from myuser;
+
+select has_database_privilege('gurkanindibay', 'CONNECT');
+\c - - - :worker_1_port
+select has_database_privilege('gurkanindibay', 'CONNECT');
+\c - - - :master_port
+
+drop user myuser;
+
+
+
+
+
+
 
 
 
