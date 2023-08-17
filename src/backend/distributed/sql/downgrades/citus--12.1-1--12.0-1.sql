f641860175a1f47ec8a1904ca8d054edd42df34f
gindibay <gindibay@microsoft.com>
2023-08-17 15:30:15 +0300

    Fixes test issues

--- src/backend/distributed/sql/downgrades/citus--12.1-1--12.0-1.sql
+++ src/backend/distributed/sql/downgrades/citus--12.1-1--12.0-1.sql
@@ -1,7 +1,7 @@
 -- citus--12.1-1--12.0-1
 
 
-DROP FUNCTION pg_catalog.citus_pause_node_within_txn(int);
+DROP FUNCTION pg_catalog.citus_pause_node_within_txn(int,bool,int);
 
 
 -- we have modified the relevant upgrade script to include any_value changes
