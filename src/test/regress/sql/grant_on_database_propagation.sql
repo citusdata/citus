7e5d340998d750d21f76df7352fb2eca77ae9700
gindibay <gindibay@microsoft.com>
2023-08-21 10:17:02 +0300

    Fixes review comments

--- src/test/regress/sql/grant_on_database_propagation.sql
+++ src/test/regress/sql/grant_on_database_propagation.sql
@@ -10,23 +10,19 @@ SET search_path TO grant_on_database_propagation;
 create user myuser;
 
 grant create on database regression to myuser;
-set role myuser;
 
-select has_database_privilege('regression', 'CREATE');
+select has_database_privilege('myuser','regression', 'CREATE');
 \c - - - :worker_1_port;
-select has_database_privilege('regression', 'CREATE');
+select has_database_privilege('myuser','regression', 'CREATE');
 \c - - - :master_port
 
-RESET ROLE;
-
 revoke create on database regression from myuser;
-set role myuser;
 
-select has_database_privilege('regression', 'CREATE');
+
+select has_database_privilege('myuser','regression', 'CREATE');
 \c - - - :worker_1_port
 
-set role myuser;
-select has_database_privilege('regression', 'CREATE');
+select has_database_privilege('myuser','regression', 'CREATE');
 \c - - - :master_port
 
 drop user myuser;
@@ -36,23 +32,18 @@ drop user myuser;
 create user myuser;
 
 grant CONNECT on database regression to myuser;
-set role myuser;
 
-select has_database_privilege('regression', 'CONNECT');
+select has_database_privilege('myuser','regression', 'CONNECT');
 \c - - - :worker_1_port;
-select has_database_privilege('regression', 'CONNECT');
+select has_database_privilege('myuser','regression', 'CONNECT');
 \c - - - :master_port
 
-RESET ROLE;
-
 revoke connect on database regression from myuser;
-set role myuser;
 
-select has_database_privilege('regression', 'CONNECT');
+select has_database_privilege('myuser','regression', 'CONNECT');
 \c - - - :worker_1_port
 
-set role myuser;
-select has_database_privilege('regression', 'CONNECT');
+select has_database_privilege('myuser','regression', 'CONNECT');
 \c - - - :master_port
 
 drop user myuser;
@@ -64,23 +55,18 @@ create user myuser;
 
 -- test grant/revoke temp on database
 grant TEMP on database regression to myuser;
-set role myuser;
 
-select has_database_privilege('regression', 'TEMP');
+select has_database_privilege('myuser','regression', 'TEMP');
 \c - - - :worker_1_port;
-select has_database_privilege('regression', 'TEMP');
+select has_database_privilege('myuser','regression', 'TEMP');
 \c - - - :master_port
 
-RESET ROLE;
-
 revoke TEMP on database regression from myuser;
-set role myuser;
 
-select has_database_privilege('regression', 'TEMP');
+select has_database_privilege('myuser','regression', 'TEMP');
 \c - - - :worker_1_port
 
-set role myuser;
-select has_database_privilege('regression', 'TEMP');
+select has_database_privilege('myuser','regression', 'TEMP');
 \c - - - :master_port
 
 drop user myuser;
@@ -92,23 +78,18 @@ create user myuser;
 
 -- test grant/revoke temporary on database
 grant TEMPORARY on database regression to myuser;
-set role myuser;
 
-select has_database_privilege('regression', 'TEMPORARY');
+select has_database_privilege('myuser','regression', 'TEMPORARY');
 \c - - - :worker_1_port;
-select has_database_privilege('regression', 'TEMPORARY');
+select has_database_privilege('myuser','regression', 'TEMPORARY');
 \c - - - :master_port
 
-RESET ROLE;
-
 revoke TEMPORARY on database regression from myuser;
-set role myuser;
 
-select has_database_privilege('regression', 'TEMPORARY');
+select has_database_privilege('myuser','regression', 'TEMPORARY');
 \c - - - :worker_1_port
 
-set role myuser;
-select has_database_privilege('regression', 'TEMPORARY');
+select has_database_privilege('myuser','regression', 'TEMPORARY');
 \c - - - :master_port
 
 drop user myuser;
@@ -118,35 +99,31 @@ drop user myuser;
 create user myuser;
 
 grant ALL on database regression to myuser;
-set role myuser;
 
-select has_database_privilege('regression', 'CREATE');
-select has_database_privilege('regression', 'CONNECT');
-select has_database_privilege('regression', 'TEMP');
-select has_database_privilege('regression', 'TEMPORARY');
+select has_database_privilege('myuser','regression', 'CREATE');
+select has_database_privilege('myuser','regression', 'CONNECT');
+select has_database_privilege('myuser','regression', 'TEMP');
+select has_database_privilege('myuser','regression', 'TEMPORARY');
 \c - - - :worker_1_port;
-select has_database_privilege('regression', 'CREATE');
-select has_database_privilege('regression', 'CONNECT');
-select has_database_privilege('regression', 'TEMP');
-select has_database_privilege('regression', 'TEMPORARY');
+select has_database_privilege('myuser','regression', 'CREATE');
+select has_database_privilege('myuser','regression', 'CONNECT');
+select has_database_privilege('myuser','regression', 'TEMP');
+select has_database_privilege('myuser','regression', 'TEMPORARY');
 \c - - - :master_port
 
-RESET ROLE;
 
 revoke ALL on database regression from myuser;
-set role myuser;
 
-select has_database_privilege('regression', 'CREATE');
-select has_database_privilege('regression', 'CONNECT');
-select has_database_privilege('regression', 'TEMP');
-select has_database_privilege('regression', 'TEMPORARY');
+select has_database_privilege('myuser','regression', 'CREATE');
+select has_database_privilege('myuser','regression', 'CONNECT');
+select has_database_privilege('myuser','regression', 'TEMP');
+select has_database_privilege('myuser','regression', 'TEMPORARY');
 \c - - - :worker_1_port
 
-set role myuser;
-select has_database_privilege('regression', 'CREATE');
-select has_database_privilege('regression', 'CONNECT');
-select has_database_privilege('regression', 'TEMP');
-select has_database_privilege('regression', 'TEMPORARY');
+select has_database_privilege('myuser','regression', 'CREATE');
+select has_database_privilege('myuser','regression', 'CONNECT');
+select has_database_privilege('myuser','regression', 'TEMP');
+select has_database_privilege('myuser','regression', 'TEMPORARY');
 \c - - - :master_port
 
 drop user myuser;
@@ -156,35 +133,32 @@ drop user myuser;
 create user myuser;
 
 grant CREATE,CONNECT,TEMP,TEMPORARY on database regression to myuser;
-set role myuser;
 
-select has_database_privilege('regression', 'CREATE');
-select has_database_privilege('regression', 'CONNECT');
-select has_database_privilege('regression', 'TEMP');
-select has_database_privilege('regression', 'TEMPORARY');
+select has_database_privilege('myuser','regression', 'CREATE');
+select has_database_privilege('myuser','regression', 'CONNECT');
+select has_database_privilege('myuser','regression', 'TEMP');
+select has_database_privilege('myuser','regression', 'TEMPORARY');
 \c - - - :worker_1_port;
-select has_database_privilege('regression', 'CREATE');
-select has_database_privilege('regression', 'CONNECT');
-select has_database_privilege('regression', 'TEMP');
-select has_database_privilege('regression', 'TEMPORARY');
+select has_database_privilege('myuser','regression', 'CREATE');
+select has_database_privilege('myuser','regression', 'CONNECT');
+select has_database_privilege('myuser','regression', 'TEMP');
+select has_database_privilege('myuser','regression', 'TEMPORARY');
 \c - - - :master_port
 
 RESET ROLE;
 
 revoke CREATE,CONNECT,TEMP,TEMPORARY on database regression from myuser;
-set role myuser;
 
-select has_database_privilege('regression', 'CREATE');
-select has_database_privilege('regression', 'CONNECT');
-select has_database_privilege('regression', 'TEMP');
-select has_database_privilege('regression', 'TEMPORARY');
+select has_database_privilege('myuser','regression', 'CREATE');
+select has_database_privilege('myuser','regression', 'CONNECT');
+select has_database_privilege('myuser','regression', 'TEMP');
+select has_database_privilege('myuser','regression', 'TEMPORARY');
 \c - - - :worker_1_port
 
-set role myuser;
-select has_database_privilege('regression', 'CREATE');
-select has_database_privilege('regression', 'CONNECT');
-select has_database_privilege('regression', 'TEMP');
-select has_database_privilege('regression', 'TEMPORARY');
+select has_database_privilege('myuser','regression', 'CREATE');
+select has_database_privilege('myuser','regression', 'CONNECT');
+select has_database_privilege('myuser','regression', 'TEMP');
+select has_database_privilege('myuser','regression', 'TEMPORARY');
 \c - - - :master_port
 
 drop user myuser;
@@ -197,51 +171,78 @@ create user myuser_1;
 grant CREATE,CONNECT,TEMP,TEMPORARY on database regression to myuser;
 
 set role myuser;
-
+--here since myuser does not have grant option, it should fail
 grant CREATE,CONNECT,TEMP,TEMPORARY on database regression to myuser_1;
 
-set role myuser_1;
-select has_database_privilege('regression', 'CREATE');
-select has_database_privilege('regression', 'CONNECT');
-select has_database_privilege('regression', 'TEMP');
-select has_database_privilege('regression', 'TEMPORARY');
+select has_database_privilege('myuser_1','regression', 'CREATE');
+select has_database_privilege('myuser_1','regression', 'CONNECT');
+select has_database_privilege('myuser_1','regression', 'TEMP');
+select has_database_privilege('myuser_1','regression', 'TEMPORARY');
 
+\c - - - :worker_1_port
+
+select has_database_privilege('myuser_1','regression', 'CREATE');
+select has_database_privilege('myuser_1','regression', 'CONNECT');
+select has_database_privilege('myuser_1','regression', 'TEMP');
+select has_database_privilege('myuser_1','regression', 'TEMPORARY');
+
+\c - - - :master_port
 
 RESET ROLE;
 
 grant CREATE,CONNECT,TEMP,TEMPORARY on database regression to myuser with grant option;
 set role myuser;
 
+--here since myuser have grant option, it should succeed
 grant CREATE,CONNECT,TEMP,TEMPORARY on database regression to myuser_1 granted by myuser;
 
-set role myuser_1;
-select has_database_privilege('regression', 'CREATE');
-select has_database_privilege('regression', 'CONNECT');
-select has_database_privilege('regression', 'TEMP');
-select has_database_privilege('regression', 'TEMPORARY');
+select has_database_privilege('myuser_1','regression', 'CREATE');
+select has_database_privilege('myuser_1','regression', 'CONNECT');
+select has_database_privilege('myuser_1','regression', 'TEMP');
+select has_database_privilege('myuser_1','regression', 'TEMPORARY');
+
+\c - - - :worker_1_port
+
+select has_database_privilege('myuser_1','regression', 'CREATE');
+select has_database_privilege('myuser_1','regression', 'CONNECT');
+select has_database_privilege('myuser_1','regression', 'TEMP');
+select has_database_privilege('myuser_1','regression', 'TEMPORARY');
+
+\c - - - :master_port
+
 
 RESET ROLE;
 
-revoke CREATE,CONNECT,TEMP,TEMPORARY on database regression from myuser_1;
-revoke grant option for CREATE,CONNECT,TEMP,TEMPORARY on database regression from myuser cascade;
+--below test should fail and should throw an error since myuser_1 still have the dependent privileges
+revoke  CREATE,CONNECT,TEMP,TEMPORARY on database regression from myuser restrict;
+--below test should fail and should throw an error since myuser_1 still have the dependent privileges
+revoke grant option for CREATE,CONNECT,TEMP,TEMPORARY on database regression from myuser restrict ;
 
-grant CREATE,CONNECT,TEMP,TEMPORARY on database regression to myuser;
+--below test should succeed and should not throw any error since myuser_1 privileges are revoked with cascade
+revoke grant option for CREATE,CONNECT,TEMP,TEMPORARY on database regression from myuser cascade ;
 
-set role myuser;
+--here we test if myuser still have the privileges after revoke grant option for
 
-grant CREATE,CONNECT,TEMP,TEMPORARY on database regression to myuser_1;
+select has_database_privilege('myuser','regression', 'CREATE');
+select has_database_privilege('myuser','regression', 'CONNECT');
+select has_database_privilege('myuser','regression', 'TEMP');
+select has_database_privilege('myuser','regression', 'TEMPORARY');
 
-set role myuser_1;
+\c - - - :worker_1_port
 
-select has_database_privilege('regression', 'CREATE');
-select has_database_privilege('regression', 'CONNECT');
-select has_database_privilege('regression', 'TEMP');
-select has_database_privilege('regression', 'TEMPORARY');
+select has_database_privilege('myuser','regression', 'CREATE');
+select has_database_privilege('myuser','regression', 'CONNECT');
+select has_database_privilege('myuser','regression', 'TEMP');
+select has_database_privilege('myuser','regression', 'TEMPORARY');
+
+\c - - - :master_port
 
 reset role;
 
-revoke  CREATE,CONNECT,TEMP,TEMPORARY on database regression from myuser_1;
-revoke  CREATE,CONNECT,TEMP,TEMPORARY on database regression from myuser cascade;
+
+
+revoke  CREATE,CONNECT,TEMP,TEMPORARY on database regression from myuser;
+revoke CREATE,CONNECT,TEMP,TEMPORARY on database regression from myuser_1;
 
 drop user myuser_1;
 drop user myuser;
@@ -259,57 +260,105 @@ SELECT result FROM run_command_on_workers($$create database test_db$$);
 revoke connect,temp,temporary  on database test_db from public;
 
 grant CREATE,CONNECT,TEMP,TEMPORARY on database regression,test_db to myuser,myuser_1;
-set role myuser;
 
-select has_database_privilege('regression', 'CREATE');
-select has_database_privilege('regression', 'CONNECT');
-select has_database_privilege('regression', 'TEMP');
-select has_database_privilege('regression', 'TEMPORARY');
+select has_database_privilege('myuser','regression', 'CREATE');
+select has_database_privilege('myuser','regression', 'CONNECT');
+select has_database_privilege('myuser','regression', 'TEMP');
+select has_database_privilege('myuser','regression', 'TEMPORARY');
 
-select has_database_privilege('test_db', 'CREATE');
-select has_database_privilege('test_db', 'CONNECT');
-select has_database_privilege('test_db', 'TEMP');
-select has_database_privilege('test_db', 'TEMPORARY');
+select has_database_privilege('myuser','test_db', 'CREATE');
+select has_database_privilege('myuser','test_db', 'CONNECT');
+select has_database_privilege('myuser','test_db', 'TEMP');
+select has_database_privilege('myuser','test_db', 'TEMPORARY');
 
-set role myuser_1;
-select has_database_privilege('regression', 'CREATE');
-select has_database_privilege('regression', 'CONNECT');
-select has_database_privilege('regression', 'TEMP');
-select has_database_privilege('regression', 'TEMPORARY');
+select has_database_privilege('myuser_1','regression', 'CREATE');
+select has_database_privilege('myuser_1','regression', 'CONNECT');
+select has_database_privilege('myuser_1','regression', 'TEMP');
+select has_database_privilege('myuser_1','regression', 'TEMPORARY');
 
-select has_database_privilege('test_db', 'CREATE');
-select has_database_privilege('test_db', 'CONNECT');
-select has_database_privilege('test_db', 'TEMP');
-select has_database_privilege('test_db', 'TEMPORARY');
+select has_database_privilege('myuser_1','test_db', 'CREATE');
+select has_database_privilege('myuser_1','test_db', 'CONNECT');
+select has_database_privilege('myuser_1','test_db', 'TEMP');
+select has_database_privilege('myuser_1','test_db', 'TEMPORARY');
+
+\c - - - :worker_1_port
+
+select has_database_privilege('myuser','regression', 'CREATE');
+select has_database_privilege('myuser','regression', 'CONNECT');
+select has_database_privilege('myuser','regression', 'TEMP');
+select has_database_privilege('myuser','regression', 'TEMPORARY');
+
+select has_database_privilege('myuser','test_db', 'CREATE');
+select has_database_privilege('myuser','test_db', 'CONNECT');
+select has_database_privilege('myuser','test_db', 'TEMP');
+select has_database_privilege('myuser','test_db', 'TEMPORARY');
+
+select has_database_privilege('myuser_1','regression', 'CREATE');
+select has_database_privilege('myuser_1','regression', 'CONNECT');
+select has_database_privilege('myuser_1','regression', 'TEMP');
+select has_database_privilege('myuser_1','regression', 'TEMPORARY');
+
+select has_database_privilege('myuser_1','test_db', 'CREATE');
+select has_database_privilege('myuser_1','test_db', 'CONNECT');
+select has_database_privilege('myuser_1','test_db', 'TEMP');
+select has_database_privilege('myuser_1','test_db', 'TEMPORARY');
+
+\c - - - :master_port
 
 RESET ROLE;
+--below test should fail and should throw an error
+revoke  CREATE,CONNECT,TEMP,TEMPORARY on database regression,test_db from myuser ;
 
-
+--below test should succeed and should not throw any error
 revoke  CREATE,CONNECT,TEMP,TEMPORARY on database regression,test_db from myuser_1;
+
+--below test should succeed and should not throw any error
 revoke  CREATE,CONNECT,TEMP,TEMPORARY on database regression,test_db from myuser cascade;
 
-set role myuser;
 
-select has_database_privilege('regression', 'CREATE');
-select has_database_privilege('regression', 'CONNECT');
-select has_database_privilege('regression', 'TEMP');
-select has_database_privilege('regression', 'TEMPORARY');
+select has_database_privilege('myuser','regression', 'CREATE');
+select has_database_privilege('myuser','regression', 'CONNECT');
+select has_database_privilege('myuser','regression', 'TEMP');
+select has_database_privilege('myuser','regression', 'TEMPORARY');
 
-select has_database_privilege('test_db', 'CREATE');
-select has_database_privilege('test_db', 'CONNECT');
-select has_database_privilege('test_db', 'TEMP');
-select has_database_privilege('test_db', 'TEMPORARY');
+select has_database_privilege('myuser','test_db', 'CREATE');
+select has_database_privilege('myuser','test_db', 'CONNECT');
+select has_database_privilege('myuser','test_db', 'TEMP');
+select has_database_privilege('myuser','test_db', 'TEMPORARY');
 
-set role myuser_1;
-select has_database_privilege('regression', 'CREATE');
-select has_database_privilege('regression', 'CONNECT');
-select has_database_privilege('regression', 'TEMP');
-select has_database_privilege('regression', 'TEMPORARY');
+select has_database_privilege('myuser_1','regression', 'CREATE');
+select has_database_privilege('myuser_1','regression', 'CONNECT');
+select has_database_privilege('myuser_1','regression', 'TEMP');
+select has_database_privilege('myuser_1','regression', 'TEMPORARY');
 
-select has_database_privilege('test_db', 'CREATE');
-select has_database_privilege('test_db', 'CONNECT');
-select has_database_privilege('test_db', 'TEMP');
-select has_database_privilege('test_db', 'TEMPORARY');
+select has_database_privilege('myuser_1','test_db', 'CREATE');
+select has_database_privilege('myuser_1','test_db', 'CONNECT');
+select has_database_privilege('myuser_1','test_db', 'TEMP');
+select has_database_privilege('myuser_1','test_db', 'TEMPORARY');
+
+\c - - - :worker_1_port
+
+select has_database_privilege('myuser','regression', 'CREATE');
+select has_database_privilege('myuser','regression', 'CONNECT');
+select has_database_privilege('myuser','regression', 'TEMP');
+select has_database_privilege('myuser','regression', 'TEMPORARY');
+
+select has_database_privilege('myuser','test_db', 'CREATE');
+select has_database_privilege('myuser','test_db', 'CONNECT');
+select has_database_privilege('myuser','test_db', 'TEMP');
+select has_database_privilege('myuser','test_db', 'TEMPORARY');
+
+select has_database_privilege('myuser_1','regression', 'CREATE');
+select has_database_privilege('myuser_1','regression', 'CONNECT');
+select has_database_privilege('myuser_1','regression', 'TEMP');
+select has_database_privilege('myuser_1','regression', 'TEMPORARY');
+
+select has_database_privilege('myuser_1','test_db', 'CREATE');
+select has_database_privilege('myuser_1','test_db', 'CONNECT');
+select has_database_privilege('myuser_1','test_db', 'TEMP');
+select has_database_privilege('myuser_1','test_db', 'TEMPORARY');
+
+\c - - - :master_port
 
 reset role;
 
