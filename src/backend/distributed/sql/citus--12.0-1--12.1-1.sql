b8bded6454561c6e3fe4995969d5e59eb2643cdb
Gürkan İndibay <gindibay@microsoft.com>
2023-09-01 11:39:30 +0300

    Adds citus_pause_node udf (#7089)
    DESCRIPTION: Presenting citus_pause_node UDF enabling pausing by
    node_id.
    
    citus_pause_node takes a node_id parameter and fetches all the shards in
    that node and puts AccessExclusiveLock on all the shards inside that
    node. With this lock, insert is disabled, until citus_pause_node
    transaction is closed.
    
    ---------
    
    Co-authored-by: Hanefi Onaldi <Hanefi.Onaldi@microsoft.com>

--- src/backend/distributed/sql/citus--12.0-1--12.1-1.sql
+++ src/backend/distributed/sql/citus--12.0-1--12.1-1.sql
@@ -2,6 +2,7 @@
 
 -- bump version to 12.1-1
 
+#include "udfs/citus_pause_node_within_txn/12.1-1.sql"
 #include "udfs/citus_prepare_pg_upgrade/12.1-1.sql"
 #include "udfs/citus_finish_pg_upgrade/12.1-1.sql"
 
