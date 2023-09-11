12254398cb515c1340f8a30fa903b24a814585b1
gindibay <gindibay@microsoft.com>
2023-09-12 02:46:22 +0300

    Fixes review notes

--- src/test/regress/sql/alter_database_propagation.sql
+++ src/test/regress/sql/alter_database_propagation.sql
@@ -7,30 +7,12 @@ set citus.grep_remote_commands = '%ALTER DATABASE%';
 alter database regression ALLOW_CONNECTIONS false;
 
 
-DO $$
-declare
-    v_connlimit_initial numeric;
-    v_connlimit_fetched int;
-begin
-    select datconnlimit into v_connlimit_initial from pg_database where datname = 'regression';
-    alter database regression with CONNECTION LIMIT 100;
-    select datconnlimit into v_connlimit_fetched from pg_database where datname = 'regression';
-    raise notice 'v_connlimit_initial: %, v_connlimit_fetched: %', v_connlimit_initial, v_connlimit_fetched;
-    execute 'alter database regression with CONNECTION LIMIT ' || v_connlimit_initial;
-    select datconnlimit into v_connlimit_fetched from pg_database where datname = 'regression';
-    raise notice 'v_connlimit_initial: %, v_connlimit_fetched: %', v_connlimit_initial, v_connlimit_fetched;
-
-    alter database regression with IS_TEMPLATE true CONNECTION LIMIT 100;
-    execute 'alter database regression with IS_TEMPLATE false  CONNECTION LIMIT' || v_connlimit_initial;
-
-    alter database regression with IS_TEMPLATE true;
-    select datistemplate from pg_database where datname = 'regression';
-
-    alter database regression with IS_TEMPLATE false;
-    select datistemplate from pg_database where datname = 'regression';
-end;
-$$
-language plpgsql;
-
+alter database regression with CONNECTION LIMIT 100;
+alter database regression with IS_TEMPLATE true CONNECTION LIMIT 50;
+alter database regression with CONNECTION LIMIT -1;
+alter database regression with IS_TEMPLATE true;
+alter database regression with IS_TEMPLATE false;
+-- this statement will get error since we don't have a multiple database support for now
+alter database regression rename to regression2;
 
 set citus.log_remote_commands = false;
