e5e64b74541261bd06505a536c89fd5a0f5dc278
Gürkan İndibay <gindibay@microsoft.com>
2023-09-12 14:09:15 +0300

    Adds alter database propagation - with and refresh collation  (#7172)
    DESCRIPTION: Adds ALTER DATABASE WITH ... and REFRESH COLLATION VERSION
    support
    
    This PR adds supports for basic ALTER DATABASE statements propagation 
    support. Below statements are supported:
    ALTER DATABASE <database_name> with IS_TEMPLATE <true/false>;
    ALTER DATABASE <database_name> with CONNECTION LIMIT <integer_value>;
    ALTER DATABASE <database_name> REFRESH COLLATION VERSION;
    
    ---------
    
    Co-authored-by: Jelte Fennema-Nio <jelte.fennema@microsoft.com>

--- src/backend/distributed/commands/distribute_object_ops.c
+++ src/backend/distributed/commands/distribute_object_ops.c
@@ -444,6 +444,30 @@ static DistributeObjectOps Database_Grant = {
 	.markDistributed = false,
 };
 
+static DistributeObjectOps Database_Alter = {
+	.deparse = DeparseAlterDatabaseStmt,
+	.qualify = NULL,
+	.preprocess = PreprocessAlterDatabaseStmt,
+	.postprocess = NULL,
+	.objectType = OBJECT_DATABASE,
+	.operationType = DIST_OPS_ALTER,
+	.address = NULL,
+	.markDistributed = false,
+};
+
+#if PG_VERSION_NUM >= PG_VERSION_15
+static DistributeObjectOps Database_RefreshColl = {
+	.deparse = DeparseAlterDatabaseRefreshCollStmt,
+	.qualify = NULL,
+	.preprocess = PreprocessAlterDatabaseRefreshCollStmt,
+	.postprocess = NULL,
+	.objectType = OBJECT_DATABASE,
+	.operationType = DIST_OPS_ALTER,
+	.address = NULL,
+	.markDistributed = false,
+};
+#endif
+
 static DistributeObjectOps Domain_Alter = {
 	.deparse = DeparseAlterDomainStmt,
 	.qualify = QualifyAlterDomainStmt,
@@ -1272,7 +1296,6 @@ static DistributeObjectOps Trigger_Rename = {
 	.markDistributed = false,
 };
 
-
 /*
  * GetDistributeObjectOps looks up the DistributeObjectOps which handles the node.
  *
@@ -1283,6 +1306,18 @@ GetDistributeObjectOps(Node *node)
 {
 	switch (nodeTag(node))
 	{
+		case T_AlterDatabaseStmt:
+		{
+			return &Database_Alter;
+		}
+
+#if PG_VERSION_NUM >= PG_VERSION_15
+		case T_AlterDatabaseRefreshCollStmt:
+		{
+			return &Database_RefreshColl;
+		}
+
+#endif
 		case T_AlterDomainStmt:
 		{
 			return &Domain_Alter;
--- src/backend/distributed/deparser/deparse_database_stmts.c
+++ src/backend/distributed/deparser/deparse_database_stmts.c
@@ -20,9 +20,12 @@
 
 #include "distributed/deparser.h"
 #include "distributed/citus_ruleutils.h"
+#include "commands/defrem.h"
+#include "distributed/deparser.h"
+#include "distributed/log_utils.h"
 
 static void AppendAlterDatabaseOwnerStmt(StringInfo buf, AlterOwnerStmt *stmt);
-
+static void AppendAlterDatabaseStmt(StringInfo buf, AlterDatabaseStmt *stmt);
 
 char *
 DeparseAlterDatabaseOwnerStmt(Node *node)
@@ -82,6 +85,52 @@ AppendGrantOnDatabaseStmt(StringInfo buf, GrantStmt *stmt)
 }
 
 
+static void
+AppendDefElemConnLimit(StringInfo buf, DefElem *def)
+{
+	appendStringInfo(buf, " CONNECTION LIMIT %ld", (long int) defGetNumeric(def));
+}
+
+
+static void
+AppendAlterDatabaseStmt(StringInfo buf, AlterDatabaseStmt *stmt)
+{
+	appendStringInfo(buf, "ALTER DATABASE %s ", quote_identifier(stmt->dbname));
+
+	if (stmt->options)
+	{
+		ListCell *cell = NULL;
+		appendStringInfo(buf, "WITH ");
+		foreach(cell, stmt->options)
+		{
+			DefElem *def = castNode(DefElem, lfirst(cell));
+			if (strcmp(def->defname, "is_template") == 0)
+			{
+				appendStringInfo(buf, "IS_TEMPLATE %s",
+								 quote_literal_cstr(strVal(def->arg)));
+			}
+			else if (strcmp(def->defname, "connection_limit") == 0)
+			{
+				AppendDefElemConnLimit(buf, def);
+			}
+			else if (strcmp(def->defname, "allow_connections") == 0)
+			{
+				ereport(ERROR,
+						errmsg("ALLOW_CONNECTIONS is not supported"));
+			}
+			else
+			{
+				ereport(ERROR,
+						errmsg("unrecognized ALTER DATABASE option: %s",
+							   def->defname));
+			}
+		}
+	}
+
+	appendStringInfo(buf, ";");
+}
+
+
 char *
 DeparseGrantOnDatabaseStmt(Node *node)
 {
@@ -95,3 +144,37 @@ DeparseGrantOnDatabaseStmt(Node *node)
 
 	return str.data;
 }
+
+
+char *
+DeparseAlterDatabaseStmt(Node *node)
+{
+	AlterDatabaseStmt *stmt = castNode(AlterDatabaseStmt, node);
+
+	StringInfoData str = { 0 };
+	initStringInfo(&str);
+
+	AppendAlterDatabaseStmt(&str, stmt);
+
+	return str.data;
+}
+
+
+#if PG_VERSION_NUM >= PG_VERSION_15
+char *
+DeparseAlterDatabaseRefreshCollStmt(Node *node)
+{
+	AlterDatabaseRefreshCollStmt *stmt = (AlterDatabaseRefreshCollStmt *) node;
+
+	StringInfoData str;
+	initStringInfo(&str);
+
+	appendStringInfo(&str, "ALTER DATABASE %s REFRESH COLLATION VERSION;",
+					 quote_identifier(
+						 stmt->dbname));
+
+	return str.data;
+}
+
+
+#endif
