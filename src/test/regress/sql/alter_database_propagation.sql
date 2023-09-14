7c0b2897615f1f0bbf1d7ea964e1e7786f40f300
Gürkan İndibay <gindibay@microsoft.com>
2023-09-14 16:29:16 +0300

    Adds alter database set option (#7181)
    DESCRIPTION: Adds support for ALTER DATABASE <db_name> SET .. statement
    propagation
    SET statements in Postgres has a common structure which is already being
    used in Alter Function
    statement. 
    In this PR, I added a util file; citus_setutils and made it usable for
    both for
    alter database<db_name>set .. and alter function ... set ... statements.
    With this PR, below statements will be propagated
    ```sql
    ALTER DATABASE name SET configuration_parameter { TO | = } { value | DEFAULT }
    ALTER DATABASE name SET configuration_parameter FROM CURRENT
    ALTER DATABASE name RESET configuration_parameter
    ALTER DATABASE name RESET ALL
    ```
    Additionally, there was a bug in processing float values in the common
    code block.
    I fixed this one as well
    
    Previous
    ```C
    case T_Float:
    			{
    				appendStringInfo(buf, " %s", strVal(value));
    				break;
    			}
    ```
    Now
    ```C
    case T_Float:
    			{
    				appendStringInfo(buf, " %s", nodeToString(value));
    				break;
    			}
    ```

--- src/test/regress/sql/alter_database_propagation.sql
+++ src/test/regress/sql/alter_database_propagation.sql
@@ -1,6 +1,7 @@
 set citus.log_remote_commands = true;
 set citus.grep_remote_commands = '%ALTER DATABASE%';
 
+
 -- since ALLOW_CONNECTIONS alter option should be executed in a different database
 -- and since we don't have a multiple database support for now,
 -- this statement will get error
@@ -14,5 +15,45 @@ alter database regression with IS_TEMPLATE true;
 alter database regression with IS_TEMPLATE false;
 -- this statement will get error since we don't have a multiple database support for now
 alter database regression rename to regression2;
+
+alter database regression set default_transaction_read_only = true;
+
+set default_transaction_read_only = false;
+
+alter database regression set default_transaction_read_only from current;
+alter database regression set default_transaction_read_only to DEFAULT;
+alter database regression RESET default_transaction_read_only;
+
+alter database regression SET TIME ZONE '-7';
+
+alter database regression set TIME ZONE LOCAL;
+alter database regression set TIME ZONE DEFAULT;
+alter database regression RESET TIME ZONE;
+
+alter database regression SET TIME ZONE INTERVAL '-08:00' HOUR TO MINUTE;
+
+alter database regression RESET TIME ZONE;
+
+
+alter database regression set default_transaction_isolation = 'serializable';
+set default_transaction_isolation = 'read committed';
+
+alter database regression set default_transaction_isolation from current;
+alter database regression set default_transaction_isolation to DEFAULT;
+alter database regression RESET default_transaction_isolation;
+
+alter database regression set statement_timeout = 1000;
+set statement_timeout = 2000;
+
+alter database regression set statement_timeout from current;
+alter database regression set statement_timeout to DEFAULT;
+alter database regression RESET statement_timeout;
+
+alter database regression set lock_timeout = 1201.5;
+set lock_timeout = 1202.5;
+
+alter database regression set lock_timeout from current;
+alter database regression set lock_timeout to DEFAULT;
+alter database regression RESET lock_timeout;
 
 set citus.log_remote_commands = false;
