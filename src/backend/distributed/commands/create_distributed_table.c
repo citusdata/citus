6ba68a978fc9a0e33152763227224841f5c62abe
Onur Tirtir <onurcantirtir@gmail.com>
2023-09-25 11:14:35 +0300

    Make sure to disallow creating a replicated distributed table concurrently (#7219)
    See explanation in https://github.com/citusdata/citus/issues/7216.
    Fixes https://github.com/citusdata/citus/issues/7216.
    
    DESCRIPTION: Makes sure to disallow creating a replicated distributed
    table concurrently
    
    (cherry picked from commit 111b4c19bc0a429abc5be56dbb6ded9f080fa110)

--- src/backend/distributed/commands/create_distributed_table.c
+++ src/backend/distributed/commands/create_distributed_table.c
@@ -415,6 +415,19 @@ CreateDistributedTableConcurrently(Oid relationId, char *distributionColumnName,
 	if (!IsColocateWithDefault(colocateWithTableName) && !IsColocateWithNone(
 			colocateWithTableName))
 	{
+		if (replicationModel != REPLICATION_MODEL_STREAMING)
+		{
+			ereport(ERROR, (errmsg("cannot create distributed table "
+								   "concurrently because Citus allows "
+								   "concurrent table distribution only when "
+								   "citus.shard_replication_factor = 1"),
+							errhint("table %s is requested to be colocated "
+									"with %s which has "
+									"citus.shard_replication_factor > 1",
+									get_rel_name(relationId),
+									colocateWithTableName)));
+		}
+
 		EnsureColocateWithTableIsValid(relationId, distributionMethod,
 									   distributionColumnName,
 									   colocateWithTableName);
