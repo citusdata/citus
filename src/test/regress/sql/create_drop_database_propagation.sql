e46c9cff9b90dd49083bac026561d57706960826
gindibay <gindibay@microsoft.com>
2023-10-04 23:11:48 +0300

    Changes encoding

--- src/test/regress/sql/create_drop_database_propagation.sql
+++ src/test/regress/sql/create_drop_database_propagation.sql
@@ -4,20 +4,20 @@ create user create_drop_db_test_user;
 
 CREATE DATABASE mydatabase
     WITH TEMPLATE = 'template0'
-          OWNER = create_drop_db_test_user
-          CONNECTION LIMIT = 10
-          ENCODING = 'UTF8'
-          STRATEGY = 'wal_log'
-          LOCALE = 'en_US.UTF-8'
-          LC_COLLATE = 'en_US.UTF-8'
-          LC_CTYPE = 'en_US.UTF-8'
-          ICU_LOCALE = 'en_US.UTF-8'
-          LOCALE_PROVIDER = 'icu'
-          COLLATION_VERSION = '1.0'
-          TABLESPACE = test_tablespace
-          ALLOW_CONNECTIONS = true
-          IS_TEMPLATE = false
-          OID = 966345;
+            OWNER = create_drop_db_test_user
+            CONNECTION LIMIT = 10
+            ENCODING = 'UTF8'
+            STRATEGY = 'wal_log'
+            LOCALE = 'C'
+            LC_COLLATE = 'C'
+            LC_CTYPE = 'C'
+            ICU_LOCALE = 'C'
+            LOCALE_PROVIDER = 'icu'
+            COLLATION_VERSION = '1.0'
+            TABLESPACE = test_tablespace
+            ALLOW_CONNECTIONS = true
+            IS_TEMPLATE = false
+            OID = 966345;
 
 SELECT pd.datname  , pd.datdba, pd.encoding, pd.datlocprovider,
 pd.datistemplate, pd.datallowconn, pd.datconnlimit,
