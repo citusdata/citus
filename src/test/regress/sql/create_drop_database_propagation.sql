d0bd20c9b1f7d385b58ce2ded6219ab4a4eaefc6
gindibay <gindibay@microsoft.com>
2023-10-05 17:11:07 +0300

    Fixes tests

--- src/test/regress/sql/create_drop_database_propagation.sql
+++ src/test/regress/sql/create_drop_database_propagation.sql
@@ -1,45 +1,111 @@
-set citus.enable_create_database_propagation=on;
+
+
+\set create_drop_db_tablespace :abs_srcdir '/tmp_check/ts3'
+CREATE TABLESPACE create_drop_db_tablespace LOCATION :'create_drop_db_tablespace';
+
+\c - - - :worker_1_port
+\set create_drop_db_tablespace :abs_srcdir '/tmp_check/ts4'
+CREATE TABLESPACE create_drop_db_tablespace LOCATION :'create_drop_db_tablespace';
+
+\c - - - :worker_2_port
+\set create_drop_db_tablespace :abs_srcdir '/tmp_check/ts5'
+CREATE TABLESPACE create_drop_db_tablespace LOCATION :'create_drop_db_tablespace';
+
+\c - - - :master_port
 create user create_drop_db_test_user;
 
+set citus.enable_create_database_propagation=on;
 
 CREATE DATABASE mydatabase
     WITH TEMPLATE = 'template0'
             OWNER = create_drop_db_test_user
             CONNECTION LIMIT = 10
             ENCODING = 'UTF8'
-            STRATEGY = 'wal_log'
-            LOCALE = 'C'
             LC_COLLATE = 'C'
             LC_CTYPE = 'C'
-            ICU_LOCALE = 'C'
-            LOCALE_PROVIDER = 'icu'
-            COLLATION_VERSION = '1.0'
             TABLESPACE = test_tablespace
             ALLOW_CONNECTIONS = true
-            IS_TEMPLATE = false
-            OID = 966345;
+            IS_TEMPLATE = false;
 
-SELECT pd.datname  , pd.datdba, pd.encoding, pd.datlocprovider,
+SELECT pd.datname  , pd.datdba, pd.encoding,
 pd.datistemplate, pd.datallowconn, pd.datconnlimit,
-pd.datcollate , pd. datctype  , pd.daticulocale, pd.datcollversion,
-pd.datacl, rolname AS database_owner, pa.rolname AS database_owner, pt.spcname AS tablespace
+pd.datcollate , pd. datctype  ,  pd.datacl, rolname AS database_owner,
+pa.rolname AS database_owner, pt.spcname AS tablespace
 FROM pg_database pd
 JOIN pg_authid pa ON pd.datdba = pa.oid
 join pg_tablespace pt on pd.dattablespace = pt.oid
 WHERE datname = 'mydatabase';
 
+\c - - - :worker_1_port
+
+SELECT pd.datname  , pd.datdba, pd.encoding,
+pd.datistemplate, pd.datallowconn, pd.datconnlimit,
+pd.datcollate , pd. datctype  ,  pd.datacl, rolname AS database_owner,
+pa.rolname AS database_owner, pt.spcname AS tablespace
+FROM pg_database pd
+JOIN pg_authid pa ON pd.datdba = pa.oid
+join pg_tablespace pt on pd.dattablespace = pt.oid
+WHERE datname = 'mydatabase';
+\c - - - :worker_2_port
+
+SELECT pd.datname  , pd.datdba, pd.encoding,
+pd.datistemplate, pd.datallowconn, pd.datconnlimit,
+pd.datcollate , pd. datctype  ,  pd.datacl, rolname AS database_owner,
+pa.rolname AS database_owner, pt.spcname AS tablespace
+FROM pg_database pd
+JOIN pg_authid pa ON pd.datdba = pa.oid
+join pg_tablespace pt on pd.dattablespace = pt.oid
+WHERE datname = 'mydatabase';
+
+\c - - - :master_port
+set citus.enable_create_database_propagation=on;
 drop database mydatabase;
 
-SELECT pd.datname  , pd.datdba, pd.encoding, pd.datlocprovider,
+SELECT pd.datname  , pd.datdba, pd.encoding,
 pd.datistemplate, pd.datallowconn, pd.datconnlimit,
-pd.datcollate , pd. datctype  , pd.daticulocale, pd.datcollversion,
-pd.datacl, rolname AS database_owner, pa.rolname AS database_owner, pt.spcname AS tablespace
+pd.datcollate , pd. datctype  ,  pd.datacl, rolname AS database_owner,
+pa.rolname AS database_owner, pt.spcname AS tablespace
 FROM pg_database pd
 JOIN pg_authid pa ON pd.datdba = pa.oid
 join pg_tablespace pt on pd.dattablespace = pt.oid
 WHERE datname = 'mydatabase';
 
+\c - - - :worker_1_port
+
+SELECT pd.datname  , pd.datdba, pd.encoding,
+pd.datistemplate, pd.datallowconn, pd.datconnlimit,
+pd.datcollate , pd. datctype  ,  pd.datacl, rolname AS database_owner,
+pa.rolname AS database_owner, pt.spcname AS tablespace
+FROM pg_database pd
+JOIN pg_authid pa ON pd.datdba = pa.oid
+join pg_tablespace pt on pd.dattablespace = pt.oid
+WHERE datname = 'mydatabase';
+
+\c - - - :worker_2_port
+
+SELECT pd.datname  , pd.datdba, pd.encoding,
+pd.datistemplate, pd.datallowconn, pd.datconnlimit,
+pd.datcollate , pd. datctype  ,  pd.datacl, rolname AS database_owner,
+pa.rolname AS database_owner, pt.spcname AS tablespace
+FROM pg_database pd
+JOIN pg_authid pa ON pd.datdba = pa.oid
+join pg_tablespace pt on pd.dattablespace = pt.oid
+WHERE datname = 'mydatabase';
+
+\c - - - :master_port
+
+drop tablespace create_drop_db_tablespace;
+
+\c - - - :worker_1_port
+
+drop tablespace create_drop_db_tablespace;
+
+\c - - - :worker_2_port
+
+drop tablespace create_drop_db_tablespace;
+
+\c - - - :master_port
+
 drop user create_drop_db_test_user;
-set citus.enable_create_database_propagation=off;
 
 
