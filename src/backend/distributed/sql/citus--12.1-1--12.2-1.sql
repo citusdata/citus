1e91b144e2ca5a16cbc1d66f6b10a1b1ced05ac4
gindibay <gindibay@microsoft.com>
2023-10-14 20:49:26 +0300

    Fixed review issues

--- src/backend/distributed/sql/citus--12.1-1--12.2-1.sql
+++ src/backend/distributed/sql/citus--12.1-1--12.2-1.sql
@@ -1,16 +1,4 @@
 -- citus--12.1-1--12.2-1
-
---
--- citus_internal_database_command creates a database according to the given command.
-
-CREATE OR REPLACE FUNCTION pg_catalog.citus_internal_database_command(command text)
- RETURNS void
- LANGUAGE C
- STRICT
-AS 'MODULE_PATHNAME', $$citus_internal_database_command$$;
-COMMENT ON FUNCTION pg_catalog.citus_internal_database_command(text) IS
- 'run a database command without transaction block restrictions';
-
 -- bump version to 12.2-1
-
+#include "udfs/citus_internal_database_command/12.2-1.sql"
 #include "udfs/citus_add_rebalance_strategy/12.2-1.sql"
---
+++ src/backend/distributed/sql/udfs/citus_internal_database_command/latest.sql
@@ -0,0 +1,10 @@
+--
+-- citus_internal_database_command creates a database according to the given command.
+
+CREATE OR REPLACE FUNCTION pg_catalog.citus_internal_database_command(command text)
+ RETURNS void
+ LANGUAGE C
+ STRICT
+AS 'MODULE_PATHNAME', $$citus_internal_database_command$$;
+COMMENT ON FUNCTION pg_catalog.citus_internal_database_command(text) IS
+ 'run a database command without transaction block restrictions';
