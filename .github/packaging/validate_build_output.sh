f5e37bf6682a4190c097fcca134343de68f1774f
Nils Dijk <nils@citusdata.com>
2023-11-10 13:45:35 +0000

    add root of tools to python path

--- .github/packaging/validate_build_output.sh
+++ .github/packaging/validate_build_output.sh
@@ -34,6 +34,7 @@ if [[ ${package_type} == "rpm" && $(get_rpm_os_version) == 7* ]]; then
     python3 -m pip install 'urllib3<2'
 fi
 
+export PYTHONPATH=/
 python3 -m tools.packaging_automation.validate_build_output --output_file output.log \
                                                             --ignore_file .github/packaging/packaging_ignore.yml \
                                                             --package_type ${package_type}
