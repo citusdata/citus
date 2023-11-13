2ca6e2c3580db227f86743caac6dbd0eed3d164c
Onur Tirtir <onurcantirtir@gmail.com>
2023-11-13 11:43:31 +0300

    indent

--- src/backend/distributed/metadata/metadata_sync.c
+++ src/backend/distributed/metadata/metadata_sync.c
@@ -3959,8 +3959,8 @@ citus_internal_database_command(PG_FUNCTION_ARGS)
 	else
 	{
 		ereport(ERROR, (errmsg("citus_internal_database_command() can only be used "
-                               "for CREATE DATABASE and DROP DATABASE commands by "
-                               "Citus.")));
+							   "for CREATE DATABASE and DROP DATABASE commands by "
+							   "Citus.")));
 	}
 
 	/* rollback GUCs to the state before this session */
