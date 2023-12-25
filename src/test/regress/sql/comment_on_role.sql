339627fe9c1927deb0696ec506c17bef76c2cd75
gurkanindibay <gindibay@microsoft.com>
2023-12-25 19:44:34 +0300

    Fixes review comments

--- src/test/regress/sql/comment_on_role.sql
+++ src/test/regress/sql/comment_on_role.sql
@@ -2,41 +2,41 @@ set citus.log_remote_commands to on;
 
 set citus.grep_remote_commands to 'COMMENT ON ROLE';
 
-create role role1;
+create role "role1-\!escape";
 
-comment on ROLE role1 is 'test-comment';
-
-SELECT result FROM run_command_on_all_nodes(
-  $$
-    SELECT ds.description AS role_comment
-    FROM pg_roles r
-    LEFT JOIN pg_shdescription ds ON r.oid = ds.objoid
-    WHERE r.rolname = 'role1';
-  $$
-);
-
-comment on role role1 is 'comment-needs\!escape';
+comment on ROLE "role1-\!escape" is 'test-comment';
 
 SELECT result FROM run_command_on_all_nodes(
   $$
     SELECT ds.description AS role_comment
     FROM pg_roles r
     LEFT JOIN pg_shdescription ds ON r.oid = ds.objoid
-    WHERE r.rolname = 'role1';
+    WHERE r.rolname = 'role1-\!escape';
   $$
 );
 
-comment on role role1 is NULL;
+comment on role "role1-\!escape" is 'comment-needs\!escape';
 
 SELECT result FROM run_command_on_all_nodes(
   $$
     SELECT ds.description AS role_comment
     FROM pg_roles r
     LEFT JOIN pg_shdescription ds ON r.oid = ds.objoid
-    WHERE r.rolname = 'role1';
+    WHERE r.rolname = 'role1-\!escape';
   $$
 );
 
-drop role role1;
+comment on role "role1-\!escape" is NULL;
+
+SELECT result FROM run_command_on_all_nodes(
+  $$
+    SELECT ds.description AS role_comment
+    FROM pg_roles r
+    LEFT JOIN pg_shdescription ds ON r.oid = ds.objoid
+    WHERE r.rolname = 'role1-\!escape';
+  $$
+);
+
+drop role "role1-\!escape";
 reset citus.grep_remote_commands;
 reset citus.log_remote_commands;
