34b8914c6957ac649dbfbe1dd74cb67f7669f1ed
gurkanindibay <gindibay@microsoft.com>
2024-01-24 17:51:28 +0300

    Fixes grantor on role metadata sync

--- src/backend/distributed/deparser/deparse_role_stmts.c
+++ src/backend/distributed/deparser/deparse_role_stmts.c
@@ -486,7 +486,6 @@ AppendGrantRoleStmt(StringInfo buf, GrantRoleStmt *stmt)
 	appendStringInfo(buf, "%s ", stmt->is_grant ? " TO " : " FROM ");
 	AppendRoleList(buf, stmt->grantee_roles);
 	AppendGrantWithAdminOption(buf, stmt);
-	AppendGrantedByInGrantForRoleSpec(buf, stmt->grantor, stmt->is_grant);
 	AppendGrantRestrictAndCascadeForRoleSpec(buf, stmt->behavior, stmt->is_grant);
 	AppendGrantedByInGrantForRoleSpec(buf, stmt->grantor, stmt->is_grant);
 	appendStringInfo(buf, ";");
