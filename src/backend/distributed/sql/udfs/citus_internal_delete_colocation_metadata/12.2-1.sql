89228fcb4ae8b6b469f4c7b46594e7fd7a57fd4d
eaydingol <ebruaydin@gmail.com>
2024-01-31 13:45:22 +0000

    Style fixes

--- src/backend/distributed/sql/udfs/citus_internal_delete_colocation_metadata/12.2-1.sql
+++ src/backend/distributed/sql/udfs/citus_internal_delete_colocation_metadata/12.2-1.sql
@@ -7,7 +7,7 @@ CREATE OR REPLACE FUNCTION citus_internal.delete_colocation_metadata(
 
 COMMENT ON FUNCTION citus_internal.delete_colocation_metadata(int) IS
     'deletes a co-location group from pg_dist_colocation';
-    
+
 CREATE OR REPLACE FUNCTION pg_catalog.citus_internal_delete_colocation_metadata(
 							colocation_id int)
     RETURNS void
