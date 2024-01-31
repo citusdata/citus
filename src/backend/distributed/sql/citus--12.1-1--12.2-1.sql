f9b61cd9739858cde5b5dfaeb5ee2bd359feeb5b
eaydingol <ebruaydin@gmail.com>
2024-01-31 12:43:53 +0000

    Move citus_internal_delete_placement_metadata and citus_internal_delete_partition_metadata

--- src/backend/distributed/sql/citus--12.1-1--12.2-1.sql
+++ src/backend/distributed/sql/citus--12.1-1--12.2-1.sql
@@ -34,3 +34,5 @@ REVOKE ALL ON FUNCTION citus_internal.start_management_transaction FROM PUBLIC;
 #include "udfs/citus_internal_add_tenant_schema/12.2-1.sql"
 #include "udfs/citus_internal_adjust_local_clock_to_remote/12.2-1.sql"
 #include "udfs/citus_internal_delete_colocation_metadata/12.2-1.sql"
+#include "udfs/citus_internal_delete_partition_metadata/12.2-1.sql"
+#include "udfs/citus_internal_delete_placement_metadata/12.2-1.sql"
