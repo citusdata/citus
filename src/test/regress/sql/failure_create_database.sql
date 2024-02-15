d7f09f72dafc7c57255a6b19d15eff0cdbd1cef4
Onur Tirtir <onurcantirtir@gmail.com>
2024-02-15 15:53:56 +0300

    and one more

--- src/test/regress/sql/failure_create_database.sql
+++ src/test/regress/sql/failure_create_database.sql
@@ -68,4 +68,12 @@ CALL citus_cleanup_orphaned_resources();
 SELECT bool_and(result::boolean) AS no_temp_databases_on_any_nodes FROM run_command_on_all_nodes($$SELECT COUNT(*)=0 FROM pg_database WHERE datname LIKE 'citus_temp_database_%'$$);
 SELECT * FROM public.check_database_on_all_nodes($$db1$$) ORDER BY node_type, result;
 
+SELECT citus.mitmproxy('conn.onParse(query="^WITH distributed_object_data").kill()');
+CREATE DATABASE db1;
+SELECT citus.mitmproxy('conn.allow()');
+
+CALL citus_cleanup_orphaned_resources();
+SELECT bool_and(result::boolean) AS no_temp_databases_on_any_nodes FROM run_command_on_all_nodes($$SELECT COUNT(*)=0 FROM pg_database WHERE datname LIKE 'citus_temp_database_%'$$);
+SELECT * FROM public.check_database_on_all_nodes($$db1$$) ORDER BY node_type, result;
+
 RESET client_min_messages;
