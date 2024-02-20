3a5cc909a41a8d79846b0151bdb462c8993cced8
gurkanindibay <gindibay@microsoft.com>
2024-02-20 10:19:19 +0300

    Fixes test errors after merge

--- src/test/regress/sql/metadata_sync_from_non_maindb.sql
+++ src/test/regress/sql/metadata_sync_from_non_maindb.sql
@@ -1,7 +1,15 @@
+
 CREATE SCHEMA metadata_sync_2pc_schema;
+
 SET search_path TO metadata_sync_2pc_schema;
+
 set citus.enable_create_database_propagation to on;
+
+
 CREATE DATABASE metadata_sync_2pc_db;
+
+revoke connect,temp,temporary  on database metadata_sync_2pc_db from public;
+
 
 \c metadata_sync_2pc_db
 SHOW citus.main_db;
@@ -18,9 +26,24 @@ select 1 from citus_remove_node('localhost', :worker_2_port);
 \c metadata_sync_2pc_db
 grant "grant_role2pc'_user1","grant_role2pc'_user2" to "grant_role2pc'_user3" WITH ADMIN OPTION;
 grant "grant_role2pc'_user1","grant_role2pc'_user2" to grant_role2pc_user4,grant_role2pc_user5 granted by "grant_role2pc'_user3";
+--test for grant on database
+\c metadata_sync_2pc_db - - :master_port
+grant create on database metadata_sync_2pc_db to "grant_role2pc'_user1";
+grant connect on database metadata_sync_2pc_db to "grant_role2pc'_user2";
+grant ALL on database metadata_sync_2pc_db to "grant_role2pc'_user3";
 
 \c regression
+
+select check_database_privileges('grant_role2pc''_user1','metadata_sync_2pc_db',ARRAY['CREATE']);
+select check_database_privileges('grant_role2pc''_user2','metadata_sync_2pc_db',ARRAY['CONNECT']);
+select check_database_privileges('grant_role2pc''_user3','metadata_sync_2pc_db',ARRAY['CREATE','CONNECT','TEMP','TEMPORARY']);
+
+
+
+\c regression
+set citus.enable_create_database_propagation to on;
 select 1 from citus_add_node('localhost', :worker_2_port);
+
 
 select result FROM run_command_on_all_nodes($$
 SELECT array_to_json(array_agg(row_to_json(t)))
@@ -33,19 +56,29 @@ FROM (
 ) t
 $$);
 
+select check_database_privileges('grant_role2pc''_user1','metadata_sync_2pc_db',ARRAY['CREATE']);
+select check_database_privileges('grant_role2pc''_user2','metadata_sync_2pc_db',ARRAY['CONNECT']);
+select check_database_privileges('grant_role2pc''_user3','metadata_sync_2pc_db',ARRAY['CREATE','CONNECT','TEMP','TEMPORARY']);
+
+
 \c metadata_sync_2pc_db
 revoke "grant_role2pc'_user1","grant_role2pc'_user2" from grant_role2pc_user4,grant_role2pc_user5 granted by "grant_role2pc'_user3";
 
 revoke admin option for "grant_role2pc'_user1","grant_role2pc'_user2" from "grant_role2pc'_user3";
 
 revoke "grant_role2pc'_user1","grant_role2pc'_user2" from "grant_role2pc'_user3";
+revoke ALL on database metadata_sync_2pc_db from "grant_role2pc'_user3";
+revoke CONNECT on database metadata_sync_2pc_db from "grant_role2pc'_user2";
+revoke CREATE on database metadata_sync_2pc_db from "grant_role2pc'_user1";
 
 \c regression
 
 drop user "grant_role2pc'_user1","grant_role2pc'_user2","grant_role2pc'_user3",grant_role2pc_user4,grant_role2pc_user5;
 set citus.enable_create_database_propagation to on;
 drop database metadata_sync_2pc_db;
+
 drop schema metadata_sync_2pc_schema;
 
 reset citus.enable_create_database_propagation;
 reset search_path;
+
