e0d06601897e39ee6c0a17a5ee13e7564664658c
Onur Tirtir <onurcantirtir@gmail.com>
2024-02-21 13:02:22 +0300

    add a few more tests

--- src/test/regress/sql/other_databases.sql
+++ src/test/regress/sql/other_databases.sql
@@ -127,7 +127,52 @@ DROP DATABASE other_db4;
 SELECT * FROM public.check_database_on_all_nodes('other_db4') ORDER BY node_type;
 
 DROP DATABASE worker_other_db;
+
+CREATE DATABASE other_db5;
+
+-- disable create database propagation for the next test
+SELECT result FROM run_command_on_all_nodes($$ALTER SYSTEM SET citus.enable_create_database_propagation TO false$$);
+SELECT result FROM run_command_on_all_nodes($$SELECT pg_reload_conf()$$);
+SELECT pg_sleep(0.1);
+
+\c other_db5 - - :worker_2_port
+
+-- locally create a database
+CREATE DATABASE local_db;
+
+\c regression - - -
+
+-- re-enable create database propagation
+SELECT result FROM run_command_on_all_nodes($$ALTER SYSTEM SET citus.enable_create_database_propagation TO true$$);
+SELECT result FROM run_command_on_all_nodes($$SELECT pg_reload_conf()$$);
+SELECT pg_sleep(0.1);
+
+\c other_db5 - - :master_port
+
+-- Test a scenario where create database fails because the database
+-- already exists on another node and we don't crash etc.
+CREATE DATABASE local_db;
+
+\c regression - - -
+
+SELECT * FROM public.check_database_on_all_nodes('local_db') ORDER BY node_type, result;
+
+\c - - - :worker_2_port
+
+-- locally drop the database for cleanup purposes
+SELECT result FROM run_command_on_all_nodes($$ALTER SYSTEM SET citus.enable_create_database_propagation TO false$$);
+SELECT result FROM run_command_on_all_nodes($$SELECT pg_reload_conf()$$);
+SELECT pg_sleep(0.1);
+
+DROP DATABASE local_db;
+
+SELECT result FROM run_command_on_all_nodes($$ALTER SYSTEM SET citus.enable_create_database_propagation TO true$$);
+SELECT result FROM run_command_on_all_nodes($$SELECT pg_reload_conf()$$);
+SELECT pg_sleep(0.1);
+
 \c - - - :master_port
+
+DROP DATABASE other_db5;
 
 SELECT result FROM run_command_on_all_nodes($$ALTER SYSTEM SET citus.enable_create_database_propagation TO false$$);
 SELECT result FROM run_command_on_all_nodes($$SELECT pg_reload_conf()$$);
