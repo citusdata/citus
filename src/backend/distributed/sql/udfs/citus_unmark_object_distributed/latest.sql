2dad29ec8e4297aa122b3e8282d4a5098a4b4c16
gurkanindibay <gindibay@microsoft.com>
2024-02-26 10:57:28 +0300

    Fixes sql snapshot issue

--- src/backend/distributed/sql/udfs/citus_unmark_object_distributed/latest.sql
+++ src/backend/distributed/sql/udfs/citus_unmark_object_distributed/latest.sql
@@ -3,8 +3,5 @@ CREATE FUNCTION pg_catalog.citus_unmark_object_distributed(classid oid, objid oi
     LANGUAGE C STRICT
     AS 'MODULE_PATHNAME', $$citus_unmark_object_distributed$$;
 COMMENT ON FUNCTION pg_catalog.citus_unmark_object_distributed(classid oid, objid oid, objsubid int,checkobjectexistence boolean)
-    IS 'remove an object address from citus.pg_dist_object once the object has been deleted.
-    This method is overload of citus_unmark_object_distributed with additionl parameter checkobjectexistence
-    In regular scenario, if object exists on database, pg_dist_object record is not being deleted
-    However, when we send a command which deletes pg_dist_object,we need to get oid of the object,
-    so we need to delete the record before actual drop operation .';
+    IS 'Removes an object from citus.pg_dist_object after deletion. This version allows checking if the object exists before deletion.
+    If checkobjectexistence is true, object existence check performed. Otherwise, object existence check is skipped.';
