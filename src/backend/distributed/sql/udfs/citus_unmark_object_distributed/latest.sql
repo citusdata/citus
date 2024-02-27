396d3268f2d3f4205ac57f308f30893cbc47147a
gurkanindibay <gindibay@microsoft.com>
2024-02-27 15:21:48 +0300

    Fixes build error

--- src/backend/distributed/sql/udfs/citus_unmark_object_distributed/latest.sql
+++ src/backend/distributed/sql/udfs/citus_unmark_object_distributed/latest.sql
@@ -2,6 +2,6 @@ CREATE FUNCTION pg_catalog.citus_unmark_object_distributed(classid oid, objid oi
     RETURNS void
     LANGUAGE C STRICT
     AS 'MODULE_PATHNAME', $$citus_unmark_object_distributed$$;
-COMMENT ON FUNCTION pg_catalog.citus_unmark_object_distributed(classid oid, objid oid, objsubid int, checkobjectexistence boolean DEFAULT true)
+COMMENT ON FUNCTION pg_catalog.citus_unmark_object_distributed(classid oid, objid oid, objsubid int, checkobjectexistence boolean )
     IS 'Removes an object from citus.pg_dist_object after deletion. If checkobjectexistence is true, object existence check performed.
     Otherwise, object existence check is skipped.';
