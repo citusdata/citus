a60bb2c196a073f1df42efd5b9abb55ac9419214
gurkanindibay <gindibay@microsoft.com>
2024-02-27 19:20:31 +0300

    Fixes sql snapshot error

--- src/backend/distributed/sql/udfs/citus_unmark_object_distributed/latest.sql
+++ src/backend/distributed/sql/udfs/citus_unmark_object_distributed/latest.sql
@@ -3,5 +3,5 @@ CREATE FUNCTION pg_catalog.citus_unmark_object_distributed(classid oid, objid oi
     LANGUAGE C STRICT
     AS 'MODULE_PATHNAME', $$citus_unmark_object_distributed$$;
 COMMENT ON FUNCTION pg_catalog.citus_unmark_object_distributed(classid oid, objid oid, objsubid int, checkobjectexistence boolean)
-    IS 'Removes an object from citus.pg_dist_object after deletion. If checkobjectexistence is true, object existence check performed.
-    Otherwise, object existence check is skipped.';
+    IS 'Removes an object from citus.pg_dist_object after deletion. If checkobjectexistence is true, object existence check performed.'
+       'Otherwise, object existence check is skipped.';
