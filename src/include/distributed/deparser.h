fdd658acecced724f275429094d4d381c1b9fe4b
Emel Şimşek <emelhasdal@gmail.com>
2024-03-20 11:06:05 +0300

    Fix crash caused by some form of ALTER TABLE ADD COLUMN statements. (#7522)
    DESCRIPTION: Fixes a crash caused by some form of ALTER TABLE ADD COLUMN
    statements. When adding multiple columns, if one of the ADD COLUMN
    statements contains a FOREIGN constraint ommitting the referenced
    columns in the statement, a SEGFAULT occurs.
    
    For instance, the following statement results in a crash:
    
    ```
      ALTER TABLE lt ADD COLUMN new_col1 bool,
                              ADD COLUMN new_col2 int references rt;
    
    ```                      
    
    
    Fixes #7520.

--- src/include/distributed/deparser.h
+++ src/include/distributed/deparser.h
@@ -121,6 +121,8 @@ extern void AppendGrantedByInGrant(StringInfo buf, GrantStmt *stmt);
 extern void AppendGrantSharedPrefix(StringInfo buf, GrantStmt *stmt);
 extern void AppendGrantSharedSuffix(StringInfo buf, GrantStmt *stmt);
 
+extern void AppendColumnNameList(StringInfo buf, List *columns);
+
 /* Common deparser utils */
 
 typedef struct DefElemOptionFormat
