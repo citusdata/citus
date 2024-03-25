35c808e1200ad7a8e6c5b67fd1f1f6a59c7f03fb
gurkanindibay <gindibay@microsoft.com>
2024-03-25 13:16:41 +0300

    Adds follower ddl propagation

--- src/test/regress/sql/multi_follower_sanity_check.sql
+++ src/test/regress/sql/multi_follower_sanity_check.sql
@@ -11,6 +11,19 @@ ALTER SYSTEM SET citus.enable_ddl_propagation = 'true';
 SELECT pg_reload_conf();
 
 \c - - - :follower_master_port
+ALTER SYSTEM SET citus.enable_ddl_propagation = 'true';
+SELECT pg_reload_conf();
+
+\c - - - :follower_worker_1_port
+ALTER SYSTEM SET citus.enable_ddl_propagation = 'true';
+SELECT pg_reload_conf();
+
+\c - - - :follower_worker_2_port
+ALTER SYSTEM SET citus.enable_ddl_propagation = 'true';
+SELECT pg_reload_conf();
+
+
+\c - - - :follower_master_port
 CREATE TABLE tab (a int);
 \c - - - :follower_worker_1_port
 CREATE TABLE tab (a int);
