3564492c9020cddccd13fa74abdf37ad4139d3fc
gurkanindibay <gindibay@microsoft.com>
2024-03-25 16:56:00 +0300

    Rollbacks failure setup

--- src/test/regress/sql/failure_setup.sql
+++ src/test/regress/sql/failure_setup.sql
@@ -1,14 +1,3 @@
-ALTER SYSTEM SET citus.enable_ddl_propagation = 'true';
-SELECT pg_reload_conf();
-
-\c - - - :worker_1_port
-ALTER SYSTEM SET citus.enable_ddl_propagation = 'true';
-SELECT pg_reload_conf();
-
-\c - - - :worker_2_port
-ALTER SYSTEM SET citus.enable_ddl_propagation = 'true';
-SELECT pg_reload_conf();
-
 \c - - - :master_port
 SELECT citus.mitmproxy('conn.allow()');
 
