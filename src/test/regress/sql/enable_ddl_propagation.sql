68985c21d621bfce8ac4dca5c40a1bd20e0dbe58
gurkanindibay <gindibay@microsoft.com>
2024-03-25 16:03:39 +0300

    Fixes enable_ddl propagation

--- src/test/regress/sql/enable_ddl_propagation.sql
+++ src/test/regress/sql/enable_ddl_propagation.sql
@@ -1,2 +1,2 @@
-SELECT run_command_on_all_nodes('ALTER SYSTEM SET citus.enable_ddl_propagation TO ON');
-SELECT run_command_on_all_nodes('SELECT pg_reload_conf()');
+SELECT result from  run_command_on_all_nodes('ALTER SYSTEM SET citus.enable_ddl_propagation TO ON');
+SELECT result from  run_command_on_all_nodes('SELECT pg_reload_conf()');
