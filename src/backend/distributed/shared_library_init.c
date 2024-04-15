3ce4620b1bcd68fdf7123ea6b03e6955fe0c77f7
Onur Tirtir <onurcantirtir@gmail.com>
2024-04-15 12:51:11 +0300

    Allow providing "host" parameter via citus.node_conninfo (#7541)
    And when that is the case, directly use it as "host" parameter for the
    connections between nodes and use the "hostname" provided in
    pg_dist_node / pg_dist_poolinfo as "hostaddr" to avoid host name lookup.
    
    This is to avoid allowing dns resolution (and / or setting up DNS names
    for each host in the cluster). This already works currently when using
    IPs in the hostname. The only use of setting host is that you can then
    use sslmode=verify-full and it will validate that the hostname matches
    the certificate provided by the node you're connecting too.
    
    It would be more flexible to make this a per-node setting, but that
    requires SQL changes. And we'd like to backport this change, and
    backporting such a sql change would be quite hard while backporting this
    change would be very easy. And in many setups, a different hostname for
    TLS validation is actually not needed. The reason for that is
    query-from-any node: With query-from-any-node all nodes usually have a
    certificate that is valid for the same "cluster hostname", either using
    a wildcard cert or a Subject Alternative Name (SAN). Because if you load
    balance across nodes you don't know which node you're connecting to, but
    you still want TLS validation to do it's job. So with this change you
    can use this same "cluster hostname" for TLS validation within the
    cluster. Obviously this means you don't validate that you're connecting
    to a particular node, just that you're connecting to one of the nodes in
    the cluster, but that should be fine from a security perspective (in
    most cases).
    
    Note to self: This change requires updating
    
    https://docs.citusdata.com/en/latest/develop/api_guc.html#citus-node-conninfo-text.
    
    DESCRIPTION: Allows overwriting host name for all inter-node connections
    by supporting "host" parameter in citus.node_conninfo
    
    (cherry picked from commit 3586aab17a7ff2fef8f336b6528376552a5d4c2c)

--- src/backend/distributed/shared_library_init.c
+++ src/backend/distributed/shared_library_init.c
@@ -2892,6 +2892,7 @@ NodeConninfoGucCheckHook(char **newval, void **extra, GucSource source)
 			#if defined(ENABLE_GSS) && defined(ENABLE_SSPI)
 		"gsslib",
 			#endif
+		"host",
 		"keepalives",
 		"keepalives_count",
 		"keepalives_idle",
