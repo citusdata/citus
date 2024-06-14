06e9c299502af5163fa848a89766fe50cb13371c
paragjain <paragjain@microsoft.com>
2024-06-14 04:35:58 +0000

    some more

--- src/backend/distributed/planner/merge_planner.c
+++ src/backend/distributed/planner/merge_planner.c
@@ -1123,6 +1123,7 @@ DeferErrorIfRoutableMergeNotSupported(Query *query, List *rangeTableList,
 	 * However, before proceeding with this delegation, we need to confirm that the user is utilizing
 	 * the distribution column of the source table in the Insert variable.
 	 * If this is not the case, we should refrain from pushing down the query.
+	 * This is just a deffered error which will be handle by caller.
 	 */
 
 	Var *insertVar =
@@ -1131,10 +1132,10 @@ DeferErrorIfRoutableMergeNotSupported(Query *query, List *rangeTableList,
 		!IsDistributionColumnInMergeSource((Expr *) insertVar, query, true))
 	{
 		ereport(DEBUG1, (errmsg(
-							 "MERGE INSERT must use the source table distribution column value, try repartitioning")));
+							 "MERGE INSERT must use the source table distribution column value for push down to workers. Otherwise, repartitioning will be applied")));
 		return DeferredError(ERRCODE_FEATURE_NOT_SUPPORTED,
-							 "MERGE INSERT must use the source table "
-							 "distribution column value", NULL, NULL);
+							 "MERGE INSERT must use the source table distribution column value for push down to workers. Otherwise, repartitioning will be applied",
+							 NULL, NULL);
 	}
 	return NULL;
 }
