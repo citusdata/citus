dd19847c979a5cfd74152396b746d739b095a671
naisila <nicypp@gmail.com>
2024-07-10 12:09:27 +0200

    Use ResourceOwner to track WaitEventSet in PG17
    Relevant PG commit:
    50c67c2019ab9ade8aa8768bfe604cd802fe8591
    https://github.com/postgres/postgres/commit/50c67c2019ab9ade8aa8768bfe604cd802fe8591

--- src/backend/distributed/executor/adaptive_executor.c
+++ src/backend/distributed/executor/adaptive_executor.c
@@ -4755,7 +4755,7 @@ BuildWaitEventSet(List *sessionList)
 	int eventSetSize = GetEventSetSize(sessionList);
 
 	WaitEventSet *waitEventSet =
-		CreateWaitEventSet(CurrentMemoryContext, eventSetSize);
+		CreateWaitEventSet(WaitEventSetTracker_compat, eventSetSize);
 
 	WorkerSession *session = NULL;
 	foreach_declared_ptr(session, sessionList)
