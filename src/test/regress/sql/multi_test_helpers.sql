74d945f5ae1c0c70375196e4ec0b9a1a51fd08d9
Naisila Puka <37271756+naisila@users.noreply.github.com>
2025-01-02 12:32:36 +0300

    PG17 - Propagate EXPLAIN options: MEMORY and SERIALIZE (#7802)
    DESCRIPTION: Propagates MEMORY and SERIALIZE options of EXPLAIN
    
    The options for `MEMORY` can be true or false. Default is false.
    The options for `SERIALIZE` can be none, text or binary. Default is
    none.
    
    I referred to how we added support for WAL option in this PR [Support
    EXPLAIN(ANALYZE, WAL)](https://github.com/citusdata/citus/pull/4196).
    For the tests however, I used the same tests as Postgres, not like the
    tests in the WAL PR. I used exactly the same tests as Postgres does, I
    simply distributed the table beforehand. See below the relevant Postgres
    commits from where you can see the tests added as well:
    - [Add EXPLAIN
    (MEMORY)](https://github.com/postgres/postgres/commit/5de890e36)
    - [Invent SERIALIZE option for
    EXPLAIN.](https://github.com/postgres/postgres/commit/06286709e)
    
    This PR required a lot of copying of Postgres static functions regarding
    how `EXPLAIN` works for `MEMORY` and `SERIALIZE` options. Specifically,
    these copy-pastes were required for updating `ExplainWorkerPlan()`
    function, which is in fact based on postgres' `ExplainOnePlan()`:
    ```C
    /* copied from explain.c to update ExplainWorkerPlan() in citus according to ExplainOnePlan() in postgres */
    #define BYTES_TO_KILOBYTES(b)
    typedef struct SerializeMetrics
    static bool peek_buffer_usage(ExplainState *es, const BufferUsage *usage);
    static void show_buffer_usage(ExplainState *es, const BufferUsage *usage);
    static void show_memory_counters(ExplainState *es, const MemoryContextCounters *mem_counters);
    static void ExplainIndentText(ExplainState *es);
    static void ExplainPrintSerialize(ExplainState *es, SerializeMetrics *metrics);
    static SerializeMetrics GetSerializationMetrics(DestReceiver *dest);
    ```
    
    _Note_: it looks like we were missing some `buffers` option details as
    well. I put them together with the memory option, like the code in
    Postgres explain.c, as I didn't want to change the copied code. However,
    I tested locally and there is no big deal in previous Citus versions,
    and you can also see that existing Citus tests with `buffers true`
    didn't change. Therefore, I prefer not to backport "buffers" changes to
    previous versions.

--- src/test/regress/sql/multi_test_helpers.sql
+++ src/test/regress/sql/multi_test_helpers.sql
@@ -747,3 +747,31 @@ BEGIN
     RETURN NEXT;
   END LOOP;
 END; $$ language plpgsql;
+
+-- To produce stable regression test output, it's usually necessary to
+-- ignore details such as exact costs or row counts.  These filter
+-- functions replace changeable output details with fixed strings.
+-- Copied from PG explain.sql
+
+create function explain_filter(text) returns setof text
+language plpgsql as
+$$
+declare
+    ln text;
+begin
+    for ln in execute $1
+    loop
+        -- Replace any numeric word with just 'N'
+        ln := regexp_replace(ln, '-?\m\d+\M', 'N', 'g');
+        -- In sort output, the above won't match units-suffixed numbers
+        ln := regexp_replace(ln, '\m\d+kB', 'NkB', 'g');
+        -- Ignore text-mode buffers output because it varies depending
+        -- on the system state
+        CONTINUE WHEN (ln ~ ' +Buffers: .*');
+        -- Ignore text-mode "Planning:" line because whether it's output
+        -- varies depending on the system state
+        CONTINUE WHEN (ln = 'Planning:');
+        return next ln;
+    end loop;
+end;
+$$;
