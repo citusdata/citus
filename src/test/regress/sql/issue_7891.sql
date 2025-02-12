262a9b611e3db6c6cf2ca47bddb76315fe25da05
Mehmet Yilmaz <mehmetyilmaz@microsoft.com>
2025-02-12 18:28:28 +0000

    Enhance regression test for issue 7891 to verify planner's handling of local and reference tables

--- src/test/regress/sql/issue_7891.sql
+++ src/test/regress/sql/issue_7891.sql
@@ -1,3 +1,6 @@
+-- This construct has been used as a regression test to ensure that the planner
+-- correctly distinguishes between "local" and "reference" tables, avoiding an erroneous 0-task plan.
+-- https://github.com/citusdata/citus/issues/7891
 CREATE SCHEMA issue_7891;
 SET search_path TO issue_7891;
 
@@ -42,25 +45,8 @@ SELECT 't2_ref data' AS label, * FROM t2_ref;
 -- Historically might produce a 0-task plan if the planner incorrectly fails to
 -- treat t4_pg/t2_ref as local/reference.
 --
---- EXPLAIN of update (problem scenario) ---
-EXPLAIN (VERBOSE, COSTS OFF)
-UPDATE t6_pg
-   SET vkey = 43
- WHERE EXISTS (
-   SELECT (SELECT c15 FROM t2_ref)
-   FROM t4_pg
-);
 
---- EXPLAIN reversing subquery usage ---
-EXPLAIN (VERBOSE, COSTS OFF)
-UPDATE t6_pg
-   SET vkey = 43
- WHERE EXISTS (
-   SELECT (SELECT c22 FROM t4_pg)
-   FROM t2_ref
-);
-
--- Now actually do the update to confirm it works
+-- The outer subquery iterates over every row in table t4_pg.
 UPDATE t6_pg
    SET vkey = 43
  WHERE EXISTS (
@@ -71,4 +57,16 @@ UPDATE t6_pg
 -- Show final data
 SELECT 't6_pg after' AS label, * FROM t6_pg;
 
+-- The outer subquery iterates over rows from the reference table t2_ref
+UPDATE t6_pg 
+   SET vkey = 43
+ WHERE EXISTS ( 
+   SELECT (SELECT c22 FROM t4_pg) 
+   FROM t2_ref
+);
+
+-- Show final data
+SELECT 't6_pg after' AS label, * FROM t6_pg;
+
+SET client_min_messages TO WARNING;
 DROP SCHEMA issue_7891 CASCADE;
