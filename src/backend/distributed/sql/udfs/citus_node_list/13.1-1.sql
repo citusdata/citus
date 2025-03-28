32ba63c4b38457e46a1e1b25292a0f687bae992d
Alper Kocatas <alperkocatas@gmail.com>
2025-03-28 09:25:22 +0000

    fix indendation and style

--- src/backend/distributed/sql/udfs/citus_node_list/13.1-1.sql
+++ src/backend/distributed/sql/udfs/citus_node_list/13.1-1.sql
@@ -4,9 +4,9 @@ CREATE TYPE pg_catalog.citus_noderole AS ENUM (
 );
 
 CREATE OR REPLACE FUNCTION pg_catalog.citus_node_list(
-  IN active BOOL DEFAULT null, 
-  IN role citus_noderole DEFAULT null, 
-  OUT node_name text, 
+  IN active BOOL DEFAULT null,
+  IN role citus_noderole DEFAULT null,
+  OUT node_name text,
   OUT node_port bigint)
     RETURNS SETOF record
     LANGUAGE C VOLATILE ROWS 100
