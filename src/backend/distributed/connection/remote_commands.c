a9323913d7de2b07625a4d0c827c2b98d9e14baf
Mehmet Yilmaz <mehmetyilmaz@microsoft.com>
2025-06-13 14:27:18 +0000

    multi_cluster_management errors: ambiguous collation in LIKE
    Add missing include for pg_collation in remote_commands.c

--- src/backend/distributed/connection/remote_commands.c
+++ src/backend/distributed/connection/remote_commands.c
@@ -19,6 +19,7 @@
 #include "utils/builtins.h"
 #include "utils/fmgrprotos.h"
 #include "utils/palloc.h"
+#include "catalog/pg_collation.h"
 
 #include "distributed/cancel_utils.h"
 #include "distributed/connection_management.h"
@@ -371,8 +372,9 @@ CommandMatchesLogGrepPattern(const char *command)
 	if (GrepRemoteCommands && strnlen(GrepRemoteCommands, NAMEDATALEN) > 0)
 	{
 		Datum boolDatum =
-			DirectFunctionCall2(textlike, CStringGetTextDatum(command),
-								CStringGetTextDatum(GrepRemoteCommands));
+			DirectFunctionCall2Coll(textlike, DEFAULT_COLLATION_OID,
+									CStringGetTextDatum(command),
+									CStringGetTextDatum(GrepRemoteCommands));
 
 		return DatumGetBool(boolDatum);
 	}
