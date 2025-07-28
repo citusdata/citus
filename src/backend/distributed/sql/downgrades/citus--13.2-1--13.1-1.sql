83bbfe76950cc709d81cbeaff905de80205a9244
Onur Tirtir <onurcantirtir@gmail.com>
2025-07-28 17:03:13 +0300

    Not automatically create citus_columnar when creating citus extension

--- src/backend/distributed/sql/downgrades/citus--13.2-1--13.1-1.sql
+++ src/backend/distributed/sql/downgrades/citus--13.2-1--13.1-1.sql
@@ -3,3 +3,9 @@
 
 DROP FUNCTION IF EXISTS pg_catalog.worker_last_saved_explain_analyze();
 #include "../udfs/worker_last_saved_explain_analyze/9.4-1.sql"
+
+-- Note that we intentionally don't add the old columnar objects back to the "citus"
+-- extension in this downgrade script, even if they were present in the older version.
+--
+-- If the user wants to create "citus_columnar" extension later, "citus_columnar"
+-- will anyway properly create them at the scope of that extension.
