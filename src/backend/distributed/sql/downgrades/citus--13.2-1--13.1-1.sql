c183634207e522818f00f4a257f83a0f1b439f5e
Onur Tirtir <onurcantirtir@gmail.com>
2025-07-31 13:30:12 +0300

    Move "DROP FUNCTION" for older version of UDF to correct file (#8085)
    We never update an older version of a SQL object for consistency across
    release tags, so this commit moves "DROP FUNCTION .." for the older
    version of "pg_catalog.worker_last_saved_explain_analyze();" to the
    appropriate migration script.
    
    See https://github.com/citusdata/citus/pull/8017.

--- src/backend/distributed/sql/downgrades/citus--13.2-1--13.1-1.sql
+++ src/backend/distributed/sql/downgrades/citus--13.2-1--13.1-1.sql
@@ -1,3 +1,5 @@
 -- citus--13.2-1--13.1-1
 -- downgrade version to 13.1-1
+
+DROP FUNCTION IF EXISTS pg_catalog.worker_last_saved_explain_analyze();
 #include "../udfs/worker_last_saved_explain_analyze/9.4-1.sql"
