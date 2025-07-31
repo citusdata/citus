20a67b3d5d5f23150746a31c16f2be1d6b1f90e2
Onur Tirtir <onurcantirtir@gmail.com>
2025-07-31 12:11:10 +0300

    Move "DROP FUNCTION" for older version of UDF to correct file

--- src/backend/distributed/sql/udfs/worker_last_saved_explain_analyze/9.4-1.sql
+++ src/backend/distributed/sql/udfs/worker_last_saved_explain_analyze/9.4-1.sql
@@ -1,6 +1,4 @@
 
-DROP FUNCTION IF EXISTS pg_catalog.worker_last_saved_explain_analyze();
-
 CREATE OR REPLACE FUNCTION pg_catalog.worker_last_saved_explain_analyze()
     RETURNS TABLE(explain_analyze_output TEXT, execution_duration DOUBLE PRECISION)
     LANGUAGE C STRICT
