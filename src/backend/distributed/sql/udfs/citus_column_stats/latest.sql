55e857e695f38588b05acd219ac04305f30adb4a
naisila <nicypp@gmail.com>
2025-08-03 00:41:50 +0300

    Convert from function to view Part 1

--- src/backend/distributed/sql/udfs/citus_column_stats/latest.sql
+++ src/backend/distributed/sql/udfs/citus_column_stats/latest.sql
@@ -1,61 +1,59 @@
-CREATE OR REPLACE FUNCTION pg_catalog.citus_column_stats(
-    qualified_table_name text)
-RETURNS TABLE (
-    attname text,
-    null_frac float4,
-    most_common_vals text[],
-    most_common_freqs float4[]
-)
-AS $func$
-BEGIN
-    IF NOT EXISTS (SELECT * FROM pg_dist_partition WHERE logicalrelid = qualified_table_name::regclass) THEN
-        RAISE EXCEPTION 'Not a Citus table';
-    ELSE
-        RETURN QUERY
+SET search_path = 'pg_catalog';
+DROP VIEW IF EXISTS pg_catalog.citus_column_stats;
 
-        WITH most_common_vals_json AS (
-        SELECT * FROM run_command_on_shards(qualified_table_name,
-                $$ SELECT json_agg(row_to_json(shard_stats)) FROM (
-                SELECT attname, s.null_frac, most_common_vals, most_common_freqs, c.reltuples AS reltuples
-                FROM pg_stats s RIGHT JOIN pg_class c ON (s.tablename = c.relname)
-                WHERE c.relname = '%s') shard_stats $$ )),
+CREATE OR REPLACE VIEW citus.citus_column_stats AS
 
-        table_reltuples_json AS (
-        SELECT distinct(shardid),
-               (json_array_elements(result::json)->>'reltuples')::bigint AS shard_reltuples
-               FROM most_common_vals_json),
+WITH most_common_vals_double_json AS (
+    SELECT ( SELECT json_agg(row_to_json(f)) FROM ( SELECT * FROM run_command_on_shards(logicalrelid,
+            $$ SELECT json_agg(row_to_json(shard_stats)) FROM (
+            SELECT '$$ || logicalrelid || $$' AS dist_table, attname, s.null_frac,
+                   most_common_vals, most_common_freqs, c.reltuples AS reltuples
+            FROM pg_stats s RIGHT JOIN pg_class c ON (s.tablename = c.relname)
+            WHERE c.relname = '%s') shard_stats $$ ))f)
+        FROM pg_dist_partition),
 
-        table_reltuples AS (
-        SELECT sum(shard_reltuples) AS table_reltuples FROM table_reltuples_json),
+most_common_vals_json AS (
+    SELECT (json_array_elements(json_agg)->>'result') AS result,
+	       (json_array_elements(json_agg)->>'shardid') AS shardid
+    FROM most_common_vals_double_json),
 
-        most_common_vals AS (
-        SELECT shardid,
-               (json_array_elements(result::json)->>'attname')::text AS attname,
-               (json_array_elements(result::json)->>'null_frac')::float4 AS null_frac,
-               json_array_elements_text((json_array_elements(result::json)->>'most_common_vals')::json)::text AS common_val,
-               json_array_elements_text((json_array_elements(result::json)->>'most_common_freqs')::json)::float4 AS common_freq,
-               (json_array_elements(result::json)->>'reltuples')::bigint AS shard_reltuples
-        FROM most_common_vals_json),
+table_reltuples_json AS (
+    SELECT distinct(shardid),
+           (json_array_elements(result::json)->>'reltuples')::bigint AS shard_reltuples,
+	       (json_array_elements(result::json)->>'dist_table')::regclass AS dist_table
+    FROM most_common_vals_json),
 
-        common_val_occurrence AS (
-        SELECT m.attname, common_val,
-                sum(common_freq * shard_reltuples)::bigint AS occurrence,
-                any_value(m.null_frac * shard_reltuples)::bigint AS null_occurrences
-        FROM most_common_vals m
-        GROUP BY m.attname, common_val
-        ORDER BY m.attname, occurrence DESC, common_val)
+table_reltuples AS (
+        SELECT dist_table, sum(shard_reltuples) AS table_reltuples FROM table_reltuples_json GROUP BY dist_table),
 
-        SELECT c.attname,
-               any_value((null_occurrences/t.table_reltuples)::float4) AS null_frac,
-               ARRAY_agg(common_val) AS most_common_vals,
-               ARRAY_agg((occurrence/t.table_reltuples)::float4) AS most_common_freqs
-        FROM common_val_occurrence c, table_reltuples t
-        GROUP BY c.attname;
+most_common_vals AS (
+    SELECT (json_array_elements(result::json)->>'dist_table')::regclass AS dist_table,
+           (json_array_elements(result::json)->>'attname')::text AS attname,
+           (json_array_elements(result::json)->>'null_frac')::float4 AS null_frac,
+           json_array_elements_text((json_array_elements(result::json)->>'most_common_vals')::json)::text AS common_val,
+           json_array_elements_text((json_array_elements(result::json)->>'most_common_freqs')::json)::float4 AS common_freq,
+           (json_array_elements(result::json)->>'reltuples')::bigint AS shard_reltuples
+    FROM most_common_vals_json),
 
-    END IF;
-END;
-$func$ LANGUAGE plpgsql;
+common_val_occurrence AS (
+    SELECT dist_table, m.attname, common_val,
+            sum(common_freq * shard_reltuples)::bigint AS occurrence,
+            any_value(m.null_frac * shard_reltuples)::bigint AS null_occurrences
+    FROM most_common_vals m
+    GROUP BY dist_table, m.attname, common_val
+    ORDER BY m.attname, occurrence DESC, common_val)
 
-COMMENT ON FUNCTION pg_catalog.citus_column_stats(
-    qualified_table_name text)
-    IS 'provides some pg_stats for columns of input Citus table';
+SELECT n.nspname AS schemaname, p.relname AS tablename, c.attname,
+       CASE WHEN t.table_reltuples::bigint = 0 THEN 0
+       ELSE any_value((null_occurrences/t.table_reltuples)::float4) END AS null_frac,
+       ARRAY_agg(common_val) AS most_common_vals,
+       CASE WHEN t.table_reltuples::bigint = 0 THEN NULL
+       ELSE ARRAY_agg((occurrence/t.table_reltuples)::float4) END AS most_common_freqs
+FROM common_val_occurrence c, table_reltuples t, pg_class p, pg_namespace n
+WHERE c.dist_table = t.dist_table AND c.dist_table::regclass::oid = p.oid AND p.relnamespace = n.oid
+GROUP BY c.dist_table, c.attname, t.table_reltuples, n.nspname, p.relname;
+
+ALTER VIEW citus.citus_column_stats SET SCHEMA pg_catalog;
+GRANT SELECT ON pg_catalog.citus_column_stats TO PUBLIC;
+
+RESET search_path;
--- src/test/regress/sql/citus_aggregated_stats.sql
+++ src/test/regress/sql/citus_aggregated_stats.sql
@@ -1,7 +1,9 @@
+CREATE SCHEMA citus_aggregated_stats;
+SET search_path TO citus_aggregated_stats, public;
 SET citus.shard_count = 2;
 
 CREATE USER user1;
-GRANT ALL ON SCHEMA public TO user1;
+GRANT ALL ON SCHEMA citus_aggregated_stats, public TO user1;
 
 SET SESSION AUTHORIZATION user1;
 
@@ -22,16 +24,20 @@ SET row_security TO ON;
 
 ANALYZE current_check;
 
-SELECT attname, null_frac, most_common_vals, most_common_freqs FROM pg_stats
-  WHERE tablename = 'current_check'
+SELECT schemaname, tablename, attname, null_frac, most_common_vals, most_common_freqs FROM pg_stats
+  WHERE tablename IN ('current_check', 'dist_current_check_0', 'dist_current_check_1')
   ORDER BY 1;
 
-SELECT * FROM citus_column_stats('current_check');
+SELECT * FROM citus_column_stats
+  WHERE tablename IN ('current_check', 'dist_current_check_0', 'dist_current_check_1')
+  ORDER BY 1;
 
-CREATE TABLE dist_current_check (currentid int, payload text, rlsuser text);
-SELECT create_distributed_table('dist_current_check', 'currentid');
+CREATE TABLE dist_current_check_0 (currentid int, payload text, rlsuser text);
+CREATE TABLE dist_current_check_1 (currentid int, payload text, rlsuser text);
+SELECT create_distributed_table('dist_current_check_0', 'currentid');
+SELECT create_distributed_table('dist_current_check_1', 'currentid');
 
-INSERT INTO dist_current_check VALUES
+INSERT INTO dist_current_check_0 VALUES
     (1, 'abc', 'user1'),
     (3, 'cde', 'user1'),
     (4, 'def', 'user1'),
@@ -39,16 +45,26 @@ INSERT INTO dist_current_check VALUES
     (3, 'cde', 'user2'),
     (5, NULL, NULL);
 
-ANALYZE dist_current_check;
+INSERT INTO dist_current_check_1 VALUES
+    (1, 'abc', 'user1'),
+    (3, 'cde', 'user1'),
+    (4, 'def', 'user1'),
+    (4, 'def', 'user1'),
+    (3, 'cde', 'user2'),
+    (5, NULL, NULL);
 
-SELECT attname, null_frac, most_common_vals, most_common_freqs FROM pg_stats
-  WHERE tablename = 'dist_current_check'
+ANALYZE dist_current_check_0;
+ANALYZE dist_current_check_1;
+
+SELECT schemaname, tablename, attname, null_frac, most_common_vals, most_common_freqs FROM pg_stats
+  WHERE tablename IN ('current_check', 'dist_current_check_0', 'dist_current_check_1')
   ORDER BY 1;
 
-SELECT * FROM citus_column_stats('dist_current_check');
+SELECT * FROM citus_column_stats
+  WHERE tablename IN ('current_check', 'dist_current_check_0', 'dist_current_check_1')
+  ORDER BY 1;
 
-DROP TABLE current_check;
-DROP TABLE dist_current_check;
+DROP SCHEMA citus_aggregated_stats CASCADE;
 
 RESET SESSION AUTHORIZATION;
 REVOKE ALL ON SCHEMA public FROM user1;
