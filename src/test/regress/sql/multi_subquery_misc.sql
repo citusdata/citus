3b4585c2646ae2e27710406703ee6be7b459ba36
eaydingol <ebruaydin@gmail.com>
2025-08-06 10:10:31 +0300

    Update test dependencies

--- src/test/regress/sql/multi_subquery_misc.sql
+++ src/test/regress/sql/multi_subquery_misc.sql
@@ -3,7 +3,7 @@
 --   (ii)  PL/PGSQL functions
 --   (iii) SQL functions
 
--- the tables that are used depends to multi_insert_select_behavioral_analytics_create_table.sql
+-- the tables that are used depends to multi_behavioral_analytics_create_table.sql
 
 -- We don't need shard id sequence here, so commented out to prevent conflicts with concurrent tests
 SET citus.enable_router_execution TO false;
