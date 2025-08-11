a8036ed9c136ec24c091a56e8066d829ddede4d5
Muhammad Usama <m.usama@gmail.com>
2025-08-11 10:49:49 +0300

    Fixing the path in downgrade script

--- src/backend/distributed/sql/downgrades/citus--13.2-1--13.1-1.sql
+++ src/backend/distributed/sql/downgrades/citus--13.2-1--13.1-1.sql
@@ -1,7 +1,7 @@
 -- citus--13.2-1--13.1-1
 -- downgrade version to 13.1-1
 DROP FUNCTION IF EXISTS citus_internal.citus_internal_copy_single_shard_placement(bigint, integer, integer, integer, citus.shard_transfer_mode);
-#include "udfs/citus_rebalance_start/11.1-1.sql"
+#include "../udfs/citus_rebalance_start/11.1-1.sql"
 
 DROP FUNCTION IF EXISTS pg_catalog.worker_last_saved_explain_analyze();
 #include "../udfs/worker_last_saved_explain_analyze/9.4-1.sql"
