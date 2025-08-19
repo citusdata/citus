8a11d37d478a91eb027d09a4d9527ddf29d060c2
naisila <nicypp@gmail.com>
2025-08-19 22:19:52 +0300

    Add citus local table test

--- src/test/regress/sql/citus_aggregated_stats.sql
+++ src/test/regress/sql/citus_aggregated_stats.sql
@@ -27,17 +27,21 @@ SET row_security TO ON;
 ANALYZE current_check;
 
 SELECT schemaname, tablename, attname, null_frac, most_common_vals, most_common_freqs FROM pg_stats
-  WHERE tablename IN ('current_check', 'dist_current_check', 'ref_current_check')
+  WHERE tablename IN ('current_check', 'dist_current_check', 'ref_current_check', 'citus_local_current_check')
   ORDER BY 1;
 
 SELECT * FROM citus_stats
-  WHERE tablename IN ('current_check', 'dist_current_check', 'ref_current_check')
+  WHERE tablename IN ('current_check', 'dist_current_check', 'ref_current_check', 'citus_local_current_check')
   ORDER BY 1;
 
+-- test various Citus table types in citus_stats
 CREATE TABLE dist_current_check (currentid int, payload text, rlsuser text);
 CREATE TABLE ref_current_check (currentid int, payload text, rlsuser text);
+CREATE TABLE citus_local_current_check (currentid int, payload text, rlsuser text);
+
 SELECT create_distributed_table('dist_current_check', 'currentid');
 SELECT create_reference_table('ref_current_check');
+SELECT citus_add_local_table_to_metadata('citus_local_current_check');
 
 INSERT INTO dist_current_check VALUES
     (1, 'abc', 'user1'),
@@ -57,15 +61,25 @@ INSERT INTO ref_current_check VALUES
     (5, NULL, NULL),
     (4, 'def', 'user1');
 
+INSERT INTO citus_local_current_check VALUES
+    (1, 'abc', 'user1'),
+    (3, 'cde', 'user1'),
+    (4, 'def', 'user1'),
+    (4, 'def', 'user1'),
+    (3, 'cde', 'user2'),
+    (5, NULL, NULL),
+    (4, 'def', 'user1');
+
 ANALYZE dist_current_check;
 ANALYZE ref_current_check;
+ANALYZE citus_local_current_check;
 
 SELECT schemaname, tablename, attname, null_frac, most_common_vals, most_common_freqs FROM pg_stats
-  WHERE tablename IN ('current_check', 'dist_current_check', 'ref_current_check')
+  WHERE tablename IN ('current_check', 'dist_current_check', 'ref_current_check', 'citus_local_current_check')
   ORDER BY 1;
 
 SELECT * FROM citus_stats
-  WHERE tablename IN ('current_check', 'dist_current_check', 'ref_current_check')
+  WHERE tablename IN ('current_check', 'dist_current_check', 'ref_current_check', 'citus_local_current_check')
   ORDER BY 1;
 
 RESET SESSION AUTHORIZATION;
