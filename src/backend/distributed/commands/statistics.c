28a3599e136db5a47cc8a25120f6d24e9b7ab64a
naisila <nicypp@gmail.com>
2025-09-26 16:32:26 +0300

    Address review

--- src/backend/distributed/commands/statistics.c
+++ src/backend/distributed/commands/statistics.c
@@ -69,12 +69,14 @@ PreprocessCreateStatisticsStmt(Node *node, const char *queryString,
 {
 	CreateStatsStmt *stmt = castNode(CreateStatsStmt, node);
 
-	RangeVar *relation = (RangeVar *) linitial(stmt->relations);
+	Node *relationNode = (Node *) linitial(stmt->relations);
 
-	if (!IsA(relation, RangeVar))
+	if (!IsA(relationNode, RangeVar))
 	{
 		return NIL;
 	}
+
+	RangeVar *relation = (RangeVar *) relationNode;
 
 	Oid relationId = RangeVarGetRelid(relation, ShareUpdateExclusiveLock, false);
 
