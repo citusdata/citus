7004f0be83e1bdcd9f7bcdfbe5591b16b4e841d5
Onur Tirtir <onurcantirtir@gmail.com>
2025-11-12 15:44:09 +0300

    Allow "alter table set schema" from any node to move tables into distributed-schemas

--- src/backend/distributed/operations/replicate_none_dist_table_shard.c
+++ src/backend/distributed/operations/replicate_none_dist_table_shard.c
@@ -17,48 +17,71 @@
 
 #include "distributed/adaptive_executor.h"
 #include "distributed/commands.h"
+#include "distributed/commands/multi_copy.h"
 #include "distributed/commands/utility_hook.h"
 #include "distributed/coordinator_protocol.h"
 #include "distributed/deparse_shard_query.h"
 #include "distributed/listutils.h"
+#include "distributed/multi_executor.h"
+#include "distributed/multi_partitioning_utils.h"
 #include "distributed/replicate_none_dist_table_shard.h"
 #include "distributed/shard_utils.h"
 #include "distributed/worker_manager.h"
 #include "distributed/worker_protocol.h"
+#include "distributed/worker_shard_copy.h"
 
 
+static PlannedStmt * PlanReadCopyableColumnsFromTableQuery(Oid relationId);
+static void CopyFromQueryIntoNoneDistTable(PlannedStmt *selectPlan,
+										   Oid noneDistTableId);
 static void CreateForeignKeysFromReferenceTablesOnShards(Oid noneDistTableId);
 static Oid ForeignConstraintGetReferencingTableId(const char *queryString);
 static void EnsureNoneDistTableWithCoordinatorPlacement(Oid noneDistTableId);
-static void SetLocalEnableManualChangesToShard(bool state);
 
 
 /*
- * NoneDistTableReplicateCoordinatorPlacement replicates local (presumably
- * coordinator) shard placement of given none-distributed table to given
+ * NoneDistTableReplicateCoordinatorPlacement replicates the coordinator
+ * shard placement of given none-distributed table to given
  * target nodes and inserts records for new placements into pg_dist_placement.
  */
 void
 NoneDistTableReplicateCoordinatorPlacement(Oid noneDistTableId,
 										   List *targetNodeList)
 {
-	EnsureCoordinator();
+	EnsurePropagationToCoordinator();
 	EnsureNoneDistTableWithCoordinatorPlacement(noneDistTableId);
 
 	/*
-	 * We don't expect callers try to replicate the shard to remote nodes
-	 * if some of the remote nodes have a placement for the shard already.
+	 * We don't expect callers try to replicate the shard to worker nodes
+	 * if some of the worker nodes have a placement for the shard already.
 	 */
 	int64 shardId = GetFirstShardId(noneDistTableId);
-	List *remoteShardPlacementList =
+	List *nonCoordShardPlacementList =
 		FilterShardPlacementList(ActiveShardPlacementList(shardId),
-								 IsRemoteShardPlacement);
-	if (list_length(remoteShardPlacementList) > 0)
+								 IsNonCoordShardPlacement);
+	if (list_length(nonCoordShardPlacementList) > 0)
 	{
-		ereport(ERROR, (errmsg("table already has a remote shard placement")));
+		ereport(ERROR, (errmsg("table already has a shard placement on a worker")));
 	}
 
 	uint64 shardLength = ShardLength(shardId);
+
+	/*
+	 * If it's not a partitioned table, then plan the query to read data
+	 * from the coordinator placement. We plan the query before inserting
+	 * new placements to force the planner to consider only the coordinator
+	 * placement.
+	 *
+	 * We skip copying from partitioned tables because we will copy the
+	 * data from partition to partition's shards. To signal that, we leave
+	 * readCoordPlacementPlan as null when that's the case.
+	 */
+	PlannedStmt *readCoordPlacementPlan = NULL;
+	if (!PartitionedTable(noneDistTableId))
+	{
+		readCoordPlacementPlan =
+			PlanReadCopyableColumnsFromTableQuery(noneDistTableId);
+	}
 
 	/* insert new placements to pg_dist_placement */
 	List *insertedPlacementList = NIL;
@@ -79,21 +102,27 @@ NoneDistTableReplicateCoordinatorPlacement(Oid noneDistTableId,
 						  useExclusiveConnection);
 
 	/* fetch coordinator placement before deleting it */
-	Oid localPlacementTableId = GetTableLocalShardOid(noneDistTableId, shardId);
 	ShardPlacement *coordinatorPlacement =
 		linitial(ActiveShardPlacementListOnGroup(shardId, COORDINATOR_GROUP_ID));
 
 	/*
-	 * CreateForeignKeysFromReferenceTablesOnShards and CopyFromLocalTableIntoDistTable
+	 * CreateForeignKeysFromReferenceTablesOnShards and CopyFromQueryIntoNoneDistTable
 	 * need to ignore the local placement, hence we temporarily delete it before
 	 * calling them.
 	 */
 	DeleteShardPlacementRowGlobally(coordinatorPlacement->placementId);
 
-	/* and copy data from local placement to new placements */
-	CopyFromLocalTableIntoDistTable(
-		localPlacementTableId, noneDistTableId
-		);
+	/* and copy data from local placement to new placements, if needed */
+	if (readCoordPlacementPlan)
+	{
+		/*
+		 * Note that today the callers of this function already hold an
+		 * exclusive lock on the provided table. In the future, if a caller
+		 * does not hold such a lock, here we need to block writes to the
+		 * shard placement on the coordinator before copying data.
+		 */
+		CopyFromQueryIntoNoneDistTable(readCoordPlacementPlan, noneDistTableId);
+	}
 
 	/*
 	 * CreateShardsOnWorkers only creates the foreign keys where given relation
@@ -116,12 +145,12 @@ NoneDistTableReplicateCoordinatorPlacement(Oid noneDistTableId,
 
 /*
  * NoneDistTableDeleteCoordinatorPlacement deletes pg_dist_placement record for
- * local (presumably coordinator) shard placement of given none-distributed table.
+ * the coordinator shard placement of given none-distributed table.
  */
 void
 NoneDistTableDeleteCoordinatorPlacement(Oid noneDistTableId)
 {
-	EnsureCoordinator();
+	EnsurePropagationToCoordinator();
 	EnsureNoneDistTableWithCoordinatorPlacement(noneDistTableId);
 
 	int64 shardId = GetFirstShardId(noneDistTableId);
@@ -130,40 +159,24 @@ NoneDistTableDeleteCoordinatorPlacement(Oid noneDistTableId)
 	ShardPlacement *coordinatorPlacement =
 		linitial(ActiveShardPlacementListOnGroup(shardId, COORDINATOR_GROUP_ID));
 
-	/* remove the old placement from metadata of local node, i.e., coordinator */
+	/* remove the old placement from metadata */
 	DeleteShardPlacementRowGlobally(coordinatorPlacement->placementId);
 }
 
 
 /*
- * NoneDistTableDropCoordinatorPlacementTable drops local (presumably coordinator)
+ * NoneDistTableDropCoordinatorPlacementTable drops the coordinator
  * shard placement table of given none-distributed table.
  */
 void
 NoneDistTableDropCoordinatorPlacementTable(Oid noneDistTableId)
 {
-	EnsureCoordinator();
+	EnsurePropagationToCoordinator();
 
 	if (HasDistributionKey(noneDistTableId))
 	{
 		ereport(ERROR, (errmsg("table is not a none-distributed table")));
 	}
-
-	/*
-	 * We undistribute Citus local tables that are not chained with any reference
-	 * tables via foreign keys at the end of the utility hook.
-	 * Here we temporarily set the related GUC to off to disable the logic for
-	 * internally executed DDL's that might invoke this mechanism unnecessarily.
-	 *
-	 * We also temporarily disable citus.enable_manual_changes_to_shards GUC to
-	 * allow given command to modify shard. Note that we disable it only for
-	 * local session because changes made to shards are allowed for Citus internal
-	 * backends anyway.
-	 */
-	int saveNestLevel = NewGUCNestLevel();
-
-	SetLocalEnableLocalReferenceForeignKeys(false);
-	SetLocalEnableManualChangesToShard(true);
 
 	StringInfo dropShardCommand = makeStringInfo();
 	int64 shardId = GetFirstShardId(noneDistTableId);
@@ -176,7 +189,22 @@ NoneDistTableDropCoordinatorPlacementTable(Oid noneDistTableId)
 	task->taskId = INVALID_TASK_ID;
 	task->taskType = DDL_TASK;
 	task->replicationModel = REPLICATION_MODEL_INVALID;
-	SetTaskQueryString(task, dropShardCommand->data);
+
+	/*
+	 * We undistribute Citus local tables that are not chained with any reference
+	 * tables via foreign keys at the end of the utility hook.
+	 * So we need to temporarily set the related GUC to off to disable the logic for
+	 * internally executed DDL's that might invoke this mechanism unnecessarily.
+	 *
+	 * We also temporarily disable citus.enable_manual_changes_to_shards GUC to
+	 * allow given command to modify shard.
+	 */
+	List *taskQueryStringList = list_make3(
+		"SET LOCAL citus.enable_local_reference_table_foreign_keys TO OFF;",
+		"SET LOCAL citus.enable_manual_changes_to_shards TO ON;",
+		dropShardCommand->data
+		);
+	SetTaskQueryStringList(task, taskQueryStringList);
 
 	ShardPlacement *targetPlacement = CitusMakeNode(ShardPlacement);
 	SetPlacementNodeMetadata(targetPlacement, CoordinatorNodeIfAddedAsWorkerOrError());
@@ -185,8 +213,80 @@ NoneDistTableDropCoordinatorPlacementTable(Oid noneDistTableId)
 
 	bool localExecutionSupported = true;
 	ExecuteUtilityTaskList(list_make1(task), localExecutionSupported);
+}
 
-	AtEOXact_GUC(true, saveNestLevel);
+
+/*
+ * PlanReadCopyableColumnsFromTableQuery creates a plan to read
+ * copyable columns from given relation.
+ */
+static PlannedStmt *
+PlanReadCopyableColumnsFromTableQuery(Oid relationId)
+{
+	Relation relation = RelationIdGetRelation(relationId);
+
+	StringInfo queryString = makeStringInfo();
+	appendStringInfo(queryString, "SELECT %s FROM %s",
+					 CopyableColumnNamesFromTupleDesc(RelationGetDescr(relation)),
+					 generate_qualified_relation_name(relationId));
+
+	RelationClose(relation);
+
+	Query *query = ParseQueryString(queryString->data, NULL, 0);
+	return planner(query, queryString->data, 0, NULL);
+}
+
+
+/*
+ * CopyFromQueryIntoNoneDistTable executes given query and copies data
+ * into the shard placements of given none-distributed table.
+ *
+ * We use CitusCopyDestReceiver to pass the tuples to relevant shard
+ * placements of the none-distributed table, which opens connections on
+ * demand / if needed and starts a COPY for each shard placement that
+ * will have data.
+ *
+ * And for the execution of the SELECT query, we go through the executor.
+ *
+ * So for both the query execution and data copying, the purpose is to
+ * make sure to correctly decide using local execution or remote execution
+ * when accessing shard placements for reads and writes.
+ */
+static void
+CopyFromQueryIntoNoneDistTable(PlannedStmt *selectPlan, Oid noneDistTableId)
+{
+	if (HasDistributionKey(noneDistTableId))
+	{
+		ereport(ERROR, (errmsg("table is not a none-distributed table")));
+	}
+
+	Relation relation = RelationIdGetRelation(noneDistTableId);
+
+	TupleDesc tupleDescriptor = RelationGetDescr(relation);
+	List *copyableColumnNameList =
+		CopyablePlainColumnNameListFromTupleDesc(tupleDescriptor);
+
+	RelationClose(relation);
+
+	EState *estate = CreateExecutorState();
+
+	const bool nonPublishableData = false;
+
+	/* safe to pass INVALID_PARTITION_COLUMN_INDEX for a none-dist table */
+	const int partitionColumnIndex = INVALID_PARTITION_COLUMN_INDEX;
+
+	/* we don't track query counters when copying data into shards */
+	const bool trackQueryCounters = false;
+	DestReceiver *copyDest =
+		(DestReceiver *) CreateCitusCopyDestReceiver(noneDistTableId,
+													 copyableColumnNameList,
+													 partitionColumnIndex,
+													 estate, NULL, nonPublishableData,
+													 trackQueryCounters);
+
+	ExecutePlanIntoDestReceiver(selectPlan, NULL, copyDest);
+
+	FreeExecutorState(estate);
 }
 
 
@@ -198,7 +298,7 @@ NoneDistTableDropCoordinatorPlacementTable(Oid noneDistTableId)
 static void
 CreateForeignKeysFromReferenceTablesOnShards(Oid noneDistTableId)
 {
-	EnsureCoordinator();
+	EnsurePropagationToCoordinator();
 
 	if (HasDistributionKey(noneDistTableId))
 	{
@@ -286,18 +386,4 @@ EnsureNoneDistTableWithCoordinatorPlacement(Oid noneDistTableId)
 	{
 		ereport(ERROR, (errmsg("table does not have a coordinator placement")));
 	}
-}
-
-
-/*
- * SetLocalEnableManualChangesToShard locally enables
- * citus.enable_manual_changes_to_shards GUC.
- */
-static void
-SetLocalEnableManualChangesToShard(bool state)
-{
-	set_config_option("citus.enable_manual_changes_to_shards",
-					  state ? "on" : "off",
-					  (superuser() ? PGC_SUSET : PGC_USERSET), PGC_S_SESSION,
-					  GUC_ACTION_LOCAL, true, 0, false);
 }
