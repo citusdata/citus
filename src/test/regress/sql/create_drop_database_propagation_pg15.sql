529b303b6d59a30a60275d722031141706255065
Naisila Puka <37271756+naisila@users.noreply.github.com>
2025-12-12 13:00:29 +0300

    Cleanup from dropping PG14 support (#8377)
    Should have been part of
    https://github.com/citusdata/citus/commit/3b1c082791592097e2c944ef98ec86e4234882bf
    
    This commit deletes some redundant alternative test outputs which were
    created for PG14. `server_version_ge_15` means "Server version greater
    than or equal to 15".

--- src/test/regress/sql/create_drop_database_propagation_pg15.sql
+++ src/test/regress/sql/create_drop_database_propagation_pg15.sql
@@ -1,14 +1,3 @@
---
--- PG15
---
-SHOW server_version \gset
-SELECT substring(:'server_version', '\d+')::int >= 15 AS server_version_ge_15
-\gset
-\if :server_version_ge_15
-\else
-\q
-\endif
-
 -- create/drop database for pg >= 15
 
 set citus.enable_create_database_propagation=on;
