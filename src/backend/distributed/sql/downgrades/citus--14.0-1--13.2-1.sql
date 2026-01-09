6918e92aa6e86855817b37aa767fcaf0bf572dc0
Vinod Sridharan <14185211+visridha@users.noreply.github.com>
2026-01-09 20:22:09 +0000

    Downgrade

--- src/backend/distributed/sql/downgrades/citus--14.0-1--13.2-1.sql
+++ src/backend/distributed/sql/downgrades/citus--14.0-1--13.2-1.sql
@@ -3,3 +3,9 @@
 
 #include "../udfs/citus_prepare_pg_upgrade/13.0-1.sql"
 #include "../udfs/citus_finish_pg_upgrade/13.2-1.sql"
+
+DROP AGGREGATE IF EXISTS pg_catalog.worker_binary_partial_agg(oid, anyelement);
+DROP AGGREGATE IF EXISTS pg_catalog.coord_binary_combine_agg(oid, bytea, anyelement);
+DROP FUNCTION IF EXISTS pg_catalog.worker_binary_partial_agg_ffunc(internal);
+DROP FUNCTION IF EXISTS pg_catalog.coord_binary_combine_agg_sfunc(internal, oid, bytea, anyelement);
+DROP FUNCTION IF EXISTS pg_catalog.coord_binary_combine_agg_ffunc(internal, oid, bytea, anyelement);
