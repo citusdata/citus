ba1871b5909faf53f1f5cf5749569365fb4cd065
Vinod Sridharan <14185211+visridha@users.noreply.github.com>
2026-01-14 05:06:53 -0800

    Support binary transfer from worker to coordinator for custom aggregates (#8446)
    DESCRIPTION: Support binary transfer from worker to coordinator for
    custom aggregates
    This fixes one issue and improves serialization of worker aggregates.
    
    Firstly, EXPLAIN ANALYZE of group aggregates on a sharded cluster fails
    on custom aggregates with custom combine functions. This is because they
    create a record pseudo type on the worker response that is a cstring
    which is not allowed (this is not an issue for the non-explain case
    since it does not create a record). To handle this, we introduce a
    binary version of the worker_partial_aggregate that uses a type's
    send/receive function to serialize the payload as a bytea/binary type.
    
    In general, we prefer binary serialization for worker -> coordinator
    transmission of state since the assumption is that binary receive/send
    functions are more efficient than serializing and deserializing via text
    I/O

--- src/backend/distributed/sql/citus--13.2-1--14.0-1.sql
+++ src/backend/distributed/sql/citus--13.2-1--14.0-1.sql
@@ -3,3 +3,9 @@
 
 #include "udfs/citus_prepare_pg_upgrade/14.0-1.sql"
 #include "udfs/citus_finish_pg_upgrade/14.0-1.sql"
+
+#include "udfs/worker_binary_partial_agg_ffunc/14.0-1.sql"
+#include "udfs/coord_binary_combine_agg_sfunc/14.0-1.sql"
+#include "udfs/coord_binary_combine_agg_ffunc/14.0-1.sql"
+#include "udfs/worker_binary_partial_agg/14.0-1.sql"
+#include "udfs/coord_binary_combine_agg/14.0-1.sql"
