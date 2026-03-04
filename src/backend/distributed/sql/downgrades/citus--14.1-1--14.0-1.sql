04b5f71a4258eefb21023335077fc5300070414d
Onur Tirtir <onurcantirtir@gmail.com>
2026-03-04 14:42:13 +0300

    Acquire colocation id lock on the coordinator when the operation is run from a worker
    Given the recent support added to create distributed-schema tables from
    any node; in the code-paths that can now acquire a colocation id lock
    from a worker, make sure to acquire the lock on the coordinator, maybe
    by using a remote connection that won't be closed until the end of the
    transaction.
    
    EnsureReferenceTablesExistOnAllNodesExtended() is also one of those
    code-paths, but normally it needs to release the lock at certain points,
    so it'll be handled in a separate commit differently. The code-paths
    that are handled in this commit are the ones that don't need to release
    the lock until the end of the transaction.

--- src/backend/distributed/sql/downgrades/citus--14.1-1--14.0-1.sql
+++ src/backend/distributed/sql/downgrades/citus--14.1-1--14.0-1.sql
@@ -2,5 +2,8 @@
 -- downgrade version to 14.0-1
 
 DROP FUNCTION IF EXISTS citus_internal.get_next_colocation_id();
+
 DROP FUNCTION IF EXISTS citus_internal.adjust_identity_column_seq_settings(regclass, bigint, boolean);
 DROP FUNCTION IF EXISTS pg_catalog.worker_apply_sequence_command(text, regtype, bigint, boolean);
+
+DROP FUNCTION IF EXISTS citus_internal.lock_colocation_id(int, int);
