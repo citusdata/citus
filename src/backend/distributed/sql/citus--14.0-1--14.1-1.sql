98ff5d4295930c78c3599454cff16ecbf1d95c98
Onur Tirtir <onurcantirtir@gmail.com>
2026-05-04 11:42:18 +0300

    Preserve "sequence range" and "nextval" behavior when creating distributed-schema tables from workers

--- src/backend/distributed/sql/citus--14.0-1--14.1-1.sql
+++ src/backend/distributed/sql/citus--14.0-1--14.1-1.sql
@@ -2,3 +2,5 @@
 -- bump version to 14.1-1
 
 #include "udfs/citus_internal_get_next_colocation_id/14.1-1.sql"
+#include "udfs/citus_internal_adjust_identity_column_seq_settings/14.1-1.sql"
+#include "udfs/worker_apply_sequence_command/14.1-1.sql"
