96a4f00cde87c187df0f5642a0ceb58060848ad5
Onur Tirtir <onurcantirtir@gmail.com>
2026-05-15 14:01:19 +0000

    more

--- src/backend/distributed/metadata/metadata_sync.c
+++ src/backend/distributed/metadata/metadata_sync.c
@@ -4145,37 +4145,6 @@ citus_internal_delete_colocation_metadata(PG_FUNCTION_ARGS)
 	{
 		/* this UDF is not allowed allowed for executing as a separate command */
 		EnsureCitusInitiatedOperation();
-
-		if (IsTenantSchemaColocationGroup(colocationId))
-		{
-			/*
-			 * If this colocation group belongs to a distributed schema, then this UDF
-			 * is being executed when undistributing the schema. In that case, here we
-			 * enforce being the owner of the schema as we do in citus_schema_undistribute().
-			 */
-			EnsureSchemaOwner(ColocationIdGetTenantSchemaId(colocationId));
-		}
-		else
-		{
-			/*
-			 * There is no reasonable way to associate a colocation group with an owner
-			 * as the tables belonging to the colocation group can have different owners.
-			 * However, we know that we delete a colocation group only after all the tables
-			 * belonging to the colocation group are dropped / undistributed. For this
-			 * reason, instead here we ensure that there is no table belonging to this
-			 * colocation group, which is a strong signal that the colocation group is not
-			 * in use and it's safe to delete.
-			 */
-			List *colocatedTableList = ColocationGroupTableList(colocationId, 1);
-			if (list_length(colocatedTableList) > 0)
-			{
-				ereport(ERROR, (errcode(ERRCODE_OBJECT_NOT_IN_PREREQUISITE_STATE),
-								errmsg("Colocation group with id %d cannot be "
-									   "deleted because there are still tables "
-									   "belonging to this colocation group.",
-									   colocationId)));
-			}
-		}
 	}
 
 	DeleteColocationGroupLocally(colocationId);
@@ -4201,6 +4170,14 @@ citus_internal_add_tenant_schema(PG_FUNCTION_ARGS)
 
 	PG_ENSURE_ARGNOTNULL(1, "colocation_id");
 	uint32 colocationId = PG_GETARG_INT32(1);
+
+	if (!ShouldSkipMetadataChecks())
+	{
+		/* this UDF is not allowed allowed for executing as a separate command */
+		EnsureCitusInitiatedOperation();
+
+		EnsureSchemaOwner(schemaId);
+	}
 
 	InsertTenantSchemaLocally(schemaId, colocationId);
 
