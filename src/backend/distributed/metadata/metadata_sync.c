96feb254dca327a0cee4fde4e115d175f4183c64
Onur Tirtir <onurcantirtir@gmail.com>
2026-05-20 19:54:52 +0300

    Enforce object ownership for more citus-internal UDFs (#8587)
    DESCRIPTION: Enforces object-ownership for more citus-internal UDFs for
    security.
    
    (cherry picked from commit 10c3a8be245d4c212411199a2b01de463486e376)

--- src/backend/distributed/metadata/metadata_sync.c
+++ src/backend/distributed/metadata/metadata_sync.c
@@ -3913,6 +3913,12 @@ citus_internal_delete_placement_metadata(PG_FUNCTION_ARGS)
 	PG_ENSURE_ARGNOTNULL(0, "placement_id");
 	int64 placementId = PG_GETARG_INT64(0);
 
+	bool missingOk = false;
+	GroupShardPlacement *placement = LookupGroupPlacementByPlacementId(placementId,
+																	   missingOk);
+
+	EnsureShardOwner(placement->shardId, missingOk);
+
 	if (!ShouldSkipMetadataChecks())
 	{
 		/* this UDF is not allowed allowed for executing as a separate command */
@@ -4284,6 +4290,14 @@ citus_internal_add_tenant_schema(PG_FUNCTION_ARGS)
 	PG_ENSURE_ARGNOTNULL(1, "colocation_id");
 	uint32 colocationId = PG_GETARG_INT32(1);
 
+	EnsureSchemaOwner(schemaId);
+
+	if (!ShouldSkipMetadataChecks())
+	{
+		/* this UDF is not allowed allowed for executing as a separate command */
+		EnsureCitusInitiatedOperation();
+	}
+
 	InsertTenantSchemaLocally(schemaId, colocationId);
 
 	PG_RETURN_VOID();
@@ -4305,6 +4319,14 @@ citus_internal_delete_tenant_schema(PG_FUNCTION_ARGS)
 
 	PG_ENSURE_ARGNOTNULL(0, "schema_id");
 	Oid schemaId = PG_GETARG_OID(0);
+
+	EnsureSchemaOwner(schemaId);
+
+	if (!ShouldSkipMetadataChecks())
+	{
+		/* this UDF is not allowed allowed for executing as a separate command */
+		EnsureCitusInitiatedOperation();
+	}
 
 	DeleteTenantSchemaLocally(schemaId);
 
@@ -4333,6 +4355,8 @@ citus_internal_update_none_dist_table_metadata(PG_FUNCTION_ARGS)
 
 	PG_ENSURE_ARGNOTNULL(3, "auto_converted");
 	bool autoConverted = PG_GETARG_BOOL(3);
+
+	EnsureTableOwner(relationId);
 
 	if (!ShouldSkipMetadataChecks())
 	{
